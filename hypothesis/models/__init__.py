3a74b549c22599817338b2122ab4ecbb71228bb2
Randall Leeds <randall.leeds@gmail.com>
2012-03-21 16:15:17 -0700

    Pyramid 1.3 imperative session properties

--- hypothesis/models/__init__.py
+++ hypothesis/models/__init__.py
@@ -4,16 +4,8 @@ from sqlalchemy import engine_from_config
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from apex import RequestFactory
-
 DBSession = scoped_session(sessionmaker())
 Base = declarative_base()
-
-class HypothesisRequestFactory(RequestFactory):
-    @reify
-    def db(self):
-        self.registry.settings['db.sessionmaker']()
-    
 
 def includeme(config):
     """
@@ -26,23 +18,25 @@ def includeme(config):
     engine = engine_from_config(config.get_settings(), 'sqlalchemy.')
     DBSession.configure(bind=engine)
 
-    # Add a setting to the registry for easy access to the sessionmaker
-    config.add_settings({'db.sessionmaker': sessionmaker(bind=engine)})
-
     # Set up the SQLAlchemy declarative configuration
     Base.metadata.bind = engine
     Base.metadata.create_all(engine)
+    maker = sessionmaker(bind=engine)
 
-    # Scan for all the models
-    config.scan(__name__)
+    # Reify the database session maker on the request object
+    config.set_request_property(lambda request: maker(), 'db', reify=True)
 
-    # Set up the apex system
+    # Add settings for apex authentication system
     config.add_settings({
         'apex.apex_template': 'hypothesis:templates/forms/auth.jinja2',
-        'apex.register_form_class': 'hypothesis.models.auth.RegisterForm',
+        'apex.register_form_class': 'hypothesis.forms.auth.RegisterForm',
         'apex.use_request_factory': False
     })
     config.include('apex', route_prefix='/auth')
 
-    # Register the request factory
-    config.set_request_factory(HypothesisRequestFactory)
+    # Reify the authenticated user on the request object
+    config.set_request_property(
+        'apex.RequestFactory.user.wrapped', 'user', reify=True)
+
+    # Scan for all the models
+    config.scan(__name__)
