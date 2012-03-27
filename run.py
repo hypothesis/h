7b41b6bb4f78b473b55e9ac1cd26a6c3fce87e0a
Randall Leeds <randall.leeds@gmail.com>
2012-03-26 22:53:17 -0700

    friendly waitress and gunicorn testing

--- run.py
+++ run.py
@@ -2,46 +2,47 @@ from pyramid.config import Configurator
 
 from hypothesis import create_app
 
+# Development setup
+
+settings = {
+    'reload_all': True,
+    'debug_all': True,
+
+    'sqlalchemy.url': 'sqlite:///hypothesis.db',
+
+    'apex.session_secret': '535510n_53cr37',
+    'apex.auth_secret': '4u7h_53cr37',
+    'apex.came_from_route': 'home',
+    'apex.velruse_providers': [],
+    'apex.no_csrf': 'apex_callback,store',
+
+    'velruse.endpoint': 'http://localhost:8080/auth/apex_callback',
+    'velruse.store': 'velruse.store.sqlstore',
+    'velruse.providers': [],
+
+    'hypothesis.api_secret': '00000000-0000-0000-0000-000000000000',
+    'hypothesis.consumer_key': 'hypothes.is'
+}
+config = Configurator(settings=settings)
+config.include('pyramid_debugtoolbar')
+application = create_app(config)
+
+# Serve the demo configuration if run from the command line
+
 if __name__ == '__main__':
-    import logging
-    logging.basicConfig()
-    log = logging.getLogger(__file__)
-    log.setLevel(logging.DEBUG)
-    
-    from wsgiref.simple_server import make_server
-
-    settings = {
-        'reload_all': True,
-        'debug_all': True,
-
-        'sqlalchemy.url': 'sqlite:///hypothesis.db',
-
-        'apex.session_secret': '535510n_53cr37',
-        'apex.auth_secret': '4u7h_53cr37',
-        'apex.came_from_route': 'home',
-        'apex.velruse_providers': [],
-        'apex.no_csrf': 'apex_callback,store',
-
-        'velruse.endpoint': 'http://localhost:8080/auth/apex_callback',
-        'velruse.store': 'velruse.store.sqlstore',
-        'velruse.providers': [],
-
-        'hypothesis.api_secret': '00000000-0000-0000-0000-000000000000',
-        'hypothesis.consumer_key': 'hypothes.is'
-    }
-    
-    config = Configurator(settings=settings)
-    config.include('pyramid_debugtoolbar')
-    app = create_app(config)
-
-    log.info("Starting server...")
-    server = make_server('127.0.0.1', 8080, app)
-    log.info("Serving at http://localhost:8080/")
+    from functools import partial
+    try:
+        from waitress import serve
+        run = partial(serve, application, host='127.0.0.1', port=8080)
+    except ImportError:
+        from wsgiref.simple_server import make_server
+        run = make_server('127.0.0.1', 8080, application).serve_forever
+        print 'serving on http://localhost:8080'
 
     try:
-        server.serve_forever()
+        run()
     except KeyboardInterrupt:
         # Catch this to avoid logging a traceback
         pass
 
-    log.info("Terminated.")
+    print "\nTerminated."
