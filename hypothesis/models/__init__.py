fd770b3219864a7916ce8416b7609a987c375350
Randall Leeds <randall.leeds@gmail.com>
2012-03-30 03:43:48 -0700

    leverage apex better to simplify model setup

--- hypothesis/models/__init__.py
+++ hypothesis/models/__init__.py
@@ -1,34 +1,7 @@
-from pyramid.decorator import reify
-
-from sqlalchemy import engine_from_config
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
-from zope.sqlalchemy import ZopeTransactionExtension
-
-DBSession = scoped_session(sessionmaker())
-Base = declarative_base()
+from apex.models import Base, DBSession
 
 def includeme(config):
-    """
-    Configures the sqlalchemy engine from the application configuration, scans
-    the package for ORM models, sets up the apex authentication system
-    and configures the pyramid request factory.
-    """
-
-    # Create a SQLAlchemy engine based on the Pyramid Configurator's settings
-    engine = engine_from_config(config.get_settings(), 'sqlalchemy.')
-
-    # Set up the SQLAlchemy declarative configuration
-    DBSession.configure(bind=engine)
-    Base.metadata.bind = engine
-    Base.metadata.create_all(engine)
-
-    # Set up request transaction management
-    config.include('pyramid_tm')
-
-    # Reify a database session on the request object
-    maker = sessionmaker(bind=engine, extension=ZopeTransactionExtension)
-    config.set_request_property(lambda request: maker(), 'db', reify=True)
-
-    # Scan for all the models
     config.scan(__name__)
+    config.include('apex')
+    config.include('pyramid_tm')
+    config.set_request_property(lambda request: DBSession(), 'db', reify=True)
