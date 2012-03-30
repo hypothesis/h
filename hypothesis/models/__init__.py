708625749be2181722205e73bc6ed00aeee2726c
Randall Leeds <randall.leeds@gmail.com>
2012-03-30 02:58:16 -0700

    use pyramid_tm and zope transactions

--- hypothesis/models/__init__.py
+++ hypothesis/models/__init__.py
@@ -3,6 +3,7 @@ from pyramid.decorator import reify
 from sqlalchemy import engine_from_config
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
+from zope.sqlalchemy import ZopeTransactionExtension
 
 DBSession = scoped_session(sessionmaker())
 Base = declarative_base()
@@ -16,27 +17,18 @@ def includeme(config):
 
     # Create a SQLAlchemy engine based on the Pyramid Configurator's settings
     engine = engine_from_config(config.get_settings(), 'sqlalchemy.')
-    DBSession.configure(bind=engine)
 
     # Set up the SQLAlchemy declarative configuration
+    DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     Base.metadata.create_all(engine)
-    maker = sessionmaker(bind=engine)
 
-    # Reify the database session maker on the request object
+    # Set up request transaction management
+    config.include('pyramid_tm')
+
+    # Reify a database session on the request object
+    maker = sessionmaker(bind=engine, extension=ZopeTransactionExtension)
     config.set_request_property(lambda request: maker(), 'db', reify=True)
-
-    # Add settings for apex authentication system
-    config.add_settings({
-        'apex.apex_template': 'hypothesis:templates/forms/auth.jinja2',
-        'apex.register_form_class': 'hypothesis.forms.auth.RegisterForm',
-        'apex.use_request_factory': False
-    })
-    config.include('apex', route_prefix='/auth')
-
-    # Reify the authenticated user on the request object
-    config.set_request_property(
-        'apex.RequestFactory.user.wrapped', 'user', reify=True)
 
     # Scan for all the models
     config.scan(__name__)
