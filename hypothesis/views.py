5c8dab3f6330113dc9da4ff72deaf934c810dd9c
Randall Leeds <randall.leeds@gmail.com>
2012-04-16 23:22:33 -0700

    #1

--- hypothesis/views.py
+++ hypothesis/views.py
@@ -1,16 +1,12 @@
-from apex import login, logout, register
+from apex import login
 
-from pyramid.security import authenticated_userid
-from pyramid.view import view_config
-
-@view_config(route_name='home', renderer='home.jinja2')
 def home(request):
     if request.user is None: return login(request)
     return {}
 
 def includeme(config):
-    config.scan(__name__)
     config.include('pyramid_jinja2')
     config.add_jinja2_extension('jinja2.ext.do')
     config.add_jinja2_search_path('hypothesis:templates')
     config.add_jinja2_search_path('apex:templates')
+    config.add_view(home, route_name='home', renderer='home.jinja2')
