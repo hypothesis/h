0c36da8b7b993f4b3c51c30b4e19195061f1d0db
Randall Leeds <randall.leeds@gmail.com>
2012-04-16 17:18:02 -0700

    flash messages and resources in base template

--- hypothesis/views.py
+++ hypothesis/views.py
@@ -4,8 +4,6 @@ from . forms.auth import LoginForm, RegisterForm
 
 @view_config(route_name='home', renderer='home.jinja2')
 def home(request):
-    request.need('.resources:site_styles')
-
     action = request.params.get('action', request.user and 'logout' or 'login')
     form = None
     if request.user is None:
@@ -23,5 +21,6 @@ def home(request):
 def includeme(config):
     config.scan(__name__)
     config.include('pyramid_jinja2')
+    config.add_jinja2_extension('jinja2.ext.do')
     config.add_jinja2_search_path('hypothesis:templates')
     config.add_jinja2_search_path('apex:templates')
