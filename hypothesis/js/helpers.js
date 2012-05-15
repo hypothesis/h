d9fb5d1e2d89a4cfd964408e12536542f6050b86
Randall Leeds <randall.leeds@gmail.com>
2012-05-15 11:21:45 -0700

    add missing handlebars helpers js

---
+++ hypothesis/js/helpers.js
@@ -0,0 +1,48 @@
+/* 
+ * http://stackoverflow.com/a/7641812
+ */
+var Handlebars
+
+Handlebars.registerHelper('fuzzyTime', function (date) {
+  var delta = Math.round((+new Date - new Date(date)) / 1000)
+
+  var minute = 60,
+  hour = minute * 60,
+  day = hour * 24,
+  week = day * 7
+
+  var fuzzy
+
+  if (delta < 30) {
+    fuzzy = 'moments ago.'
+  } else if (delta < minute) {
+    fuzzy = delta + ' seconds ago.'
+  } else if (delta < 2 * minute) {
+    fuzzy = 'a minute ago.'
+  } else if (delta < hour) {
+    fuzzy = Math.floor(delta / minute) + ' minutes ago.'
+  } else if (Math.floor(delta / hour) == 1) {
+    fuzzy = '1 hour ago.'
+  } else if (delta < day) {
+    fuzzy = Math.floor(delta / hour) + ' hours ago.'
+  } else if (delta < day * 2) {
+    fuzzy = 'yesterday'
+  }
+  
+  return fuzzy
+})
+
+Handlebars.registerHelper('splitUser', function (user, options) {
+  var parts = this.user.split(/(?:acct:)|@/);
+  return options.fn({
+    name: parts[1],
+    provider: parts[2]
+  })
+})
+
+Handlebars.registerHelper('formatUser', function (user) {
+  return new Handlebars.SafeString(
+    user.replace(
+        /^acct:([^@]+)@(.+)$/,
+      '$1 <span class="hyp-provider">on $2</span>'))
+})
