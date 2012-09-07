97dd07d168140bfdff8033b2b254800daea5d20c
Randall Leeds <tilgovi@hypothes.is>
2012-09-07 11:32:14 -0700

    add subscribers for macro blocks

---
+++ h/subscribers.py
@@ -0,0 +1,12 @@
+from pyramid.events import subscriber
+from pyramid.events import BeforeRender
+from pyramid.renderers import get_renderer
+
+
+@subscriber(BeforeRender)
+def add_render_view_global(event):
+    event['blocks'] = get_renderer('templates/blocks.pt').implementation()
+
+
+def includeme(config):
+    config.scan(__name__)
