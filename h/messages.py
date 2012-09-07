a8f090a8c101e8593a6bec85d147248504430cf8
Randall Leeds <tilgovi@hypothes.is>
2012-09-07 13:10:49 -0700

    add translation messages module

---
+++ h/messages.py
@@ -0,0 +1,5 @@
+from pyramid.i18n import TranslationStringFactory
+_ = TranslationStringFactory(__package__)
+
+MISSING_PARAMETER = _('Missing parameter')
+NOT_LOGGED_IN = _('You must be logged in to do that!')
