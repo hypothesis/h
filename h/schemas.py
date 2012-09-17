b1b45b27ce9b74f4148354bb32671bbd327e9bbf
Randall Leeds <tilgovi@hypothes.is>
2012-09-16 18:57:25 -0700

    dead code

--- h/schemas.py
+++ h/schemas.py
@@ -5,51 +5,6 @@ import pyramid_deform
 from h import api
 
 
-class ControllerWidget(deform.widget.MappingWidget):
-    def deserialize(self, field, pstruct):
-        error = None
-        result = {}
-
-        if pstruct is colander.null:
-            pstruct = {}
-
-        formid = pstruct.get('__formid__', '')
-        parts = formid.split('_')
-        if len(parts) > 1:
-            formid = parts[0]
-            parts = parts[1:].join('_')
-            pstruct['__formid__'] = parts
-        else:
-            parts = ''
-
-        for num, subfield in enumerate(field.children):
-            name = subfield.name
-            subval = pstruct.get(name, colander.null)
-
-            try:
-                if name != formid:
-                    result[name] = subfield.deserialize(colander.null)
-                else:
-                    result[name] = subfield.deserialize(subval)
-            except colander.Invalid as e:
-                result[name] = e.value
-                if error is None:
-                    error = colander.Invalid(field.schema, value=result)
-                error.add(e, num)
-
-        if error is not None:
-            raise error
-
-        return result
-
-    def serialize(self, field, cstruct, readonly=False):
-        if cstruct is (colander.null, None):
-            cstruct = {}
-        return
-
-class Controller(colander.Mapping):
-    widget_maker = ControllerWidget
-
 class PersonaSchema(pyramid_deform.CSRFSchema):
     id = colander.SchemaNode(
         colander.Integer(),
