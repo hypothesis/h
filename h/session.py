f2eac2fb0622b3ec9a867fea679bf5001de5ed21
Randall Leeds <tilgovi@hypothes.is>
2012-12-19 00:31:59 -0800

    massively simplify XSRF-TOKEN cookie handling
    Fixes all outstanding csrf issues. Should make the login smooth.

--- h/session.py
+++ h/session.py
@@ -1,4 +1,3 @@
-from beaker.session import Session, SessionObject
 from beaker.util import coerce_session_params
 from pyramid.interfaces import ISession
 from pyramid.settings import asbool
@@ -7,58 +6,40 @@ from zope.interface import implementer
 
 
 def AngularSessionFactoryConfig(**options):
-    class PyramidAngularSessionObject(SessionObject):
-        _csrft_options = {
-            'key': 'XSRF-TOKEN',
-            'cookie_domain': options.get('cookie_domain'),
-            'cookie_expires': options.get('cookie_expires', True),
-            'cookie_path': options.get('cookie_path', '/'),
-            'secure': options.get('secure', False),
-        }
+    PyramidBeakerSessionObject = BeakerSessionFactoryConfig(**options)
 
+    class PyramidAngularSessionObject(PyramidBeakerSessionObject):
         def __init__(self, request):
-            super(PyramidAngularSessionObject, self).__init__(request)
-            self.__dict__['_csrft_cookie_'] = None
+            PyramidBeakerSessionObject.__init__(self, request)
 
             def csrft_callback(request, response):
                 exception = getattr(request, 'exception', None)
-                if (exception is None or self._cookie_on_exception
-                    and self.accessed()
-                ):
-                    self.persist()
-                    csrft_cookie = self.__dict__['_csrft_cookie_']
-                    if csrft_cookie and csrft_cookie.is_new:
-                        value = csrft_cookie.request['cookie_out']
-                        response.headerlist.append(('Set-Cookie', value))
+                if exception is None and self.accessed():
+                    csrft = self.get('_csrft_', None)
+                    cookie = self.cookie
+                    headers = self.__dict__['_headers']
+                    if csrft and self.dirty():
+                        # Temporarily swap the cookie key and value
+                        # for XSRF-TOKEN and the csrf_token. This motly avoids
+                        # the need to work directly with the cookie and the
+                        # beaker configuration options.
+                        old = (self.key, self.id)
+                        try:
+                            self.key = 'XSRF-TOKEN'
+                            self._set_cookie_values()
+                            cookie[self.key].coded_value = csrft
+                            value = cookie[self.key].output(header='')
+                            response.headerlist.append(('Set-Cookie', value))
+                        finally:
+                            self.key, self.id = old
             request.add_response_callback(csrft_callback)
-
-        def new_csrf_token(self):
-            # A beaker Session is used to avoid duplicating all the
-            # messy cookie manipulation code, even though it's only used
-            # to store the CSRF token. It has the same path, domain, and
-            # security settings as the main session cookie.
-            token = super(PyramidAngularSessionObject, self).new_csrf_token()
-            csrft_cookie = Session({}, id=token, **self._csrft_options)
-            csrft_cookie._set_cookie_values()
-            csrft_cookie._update_cookie_out()
-            self.__dict__['_csrft_cookie_'] = csrft_cookie
-            return token
 
         def invalidate(self):
             # Easiest thing to do is eagerly create a new csrf immediately
             self._session().invalidate()
             self.new_csrf_token()
 
-    # Construct a beaker session.
-    BeakerSession = BeakerSessionFactoryConfig(**options)
-
-    # Mix in handling of the CSRF Token Cookie.
-    AngularSession = type(
-        'PyramidAngularSessionObject',
-        (PyramidAngularSessionObject, BeakerSession),
-        {},
-    )
-    return implementer(ISession)(AngularSession)
+    return implementer(ISession)(PyramidAngularSessionObject)
 
 
 # Lifted from pyramid_beaker
