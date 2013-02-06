1e6530dc556bb6dad6d98399740cf2bc8c0bfef7
Randall Leeds <tilgovi@hypothes.is>
2013-02-06 14:01:31 -0800

    more severe invalid form message

--- h/messages.py
+++ h/messages.py
@@ -2,6 +2,7 @@ from pyramid.i18n import TranslationStringFactory
 _ = TranslationStringFactory(__package__)
 
 
-INVALID_FORM = _('There was a problem with your form submission.')
+INVALID_FORM = _('Something has gone horribly wrong. '
+                 'Try refreshing the application')
 MISSING_PARAMETER = _('A required parameter is missing.')
 NOT_LOGGED_IN = _('You must be logged in to do that!')
