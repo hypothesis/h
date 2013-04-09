484293e925b061788d58beeb6f9048c083303e26
Randall Leeds <tilgovi@hypothes.is>
2013-04-09 01:54:20 -0700

    split oauth, flask store, and delegated store apis

--- h/api/__init__.py
+++ h/api/__init__.py
@@ -1,53 +1,9 @@
-from annotator import auth, authz, store, es
-from annotator.annotation import Annotation
+__all__ = ['auth']
 
-from flask import Flask, g
+import urlparse
 
-from pyramid.httpexceptions import HTTPBadRequest
+from annotator import auth
 from pyramid.view import view_config
-from pyramid.wsgi import wsgiapp2
-
-from h import messages, models
-
-
-def access_token(request):
-    """OAuth2 access token provider"""
-    for name in [
-        'client_id',
-        'client_secret',
-        'code',
-        'state'
-    ]:
-        if name not in request.params:
-            msg = '%s "%s".' % (messages.MISSING_PARAMETER, name)
-            raise HTTPBadRequest(msg)
-
-    raise NotImplementedError('OAuth provider not implemented yet.')
-
-
-def anonymize_delete(annotation):
-    if annotation.get('deleted', False):
-        user = annotation.get('user', '')
-        if user:
-            annotation['user'] = ''
-
-        permissions = annotation.get('permissions', {})
-        for action in permissions.keys():
-            filtered = [
-                role
-                for role in annotation['permissions'][action]
-                if role != user
-            ]
-            annotation['permissions'][action] = filtered
-
-
-def authorize(annotation, action, user=None):
-    action_field = annotation.get('permissions', {}).get(action, [])
-
-    if not action_field:
-        return True
-    else:
-        return authz.authorize(annotation, action, user)
 
 
 @view_config(renderer='string', request_method='GET', route_name='token')
@@ -61,46 +17,24 @@ def includeme(config):
     Example INI file:
 
         [app:h]
-        api.key: 00000000-0000-0000-0000-000000000000
+        api.key: api.key: 00000000-0000-0000-0000-000000000000
+        api.url: https://example.com/api/
 
     """
 
-    app = Flask('annotator')  # Create the annotator-store app
-    app.register_blueprint(store.store)  # and register the store api.
+    api_url = config.registry.settings.get('api.url', '/api')
 
-    # Set up the models
-    settings = config.get_settings()
-    if 'es.host' in settings:
-        app.config['ELASTICSEARCH_HOST'] = settings['es.host']
-    if 'es.index' in settings:
-        app.config['ELASTICSEARCH_INDEX'] = settings['es.index']
-    es.init_app(app)
-    with app.test_request_context():
-        try:
-            Annotation.create_all()
-        except:
-            Annotation.update_settings()
-            Annotation.create_all()
+    if urlparse.urlparse(api_url).scheme:
+        def set_app_url(request, elements, kw):
+            kw.setdefault('_app_url', request.registry.settings['api.url'])
+            return (elements, kw)
+        config.add_route('api', '', pregenerator=set_app_url, static=True)
+    else:
+        pattern = '/'.join([api_url.strip('/'), '*subpath'])
+        config.add_route('api', pattern)
 
-    # Configure authentication and authorization
-    app.config['AUTHZ_ON'] = True
-
-    def before_request():
-        g.auth = auth.Authenticator(models.Consumer.get_by_key)
-        g.authorize = authorize
-        g.before_annotation_update = anonymize_delete
-
-    app.before_request(before_request)
-
-    # Configure the API views -- version 1 is just an annotator.store proxy
-    api_v1 = wsgiapp2(app)
-
-    config.add_route('token', '/api/token')
-    config.add_route('api-v1', '/api/v1/*subpath')
-    config.add_route('api', '/api/*subpath')
-
-    config.add_view(api_v1, route_name='api')
-    config.add_view(api_v1, route_name='api-v1')
+    config.include('h.api.store')
 
     # And pick up the token view
+    config.add_route('token', '/token')
     config.scan(__name__)
---
+++ h/api/store.py
@@ -0,0 +1,67 @@
+from annotator import auth, authz, store, es
+from annotator.annotation import Annotation
+
+from flask import Flask, g
+
+from pyramid.wsgi import wsgiapp2
+
+from h import models
+
+
+def anonymize_deletes(annotation):
+    if annotation.get('deleted', False):
+        user = annotation.get('user', '')
+        if user:
+            annotation['user'] = ''
+
+        permissions = annotation.get('permissions', {})
+        for action in permissions.keys():
+            filtered = [
+                role
+                for role in annotation['permissions'][action]
+                if role != user
+            ]
+            annotation['permissions'][action] = filtered
+
+
+def authorize(annotation, action, user=None):
+    action_field = annotation.get('permissions', {}).get(action, [])
+
+    if not action_field:
+        return True
+    else:
+        return authz.authorize(annotation, action, user)
+
+
+def before_request():
+    g.auth = auth.Authenticator(models.Consumer.get_by_key)
+    g.authorize = authorize
+    g.before_annotation_update = anonymize_deletes
+
+
+def includeme(config):
+    app = Flask('annotator')  # Create the annotator-store app
+    app.register_blueprint(store.store)  # and register the store api.
+
+    # Set up the models
+    settings = config.get_settings()
+    if 'es.host' in settings:
+        app.config['ELASTICSEARCH_HOST'] = settings['es.host']
+    if 'es.index' in settings:
+        app.config['ELASTICSEARCH_INDEX'] = settings['es.index']
+    es.init_app(app)
+    with app.test_request_context():
+        try:
+            Annotation.create_all()
+        except:
+            Annotation.update_settings()
+            Annotation.create_all()
+
+    # Configure authentication and authorization
+    app.config['AUTHZ_ON'] = True
+    app.before_request(before_request)
+
+    # Configure the API views -- version 1 is just an annotator.store proxy
+    api_v1 = wsgiapp2(app)
+
+    config.add_view(api_v1, route_name='api')
