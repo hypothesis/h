c33706ee15bbff1b3e554f65bb1c614e3640d09d
Randall Leeds <tilgovi@hypothes.is>
2013-05-28 14:58:17 -0700

    update annotator to 1.2.6-dev-3342e40
    restores fuzzy anchoring

--- h/lib/annotator.permissions.js
+++ h/lib/annotator.permissions.js
@@ -1,12 +1,12 @@
 /*
-** Annotator 1.2.6-dev-d2ea449
+** Annotator 1.2.6-dev-3342e40
 ** https://github.com/okfn/annotator/
 **
 ** Copyright 2012 Aron Carroll, Rufus Pollock, and Nick Stenning.
 ** Dual licensed under the MIT and GPLv3 licenses.
 ** https://github.com/okfn/annotator/blob/master/LICENSE
 **
-** Built at: 2013-05-24 21:48:00Z
+** Built at: 2013-05-28 21:55:34Z
 */
 
 
