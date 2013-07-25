a1ec82d54197989dc1e4b7b0e7c7c0481ab32d7b
Randall Leeds <tilgovi@hypothes.is>
2013-07-25 04:40:13 -0700

    whitespace

--- h/assets.py
+++ h/assets.py
@@ -128,7 +128,7 @@ jquery_ui = Bundle(
     Uglify('h:lib/jquery.ui.core.js', output='lib/jquery.ui.core.min.js'),
     Uglify('h:lib/jquery.ui.widget.js', output='lib/jquery.ui.widget.min.js'),
     Uglify('h:lib/jquery.ui.position.js', output='lib/jquery.ui.position.min.js'),
-    Uglify('h:lib/jquery.ui.menu.js', output='lib/jquery.ui.menu.min.js'),    
+    Uglify('h:lib/jquery.ui.menu.js', output='lib/jquery.ui.menu.min.js'),
     Uglify('h:lib/jquery.ui.autocomplete.js', output='lib/jquery.ui.autocomplete.min.js'),
     CSS('h:lib/jquery-ui-smoothness.css'),
 )
@@ -218,7 +218,7 @@ app = Bundle(
                 'streamer',
                 'streamfilter',
                 'userstream',
-                'tagstream',                
+                'tagstream',
             )
         ],
         output='js/app.min.js'
