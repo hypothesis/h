a81daf72a8f30f0efbf0b0041ed16b2972ff4a0f
csillag <csillag@harmacolindor.com>
2013-08-29 20:22:20 +0200

    Add workaround for 3rd-party content blocking problem (#634)

--- h/browser/chrome/js/background.js
+++ h/browser/chrome/js/background.js
@@ -57,6 +57,18 @@ function setPageAction(tabId, value) {
 
 
 function onInstalled() {
+  /* We need this so that 3-rd party cookie blocking does not kill us.
+     See https://github.com/hypothesis/h/issues/634 for more info.
+     This is intended to be a temporary fix only.
+  */
+  var details = {
+    primaryPattern: 'https://hypothes.is/*',
+    setting: 'allow'
+  }
+  chrome.contentSettings.cookies.set(details)
+  chrome.contentSettings.images.set(details)
+  chrome.contentSettings.javascript.set(details)
+
   chrome.tabs.query({}, function (tabs) {
     for (var i in tabs) {
       var tabId = tabs[i].id
