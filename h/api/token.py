9870b3f15fa70a533e449c8112f9a260c018fc41
Randall Leeds <tilgovi@hypothes.is>
2013-12-24 03:10:43 -0800

    Switch to pyramid_multiauth
    Simplify the interaction between annotaton.auth and horus by using
    pyramid_multiauth and creating a separate authentication policy for
    the annotator auth token.

--- h/api/token.py
+++ h/api/token.py
@@ -1,6 +1,8 @@
 import urlparse
 
 from annotator import auth
+from pyramid.authentication import RemoteUserAuthenticationPolicy
+from pyramid.security import effective_principals
 from pyramid.view import view_config, view_defaults
 
 from h import interfaces, views
@@ -20,18 +22,45 @@ class TokenController(views.BaseController):
             'ttl': consumer.ttl,
         }
 
-        if request.user:
-            parts = {
-                'username': request.user.username,
-                'provider': request.server_name
-            }
-            message['userId'] = 'acct:%(username)s@%(provider)s' % parts
+        try:
+            message['userId'] = next(
+                p
+                for p in effective_principals(request)
+                if str(p).startswith('acct:')
+            )
+        except StopIteration:
+            pass
 
         return auth.encode_token(message, consumer.secret)
 
     @classmethod
     def __json__(cls, request):
         return cls(request)()
+
+
+class AuthTokenAuthenticationPolicy(RemoteUserAuthenticationPolicy):
+    def __init__(self, debug=False):
+        super(AuthTokenAuthenticationPolicy, self).__init__(
+            environ_key='HTTP_X_ANNOTATOR_AUTH_TOKEN',
+            callback=self.callback,
+            debug=debug,
+        )
+
+    def unauthenticated_userid(self, request):
+        token = request.environ.get(self.environ_key)
+        try:
+            return auth.decode_token(token, verify=False).get('userId')
+        except auth.TokenInvalid:
+            return None
+
+
+def groupfinder(userid, request):
+    Consumer = request.registry.queryUtility(interfaces.IConsumerClass)
+    user = auth.Authenticator(Consumer.get_by_key).request_user(request)
+    if user:
+        # TODO: Group support for auth tokens
+        return []
+    return None
 
 
 def includeme(config):
@@ -50,6 +79,11 @@ def includeme(config):
     ).strip('/')
     config.add_route('token', token_endpoint)
     config.scan(__name__)
+
+    authn_debug = config.registry.settings.get('pyramid.debug_authorization') \
+        or config.registry.settings.get('debug_authorization')
+    authn_policy = AuthTokenAuthenticationPolicy(debug=authn_debug)
+    config.set_authentication_policy(authn_policy)
 
     if not config.registry.queryUtility(interfaces.ITokenClass):
         config.registry.registerUtility(
