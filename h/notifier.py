d0660dbbd25dec95f72b5125dd6480ad1649da10
Ujvari Gergely <ujvari@hypothes.is>
2014-02-03 15:19:56 +0100

    Introducing AnnotationNotifier class

--- h/notifier.py
+++ h/notifier.py
@@ -1,5 +1,6 @@
 
 import traceback
+import re
 
 from pyramid_mailer.interfaces import IMailer
 from pyramid_mailer.message import Message
@@ -8,47 +9,66 @@ from pyramid_mailer.testing import DummyMailer
 from pyramid.renderers import render
 
 from pyramid.events import subscriber
-from h import events
+from h import events, models
+from h.interfaces import IStoreClass
 
 import logging
 log = logging.getLogger(__name__)
 
-
 class AnnotationDummyMailer(DummyMailer):
+    def __init__(self):
+        super(AnnotationDummyMailer, self).__init__()
+
+class AnnotationNotifier(object):
+    def __init__(self, request):
+        self.request = request
+        self.registry = request.registry
+        self.mailer = self.registry.queryUtility(IMailer)
+        self.store = self.registry.queryUtility(IStoreClass)(request)
+
     @property
     def template(self):
         return 'h:templates/emails/reply_notification.pt'
 
-    def __init__(self):
-        super(AnnotationDummyMailer, self).__init__()
+    def send_notification_to_owner(self, annotation, action):
+        # Get the e-mail of the owner
+        parent = self.store.read(annotation['references'][-1])
+        username = re.search("^acct:([^@]+)", parent['user']).group(1)
+        userobj = models.User.get_by_username(self.request, username)
+        recipients = [userobj.email]
+        self._send_annotation(annotation, recipients)
 
-    def send_annotation(self, request, annotation, recipients):
-        body = render(self.template, {"text": annotation['text']}, request)
+    def _send_annotation(self, annotation, recipients):
+        body = render(self.template, {"text": annotation['text']}, self.request)
 
         message = Message(subject="Re",
-                          sender="admin@hypothes.is",
+                          sender="noreply@hypothes.is",
                           recipients=recipients,
                           body=body)
-        self.send(message)
+        self.mailer.send(message)
         log.info('sent: %s' % message.to_message().as_string())
+
+
+def filter_notifications(annotation, action):
+    # For now, we only filter for newly created replies
+    if action == 'create' and 'references' in annotation and len(annotation['references']) > 0:
+        return True
+
+    return False
 
 @subscriber(events.AnnotationEvent)
 def send_notifications(event):
     log.info('send_notifications')
     try:
         action = event.action
-        if action != 'create':
+        annotation = event.annotation
+
+        if not filter_notifications(annotation, action):
             return
 
         request = event.request
-        registry = request.registry
-        annotation = event.annotation
-
-        mailer = registry.queryUtility(IMailer)
-        log.info('------- Mailer -------')
-        log.info(mailer)
-        mailer.send_annotation(request, annotation, ['test@test'])
-
+        notifier = AnnotationNotifier(request)
+        notifier.send_notification_to_owner(annotation, action)
     except:
         log.info(traceback.format_exc())
         log.info('Unexpected error occurred in send_notifications(): ' + str(event))
