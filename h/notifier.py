ffd249142ca01274f260423a686789c34c492fd9
Ujvari Gergely <ujvari@hypothes.is>
2014-02-05 18:54:27 +0100

    Using constructed e-mail template

--- h/notifier.py
+++ h/notifier.py
@@ -34,31 +34,52 @@ class AnnotationNotifier(object):
         # Get the e-mail of the owner
         parent = self.store.read(annotation['references'][-1])
 
+        if not ('quote' in parent):
+            grandparent = self.store.read(parent['references'][-1])
+            parent['quote'] = grandparent['text']
         # Do not notify me about my own message
         if annotation['user'] == parent['user']: return
 
         username = re.search("^acct:([^@]+)", parent['user']).group(1)
         userobj = models.User.get_by_username(self.request, username)
         recipients = [userobj.email]
-        self._send_annotation(annotation, recipients)
+        template_map = self._create_template_map(annotation, parent)
+        self._send_annotation(template_map, parent['id'], recipients)
 
-    def _send_annotation(self, annotation, recipients):
-        template_map = {
-            'text': annotation['text'],
-            'replier': re.search("^acct:([^@]+)", annotation['user']).group(1),
-            'reply_link': self.request.application_url + '/a/' + annotation['id'],
-            'annotation_link': self.request.application_url + '/a/' + annotation['references'][-1]
+    def _create_template_map(self, reply, parent):
+        parent_user = re.search("^acct:([^@]+)", parent['user']).group(1)
+        reply_user = re.search("^acct:([^@]+)", reply['user']).group(1)
+        parent_tags = ', '.join(parent['tags']) if 'tags' in parent else '(none)'
+        reply_tags = ', '.join(reply['tags']) if 'tags' in reply else '(none)'
+
+        return {
+            'document_title': reply['title'],
+            'document_path': parent['uri'],
+            'parent_quote': parent['quote'],
+            'parent_text': parent['text'],
+            'parent_user': parent_user,
+            'parent_tags': parent_tags,
+            'parent_timestamp': parent['created'],
+            'parent_user_profile': self.request.application_url + '/u/' + parent_user,
+            'parent_path': self.request.application_url + '/a/' + parent['id'],
+            'reply_quote': reply['quote'],
+            'reply_text': reply['text'],
+            'reply_user': reply_user,
+            'reply_tags': reply_tags,
+            'reply_timestamp': reply['created'],
+            'reply_user_profile': self.request.application_url + '/u/' + reply_user,
+            'reply_path': self.request.application_url + '/a/' + reply['id']
         }
 
-        body = render(self.template, template_map, self.request)
+    def _send_annotation(self, template_map, id, recipients):
+        body = render(self.template, template_map, id, self.request)
 
-        message = Message(subject="Reply for your annotation [" + annotation['id'] + ']',
+        message = Message(subject="Reply for your annotation [" + id + ']',
                           sender="noreply@hypothes.is",
                           recipients=recipients,
                           body=body)
         self.mailer.send(message)
         log.info('sent: %s' % message.to_message().as_string())
-
 
 def filter_notifications(annotation, action):
     # For now, we only filter for newly created replies
