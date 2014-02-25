e1c6a90b6d324311af9592e5c3b5d5aa72bd5719
Ujvari Gergely <ujvari@hypothes.is>
2014-02-25 19:58:19 +0100

    Debug commit

--- h/domain_mailer.py
+++ h/domain_mailer.py
@@ -1,5 +1,12 @@
 import re
 from urlparse import urlparse
+import requests
+import requests_cache
+#import Queue
+from gevent.queue import Queue
+from BeautifulSoup import BeautifulSoup
+import threading
+import time
 
 from pyramid.events import subscriber
 from pyramid.renderers import render
@@ -49,22 +56,67 @@ class DocumentOwnerNotificationTemplate(object):
 
 AnnotationNotifier.register_template('document_owner', DocumentOwnerNotificationTemplate.generate_notification)
 
+requests_cache.install_cache('document_cache')
+document_cache = {}
+notifications = Queue()
 
-@subscriber(events.AnnotationEvent)
-def domain_notification(event):
-    if event.action == 'create':
-        annotation = event.annotation
-        if 'document' in annotation and 'reply_to' in annotation['document']:
+
+def notification_worker():
+    log.info('---- Init worker ----')
+    while True:
+        if not notifications.empty():
+            log.info('--------- Trying to get --------------')
+            annotation, request = notifications.get()
+            uri = annotation['uri']
+            log.info('---- Got new URI ----')
+            log.info(uri)
+            r = requests.get(uri)
+            log.info('---- Got answer ----')
+            log.info(r.headers)
+            page_date = r.headers.last_modified
+
+            # Check if the page is not cached or the cache is old
+            if uri not in document_cache or document_cache[uri]['date'] != page_date:
+                log.info('---- Begin parsing ----')
+                parsed_data = BeautifulSoup(r.data)
+                documents = parsed_data.select('a[rel="reply-to"]')
+                hrefs = [d['href'] for d in documents]
+                log.info(hrefs)
+                document_cache[uri] = {
+                    'date': page_date,
+                    'hrefs': hrefs
+                }
+
+            # Now send the notifications
+            emails = document_cache[uri]
             url_struct = urlparse(annotation['uri'])
             domain = url_struct.hostname if len(url_struct.hostname) > 0 else url_struct.path
             if domain[0:4] == 'www.': domain = domain[4:]
-            notifier = AnnotationNotifier(event.request)
-            for email in annotation['document']['reply_to']:
+            notifier = AnnotationNotifier(request)
+            for email in emails:
                 # Domain matching
                 mail_domain = email.split('@')[-1]
                 if mail_domain == domain:
                     # Send notification to owners
                     notifier.send_notification_to_owner(annotation, {'email': email}, 'document_owner')
+            notifications.task_done()
+        else:
+            time.sleep(1)
+            log.info(notifications.qsize())
+
+
+@subscriber(events.AnnotationEvent)
+def domain_notification(event):
+    log.info('event!!!!')
+    if event.action == 'create':
+        log.info('put')
+        notifications.put(event)
+        log.info(notifications.qsize())
+        log.info('after put')
+
+worker = threading.Thread(target=notification_worker)
+worker.daemon = True
+worker.start()
 
 
 def includeme(config):
