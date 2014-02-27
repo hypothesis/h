a52ab3cfd20c828ed3a15ca86bee1573aea0439f
Ujvari Gergely <ujvari@hypothes.is>
2014-02-27 16:34:00 +0100

    Domain mail is now single threaded
    - Currently our ./run command does not work well with threading
    - Pyramid threadlocal objects (like: requests, config) makes multi-threading overly complicated

--- h/domain_mailer.py
+++ h/domain_mailer.py
@@ -1,8 +1,5 @@
 import re
 import logging
-import time
-import threading
-from Queue import Queue
 from urlparse import urlparse
 import traceback
 
@@ -60,72 +57,46 @@ AnnotationNotifier.register_template('document_owner', DocumentOwnerNotification
 
 requests_cache.install_cache('document_cache')
 document_cache = {}
-notifications = Queue()
-
-
-def notification_worker():
-    while True:
-        if not notifications.empty():
-            try:
-                annotation, notification, notifier = notifications.get()
-                uri = annotation['uri']
-                r = requests.get(uri)
-                for k in r.headers:
-                    log.info(k + ': ' + r.headers[k])
-                page_date = r.headers['last-modified'] if 'last-modified' in r.headers else r.headers['date']
-
-                # Check if the page is not cached or the cache is old
-                if uri not in document_cache or document_cache[uri]['date'] != page_date:
-                    parsed_data = BeautifulSoup(r.text)
-                    documents = parsed_data.select('a[rel="reply-to"]')
-                    hrefs = []
-                    for d in documents:
-                        if d['href'].lower()[0:7] == 'mailto:': hrefs.append(d['href'][7:])
-                        else: hrefs.append(d['href'])
-                    document_cache[uri] = {
-                        'date': page_date,
-                        'hrefs': hrefs
-                    }
-
-                # Now send the notifications
-                emails = document_cache[uri]['hrefs']
-                url_struct = urlparse(annotation['uri'])
-                domain = url_struct.hostname if len(url_struct.hostname) > 0 else url_struct.path
-                if domain[0:4] == 'www.': domain = domain[4:]
-                for email in emails:
-                    # Domain matching
-                    mail_domain = email.split('@')[-1]
-                    if mail_domain == domain:
-                        # Send notification to owners
-                        notification['recipients'] = [email]
-                        notifier.send_rendered_notification(notification)
-                notifications.task_done()
-            except:
-                log.error(traceback.format_exc())
-                log.error('Notification_worker error!')
-        else:
-            time.sleep(1)
 
 
 @subscriber(events.AnnotationEvent)
 def domain_notification(event):
-    log.info('event!!!!')
     if event.action == 'create':
-        # We have to render our template here,
-        # because the other thread does not see all pyramid stuff
-        notification = DocumentOwnerNotificationTemplate.generate_notification(
-            event.request, event.annotation, {'email': ''}
-        )
-        notifier = AnnotationNotifier(event.request)
-        notifications.put((event.annotation, notification, notifier))
+        try:
+            annotation = event.annotation
+            uri = annotation['uri']
+            r = requests.get(uri)
+            page_date = r.headers['last-modified'] if 'last-modified' in r.headers else r.headers['date']
 
+            # Check if the page is not cached or the cache is old
+            if uri not in document_cache or document_cache[uri]['date'] != page_date:
+                parsed_data = BeautifulSoup(r.text)
+                documents = parsed_data.select('a[rel="reply-to"]')
+                hrefs = []
+                for d in documents:
+                    if d['href'].lower()[0:7] == 'mailto:': hrefs.append(d['href'][7:])
+                    else: hrefs.append(d['href'])
+                document_cache[uri] = {
+                    'date': page_date,
+                    'hrefs': hrefs
+                }
 
-def create_thread():
-    worker = threading.Thread(target=notification_worker)
-    worker.daemon = True
-    worker.start()
+            # Now send the notifications
+            emails = document_cache[uri]['hrefs']
+            url_struct = urlparse(annotation['uri'])
+            domain = url_struct.hostname if len(url_struct.hostname) > 0 else url_struct.path
+            if domain[0:4] == 'www.': domain = domain[4:]
+            notifier = AnnotationNotifier(event.request)
+            for email in emails:
+                # Domain matching
+                mail_domain = email.split('@')[-1]
+                if mail_domain == domain:
+                    # Send notification to owners
+                    notifier.send_notification_to_owner(annotation, {'email': email}, 'document_owner')
+        except:
+            log.error(traceback.format_exc())
+            log.error('Notification_worker error!')
 
 
 def includeme(config):
-    create_thread()
     config.scan(__name__)
