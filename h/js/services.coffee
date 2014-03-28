hypothesis/h

commit a0d5b3ac6a2de3004e902427f3642abe10b2b7c2

Don't change scope on adding a reply. Fixes #1115.

h/js/services.coffee
@@ -521,6 +521,10 @@ class Hypothesis extends Annotator
     # No targets and no references means that this is a comment
     not (annotation.references?.length or annotation.target?.length)
 
+  # Is this annotation a reply?
+  isReply: (annotation) ->
+    # The presence of references means that this is a reply
+    annotation.references.length
 
   # Discard all drafts, deleting unsaved annotations from the annotator
   discardDrafts: ->

h/js/controllers.coffee
@@ -499,7 +499,8 @@ class Annotation
             if $rootScope.view isnt "Comments"
               $rootScope.applyView "Comments"
           else
-            if $rootScope.view in ["Comments", "Selection"]
+            if not annotator.isReply(annotation) and
+                $rootScope.view in ["Comments", "Selection"]
               $rootScope.applyView "Screen"
           $timeout (-> annotator.publish 'annotationCreated', annotation),
             1000

commit 73052cda5207aacede30c3f6b3f41ff61a1c1d35

Fix exception on top-level annotations, introduced in a0d5b3ac

h/js/services.coffee
@@ -524,7 +524,7 @@ class Hypothesis extends Annotator
   # Is this annotation a reply?
   isReply: (annotation) ->
     # The presence of references means that this is a reply
-    annotation.references.length
+    annotation.references?.length
 
   # Discard all drafts, deleting unsaved annotations from the annotator
   discardDrafts: ->