bd6948423a88970200b006316ae6430c0af4e93a
Randall Leeds <tilgovi@hypothes.is>
2014-04-04 12:36:15 -0700

    Auto-detect Heroku / Dokku in gunicorn.conf.py
    This sets up the forwarded_allow_ips configuration which is needed
    for the app to trust X-FORWARDED-* headers.

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,11 @@
+import os
+
+# Smart detect heroku stack and assume a trusted proxy.
+# This is a convenience that should hopefully not be too surprising.
+if 'heroku' in os.environ.get('LD_LIBRARY_PATH', ''):
+    forwarded_allow_ips = '*'
+
+
 def post_fork(_server, _worker):
     try:
         import psycogreen.gevent
