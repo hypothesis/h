38ace76877aba3731f9ad79a47e0c5bed71fb787
Randall Leeds <tilgovi@hypothes.is>
2014-04-17 16:12:16 -0700

    Work around horus assumptions about csrf tokens
    Horus assumes the form has a csrf_token. The validator has handled
    headers or form parameters since 4815084 by using the built-in csrf
    check function in Pyramid, but without including the field in the
    schema with a "missing" value horus will choke during registration
    when it tries to delete the value before taking the rest of the
    deserialized values as the attributes of the new user.

--- h/schemas.py
+++ h/schemas.py
@@ -19,6 +19,15 @@ def unique_username(node, value):
 
 
 class CSRFSchema(colander.Schema):
+    """
+    A CSRFSchema backward-compatible with the one from the hem module.
+
+    Unlike hem, this doesn't require that the csrf_token appear in the
+    serialized appstruct.
+    """
+
+    csrf_token = colander.SchemaNode(colander.String(), missing=None)
+
     def validator(self, form, value):
         request = form.bindings['request']
         check_csrf_token(request)
