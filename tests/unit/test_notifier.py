883abf91451477e779fd0b3a3bc603ba48dfc07e
Randall Leeds <tilgovi@hypothes.is>
2014-05-07 18:18:33 -0700

    In notifier tests, improve pylint
    Make some small improvements to clean up the notifier tests.
    
    - Use pylint symbolic names for disables because they are easier to
      understand when reading.
    - Disable no-self-use at the NotifierTest class block level so that
      all tests can be written as methods without lint warnings.
    - Fix some long lines
    - Shorten test names. These become more readable this way as well.
    - Remove unnecessary reassignment of h.notifier classes as test class
      attributes `rtemp` and `ntemp'.

--- tests/unit/test_notifier.py
+++ tests/unit/test_notifier.py
@@ -8,18 +8,20 @@ from h.streamer import FilterHandler
 from . import AppTestCase
 
 
-class NotifierTest(AppTestCase):  # pylint: disable=R0904
+class NotifierTest(AppTestCase):
     """All Notification related unit tests live here"""
-    class QueryMock(object):  # pylint: disable=R0903
+    # pylint: disable=no-self-use,too-many-public-methods
+
+    class QueryMock(object):
         """This class is used to simulate user subscription queries"""
-        def __init__(self, active=False, query={}, template=""):  # pylint: disable=W0102
+        # pylint: disable=too-few-public-methods
+        def __init__(self, active=False, query=None, template=""):
             self.active = active
-            self.query = query
+            self.query = query or {}
             self.template = template
 
     # Tests for handling AnnotationEvent
-    @classmethod
-    def test_send_notifications_authorizations(cls):  # pylint: disable=C0103
+    def test_authorization(self):
         """Make sure private annotations don't send notifications
         """
         annotation = {'permissions': {'read': ['acct:test@example.com']}}
@@ -30,7 +32,7 @@ class NotifierTest(AppTestCase):  # pylint: disable=R0904
             notifier.send_notifications(event)
             assert mock.call_count == 0
 
-    def test_passive_queries_do_not_get_executed(self):  # pylint: disable=C0103
+    def test_passive_queries(self):
         """Make sure if a query is passive the notifier system
         does not get called"""
         annotation = {'permissions': {'read': ["group:__world__"]}}
@@ -46,7 +48,7 @@ class NotifierTest(AppTestCase):  # pylint: disable=R0904
                 notifier.send_notifications(event)
                 assert mock_notif().send_notification_to_owner.call_count == 0
 
-    def test_call_notifier_if_query_matches(self):  # pylint: disable=C0103
+    def test_query_matches(self):
         """Make sure that the query match triggers the notifier call"""
         annotation = {
             'permissions': {'read': ["group:__world__"]},
@@ -65,9 +67,10 @@ class NotifierTest(AppTestCase):  # pylint: disable=R0904
 
                     mock_filter().match = Mock(return_value=True)
                     notifier.send_notifications(event)
-                    assert mock_notif().send_notification_to_owner.call_count == 1
+                    actual = mock_notif().send_notification_to_owner.call_count
+                    assert actual == 1
 
-    def test_if_query_does_not_match_notifier_does_not_get_called(self):  # pylint: disable=C0103
+    def test_query_mismatch(self):
         """Make sure that the lack of matching prevents calling
         the AnnotationNotifier"""
         annotation = {
@@ -87,11 +90,11 @@ class NotifierTest(AppTestCase):  # pylint: disable=R0904
 
                     mock_filter().match = Mock(return_value=False)
                     notifier.send_notifications(event)
-                    assert mock_notif().send_notification_to_owner.call_count == 0
+                    actual = mock_notif().send_notification_to_owner.call_count
+                    assert actual == 0
 
     # Tests for AnnotationNotifier
-    @classmethod
-    def test_template_registration(cls):
+    def test_template_registration(self):
         """Make sure the AnnotatioNotifier uses the given key and function
         for template registration"""
         def test_fn():
@@ -102,8 +105,7 @@ class NotifierTest(AppTestCase):  # pylint: disable=R0904
         assert 'test_template' in templates
         assert templates['test_template'] == test_fn
 
-    @classmethod
-    def test_dont_send_data_for_false_templates(cls):  # pylint: disable=C0103
+    def test_false_templates(self):
         """Make sure we do not process anything for false templates"""
         request = DummyRequest()
         notif = notifier.AnnotationNotifier(request)
@@ -111,11 +113,10 @@ class NotifierTest(AppTestCase):  # pylint: disable=R0904
         notif.send_notification_to_owner({}, {}, 'false_template')
         assert notif._send_annotation.called is False  # pylint: disable=W0212
 
-    @classmethod
-    def test_do_not_send_notification_when_status_is_bad(cls):  # pylint: disable=C0103
+    def test_bad_status(self):
         """Make sure if a renderer throws a false status (i.e. for errors)
         then no notification is sent"""
-        def test_generator(request, annotation, data): # pylint: disable=W0613
+        def test_generator(request, annotation, data):  # pylint: disable=W0613
             """Our test template function, auto fails"""
             return {"status": False}
         notifier.AnnotationNotifier.register_template("test", test_generator)
@@ -125,8 +126,7 @@ class NotifierTest(AppTestCase):  # pylint: disable=R0904
         notif.send_notification_to_owner({}, {}, 'test')
         assert notif._send_annotation.call_count == 0  # pylint: disable=W0212
 
-    @classmethod
-    def test_send_annotation_is_called_with_the_right_parameters(cls):  # pylint: disable=C0103
+    def test_template_parameters(self):
         """Make sure the body, subject, recipients fields are correct"""
         def test_generator(request, annotation, data):  # pylint: disable=W0613
             """Our test template function"""
@@ -139,33 +139,36 @@ class NotifierTest(AppTestCase):  # pylint: disable=R0904
         notifier.AnnotationNotifier.register_template("test", test_generator)
         request = DummyRequest()
         notif = notifier.AnnotationNotifier(request)
-        notif._send_annotation = Mock()  # pylint: disable=W0212
-        notif.send_notification_to_owner({}, {}, 'test')
-        assert notif._send_annotation.called_with('Test body', 'Test subject', ['Test user']) # pylint: disable=W0212
+        with patch.object(notif, '_send_annotation') as send:
+            notif.send_notification_to_owner({}, {}, 'test')
+            assert send.called_with('Test body', 'Test subject', ['Test user'])
 
-    # Tests for NotificationTemplate
-    ntemp = notifier.NotificationTemplate
-
-    @classmethod
-    def test_check_conditions_is_used_to_make_a_template_fail(cls):  # pylint: disable=C0103
+    # Tests for notifier.NotificationTemplate
+    def test_check_conditions(self):
         """Make sure if the check_conditions() returns False then the
         generate_notification returns with a false status"""
-        class FalseTemplate(cls.ntemp):  # pylint: disable=R0903,W0232
+
+        class FalseTemplate(notifier.NotificationTemplate):
             """Our false template class"""
-            @classmethod
-            def check_conditions(cls, annotation, data):  # pylint: disable=W0613
+            # pylint: disable=abstract-method
+
+            @staticmethod
+            def check_conditions(annotation, data):
                 """This is our false condition function"""
                 return False
-        assert FalseTemplate.generate_notification({}, {}, {})['status'] is False  # pylint: disable=E1101
+        result = FalseTemplate.generate_notification({}, {}, {})
+        assert result['status'] is False
 
-    @classmethod
-    def test_template_subject_recipients_generation(cls):  # pylint: disable=C0103
+    def test_subject_and_recipients(self):
         """Make sure the recipients are generated by the get_recipients() and
         template, subject are generated by render()"""
-        class TestTemplate(cls.ntemp):  # pylint: disable=R0903,W0232
+
+        class TestTemplate(notifier.NotificationTemplate):
             """Our test template class"""
+            # pylint: disable=abstract-method
+
             @staticmethod
-            def get_recipients(request, annotation, data):  # pylint: disable=W0613
+            def get_recipients(request, annotation, data):
                 """Our mock get_recipients function"""
                 return ["test user"]
 
@@ -174,52 +177,42 @@ class NotifierTest(AppTestCase):  # pylint: disable=R0904
                 """Our mock render function"""
                 return "test body", "test subject"
 
-        result = TestTemplate.generate_notification({}, {}, {})  # pylint: disable=E1101
+        result = TestTemplate.generate_notification({}, {}, {})
         assert result['status'] is True
         assert result['recipients'] == ['test user']
         assert result['rendered'] == 'test body'
         assert result['subject'] == 'test subject'
 
     # Tests for reply notifications
-    rtemp = notifier.ReplyTemplate
-
-    @classmethod
-    def test_if_reply_notification_is_registered(cls):  # pylint: disable=C0103
-        """Make sure these notifications are alive"""
-        templates = notifier.AnnotationNotifier.registered_templates
-        assert 'reply_notification' in templates
-
-    def test_if_the_right_function_is_registered(self):  # pylint: disable=C0103
+    def test_reply_registration(self):
         """We should have registered the ReplyTemplate.generate_notification"""
         templates = notifier.AnnotationNotifier.registered_templates
+        assert 'reply_notification' in templates
         registered_fn = templates['reply_notification']
-        right_fn = self.rtemp.generate_notification
+        right_fn = notifier.ReplyTemplate.generate_notification
         assert registered_fn == right_fn
 
-    @classmethod
-    def test_generated_reply_query_match(cls):  # pylint: disable=C0103
+    def test_reply_query_match(self):
         """Check if the generated query really matches on
         the parent annotation user"""
         annotation = {'parent': {'user': 'acct:testuser@testdomain'}}
         query = notifier.generate_system_reply_query('testuser', 'testdomain')
         assert FilterHandler(query).match(annotation, 'create') is True
 
-    @classmethod
-    def test_generated_reply_query_dont_match_same_domain_different_user(cls):  # pylint: disable=C0103
-        """Check if the generated query really do not match on any other user"""
+    def test_reply_username_mismatch(self):
+        """Check if the generated query requires the domain to match."""
         annotation = {'parent': {'user': 'acct:testuser2@testdomain'}}
         query = notifier.generate_system_reply_query('testuser', 'testdomain')
         assert FilterHandler(query).match(annotation, 'create') is False
 
-    @classmethod
-    def test_generated_reply_query_dont_match_same_user_different_domain(cls):  # pylint: disable=C0103
+    def test_reply_domain_mismatch(self):
         """Check if the generated query really do not match on
         any other domain"""
         annotation = {'parent': {'user': 'acct:testuser@testdomain2'}}
         query = notifier.generate_system_reply_query('testuser', 'testdomain')
         assert FilterHandler(query).match(annotation, 'create') is False
 
-    def test_dont_send_reply_notification_for_top_level_annotation(self):  # pylint: disable=C0103
+    def test_reply_ignore_root(self):
         """Don't send reply notification for a top level annotation"""
         annotation = {
             'permissions': {'read': ['group:__world__']},
@@ -230,9 +223,10 @@ class NotifierTest(AppTestCase):  # pylint: disable=R0904
         with patch('h.notifier.parent_values') as mock_parent_fn:
             mock_parent_fn.return_value = {}
             annotation['parent'] = notifier.parent_values(annotation, request)
-            assert self.rtemp.check_conditions(annotation, {}) is False
+            actual = notifier.ReplyTemplate.check_conditions(annotation, {})
+            assert actual is False
 
-    def test_dont_send_reply_notification_when_parent_user_is_the_same(self):  # pylint: disable=C0103
+    def test_reply_to_self(self):
         """Don't send reply notification to yourself"""
         annotation = {
             'permissions': {'read': ['group:__world__']},
@@ -243,9 +237,10 @@ class NotifierTest(AppTestCase):  # pylint: disable=R0904
         with patch('h.notifier.parent_values') as mock_parent_fn:
             mock_parent_fn.return_value = {'user': 'acct:testuser@domain'}
             annotation['parent'] = notifier.parent_values(annotation, request)
-            assert self.rtemp.check_conditions(annotation, {}) is False
+            actual = notifier.ReplyTemplate.check_conditions(annotation, {})
+            assert actual is False
 
-    def test_send_reply_notification_when_parent_user_is_different(self):  # pylint: disable=C0103
+    def test_reply_to_other(self):
         """Send reply notification when somebody replies to you"""
         annotation = {
             'permissions': {'read': ['group:__world__']},
@@ -256,4 +251,5 @@ class NotifierTest(AppTestCase):  # pylint: disable=R0904
         with patch('h.notifier.parent_values') as mock_parent_fn:
             mock_parent_fn.return_value = {'user': 'acct:testuser2@domain'}
             annotation['parent'] = notifier.parent_values(annotation, request)
-            assert self.rtemp.check_conditions(annotation, {}) is True
+            actual = notifier.ReplyTemplate.check_conditions(annotation, {})
+            assert actual is True
