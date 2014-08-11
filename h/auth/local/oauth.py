50dcca87f2339a907838b7890f4b8b232d0a1f38
Randall Leeds <tilgovi@hypothes.is>
2014-08-10 18:00:14 -0700

    Only set a session factory if none configured

--- h/auth/local/oauth.py
+++ h/auth/local/oauth.py
@@ -3,6 +3,7 @@ import os
 from oauthlib.oauth2 import ClientCredentialsGrant, InvalidClientError
 from pyramid.authentication import SessionAuthenticationPolicy
 from pyramid.exceptions import BadCSRFToken
+from pyramid.interfaces import ISessionFactory
 from pyramid.session import check_csrf_token, SignedCookieSessionFactory
 
 from h.api import get_consumer
@@ -29,6 +30,20 @@ class SessionGrant(ClientCredentialsGrant):
         request.client_id = request.client_id or request.client.client_id
 
 
+def session_from_config(settings, prefix='session.'):
+    """Return a session factory from the provided settings."""
+    secret_key = '{}secret'.format(prefix)
+    if secret_key not in settings:
+        # Get 32 bytes (256 bits) from a secure source (urandom) as a secret.
+        # Pyramid will add a salt to this. The salt and the secret together
+        # will still be less than the, and therefore right zero-padded to,
+        # 1024-bit block size of the default hash algorithm, sha512. However,
+        # 256 bits of random should be more than enough for session secrets.
+        settings[secret_key] = os.urandom(32)
+
+    return SignedCookieSessionFactory(settings[secret_key])
+
+
 def includeme(config):
     config.include('pyramid_oauthlib')
     config.add_grant_type(SessionGrant)
@@ -38,15 +53,9 @@ def includeme(config):
     authn_policy = SessionAuthenticationPolicy(prefix='', debug=authn_debug)
     config.set_authentication_policy(authn_policy)
 
-    # Configure the session
-    if 'session.secret' not in config.registry.settings:
-        # Get 32 bytes (256 bits) from a secure source (urandom) as a secret.
-        # Pyramid will add a salt to this. The salt and the secret together
-        # will still be less than the, and therefore right zero-padded to,
-        # 1024-bit block size of the default hash algorithm, sha512. However,
-        # 256 bits of random should be more than enough for session secrets.
-        config.registry.settings['session.secret'] = os.urandom(32)
+    def register():
+        if config.registry.queryUtility(ISessionFactory) is None:
+            session_factory = session_from_config(config.registry.settings)
+            config.registry.registerUtility(session_factory, ISessionFactory)
 
-    session_secret = config.registry.settings['session.secret']
-    session_factory = SignedCookieSessionFactory(session_secret)
-    config.set_session_factory(session_factory)
+    config.action(None, register, order=1)
