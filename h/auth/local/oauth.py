61a55c66b830670f0506bdd0c4ee45b945656926
Randall Leeds <tilgovi@hypothes.is>
2014-08-10 19:23:21 -0700

    Don't pollute the settings with default secret

--- h/auth/local/oauth.py
+++ h/auth/local/oauth.py
@@ -33,15 +33,16 @@ class SessionGrant(ClientCredentialsGrant):
 def session_from_config(settings, prefix='session.'):
     """Return a session factory from the provided settings."""
     secret_key = '{}secret'.format(prefix)
-    if secret_key not in settings:
+    secret = settings.get(secret_key)
+    if secret is None:
         # Get 32 bytes (256 bits) from a secure source (urandom) as a secret.
         # Pyramid will add a salt to this. The salt and the secret together
         # will still be less than the, and therefore right zero-padded to,
         # 1024-bit block size of the default hash algorithm, sha512. However,
         # 256 bits of random should be more than enough for session secrets.
-        settings[secret_key] = os.urandom(32)
+        secret = os.urandom(32)
 
-    return SignedCookieSessionFactory(settings[secret_key])
+    return SignedCookieSessionFactory(secret)
 
 
 def includeme(config):
