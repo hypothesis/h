6dbbbd1465c0ed6e97035f769c33db074636c91b
Aron Carroll <self@aroncarroll.com>
2014-08-12 12:00:15 +0200

    Add /dev/emails page for testing email output

--- h/testing.py
+++ h/testing.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 import logging
 
+from datetime import datetime, timedelta
+from h.notifier import ReplyTemplate
 from pyramid.view import view_config
 from pyramid_layout.layout import layout_config
 from pyramid_mailer.interfaces import IMailer
 from pyramid_mailer.testing import DummyMailer
+from pyramid.renderers import render
 
 from h.layouts import BaseLayout
 
@@ -16,6 +19,40 @@ log = logging.getLogger(__name__)  # pylint: disable=invalid-name
              route_name='pattern_library')
 def page(context, request):
     return {}
+
+@view_config(renderer='h:templates/email_preview.pt',
+             route_name='email_preview')
+def page(context, request):
+    notification_email_data = {
+        'document_title': 'A very important article',
+        'document_path': 'http://example.com/article?some-long=query',
+        'parent_quote': 'This is a selected piece of text',
+        'parent_text': 'This is the parent comment',
+        'parent_user': 'toby',
+        'parent_tags': 'comment, important, news',
+        'parent_timestamp': datetime.now() - timedelta(hours=1),
+        'parent_user_profile': 'https://hypothes.is/user:toby',
+        'parent_path': 'https://hypothes.is/a/123456789',
+        'reply_quote': '',
+        'reply_text': 'This is a reply to the parent comment',
+        'reply_user': 'anna',
+        'reply_tags': 'reply, important, critisism',
+        'reply_timestamp': datetime.now(),
+        'reply_user_profile': 'https://hypothes.is/user:anna',
+        'reply_path': 'http://hypothes.is/a/abcdefghijk',
+    }
+
+    return {
+        'emails': (
+            {
+                'title': 'Notification Email',
+                'subject': render(ReplyTemplate.subject,
+                           notification_email_data, request),
+                'rendered': render(ReplyTemplate.template,
+                            notification_email_data, request)
+            },
+        )
+    }
 
 
 @layout_config(name='pattern_library', template='h:templates/base.pt')
@@ -53,6 +90,7 @@ def includeme(config):
     config.include('pyramid_layout')
 
     config.add_route('pattern_library', '/dev/pattern-library')
+    config.add_route('email_preview', '/dev/emails')
 
     mailer = LoggingMailer()
     config.registry.registerUtility(mailer, IMailer)
