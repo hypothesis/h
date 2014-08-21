99e85d9d8cb925733f9cfcaa02341de7d148672a
BigBlueHat <byoung@bigbluehat.com>
2014-08-21 13:46:15 -0400

    WIP: working (though hand-built) add-on attempt

---
+++ h/browser/firefox/lib/main.js
@@ -0,0 +1,47 @@
+var data = require("sdk/self").data;
+var tabs = require("sdk/tabs");
+var { ToggleButton } = require("sdk/ui/button/toggle");
+var btn_config = {};
+var btn;
+
+function tabToggle(tab) {
+  if (btn.state('window').checked) {
+    tab.attach({
+      contentScriptFile: data.url('embed.js')
+    });
+  } else {
+    tab.attach({
+      contentScript: [
+        'var s = document.createElement("script");',
+        's.setAttribute("src", "' + data.url('destroy.js') + '");',
+        'document.body.appendChild(s);'
+      ]
+    });
+  }
+}
+
+btn_config = {
+  id: "hypothesis",
+  label: "Annotate",
+  contentURL: "http://hypothes.is/wp-content/uploads/2012/12/favicon1.ico",
+  icon: {
+    "18": './18.png',
+    "32": './32.png',
+    "36": './36.png',
+    "64": './64.png'
+  },
+  onClick: function(state) {
+    tabToggle(tabs.activeTab)
+  }
+};
+
+if (undefined === ToggleButton) {
+  btn = require("sdk/widget").Widget(btn_config);
+} else {
+  btn = ToggleButton(btn_config);
+}
+
+tabs.on('open', function(tab) {
+  tab.on('activate', tabToggle);
+  tab.on('pageshow', tabToggle);
+});
