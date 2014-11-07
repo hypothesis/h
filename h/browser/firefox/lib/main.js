271dc575de87f8f4b812c3042fc2bf379d84b970
BigBlueHat <byoung@bigbluehat.com>
2014-11-06 21:14:27 -0500

    Update icons to match Chrome; Handle states

--- h/browser/firefox/lib/main.js
+++ h/browser/firefox/lib/main.js
@@ -5,6 +5,22 @@ var btn_config = {};
 var btn;
 // tab state machine
 var tab_state = {};
+// icons
+var icons = {
+  'sleeping': {
+    "18": './images/sleeping_18.png',
+    "32": './images/sleeping_32.png',
+    "36": './images/sleeping_36.png',
+    "64": './images/sleeping_64.png'
+  },
+  // for all occasionas
+  'active': {
+    "18": './images/active_18.png',
+    "32": './images/active_32.png',
+    "36": './images/active_36.png',
+    "64": './images/active_64.png'
+  }
+};
 
 function tabToggle(tab) {
   if (tab_state[tab.id] === true) {
@@ -12,6 +28,10 @@ function tabToggle(tab) {
       contentScriptFile: data.url('embed.js')
     });
   } else if (undefined === tab_state[tab.id] || tab_state[tab.id] === false) {
+    btn.state(tab, {
+      label: 'Annotate',
+      icon: icons['sleeping']
+    });
     tab.attach({
       contentScript: [
         'var s = document.createElement("script");',
@@ -25,18 +45,26 @@ function tabToggle(tab) {
 btn_config = {
   id: "hypothesis",
   label: "Annotate",
-  icon: {
-    "18": './images/sleeping_18.png',
-    "32": './images/sleeping_32.png',
-    "36": './images/sleeping_36.png',
-    "64": './images/sleeping_64.png'
-  },
+  icon: icons['sleeping'],
   onChange: function() {
     // delete the window-wide state default
     this.state('window', null);
     // but turn it back on for this tab
-    this.state('tab', {checked: !this.state('tab').checked});
-    tab_state[tabs.activeTab.id] = this.state('tab').checked;
+    if (!this.state(tabs.activeTab).checked === true) {
+      this.state(tabs.activeTab, {
+        checked: true,
+        label: 'Annotating',
+        icon: icons['active']
+      });
+      tab_state[tabs.activeTab.id] = true;
+    } else {
+      this.state(tabs.activeTab, {
+        checked: false,
+        label: 'Annotate',
+        icon: icons['sleeping']
+      });
+      tab_state[tabs.activeTab.id] = false;
+    }
     tabToggle(tabs.activeTab)
   }
 };
