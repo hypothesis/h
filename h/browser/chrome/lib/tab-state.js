51860e505b92a488695979403d2dbc806320de7c
Aron Carroll <self@aroncarroll.com>
2014-11-18 16:06:13 +0000

    Document the TabState module

--- h/browser/chrome/lib/tab-state.js
+++ h/browser/chrome/lib/tab-state.js
@@ -8,7 +8,16 @@
   };
 
   /* Manages the state of the browser action button to ensure that it displays
-   * correctly for the currently active tab.
+   * correctly for the currently active tab. An onchange callback will be
+   * called when the extension changes state. This will be provided
+   * with the tabId, the current and previous states.
+   *
+   * Each state has a method to enable it such as activateTab() and a method
+   * to query the current state such as isTabActive().
+   *
+   * initialState - An Object of tabId/state keys. Ideal for loading state
+   *   from a persisted store such as local storage.
+   * onchange     - A function that recieves onchange(tabId, current, prev).
    */
   function TabState(initialState, onchange) {
     var currentState;
@@ -16,6 +25,12 @@
 
     this.onchange = onchange || null;
 
+    /* Replaces the entire state of the object with a new one.
+     *
+     * newState - An object of tabId/state pairs.
+     *
+     * Returns nothing.
+     */
     this.load = function (newState) {
       previousState = currentState || {};
       currentState = newState;
