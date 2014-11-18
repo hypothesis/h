1716237f369a6bba24f1f1bf9bdd4a69423242d4
Aron Carroll <self@aroncarroll.com>
2014-11-18 14:25:14 +0000

    Add unit tests for TabErrorCache

---
+++ h/browser/chrome/test/tab-error-cache-test.js
@@ -0,0 +1,26 @@
+describe('TabErrorCache', function () {
+ var cache;
+ var assert = chai.assert;
+
+ beforeEach(function () {
+   cache = new h.TabErrorCache();
+ });
+
+ it('allows items to be set an retrieved', function () {
+   var err = new Error('foo');
+   cache.setTabError(1, err);
+   assert.equal(cache.getTabError(1), err);
+ });
+
+ it('allows items to be removed', function () {
+   var err = new Error('foo');
+   cache.setTabError(1, err);
+   cache.unsetTabError(1);
+   assert.notEqual(cache.getTabError(1), err);
+ });
+
+ it('returns null if an item does not exist', function () {
+   var err = new Error('foo');
+   assert.isNull(cache.getTabError(1));
+ });
+});
