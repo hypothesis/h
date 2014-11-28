484f0949599c61c4f9f1d8e293aa2bcac122b14a
Aron Carroll <self@aroncarroll.com>
2014-11-28 12:21:22 +0100

    Provide help for Windows and Linux users

---
+++ h/browser/chrome/help/index.js
@@ -0,0 +1,9 @@
+// Detect the current OS and show approprite help.
+chrome.runtime.getPlatformInfo(function (info) {
+  var opts = document.querySelectorAll('[data-extension-path]');
+  [].forEach.call(opts, function (opt) {
+    if (opt.dataset.extensionPath !== info.os) {
+      opt.hidden = true;
+    }
+  });
+});
