da80dd9610e2517cd04ee1cc96f82dc794b781f9
Nick Stenning <nick@whiteink.com>
2014-12-01 11:01:13 +0100

    queue: Interface definitions don't take self
    http://docs.zope.org/zope.interface/README.html#defining-interfaces

--- h/queue/interfaces.py
+++ h/queue/interfaces.py
@@ -2,18 +2,18 @@ from zope.interface import Interface
 
 
 class IQueueHelper(Interface):
-    def __init__(self, settings):  # noqa
+    def __init__(settings):  # noqa
         """
         Initialize a queue helper from the provided settings.
         """
 
-    def get_reader(self, settings, topic, channel):  # noqa
+    def get_reader(settings, topic, channel):  # noqa
         """
         Get a queue reader in order to retrieve topic from the specified topic
         and channel.
         """
 
-    def get_writer(self, settings):  # noqa
+    def get_writer(settings):  # noqa
         """
         Get a queue writer in order to push work onto a queue.
         """
