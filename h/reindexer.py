328f79260ced604549f8f2f74322f571e73c5397
Gerben <gerben@treora.com>
2014-12-03 00:04:25 +0200

    Simplify reindexer.py using annotator-v0.13

--- h/reindexer.py
+++ h/reindexer.py
@@ -5,33 +5,9 @@ from h.models import Annotation, Document
 
 
 class Reindexer(annotator.reindexer.Reindexer):
-    # This class slighty modifies annotator's Reindexer. Besides mappings, it
-    # also sets the index settings, and does both directly at index creation.
-    # And, of course, it uses our own models instead of annotator's.
-
     es_models = Annotation, Document
 
-    def reindex(self, old_index, new_index):
-        """Reindex documents using the current mappings."""
-        conn = self.conn
-
-        if not conn.indices.exists(old_index):
-            raise ValueError("Index {0} does not exist!".format(old_index))
-
-        if conn.indices.exists(new_index):
-            raise ValueError("Index {0} already exists!".format(new_index))
-
-        # Configure index mapping and settings
-        index_config = {'mappings': {}, 'settings': {}}
-        for model in self.es_models:
-            index_config['mappings'].update(model.get_mapping())
-            if hasattr(model, '__settings__'):
-                index_config['settings'] = model.__settings__
-
-        # Create the new index
-        conn.indices.create(new_index, body=index_config)
-
-        # Do the actual reindexing.
-        self._print("Reindexing {0} to {1}..".format(old_index, new_index))
-        helpers.reindex(conn, old_index, new_index)
-        self._print("Reindexing done.")
+    def get_index_config(self):
+        index_config = super(Reindexer, self).get_index_config()
+        index_config['settings'] = Annotation.__settings__
+        return index_config
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ install_requires = [
     'BeautifulSoup4>=4.2.1',
     'SQLAlchemy>=0.8.0',
     'alembic>=0.6.3',
-    'annotator>=0.12.0,<0.13',
+    'annotator>=0.13.0,<0.14',
     'blinker>=1.3,<1.4',
     'clik==0.3.1',
     'deform>=0.9,<1.0',
