01bbf52c0935345c3ccedf7da556daba206ecaa1
Aron Carroll <self@aroncarroll.com>
2014-12-16 16:15:47 +0100

    Create some helpers to build CoffeeScript assets

--- h/asset_bundles.py
+++ h/asset_bundles.py
@@ -1,102 +1,112 @@
 from webassets import Bundle
+from pyramid_webassets import PyramidResolver
 
-app_dependencies_js = Bundle(
-    'h:static/scripts/vendor/jschannel.js',
-    'h:static/scripts/vendor/jwz.js',
-    'h:static/scripts/vendor/moment-with-langs.js',
-    'h:static/scripts/vendor/jstz.js',
-    'h:static/scripts/vendor/moment-timezone.js',
-    'h:static/scripts/vendor/moment-timezone-data.js',
-    'h:static/scripts/vendor/Markdown.Converter.js',
-    'h:static/scripts/vendor/polyfills/autofill-event.js',
-    'h:static/scripts/vendor/unorm.js',
-    'h:static/scripts/vendor/uuid.js',
-    'h:static/scripts/vendor/gettext.js',
-    'h:static/scripts/vendor/katex/katex.js',
-    'h:static/scripts/vendor/annotator.js',
-    'h:static/scripts/vendor/annotator.auth.js',
-    'h:static/scripts/vendor/annotator.document.js',
-    'h:static/scripts/vendor/annotator.permissions.js',
-    'h:static/scripts/vendor/annotator.store.js',
-    Bundle('h:static/scripts/plugin/bridge.coffee',
-           filters='coffeescript',
-           output='debug/scripts/plugin/bridge.js'),
-    Bundle('h:static/scripts/plugin/discovery.coffee',
-           filters='coffeescript',
-           output='debug/scripts/plugin/discovery.js'),
-    Bundle('h:static/scripts/plugin/threading.coffee',
-           filters='coffeescript',
-           output='debug/scripts/plugin/threading.js'))
+resolver = PyramidResolver()
 
-app_js = Bundle(
-    'h:static/scripts/app.coffee',
-    'h:static/scripts/controllers.coffee',
-    'h:static/scripts/directives.coffee',
-    'h:static/scripts/directives/*.coffee',
-    'h:static/scripts/filters.coffee',
-    'h:static/scripts/searchfilters.coffee',
-    'h:static/scripts/services.coffee',
-    'h:static/scripts/*-service.coffee',
-    'h:static/scripts/streamsearch.coffee',
-    filters='coffeescript',
-    output='debug/scripts/app.js')
 
-account_js = Bundle(
-    'h:static/scripts/account/account.coffee',
-    'h:static/scripts/account/*-controller.coffee',
-    'h:static/scripts/account/*-service.coffee',
-    output='scripts/account.js',
-    filters='coffeescript')
+def resolve_glob(path):
+    # First argument is the context, we don't have one here.
+    return resolver.search_for_source(None, path)
 
-helpers_js = Bundle(
-    'h:static/scripts/vendor/angular-bootstrap.js',
-    Bundle('h:static/scripts/helpers/helpers.coffee',
-           'h:static/scripts/helpers/*-helpers.coffee',
-           filters='coffeescript',
-           output='debug/scripts/helpers.js'))
 
-session_js = Bundle(
-    'h:static/scripts/vendor/angular-resource.js',
-    Bundle('h:static/scripts/session/session.coffee',
-           'h:static/scripts/session/*-service.coffee',
-           filters='coffeescript',
-           output='debug/scripts/session.js')
-)
+def process_path(path):
+    if not isinstance(path, basestring):
+        return path
 
-hypothesis_js = Bundle(
-    'h:static/scripts/vendor/jquery.scrollintoview.js',
-    'h:static/scripts/vendor/jschannel.js',
-    'h:static/scripts/vendor/gettext.js',
-    'h:static/scripts/vendor/annotator.js',
-    'h:static/scripts/vendor/annotator.document.js',
-    'h:static/scripts/vendor/diff_match_patch_uncompressed.js',
-    'h:static/scripts/vendor/dom_text_mapper.js',
-    'h:static/scripts/vendor/dom_text_matcher.js',
-    'h:static/scripts/vendor/text_match_engines.js',
-    'h:static/scripts/vendor/annotator.domtextmapper.js',
-    'h:static/scripts/vendor/annotator.textanchors.js',
-    'h:static/scripts/vendor/annotator.fuzzytextanchors.js',
-    'h:static/scripts/vendor/page_text_mapper_core.js',
-    'h:static/scripts/vendor/annotator.pdf.js',
-    'h:static/scripts/vendor/annotator.textanchors.js',
-    'h:static/scripts/vendor/annotator.textrange.js',
-    'h:static/scripts/vendor/annotator.textposition.js',
-    'h:static/scripts/vendor/annotator.textquote.js',
-    'h:static/scripts/vendor/annotator.texthighlights.js',
-    Bundle('h:static/scripts/plugin/bridge.coffee',
-           filters='coffeescript',
-           output='debug/scripts/plugin/bridge.js'),
-    Bundle('h:static/scripts/plugin/bucket-bar.coffee',
-           filters='coffeescript',
-           output='debug/scripts/plugin/bucket-bar.js'),
-    Bundle('h:static/scripts/plugin/toolbar.coffee',
-           filters='coffeescript',
-           output='debug/scripts/plugin/toolbar.js'),
-    Bundle('h:static/scripts/guest.coffee',
-           'h:static/scripts/host.coffee',
-           filters='coffeescript',
-           output='debug/scripts/hypothesis.js'),
-    'h:static/bootstrap.js')
+    source = 'h:static/%s' % path
+
+    # Process globs individually.
+    if '*' in source:
+        assets = (process_path(p) for p in resolve_glob(source))
+        return Bundle(*assets)
+
+    # Create a bundle per coffee file.
+    if path.endswith('.coffee'):
+        output = ('debug/%s' % path).replace('.coffee', '.js')
+        return Bundle(path, filters='coffeescript', output=output)
+
+    return source
+
+
+def create_bundle(*assets):
+    assets = (process_path(path) for path in assets)
+    return Bundle(*assets)
+
+
+app_dependencies_js = create_bundle(
+    'scripts/vendor/jschannel.js',
+    'scripts/vendor/jwz.js',
+    'scripts/vendor/moment-with-langs.js',
+    'scripts/vendor/jstz.js',
+    'scripts/vendor/moment-timezone.js',
+    'scripts/vendor/moment-timezone-data.js',
+    'scripts/vendor/Markdown.Converter.js',
+    'scripts/vendor/polyfills/autofill-event.js',
+    'scripts/vendor/unorm.js',
+    'scripts/vendor/uuid.js',
+    'scripts/vendor/gettext.js',
+    'scripts/vendor/katex/katex.js',
+    'scripts/vendor/annotator.js',
+    'scripts/vendor/annotator.auth.js',
+    'scripts/vendor/annotator.document.js',
+    'scripts/vendor/annotator.permissions.js',
+    'scripts/vendor/annotator.store.js',
+    'scripts/plugin/bridge.coffee',
+    'scripts/plugin/discovery.coffee',
+    'scripts/plugin/threading.coffee')
+
+app_js = create_bundle(
+    'scripts/app.coffee',
+    'scripts/controllers.coffee',
+    'scripts/directives.coffee',
+    'scripts/directives/*.coffee',
+    'scripts/filters.coffee',
+    'scripts/searchfilters.coffee',
+    'scripts/services.coffee',
+    'scripts/*-service.coffee',
+    'scripts/streamsearch.coffee')
+
+account_js = create_bundle(
+    'scripts/account/account.coffee',
+    'scripts/account/*-controller.coffee',
+    'scripts/account/*-service.coffee')
+
+helpers_js = create_bundle(
+    'scripts/vendor/angular-bootstrap.js',
+    'scripts/helpers/helpers.coffee',
+    'scripts/helpers/*-helpers.coffee')
+
+session_js = create_bundle(
+    'scripts/vendor/angular-resource.js',
+    'scripts/session/session.coffee',
+    'scripts/session/*-service.coffee')
+
+hypothesis_js = create_bundle(
+    'scripts/vendor/jquery.scrollintoview.js',
+    'scripts/vendor/jschannel.js',
+    'scripts/vendor/gettext.js',
+    'scripts/vendor/annotator.js',
+    'scripts/vendor/annotator.document.js',
+    'scripts/vendor/diff_match_patch_uncompressed.js',
+    'scripts/vendor/dom_text_mapper.js',
+    'scripts/vendor/dom_text_matcher.js',
+    'scripts/vendor/text_match_engines.js',
+    'scripts/vendor/annotator.domtextmapper.js',
+    'scripts/vendor/annotator.textanchors.js',
+    'scripts/vendor/annotator.fuzzytextanchors.js',
+    'scripts/vendor/page_text_mapper_core.js',
+    'scripts/vendor/annotator.pdf.js',
+    'scripts/vendor/annotator.textanchors.js',
+    'scripts/vendor/annotator.textrange.js',
+    'scripts/vendor/annotator.textposition.js',
+    'scripts/vendor/annotator.textquote.js',
+    'scripts/vendor/annotator.texthighlights.js',
+    'scripts/plugin/bridge.coffee',
+    'scripts/plugin/bucket-bar.coffee',
+    'scripts/plugin/toolbar.coffee',
+    'scripts/guest.coffee',
+    'scripts/host.coffee',
+    'bootstrap.js')
 
 app_css = Bundle(
     'h:static/scripts/vendor/katex/katex.min.css',
