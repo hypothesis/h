b270d992502d422ed5c2e0fc80c9e73592e4897a
Randall Leeds <tilgovi@hypothes.is>
2015-01-11 20:38:28 -0800

    Select queue module from settings

--- h/queue/__init__.py
+++ h/queue/__init__.py
@@ -16,5 +16,12 @@ def get_queue_writer(request, *args, **kwargs):
 
 
 def includeme(config):
+    settings = config.registry.settings
+
+    if any(key.startswith('nsq.') for key in settings.keys()):
+        config.include('h.queue.nsq')
+    else:
+        config.include('h.queue.local')
+
     config.add_request_method(get_queue_reader, name='get_queue_reader')
     config.add_request_method(get_queue_writer, name='get_queue_writer')
