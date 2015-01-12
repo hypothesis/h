63282f7aab6d4f702ad81273cadb0d1fc746ec76
Aron Carroll <self@aroncarroll.com>
2015-01-12 11:25:59 +0000

    Break up config into smaller functions

--- h/config.py
+++ h/config.py
@@ -5,6 +5,19 @@ import urlparse
 def settings_from_environment():
     settings = {}
 
+    _setup_heroku(settings)
+    _setup_elasticsearch(settings)
+    _setup_email(settings)
+    _setup_nsqd(settings)
+    _setup_redis(settings)
+    _setup_client(settings)
+    _setup_sessions(settings)
+    _setup_statsd(settings)
+
+    return settings
+
+
+def _setup_heroku(settings):
     # BONSAI_URL matches the Heroku environment variable for the Bonsai add-on
     if 'BONSAI_URL' in os.environ:
         settings['es.host'] = os.environ['BONSAI_URL']
@@ -22,6 +35,8 @@ def settings_from_environment():
     if 'REDISTOGO_URL' in os.environ:
         settings['redis.sessions.url'] = os.environ['REDISTOGO_URL'] + '0'
 
+
+def _setup_elasticsearch(settings):
     if 'ELASTICSEARCH_INDEX' in os.environ:
         settings['es.index'] = os.environ['ELASTICSEARCH_INDEX']
 
@@ -31,6 +46,8 @@ def settings_from_environment():
         es_port = os.environ['ELASTICSEARCH_PORT_9200_TCP_PORT']
         settings['es.host'] = 'http://{}:{}'.format(es_host, es_port)
 
+
+def _setup_email(settings):
     # MAILGUN_SMTP_LOGIN matches the Heroku environment variable
     if 'MAILGUN_SMTP_LOGIN' in os.environ:
         settings['mail.username'] = os.environ['MAILGUN_SMTP_LOGIN']
@@ -64,6 +81,8 @@ def settings_from_environment():
         settings['mail.host'] = mail_host
         settings['mail.port'] = mail_port
 
+
+def _setup_nsqd(settings):
     if 'NSQD_PORT' in os.environ:
         r_host = os.environ['NSQD_PORT_4150_TCP_ADDR']
         r_port = os.environ['NSQD_PORT_4150_TCP_PORT']
@@ -72,25 +91,31 @@ def settings_from_environment():
         w_port = os.environ['NSQD_PORT_4151_TCP_PORT']
         settings['nsq.writer.address'] = '{}:{}'.format(w_host, w_port)
 
+
+def _setup_redis(settings):
     if 'REDIS_PORT' in os.environ:
         redis_host = os.environ['REDIS_PORT_6379_TCP_ADDR']
         redis_port = os.environ['REDIS_PORT_6379_TCP_PORT']
         settings['redis.sessions.host'] = redis_host
         settings['redis.sessions.port'] = redis_port
 
+
+def _setup_client(settings):
     if 'CLIENT_ID' in os.environ:
         settings['h.client_id'] = os.environ['CLIENT_ID']
 
     if 'CLIENT_SECRET' in os.environ:
         settings['h.client_secret'] = os.environ['CLIENT_SECRET']
 
+
+def _setup_sessions(settings):
     if 'SESSION_SECRET' in os.environ:
         settings['session.secret'] = os.environ['SESSION_SECRET']
         settings['redis.sessions.secret'] = os.environ['SESSION_SECRET']
 
+
+def _setup_statsd(settings):
     if 'STATSD_PORT' in os.environ:
         statsd_host = urlparse.urlparse(os.environ['STATSD_PORT_8125_UDP'])
         settings['statsd.host'] = statsd_host.hostname
         settings['statsd.port'] = statsd_host.port
-
-    return settings
