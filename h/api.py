cad976bbe652ca0d0e7b4f298d53295f4a5903d6
Gergely Ujvari <ujvari@hypothes.is>
2015-01-24 14:34:45 +0100

    Fix tag search
    ES stores the tag field as tags
    
    Fix #1879

--- h/api.py
+++ h/api.py
@@ -287,7 +287,7 @@ def _add_any_field_params_into_query(search_params):
         'multi_match': {
             'query': any_terms,
             'type': 'cross_fields',
-            'fields': ['quote', 'tag', 'text', 'uri', 'user']
+            'fields': ['quote', 'tags', 'text', 'uri', 'user']
         }
     }
 
