d02d1fba1e622745a1be9763904343599edc1802
Aron Carroll <self@aroncarroll.com>
2015-01-28 12:29:50 +0100

    Update Firefox extension load path
    * We now only inject the extension once on install.
    * We remove h when the extension is disabled or uninstalled.
    * We use absolute paths to the icon assets.

--- h/browser/firefox/lib/main.js
+++ h/browser/firefox/lib/main.js
@@ -2,6 +2,7 @@ const self = require("sdk/self");
 const data = self.data;
 const tabs = require("sdk/tabs");
 const { ToggleButton } = require("sdk/ui/button/toggle");
+var bgScriptDidLoad = false;
 var btn_config = {};
 var btn;
 
@@ -10,23 +11,27 @@ var btn;
 // on why this happens or how to prevent this.
 var icons = {
   'sleeping': {
-    "18": './images/toolbar-inactive.png',
-    "32": './images/menu-item.png',
-    "36": './images/toolbar-inactive@2x.png',
-    "64": './images/menu-item@2x.png'
+    "18": data.url('images/toolbar-inactive.png'),
+    "32": data.url('images/menu-item.png'),
+    "36": data.url('images/toolbar-inactive@2x.png'),
+    "64": data.url('images/menu-item@2x.png')
   },
   // for all occasionas
   'active': {
-    "18": './images/toolbar-active.png',
-    "32": './images/menu-item.png',
-    "36": './images/toolbar-active@2x.png',
-    "64": './images/menu-item@2x.png'
+    "18": data.url('images/toolbar-active.png'),
+    "32": data.url('images/menu-item.png'),
+    "36": data.url('images/toolbar-active@2x.png'),
+    "64": data.url('images/menu-item@2x.png')
   }
 };
 
 function enable(tab) {
   tab.attach({
-    contentScriptFile: data.url('embed.js')
+    contentScript: [
+      'var s = document.createElement("script");',
+      's.setAttribute("src", "' + data.url('embed.js') + '");',
+      'document.body.appendChild(s);'
+    ]
   });
 }
 
@@ -40,27 +45,35 @@ function disable(tab) {
   });
 }
 
+function activate(btn, tab) {
+  btn.state(tab, {
+    checked: true,
+    label: 'Annotating',
+    icon: icons.active
+  });
+}
+
+function deactivate(btn, tab) {
+  btn.state(tab, {
+    checked: false,
+    label: 'Annotate',
+    icon: icons.sleeping
+  });
+}
+
 btn_config = {
   id: "hypothesis",
   label: "Annotate",
-  icon: icons['sleeping'],
-  onChange: function() {
+  icon: icons.sleeping,
+  onClick: function (state) {
     // delete the window-wide state default
     this.state('window', null);
     // but turn it back on for this tab
-    if (!this.state(tabs.activeTab).checked === true) {
-      this.state(tabs.activeTab, {
-        checked: true,
-        label: 'Annotating',
-        icon: icons['active']
-      });
+    if (this.state(tabs.activeTab).checked !== true) {
+      activate(btn, tabs.activeTab);
       enable(tabs.activeTab);
     } else {
-      this.state(tabs.activeTab, {
-        checked: false,
-        label: 'Annotate',
-        icon: icons['sleeping']
-      });
+      deactivate(btn, tabs.activeTab);
       disable(tabs.activeTab);
     }
   }
@@ -72,7 +85,7 @@ if (undefined === ToggleButton) {
   btn = ToggleButton(btn_config);
 }
 
-tabs.on('pageshow', function(tab) {
+tabs.on('pageshow', function onPageShow(tab) {
   if (btn.state(tab).checked) {
     enable(tab);
   } else {
@@ -80,16 +93,24 @@ tabs.on('pageshow', function(tab) {
   }
 });
 
-tabs.on('open', function(tab) {
+tabs.on('open', function onTabOpen(tab) {
   // h is off by default on new tabs
-  btn.state(tab).checked = false;
+  deactivate(btn, tab);
 });
 
-if (self.loadReason === 'install') {
-  tabs.open({
-    url: 'https://hypothes.is/welcome',
-    onPageShow: function() {
-      btn.click();
-    }
-  });
-}
+exports.main = function main(options, callbacks) {
+  if (options.loadReason === 'install') {
+    tabs.open({
+      url: 'https://hypothes.is/welcome',
+      onReady: function (tab) {
+        activate(btn, tab);
+      }
+    });
+  }
+};
+
+exports.onUnload = function onUnload(reason) {
+  if (reason === 'uninstall' || reason === 'disable') {
+    [].forEach.call(tabs, disable);
+  }
+};
