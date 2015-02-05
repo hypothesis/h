25b0ae9acf6b9b1886a02c526849bd3ff9f881b7
Nick Stenning <nick@whiteink.com>
2015-02-05 12:32:48 +0000

    Add a fixture for creating database sessions

--- h/conftest.py
+++ h/conftest.py
@@ -8,6 +8,13 @@ import pytest
 from pyramid import testing
 from pyramid.paster import get_appsettings
 
+import pyramid_basemodel
+import transaction
+
+from sqlalchemy import engine_from_config
+from sqlalchemy.orm import scoped_session, sessionmaker
+from zope.sqlalchemy import ZopeTransactionExtension
+
 
 @pytest.fixture(scope='session', autouse=True)
 def settings():
@@ -27,3 +34,24 @@ def config(request, settings):
     request.addfinalizer(destroy)
 
     return config
+
+
+@pytest.fixture()
+def db_session(request, settings):
+    """SQLAlchemy session."""
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    pyramid_basemodel.Session = _make_session()
+    pyramid_basemodel.bind_engine(engine, should_create=True, should_drop=True)
+
+    def destroy():
+        transaction.commit()
+        pyramid_basemodel.Base.metadata.drop_all(engine)
+        pyramid_basemodel.Session.close()
+
+    request.addfinalizer(destroy)
+
+    return pyramid_basemodel.Session
+
+
+def _make_session():
+    return scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
