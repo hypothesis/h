f3076de5b7d5154c97dcac46defdb802bbd9e634
Randall Leeds <tilgovi@hypothes.is>
2015-02-09 12:48:26 -0800

    Fix WebSocket upgrade in presence of middleware
    In our case, the Sentry middleware, raven, yields from the app response
    in order to function correctly. However, that confuses the gevent wsgi
    server into sending a "Transfer-Encoding: chunked" header, breaking the
    WebSocket upgrade for Chrome.

--- h/server.py
+++ h/server.py
@@ -3,7 +3,14 @@ from ws4py.server.geventserver import WSGIServer, WebSocketWSGIHandler
 
 
 class WSGIHandler(PyWSGIHandler, WebSocketWSGIHandler):
-    pass
+    def finalize_headers(self):
+        # Middleware may yield from the empty upgrade response, confusing this
+        # method into sending "Transfer-Encoding: chunked" and, in turn, this
+        # confuses some strict WebSocket clients.
+        for name, value in self.response_headers:
+            if name == 'Upgrade' and value == 'websocket':
+               return
+        super(WSGIHandler, self).finalize_headers()
 
 
 class Worker(GeventPyWSGIWorker):
