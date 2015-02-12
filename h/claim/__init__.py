5ce19515d91ad43e539a60785c456024352fd3b9
Aron Carroll <self@aroncarroll.com>
2015-02-12 14:58:48 +0100

    Add a utility for generating claim urls

--- h/claim/__init__.py
+++ h/claim/__init__.py
@@ -1,3 +1,13 @@
+from webob.cookies import SignedSerializer
+
+
 def includeme(config):
     config.include('.views')
-    print 'pulling in claim'
+    config.include('.util')
+
+    # We use the shared session secret, but salt it with the namespace
+    # 'h.claim' -- only messages serialized with this salt will
+    # authenticate on deserialization.
+    secret = config.registry.settings['session.secret']
+    serializer = SignedSerializer(secret, 'h.claim')
+    config.registry.claim_serializer = serializer
