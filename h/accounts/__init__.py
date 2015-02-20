d0631daddd8de97c55afcdedcee51785244cf53d
Randall Leeds <tilgovi@hypothes.is>
2015-02-20 10:11:21 -0800

    pyramid_layout and auth layout in h.accounts
    Start to move some account-specific stuff into the accounts package.

--- h/accounts/__init__.py
+++ h/accounts/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 def includeme(config):
     """A local identity provider."""
+    config.include('.layouts')
     config.include('.models')
     config.include('.schemas')
     config.include('.subscribers')
