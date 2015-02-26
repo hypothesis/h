f9162529d2e1bbfb94a916ca8b438af3479a2f06
Nick Stenning <nick@whiteink.com>
2015-02-26 21:14:40 +0000

    Simplify h.queue by removing local queue
    There's no way a local queue setup can work with external worker
    processes, so this commit removes support for a local queue. Using
    queue-based features locally will now require running nsqd.

--- h/test/queue_test.py
+++ h/test/queue_test.py
@@ -1,12 +1,14 @@
+from pyramid import testing
 from mock import patch
 
-from h.queue.nsq import NSQHelper
+from ..queue import get_reader
+from ..queue import get_writer
 
 
 @patch('gnsq.Reader')
 def test_get_reader_default(fake_reader):
-    qh = NSQHelper({})
-    qh.get_reader('ethics-in-games-journalism', 'channel4')
+    req = testing.DummyRequest()
+    get_reader(req, 'ethics-in-games-journalism', 'channel4')
     fake_reader.assert_called_with('ethics-in-games-journalism',
                                    'channel4',
                                    nsqd_tcp_addresses=['localhost:4150'])
@@ -14,9 +16,11 @@ def test_get_reader_default(fake_reader):
 
 @patch('gnsq.Reader')
 def test_get_reader(fake_reader):
-    qh = NSQHelper({'nsq.reader.addresses': "foo:1234\nbar:4567"})
-    qh.get_reader('ethics-in-games-journalism',
-                  'channel4')
+    req = testing.DummyRequest()
+    req.registry.settings.update({
+        'nsq.reader.addresses': "foo:1234\nbar:4567"
+    })
+    get_reader(req, 'ethics-in-games-journalism', 'channel4')
     fake_reader.assert_called_with('ethics-in-games-journalism',
                                    'channel4',
                                    nsqd_tcp_addresses=['foo:1234',
@@ -25,13 +29,14 @@ def test_get_reader(fake_reader):
 
 @patch('gnsq.Nsqd')
 def test_get_writer_default(fake_nsqd):
-    qh = NSQHelper({})
-    qh.get_writer()
+    req = testing.DummyRequest()
+    get_writer(req)
     fake_nsqd.assert_called_with('localhost', http_port='4151')
 
 
 @patch('gnsq.Nsqd')
 def test_get_writer(fake_nsqd):
-    qh = NSQHelper({'nsq.writer.address': 'philae:2014'})
-    qh.get_writer()
+    req = testing.DummyRequest()
+    req.registry.settings.update({'nsq.writer.address': 'philae:2014'})
+    get_writer(req)
     fake_nsqd.assert_called_with('philae', http_port='2014')
