hypothesis/h

commit 29c1500ca73c92bed78aed8bb5a29be96e11e8b9

Refactor auth and separate API from main app
Provide a way to construct just the API as a standalone WSGI app
and include this wholesale as a view inside the main site. This
means that they can use separate authentication policies for the
following benefits:

- The API can be deployed and integrated into other applications
  completely independently.

- The API app can be configured with a simpler authentication policy
  that does not access the session.

To accomplish this:

- Provide a `create_api` function in `h.app` that can construct a WSGI
  application that serves only the API.

- Extract resource endpoint authorization into a tween. This tween sets
  the initial value of the `REMOTE_USER` environment key so that the
  provided API application use the `RemoteUserAuthenticationPolicy`.

- Integrate authentication with the API by having the callback for the
  authentication policy provide the OAuth client as a consumer role.
  By not accessing the OAuth attributes in the API views, enable the
  aforementioned integration flexibility.

- Use a normal `SessionAuthenticationPolicy` for the main site.

Isolate the Annotator Auth token handling to make it easy to migrate to
the new token system.

- Generate standard web token claims and some backwards compatibility
  claims for the Annotator Auth plugin.

- Use a tween to port the X-Annotator-Auth-Token headers to isolate the
  legacy code paths from the core authentication machinery.

The`h.oauth` package can disappear completely as everything is packed
neatly into `h.auth`.

Close #1296.

h/app.py
@@ -1,26 +1,58 @@
 # -*- coding: utf-8 -*-
 """The main h application."""
 from pyramid.config import Configurator
-from pyramid.path import AssetResolver
-from pyramid.response import FileResponse
+from pyramid.renderers import JSON
+from pyramid.wsgi import wsgiapp2
+
+from .auth import acl_authz, remote_authn, session_authn
 
 
 def create_app(settings):
     """Configure and add static routes and views. Return the WSGI app."""
     config = Configurator(settings=settings)
-    config.include('h')
 
-    favicon = AssetResolver().resolve('h:favicon.ico')
-    config.add_route('favicon', '/favicon.ico')
-    config.add_view(
-        lambda request: FileResponse(favicon.abspath(), request=request),
-        route_name='favicon'
-    )
-
-    config.add_route('ok', '/ruok')
-    config.add_view(lambda request: 'imok', renderer='string', route_name='ok')
-
+    config.set_authentication_policy(session_authn)
+    config.set_authorization_policy(acl_authz)
     config.set_root_factory('h.resources.RootFactory')
+
+    config.add_subscriber('h.subscribers.add_renderer_globals',
+                          'pyramid.events.BeforeRender')
+
+    config.include('.')
+    config.include('.features')
+
+    if config.registry.feature('api'):
+        api_app = create_api(settings)
+        api_view = wsgiapp2(api_app)
+        config.add_view(api_view, name='api')
+
+    if config.registry.feature('streamer'):
+        config.include('.streamer')
+
+    if config.registry.feature('notification'):
+        config.include('.notification')
+
+    return config.make_wsgi_app()
+
+
+def create_api(settings):
+    config = Configurator(settings=settings)
+
+    config.set_authentication_policy(remote_authn)
+    config.set_authorization_policy(acl_authz)
+    config.set_root_factory('h.resources.APIResource')
+
+    config.add_renderer('json', JSON(indent=4))
+    config.add_subscriber('h.subscribers.set_user_from_oauth',
+                          'pyramid.events.ContextFound')
+    config.add_tween('h.tweens.annotator_tween_factory')
+
+    config.include('.api')
+    config.include('.auth')
+    config.include('.features')
+
+    if config.registry.feature('streamer'):
+        config.include('.streamer')
 
     return config.make_wsgi_app()
 

h/__init__.py
@@ -11,21 +11,7 @@ __all__ = ['main']
 
 def includeme(config):
     """Include config sections and setup Jinja."""
-    config.include('h.authentication')
-    config.include('h.authorization')
-    config.include('h.features')
-    config.include('h.queue')
-    config.include('h.subscribers')
-    config.include('h.views')
-
-    if config.registry.feature('api'):
-        config.include('h.api')
-
-    if config.registry.feature('streamer'):
-        config.include('h.streamer')
-
-    if config.registry.feature('notification'):
-        config.include('h.notification')
+    config.include('.views')
 
     config.include('pyramid_jinja2')
     config.add_jinja2_renderer('.js')

h/accounts/events.py
@@ -5,6 +5,5 @@ class LoginEvent(object):
 
 
 class LogoutEvent(object):
-    def __init__(self, request, user):
+    def __init__(self, request):
         self.request = request
-        self.user = user

h/accounts/subscribers.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 import datetime
 
 from pyramid import security
@@ -27,8 +28,6 @@ def login(event):
     request = event.request
     user = event.user
     userid = 'acct:{}@{}'.format(user.username, request.domain)
-
-    request.user = userid
 
     # Record a login event
     stats(request).get_counter('auth.local.login').increment()

h/accounts/views.py
@@ -126,8 +126,7 @@ class AuthController(horus.views.AuthController):
         return {'status': 'okay'}
 
     def logout(self):
-        user = self.User.get_by_id(self.request, self.request.user)
-        self.request.registry.notify(LogoutEvent(self.request, user))
+        self.request.registry.notify(LogoutEvent(self.request))
         return super(AuthController, self).logout()
 
 
@@ -284,7 +283,5 @@ def includeme(config):
     config.add_route('disable_user', '/disable/{user_id}',
                      factory=UserFactory,
                      traverse="/{user_id}")
-
     config.include('horus')
-    config.add_request_method(name='user')  # horus override (unset property)
     config.scan(__name__)

h/api.py
@@ -5,14 +5,14 @@
 import logging
 import time
 
-from annotator import auth, es
+from annotator import es
+from annotator.auth import Consumer, User
 from elasticsearch import exceptions as elasticsearch_exceptions
-from pyramid.renderers import JSON
 from pyramid.settings import asbool
 from pyramid.view import view_config
 
-from h import events
-from h.models import Annotation, Document
+from .events import AnnotationEvent
+from .models import Annotation, Document
 
 
 log = logging.getLogger(__name__)
@@ -35,7 +35,6 @@ def api_config(**kwargs):
 
 
 @api_config(context='h.resources.APIResource')
-@api_config(context='h.resources.APIResource', route_name='index')
 def index(context, request):
     """Return the API descriptor document.
 
@@ -87,14 +86,13 @@ def search(context, request):
 @api_config(context='h.resources.APIResource', name='access_token')
 def access_token(context, request):
     """The OAuth 2 access token view."""
-    if request.grant_type is None:
-        request.grant_type = 'client_credentials'
     return request.create_token_response()
 
 
 @api_config(context='h.resources.APIResource', name='token', renderer='string')
 def annotator_token(context, request):
     """The Annotator Auth token view."""
+    request.grant_type = 'client_credentials'
     response = access_token(context, request)
     return response.json_body.get('access_token', response)
 
@@ -204,16 +202,19 @@ def delete(context, request):
 
 def get_user(request):
     """Create a User object for annotator-store."""
-    userid = request.authenticated_userid
+    userid = request.unauthenticated_userid
     if userid is not None:
-        consumer = auth.Consumer(request.client.client_id)
-        return auth.User(userid, consumer, False)
+        for principal in request.effective_principals:
+            if principal.startswith('consumer:'):
+                key = principal[9:]
+                consumer = Consumer(key)
+                return User(userid, consumer, False)
     return None
 
 
 def _trigger_event(request, annotation, action):
     """Trigger any callback functions listening for AnnotationEvents."""
-    event = events.AnnotationEvent(request, annotation, action)
+    event = AnnotationEvent(request, annotation, action)
     request.registry.notify(event)
 
 
@@ -489,8 +490,6 @@ def includeme(config):
     """Configure and possibly initialize ElasticSearch and its models."""
     registry = config.registry
     settings = registry.settings
-
-    config.add_renderer('json', JSON(indent=4))
 
     # Configure ElasticSearch
     store_from_settings(settings)

h/auth.py
@@ -1,0 +1,235 @@
+# -*- coding: utf-8 -*-
+"""
+OAuth integration.
+
+Supported grant types
+---------------------
+
+- A client credentials grant using the session and authenticating the client
+  with cross-site request forgery tokens.
+
+Supported token types
+---------------------
+
+- JSON Web Tokens [jwt]_ as bearer tokens.
+
+.. [jwt] https://tools.ietf.org/html/draft-ietf-oauth-json-web-token
+
+Limitations
+-----------
+
+- No support for 3rd party clients exists yet.
+- No support for scopes yet.
+
+"""
+import datetime
+
+import jwt
+import jwt.api
+from jwt.compat import constant_time_compare
+from oauthlib.common import generate_client_id
+from oauthlib.oauth2 import RequestValidator as _RequestValidator
+from pyramid.exceptions import BadCSRFToken
+from pyramid import session
+
+from pyramid.authentication import RemoteUserAuthenticationPolicy
+from pyramid.authentication import SessionAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+from pyramid.util import action_method
+
+from .interfaces import IClientFactory
+
+LEEWAY = 240  # allowance for clock skew in verification
+
+
+class RequestValidator(_RequestValidator):
+
+    """Validates JSON Web Tokens."""
+
+    def authenticate_client(self, request):
+        client = None
+        user = None
+
+        if request.client_id is None:
+            try:
+                session.check_csrf_token(request, token='assertion')
+            except BadCSRFToken:
+                return False
+            client_id = request.registry.settings['h.client_id']
+            client = get_client(request, client_id)
+            user = request.session.get('userid')
+        elif request.client_secret is not None:
+            client_id = request.client_id
+            client_secret = request.client_secret
+            client = get_client(request, client_id, client_secret)
+
+        request.client = client
+        request.user = user
+        return request.client is not None
+
+    def save_bearer_token(self, token, request):
+        # JWT authorization is stateless
+        pass
+
+    def validate_bearer_token(self, token, scopes, request):
+        if token is None:
+            return False
+
+        try:
+            payload, signing_input, header, signature = jwt.api.load(token)
+        except jwt.InvalidTokenError:
+            return False
+
+        aud = request.host_url
+        iss = payload['iss']
+        sub = payload.get('sub')
+
+        client = get_client(request, iss)
+        if client is None:
+            return False
+
+        try:
+            jwt.api.verify_signature(payload, signing_input, header, signature,
+                                 key=client.client_secret, audience=aud,
+                                 leeway=LEEWAY)
+        except jwt.InvalidTokenError:
+            return False
+
+        request.client = client
+        request.user = sub
+        request.scopes = None
+
+        return True
+
+    def validate_grant_type(self, client_id, grant_type, client, request):
+        return True
+
+    def get_default_scopes(self, client_id, request):
+        return None
+
+    def validate_scopes(self, client_id, scopes, client, request):
+        return scopes is None
+
+
+def effective_principals(userid, request):
+    """Return the effective principals for a request.
+
+    The effective principals are the authorized userid and a consumer group
+    for the client associated with the request.
+    """
+    groups = []
+
+    if getattr(request, 'client', None) is not None:
+        consumer_group = 'consumer:{}'.format(request.client.client_id)
+        groups.append(consumer_group)
+
+    return groups
+
+
+def generate_signed_token(request):
+    """Generate a signed JSON Web Token from OAuth attributes of the request.
+
+    A JSON Web Token [jwt]_ is a token that contains a header, describing the
+    algorithm used for signing, a set of claims (the payload), and a trailing
+    signature.
+
+    .. [jwt] https://tools.ietf.org/html/draft-ietf-oauth-json-web-token
+    """
+    now = datetime.datetime.utcnow().replace(microsecond=0)
+    ttl = datetime.timedelta(seconds=request.expires_in)
+
+    claims = {
+        'iss': request.client.client_id,
+        'aud': request.host_url,
+        'sub': request.user,
+        'exp': now + ttl,
+        'iat': now,
+    }
+
+    # bw compat for the Annotator Auth plugin
+    claims.update({
+        'consumerKey': request.client.client_id,
+        'issuedAt': now.isoformat(),
+        'ttl': int(ttl.total_seconds()),
+        'userId': request.user,
+    })
+
+    claims.update(request.extra_credentials or {})
+    return jwt.encode(claims, request.client.client_secret)
+
+
+def get_client(request, client_id, client_secret=None):
+    """Get a :class:`h.oauth.IClient` instance using the configured
+    :term:`client factory` and provided ''client_id''.
+
+    Returns the client object created by the factory. Returns ``None`` if the
+    factory returns ``None`` or the provided ``client_secret`` parameter
+    does not match the ``client_secret`` attribute of the client.
+    """
+    registry = request.registry
+    factory = registry.queryUtility(IClientFactory)
+    client = factory(request, client_id)
+
+    if client is None:
+        return None
+
+    # Allow a default client, hard-coded in the settings.
+    if 'h.client_id' in request.registry.settings:
+        if client_id == request.registry.settings['h.client_id']:
+            if client.client_secret is None:
+                client_secret = request.registry.settings['h.client_secret']
+                client.client_secret = client_secret
+
+    if client_secret is not None:
+        if not constant_time_compare(client_secret, client.client_secret):
+            return None
+
+    return client
+
+
+@action_method
+def set_client_factory(config, factory):
+    """Override the :term:`client factory` in the current configuration. The
+    ``factory`` argument must support the :class:`h.oauth.IClientFactory`
+    interface or be a dotted Python name that points to a client factory.
+    """
+    def register():
+        impl = config.maybe_dotted(factory)
+        config.registry.registerUtility(impl, IClientFactory)
+
+    intr = config.introspectable('client factory', None,
+                                 config.object_description(factory),
+                                 'client factory')
+    intr['factory'] = factory
+    config.action(IClientFactory, register, introspectables=(intr,))
+
+
+remote_authn = RemoteUserAuthenticationPolicy(callback=effective_principals)
+session_authn = SessionAuthenticationPolicy('', callback=effective_principals)
+acl_authz = ACLAuthorizationPolicy()
+validator = RequestValidator()
+
+
+def includeme(config):
+    registry = config.registry
+    settings = registry.settings
+
+    config.include('pyramid_oauthlib')
+
+    # Use session credentials as a client credentials authorization grant
+    config.add_grant_type('oauthlib.oauth2.ClientCredentialsGrant',
+                          request_validator=validator)
+
+    # Use web tokens for resource authorization
+    config.add_token_type('oauthlib.oauth2.BearerToken',
+                          request_validator=validator,
+                          token_generator=generate_signed_token)
+
+    # Configure a default client factory
+    client_class = settings.get('auth.client_factory', 'h.models.Client')
+    config.add_directive('set_client_factory', set_client_factory)
+    config.set_client_factory(client_class)
+
+    # Set default client credentials
+    settings.setdefault('h.client_id', generate_client_id())
+    settings.setdefault('h.client_secret', generate_client_id())

h/authentication.py
@@ -1,83 +1,0 @@
-# -*- coding: utf-8 -*-
-"""
-OAuth / OpenID Connect authentication integration.
-
-This module provides an authentication policy, which is an instance of
-:class:`pyramid.interfaces.IAuthenticationPolicy`, that computes an OAuth
-context when the request is authenticated.
-
-Authentication is handled by session authentication. Views can call the
-``pyramid.security`` functions ``remember`` and ``forget``. When no session
-authentication is found, the any token types registered using the API provided
-by :mod:`pyramid_oauthlib` will be used to determine the authenticated client
-and the authorized user and scopes.
-"""
-import base64
-import os
-
-from pyramid.authentication import SessionAuthenticationPolicy
-
-from .authorization import WEB_SCOPES
-from .oauth import IClientFactory
-
-
-class OAuthPolicy(SessionAuthenticationPolicy):
-
-    """
-    Hybrid OAuth / session authentication.
-
-    This authentication policy first checks the session authentication and
-    then uses OAuth as a fallback. Afterward, the request will have attributes
-    for ``client``, ``user`` and ``scope``. Any or all of them may be ``None``.
-    """
-
-    def unauthenticated_userid(self, request):
-        if hasattr(request, 'user'):
-            return request.user
-
-        userid = super(OAuthPolicy, self).unauthenticated_userid(request)
-
-        if userid is None:
-            request.verify_request()
-            request.client = getattr(request, 'client', None)
-            request.user = getattr(request, 'user', None)
-            request.scopes = getattr(request, 'scopes', None)
-        else:
-            request.client = request.registry.web_client
-            request.user = userid
-            request.scopes = WEB_SCOPES
-
-        return request.user
-
-
-def register_web_client(config):
-    registry = config.registry
-    settings = registry.settings
-
-    client_factory = registry.getUtility(IClientFactory)
-    client_id = settings.get('h.client_id')
-    client_secret = settings.get('h.client_secret')
-
-    if client_id is None:
-        client_id = base64.urlsafe_b64encode(os.urandom(18))
-
-    if client_secret is None:
-        client_secret = base64.urlsafe_b64encode(os.urandom(18))
-
-    registry.web_client = client_factory(client_id)
-    registry.web_client.client_secret = client_secret
-
-
-def includeme(config):
-    config.include('.oauth')
-
-    registry = config.registry
-    settings = registry.settings
-
-    authn_debug = settings.get('debug_authorization')
-    authn_policy = OAuthPolicy(debug=authn_debug, prefix='')
-    config.set_authentication_policy(authn_policy)
-
-    client_class = settings.get('h.client_factory', 'h.models.Client')
-    config.set_client_factory(client_class)
-    config.action(None, register_web_client, args=(config,))

commit b8c7863f4c912933aa3fe7bb9c18cdd5b4193aba

Enable h.notification in the API application
The h.notification package contains subscribers for
h.events.AnnotationEvent, which send reply notification emails when new
replies are created.

Because the API is now an isolated WSGI application, it does not share a
registry with the main application. As such, we need to explicitly
include the notification package in the API application so that its
subscribers are correctly called when annotations are created through
the API.

This restores email notification functionality broken by 29c1500.

h/app.py
@@ -80,6 +80,15 @@ def create_api(settings):
     if config.registry.feature('streamer'):
         config.include('.streamer')
 
+    if config.registry.feature('notification'):
+        config.include('pyramid_jinja2')
+        config.add_jinja2_renderer('.txt')
+        config.add_jinja2_renderer('.html')
+
+        # FIXME: move subscribers into .notification.subscribers so we don't
+        # have to include the whole package
+        config.include('.notification')
+
     return config.make_wsgi_app()
 
 