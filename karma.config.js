0938c7ae2e4e31c7bcdcb9c5b5a128c344ad9b15
Nick Stenning <nick@whiteink.com>
2015-02-26 19:50:17 +0000

    Bump the browserNoActivityTimeout
    Travis is being especially slow at the moment.

--- karma.config.js
+++ karma.config.js
@@ -98,7 +98,7 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
-
+    browserNoActivityTimeout: 20000, // Travis is slow...
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
