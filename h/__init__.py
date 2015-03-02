02b4f060d970faecb51e012e26f64460162575b3
Nick Stenning <nick@whiteink.com>
2015-03-02 21:29:02 +0000

    Make it possible to instantiate the API from paste
    At the moment it's not easy to get a shell with `h.auth` configured,
    because only the API application includes `h.auth` and there is no easy
    way to get a pshell[1] for the API.
    
    This commit adds a setuptools `paste.app_factory` entrypoint for the API
    application, making it possible to run
    
        pshell development.ini#api
    
    to get a shell configured for the API.
    
    [1]: http://docs.pylonsproject.org/projects/pyramid/en/1.5-branch/narr/commandline.html

--- h/__init__.py
+++ h/__init__.py
@@ -1,19 +1,4 @@
 # -*- coding: utf-8 -*-
-"""Initialize configuration."""
-
-from .app import main
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
-
-__all__ = ['main']
-
-
-def includeme(config):
-    """Include config sections and setup Jinja."""
-    config.include('.views')
-
-    config.include('pyramid_jinja2')
-    config.add_jinja2_renderer('.js')
-    config.add_jinja2_renderer('.txt')
-    config.add_jinja2_renderer('.html')
