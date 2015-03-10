41e37a4f4e9ecde4324c932b4f1335d932694b11
Nick Stenning <nick@whiteink.com>
2015-03-10 17:46:02 +0100

    Redirect the user to the homepage on account claim
    Closes hypothesis/vision#134.

--- h/claim/test/views_test.py
+++ h/claim/test/views_test.py
@@ -77,7 +77,7 @@ def test_update_account_updates_password(user_model):
     assert user.password == 'donkeys'
 
 
-def test_update_account_redirects_to_stream(user_model):
+def test_update_account_redirects_to_index(user_model):
     user_model.get_by_id.return_value = FakeUser(password='')
 
     request = _post_request(post={'password': 'donkeys'})
@@ -85,7 +85,7 @@ def test_update_account_redirects_to_stream(user_model):
     res = update_account(request)
 
     assert isinstance(res, HTTPFound)
-    assert res.location == 'http://example.com/dummy/stream'
+    assert res.location == 'http://example.com/'
 
 
 def test_update_account_rerenders_form_on_validation_error(form, user_model):
@@ -190,6 +190,7 @@ def _post_request(post=None):
 @pytest.fixture(autouse=True)
 def routes(config):
     """Add routes used by claim package"""
+    config.add_route('index', '/')
     config.add_route('stream', '/dummy/stream')
 
 
