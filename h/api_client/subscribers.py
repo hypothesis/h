26e59f9fe41e0b78e3259b1d53453dbfa39ae2a4
Sean Hammond <git@seanh.cc>
2015-03-19 20:09:06 +0000

    Remove .api_url property from request object
    You can get this via request.api_client.base_url

--- h/api_client/subscribers.py
+++ h/api_client/subscribers.py
@@ -4,7 +4,7 @@ import pyramid.events
 import h.api_client
 
 
-def api_url(request):
+def _api_url(request):
     """Return the configured base URL for this app's Hypothesis API instance.
 
     Returns the value of the h.api_url setting from the config file,
@@ -25,13 +25,7 @@ def api_client(request):
     Configured with this app's configured API base URL.
 
     """
-    return h.api_client.Client(api_url(request))
-
-
-@pyramid.events.subscriber(pyramid.events.NewRequest)
-def add_api_url_method(event):
-    """Add a .api_url convenience property to the request."""
-    event.request.set_property(api_url)
+    return h.api_client.Client(_api_url(request))
 
 
 @pyramid.events.subscriber(pyramid.events.NewRequest)
