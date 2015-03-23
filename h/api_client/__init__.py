9522b8705a6c92174cf4879e768de46e3fe12200
Sean Hammond <git@seanh.cc>
2015-03-23 16:30:52 +0000

    Remove subscribers and simplify API settings
    api_client/subscribers.py shouldn't exist anymore because it no longer
    contains any subscribers. Move the code into __init__.py, simplify the
    validation of the config settings, and improve the error message for
    invalid timeout settings in the config file.
    
    We no longer write the normalized config settings back to the settings
    dict (consider mutating the settings dict a bad practice). Instead we
    add a get_api_client() request method with the normalized settings
    curried/closured into it.

--- h/api_client/__init__.py
+++ h/api_client/__init__.py
@@ -1,22 +1,38 @@
 # -*- coding: utf-8 -*-
 """A Python client for the Hypothesis API."""
+import api_client
 from .api_client import Client, APIError, ConnectionError, Timeout
 
 __all__ = ['Client', 'APIError', 'ConnectionError', 'Timeout']
 
 
-def _validate_api_client_timeout(settings):
+def _get_api_client(api_url, timeout, request):
+    api_url = api_url or request.resource_url(request.root, "api")
+    return api_client.Client(api_url, timeout)
+
+
+def _validate_timeout(settings):
     key = "h.api_timeout"
-    if key in settings:
-        timeout = settings[key]
+    timeout = settings.get(key)
+    if timeout is None:
+        return None
+    else:
         try:
-            timeout = float(timeout)
+            return float(timeout)
         except ValueError as err:
-            raise RuntimeError("{key} setting is invalid: {value}".format(
-                key=key, value=timeout))
-        settings["h.api_timeout"] = timeout
+            raise RuntimeError(
+                "Value {value} for setting {key} is invalid: {error}".format(
+                    value=timeout, key=key, error=err))
 
 
 def includeme(config):
-    _validate_api_client_timeout(config.registry.settings)
-    config.include('.subscribers')
+    """Add a request.get_api_client() to the request object."""
+    settings = config.registry.settings
+
+    api_url = settings.get("h.api_url", "").rstrip("/")
+    timeout = _validate_timeout(settings)
+
+    def get_api_client(request):
+        return _get_api_client(api_url, timeout, request)
+
+    config.add_request_method(get_api_client, 'api_client', reify=True)
