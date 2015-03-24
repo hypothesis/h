bb904836e88abeacc33af1bd0cab4dc58b4397b8
Nick Stenning <nick@whiteink.com>
2015-03-24 14:39:58 +0100

    Add namespace support for queue.get_writer
    Just as for get_reader in the previous commit, this adds support for an
    nsq "namespace" setting which is prefixed to the name of the topics to
    which the queue writer publishes.

--- h/queue.py
+++ h/queue.py
@@ -2,6 +2,17 @@ from pyramid.settings import aslist
 import gnsq
 
 
+class NamespacedNsqd(object):
+    def __init__(self, namespace, *args, **kwargs):
+        self.client = gnsq.Nsqd(*args, **kwargs)
+        self.namespace = namespace
+
+    def publish(self, topic, data):
+        if self.namespace is not None:
+            topic = '{0}-{1}'.format(self.namespace, topic)
+        return self.client.publish(topic, data)
+
+
 def get_reader(request, topic, channel):
     """
     Get a :py:class:`gnsq.Reader` instance configured to connect to the
@@ -11,9 +22,11 @@ def get_reader(request, topic, channel):
     The caller is responsible for adding appropriate `on_message` hooks and
     starting the reader.
     """
+    ns = request.registry.settings.get('nsq.namespace')
     addrs = aslist(request.registry.settings.get('nsq.reader.addresses',
                                                  'localhost:4150'))
-    topic = _topic_name(request, topic)
+    if ns is not None:
+        topic = '{0}-{1}'.format(ns, topic)
     reader = gnsq.Reader(topic, channel, nsqd_tcp_addresses=addrs)
     return reader
 
@@ -24,18 +37,12 @@ def get_writer(request):
     writer address configured in settings. The writer communicates over the
     nsq HTTP API and does not hold a connection open to the nsq instance.
     """
-    setting = request.registry.settings.get('nsq.writer.address',
-                                            'localhost:4151')
-    hostname, port = setting.split(':', 1)
-    nsqd = gnsq.Nsqd(hostname, http_port=port)
-    return nsqd
-
-
-def _topic_name(request, name):
     ns = request.registry.settings.get('nsq.namespace')
-    if ns is None:
-        return name
-    return '{0}-{1}'.format(ns, name)
+    addr = request.registry.settings.get('nsq.writer.address',
+                                         'localhost:4151')
+    hostname, port = addr.split(':', 1)
+    nsqd = NamespacedNsqd(ns, hostname, http_port=port)
+    return nsqd
 
 
 def includeme(config):
--- h/test/queue_test.py
+++ h/test/queue_test.py
@@ -5,6 +5,7 @@ from mock import patch
 from h import queue
 
 
+
 @patch('gnsq.Reader')
 def test_get_reader_default(fake_reader):
     req = testing.DummyRequest()
@@ -28,6 +29,11 @@ def test_get_reader(fake_reader):
 
 @patch('gnsq.Reader')
 def test_get_reader_namespace(fake_reader):
+    """
+    When the ``nsq.namespace`` setting is provided, `get_reader` should return
+    a reader that automatically prefixes the namespace onto the name of the
+    topic being read.
+    """
     req = testing.DummyRequest()
     req.registry.settings.update({
         'nsq.namespace': "abc123"
@@ -51,3 +57,22 @@ def test_get_writer(fake_nsqd):
     req.registry.settings.update({'nsq.writer.address': 'philae:2014'})
     queue.get_writer(req)
     fake_nsqd.assert_called_with('philae', http_port='2014')
+
+
+@patch('gnsq.Nsqd')
+def test_get_writer_namespace(fake_nsqd):
+    """
+    When the ``nsq.namespace`` setting is provided, `get_writer` should return
+    a writer that automatically prefixes the namespace onto the topic names
+    given to :method:`gnsq.Nsqd.publish` or :method:`gnsq.Nsqd.mpublish`.
+    """
+    fake_client = fake_nsqd.return_value
+    req = testing.DummyRequest()
+    req.registry.settings.update({
+        'nsq.namespace': "abc123"
+    })
+
+    writer = queue.get_writer(req)
+
+    writer.publish('sometopic', 'somedata')
+    fake_client.publish.assert_called_with('abc123-sometopic', 'somedata')
