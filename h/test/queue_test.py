6ab0bb80c9a33e849558a12669aac8bc80e73cce
Nick Stenning <nick@whiteink.com>
2015-03-24 13:56:50 +0100

    Update import style

--- h/test/queue_test.py
+++ h/test/queue_test.py
@@ -1,14 +1,13 @@
 from pyramid import testing
 from mock import patch
 
-from ..queue import get_reader
-from ..queue import get_writer
+from h import queue
 
 
 @patch('gnsq.Reader')
 def test_get_reader_default(fake_reader):
     req = testing.DummyRequest()
-    get_reader(req, 'ethics-in-games-journalism', 'channel4')
+    queue.get_reader(req, 'ethics-in-games-journalism', 'channel4')
     fake_reader.assert_called_with('ethics-in-games-journalism',
                                    'channel4',
                                    nsqd_tcp_addresses=['localhost:4150'])
@@ -20,7 +19,7 @@ def test_get_reader(fake_reader):
     req.registry.settings.update({
         'nsq.reader.addresses': "foo:1234\nbar:4567"
     })
-    get_reader(req, 'ethics-in-games-journalism', 'channel4')
+    queue.get_reader(req, 'ethics-in-games-journalism', 'channel4')
     fake_reader.assert_called_with('ethics-in-games-journalism',
                                    'channel4',
                                    nsqd_tcp_addresses=['foo:1234',
@@ -30,7 +29,7 @@ def test_get_reader(fake_reader):
 @patch('gnsq.Nsqd')
 def test_get_writer_default(fake_nsqd):
     req = testing.DummyRequest()
-    get_writer(req)
+    queue.get_writer(req)
     fake_nsqd.assert_called_with('localhost', http_port='4151')
 
 
@@ -38,5 +37,5 @@ def test_get_writer_default(fake_nsqd):
 def test_get_writer(fake_nsqd):
     req = testing.DummyRequest()
     req.registry.settings.update({'nsq.writer.address': 'philae:2014'})
-    get_writer(req)
+    queue.get_writer(req)
     fake_nsqd.assert_called_with('philae', http_port='2014')
