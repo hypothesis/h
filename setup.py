a3b1776f189753f7b5a47ae11369454943f38b22
Randall Leeds <tilgovi@hypothes.is>
2015-04-30 00:07:57 -0700

    Pyramid Redis Sessions is released
    And since it's not a strict dependency, but just used by the example
    production.ini, it shoudn't be in setup.py.

--- setup.py
+++ setup.py
@@ -62,7 +62,6 @@ install_requires = [
     'python-statsd>=1.7.0,<1.8.0',
     'pyramid_webassets>=0.9,<1.0',
     'pyramid-jinja2>=2.3.3',
-    'pyramid_redis_sessions>=1.0a2',
     'raven>=5.1.1,<5.2.0',
     'requests>=2.2.1',
     'ws4py>=0.3,<0.4',
