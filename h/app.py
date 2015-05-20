2fa385e8c70c645338a4310ada1e57cf739e2a45
Nick Stenning <nick@whiteink.com>
2015-05-20 14:45:20 +0200

    Add one missing config -> config.registry reversion

--- h/app.py
+++ h/app.py
@@ -106,7 +106,7 @@ def create_api(global_config, **settings):
     config.include('h.api.db')
     config.include('h.api.views')
 
-    if config.feature('queue'):
+    if config.registry.feature('queue'):
         config.include('h.queue')
         config.include('h.api.queue')
 
