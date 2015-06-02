88eaccdaf66a8d85f904f21c3cc6eb3477eb60f0
Randall Leeds <tilgovi@hypothes.is>
2015-06-02 13:23:52 -0700

    Work around ws4py KeyError
    Close #2256

--- h/server.py
+++ h/server.py
@@ -4,12 +4,17 @@ from ws4py.server.geventserver import WSGIServer, WebSocketWSGIHandler
 
 class WSGIHandler(PyWSGIHandler, WebSocketWSGIHandler):
     def finalize_headers(self):
-        # Middleware may yield from the empty upgrade response, confusing this
-        # method into sending "Transfer-Encoding: chunked" and, in turn, this
-        # confuses some strict WebSocket clients.
-        for name, value in self.response_headers:
-            if name == 'Upgrade' and value == 'websocket':
-                return
+        if self.environ.get('HTTP_UPGRADE') == 'websocket':
+            # Middleware, like Raven, may yield from the empty upgrade response,
+            # confusing this method into sending "Transfer-Encoding: chunked"
+            # and, in turn, this confuses some strict WebSocket clients.
+            if not hasattr(self.result, '__len__'):
+                self.result = list(self.result)
+
+            # ws4py 0.3.4 will try to pop the websocket from the environ
+            # even if it doesn't exist, causing a key error.
+            self.environ.setdefault('ws4py.websocket', None)
+
         super(WSGIHandler, self).finalize_headers()
 
 
