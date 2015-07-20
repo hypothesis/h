2fdfcd34f4cf8b9b2b863b3a0be40b6501da8d90
BigBlueHat <byoung@bigbluehat.com>
2015-07-20 17:19:16 -0400

    Made karma.config.js match code style

--- h/static/scripts/karma.config.js
+++ h/static/scripts/karma.config.js
@@ -7,14 +7,12 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: './',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: [
       'browserify',
       'mocha'
     ],
-
 
     // list of files / patterns to load in the browser
     files: [
@@ -49,7 +47,6 @@ module.exports = function(config) {
       '**/*-test.js'
     ],
 
-
     // list of files to exclude
     exclude: [
     ],
@@ -78,23 +75,18 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['dots'],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
