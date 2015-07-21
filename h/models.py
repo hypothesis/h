0bfd969730916036d2212f328f6312fe7eb1ba1c
Randall Leeds <tilgovi@hypothes.is>
2015-07-20 17:38:04 -0700

    Expose NispaUser in h.models

--- h/models.py
+++ h/models.py
@@ -3,6 +3,7 @@
 from h import features
 from h.accounts import models as accounts_models
 from h.api import models as api_models
+from h.api.nipsa import models as nipsa_models
 from h.notification import models as notification_models
 
 __all__ = (
@@ -11,6 +12,7 @@ __all__ = (
     'Client',
     'Document',
     'Feature',
+    'NipsaUser',
     'Subscriptions',
     'User',
 )
@@ -20,6 +22,7 @@ Activation = accounts_models.Activation
 Annotation = api_models.Annotation
 Document = api_models.Document
 Feature = features.Feature
+NipsaUser = nipsa_models.NipsaUser
 Subscriptions = notification_models.Subscriptions
 User = accounts_models.User
 
