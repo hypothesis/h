114184789bf7fa018aae7019d7432482e8f2239b
Sean Hammond <git@seanh.cc>
2015-07-23 15:46:13 +0100

    Add validation to new group form
    - create_group() validates the posted params with our colander schema,
      rerenders the form on validation failure
    
    - Views now return two items to the template: "form" and "data" (a dict
      of any values the user had entered into the form, e.g. {"name": "My
      New Group"}), so we can rerender the template with the user's data
      intact

--- h/groups/views.py
+++ h/groups/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import deform
+import colander
 from pyramid import httpexceptions as exc
 from pyramid.view import view_config
 
@@ -18,15 +19,23 @@ def create_group_form(request):
     schema = schemas.GroupSchema().bind(request=request)
     form = deform.Form(schema)
 
-    return {'form': form}
+    return {'form': form, 'data': {}}
 
 
-@view_config(route_name='group_create', request_method='POST')
+@view_config(route_name='group_create',
+             request_method='POST',
+             renderer='h:groups/templates/create_group.html.jinja2')
 def create_group(request):
     if not request.feature('groups'):
         raise exc.HTTPNotFound()
 
-    group = models.Group(name=request.params["name"])
+    form = deform.Form(schemas.GroupSchema().bind(request=request))
+    try:
+        appstruct = form.validate(request.POST.items())
+    except deform.ValidationFailure as err:
+        return {'form': form, 'data': request.params}
+
+    group = models.Group(name=appstruct["name"])
     request.db.add(group)
 
     # We need to flush the db session here so that group.id will be generated.
