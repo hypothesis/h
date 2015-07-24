316a2e29b9617e872e6b70b5484fe968606c85fd
Sean Hammond <git@seanh.cc>
2015-07-24 12:54:00 +0100

    Remove test code duplication using fixtures

--- h/groups/test/views_test.py
+++ h/groups/test/views_test.py
@@ -7,17 +7,19 @@ from pyramid import httpexceptions
 from h.groups import views
 
 
-@mock.patch('h.groups.views.deform.Form')
-@mock.patch('h.groups.views.schemas.GroupSchema')
-def test_create_group_form_404s_if_groups_feature_is_off(GroupSchema, Form):
+# The fixtures required to mock all of create_group_form()'s dependencies.
+create_group_form_fixtures = pytest.mark.usefixtures('GroupSchema', 'Form')
+
+
+@create_group_form_fixtures
+def test_create_group_form_404s_if_groups_feature_is_off():
     request = mock.Mock(feature=mock.Mock(return_value=False))
 
     with pytest.raises(httpexceptions.HTTPNotFound):
         views.create_group_form(request)
 
 
-@mock.patch('h.groups.views.deform.Form')
-@mock.patch('h.groups.views.schemas.GroupSchema')
+@create_group_form_fixtures
 def test_create_group_form_creates_form_with_GroupSchema(GroupSchema, Form):
     test_schema = mock.Mock()
     GroupSchema.return_value = mock.Mock(
@@ -28,9 +30,8 @@ def test_create_group_form_creates_form_with_GroupSchema(GroupSchema, Form):
     Form.assert_called_once_with(test_schema)
 
 
-@mock.patch('h.groups.views.deform.Form')
-@mock.patch('h.groups.views.schemas.GroupSchema')
-def test_create_group_form_returns_form(GroupSchema, Form):
+@create_group_form_fixtures
+def test_create_group_form_returns_form(Form):
     test_form = mock.Mock()
     Form.return_value = test_form
 
@@ -39,9 +40,8 @@ def test_create_group_form_returns_form(GroupSchema, Form):
     assert template_data["form"] == test_form
 
 
-@mock.patch('h.groups.views.deform.Form')
-@mock.patch('h.groups.views.schemas.GroupSchema')
-def test_create_group_form_returns_empty_form_data(GroupSchema, Form):
+@create_group_form_fixtures
+def test_create_group_form_returns_empty_form_data(Form):
     test_form = mock.Mock()
     Form.return_value = test_form
 
@@ -50,23 +50,21 @@ def test_create_group_form_returns_empty_form_data(GroupSchema, Form):
     assert template_data["data"] == {}
 
 
-@mock.patch('h.groups.views.accounts_models.User')
-@mock.patch('h.groups.views.models.Group')
-@mock.patch('h.groups.views.deform.Form')
-@mock.patch('h.groups.views.schemas.GroupSchema')
-def test_create_group_404s_if_groups_feature_is_off(
-        GroupSchema, Form, Group, User):
+# The fixtures required to mock all of create_group()'s dependencies.
+create_group_fixtures = pytest.mark.usefixtures(
+    'GroupSchema', 'Form', 'Group', 'User', '_encode_hashid')
+
+
+@create_group_fixtures
+def test_create_group_404s_if_groups_feature_is_off():
     request = mock.Mock(feature=mock.Mock(return_value=False))
 
     with pytest.raises(httpexceptions.HTTPNotFound):
         views.create_group(request)
 
 
-@mock.patch('h.groups.views.accounts_models.User')
-@mock.patch('h.groups.views.models.Group')
-@mock.patch('h.groups.views.deform.Form')
-@mock.patch('h.groups.views.schemas.GroupSchema')
-def test_create_group_inits_form_with_schema(GroupSchema, Form, Group, User):
+@create_group_fixtures
+def test_create_group_inits_form_with_schema(GroupSchema, Form):
     schema = mock.Mock()
     GroupSchema.return_value = mock.Mock(bind=mock.Mock(return_value=schema))
     request = mock.Mock(registry=mock.Mock(settings={"secret_key": "secret"}))
@@ -76,11 +74,8 @@ def test_create_group_inits_form_with_schema(GroupSchema, Form, Group, User):
     Form.assert_called_once_with(schema)
 
 
-@mock.patch('h.groups.views.accounts_models.User')
-@mock.patch('h.groups.views.models.Group')
-@mock.patch('h.groups.views.deform.Form')
-@mock.patch('h.groups.views.schemas.GroupSchema')
-def test_create_group_validates_form(GroupSchema, Form, Group, User):
+@create_group_fixtures
+def test_create_group_validates_form(Form):
     Form.return_value = form = mock.Mock()
     form.validate.return_value = {"name": "new group"}
     params = {"foo": "bar"}
@@ -93,12 +88,8 @@ def test_create_group_validates_form(GroupSchema, Form, Group, User):
     form.validate.assert_called_once_with(params.items())
 
 
-@mock.patch('h.groups.views.accounts_models.User')
-@mock.patch('h.groups.views.models.Group')
-@mock.patch('h.groups.views.deform.Form')
-@mock.patch('h.groups.views.schemas.GroupSchema')
-def test_create_group_rerenders_form_on_validation_failure(
-        GroupSchema, Form, Group, User):
+@create_group_fixtures
+def test_create_group_rerenders_form_on_validation_failure(Form):
     Form.return_value = form = mock.Mock()
     form.validate.side_effect = deform.ValidationFailure(None, None, None)
     params = {"foo": "bar"}
@@ -110,12 +101,8 @@ def test_create_group_rerenders_form_on_validation_failure(
     assert template_data['data'] == params
 
 
-@mock.patch('h.groups.views.accounts_models.User')
-@mock.patch('h.groups.views.models.Group')
-@mock.patch('h.groups.views.deform.Form')
-@mock.patch('h.groups.views.schemas.GroupSchema')
-def test_create_group_gets_user_with_authenticated_id(
-        GroupSchema, Form, Group, User):
+@create_group_fixtures
+def test_create_group_gets_user_with_authenticated_id(Form, User):
     """It uses the "name" from the validated data to create a new group."""
     Form.return_value = mock.Mock(validate=lambda data: {"name": "test-group"})
     request = mock.Mock(
@@ -127,12 +114,8 @@ def test_create_group_gets_user_with_authenticated_id(
     User.get_by_id.assert_called_once_with(request, "acct:fred@hypothes.is")
 
 
-@mock.patch('h.groups.views.accounts_models.User')
-@mock.patch('h.groups.views.models.Group')
-@mock.patch('h.groups.views.deform.Form')
-@mock.patch('h.groups.views.schemas.GroupSchema')
-def test_create_group_uses_name_from_validated_data(
-        GroupSchema, Form, Group, User):
+@create_group_fixtures
+def test_create_group_uses_name_from_validated_data(Form, User, Group):
     """It uses the "name" from the validated data to create a new group."""
     Form.return_value = mock.Mock(validate=lambda data: {"name": "test-group"})
     User.get_by_id.return_value = user = mock.Mock()
@@ -143,11 +126,8 @@ def test_create_group_uses_name_from_validated_data(
     Group.assert_called_once_with(name="test-group", creator=user)
 
 
-@mock.patch('h.groups.views.accounts_models.User')
-@mock.patch('h.groups.views.models.Group')
-@mock.patch('h.groups.views.deform.Form')
-@mock.patch('h.groups.views.schemas.GroupSchema')
-def test_create_group_adds_group_to_db(GroupSchema, Form, Group, User):
+@create_group_fixtures
+def test_create_group_adds_group_to_db(Group):
     """It should add the new group to the database session."""
     group = mock.Mock(id=6)
     Group.return_value = group
@@ -158,13 +138,8 @@ def test_create_group_adds_group_to_db(GroupSchema, Form, Group, User):
     request.db.add.assert_called_once_with(group)
 
 
-@mock.patch('h.groups.views._encode_hashid')
-@mock.patch('h.groups.views.accounts_models.User')
-@mock.patch('h.groups.views.models.Group')
-@mock.patch('h.groups.views.deform.Form')
-@mock.patch('h.groups.views.schemas.GroupSchema')
-def test_create_group_redirects_to_group_read_page(
-        GroupSchema, Form, Group, User, _encode_hashid):
+@create_group_fixtures
+def test_create_group_redirects_to_group_read_page(Group, _encode_hashid):
     """After successfully creating a new group it should redirect."""
     group = mock.Mock(id='test-id', slug='test-slug')
     Group.return_value = group
@@ -181,11 +156,8 @@ def test_create_group_redirects_to_group_read_page(
     assert redirect.location == "test-read-url"
 
 
-@mock.patch('h.groups.views.accounts_models.User')
-@mock.patch('h.groups.views.models.Group')
-@mock.patch('h.groups.views.deform.Form')
-@mock.patch('h.groups.views.schemas.GroupSchema')
-def test_create_group_with_non_ascii_name(GroupSchema, Form, Group, User):
+@create_group_fixtures
+def test_create_group_with_non_ascii_name():
     name = u"☆ ßüper Gröup ☆"
     request = mock.Mock(
         params={"name": name},
@@ -194,18 +166,20 @@ def test_create_group_with_non_ascii_name(GroupSchema, Form, Group, User):
     views.create_group(request)
 
 
-@mock.patch('h.groups.views._decode_hashid')
-@mock.patch('h.groups.views.models.Group')
-def test_read_group_404s_if_groups_feature_is_off(Group, _decode_hashid):
+# The fixtures required to mock all of read_group()'s dependencies.
+read_group_fixtures = pytest.mark.usefixtures('Group', '_decode_hashid')
+
+
+@read_group_fixtures
+def test_read_group_404s_if_groups_feature_is_off():
     request = mock.Mock(feature=mock.Mock(return_value=False))
 
     with pytest.raises(httpexceptions.HTTPNotFound):
         views.read_group(request)
 
 
-@mock.patch('h.groups.views._decode_hashid')
-@mock.patch('h.groups.views.models.Group')
-def test_read_group_decodes_hashid(Group, _decode_hashid):
+@read_group_fixtures
+def test_read_group_decodes_hashid(_decode_hashid):
     request = mock.Mock(
         matchdict={"hashid": "1"},
         registry=mock.Mock(settings={"secret_key": "secret"}))
@@ -215,8 +189,7 @@ def test_read_group_decodes_hashid(Group, _decode_hashid):
     _decode_hashid.assert_called_once_with(request, "1")
 
 
-@mock.patch('h.groups.views._decode_hashid')
-@mock.patch('h.groups.views.models.Group')
+@read_group_fixtures
 def test_read_group_gets_group_by_id(Group, _decode_hashid):
     request = mock.Mock(
         matchdict={"hashid": "1"},
@@ -228,8 +201,7 @@ def test_read_group_gets_group_by_id(Group, _decode_hashid):
     Group.get_by_id.assert_called_once_with(1)
 
 
-@mock.patch('h.groups.views._decode_hashid')
-@mock.patch('h.groups.views.models.Group')
+@read_group_fixtures
 def test_read_group_returns_the_group(Group, _decode_hashid):
     request = mock.Mock(
         matchdict={"hashid": "1"},
@@ -243,8 +215,7 @@ def test_read_group_returns_the_group(Group, _decode_hashid):
     assert template_data["group"] == group
 
 
-@mock.patch('h.groups.views._decode_hashid')
-@mock.patch('h.groups.views.models.Group')
+@read_group_fixtures
 def test_read_group_404s_when_group_does_not_exist(Group, _decode_hashid):
     request = mock.Mock(
         matchdict={"hashid": "1"},
@@ -254,3 +225,45 @@ def test_read_group_404s_when_group_does_not_exist(Group, _decode_hashid):
 
     with pytest.raises(httpexceptions.HTTPNotFound):
         views.read_group(request=request)
+
+
+@pytest.fixture
+def Form(request):
+    patcher = mock.patch('h.groups.views.deform.Form', autospec=True)
+    request.addfinalizer(patcher.stop)
+    return patcher.start()
+
+
+@pytest.fixture
+def GroupSchema(request):
+    patcher = mock.patch('h.groups.views.schemas.GroupSchema', autospec=True)
+    request.addfinalizer(patcher.stop)
+    return patcher.start()
+
+
+@pytest.fixture
+def Group(request):
+    patcher = mock.patch('h.groups.views.models.Group', autospec=True)
+    request.addfinalizer(patcher.stop)
+    return patcher.start()
+
+
+@pytest.fixture
+def User(request):
+    patcher = mock.patch('h.groups.views.accounts_models.User', autospec=True)
+    request.addfinalizer(patcher.stop)
+    return patcher.start()
+
+
+@pytest.fixture
+def _encode_hashid(request):
+    patcher = mock.patch('h.groups.views._encode_hashid', autospec=True)
+    request.addfinalizer(patcher.stop)
+    return patcher.start()
+
+
+@pytest.fixture
+def _decode_hashid(request):
+    patcher = mock.patch('h.groups.views._decode_hashid', autospec=True)
+    request.addfinalizer(patcher.stop)
+    return patcher.start()
