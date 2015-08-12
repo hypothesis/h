417d6d83ee561f03bdbe86e8204ccd5ff128830e
Randall Leeds <tilgovi@hypothes.is>
2015-08-12 13:26:24 -0700

    Remove unnecessary timezone stuff
    We're not displaying time in any timezones other than the browser
    local one. Therefore, we don't need to parse and convert time zone
    names. Moment will pull the timezone offset itself from the current
    browser by using Date.prototype.getTimezoneOffset().
    
    This eliminates 251kB (43kB gzipped) of minified code from our
    application bundle. Wow!

--- h/static/scripts/filter/moment.js
+++ h/static/scripts/filter/moment.js
@@ -1,24 +1,8 @@
-var jstz = require('jstimezonedetect').jstz;
 var moment = require('moment');
-require('moment-timezone');
 
 
 module.exports = ['$window', function ($window) {
   return function (value, format) {
-    // Determine the timezone name and browser language.
-    var timezone = jstz.determine().name();
-    var userLang = $window.navigator.language || $window.navigator.userLanguage;
-
-    // Now make a localized date and set the language.
-    var momentDate = moment(value);
-    momentDate.locale(userLang);
-
-    // Try to localize to the browser's timezone.
-    try {
-      return momentDate.tz(timezone).format('LLLL');
-    } catch (error) {
-      // For an invalid timezone, use the default.
-      return momentDate.format('LLLL');
-    }
+    return moment(value).format(format);
   };
 }];
