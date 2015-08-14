da53c632644cf447b9c9726d2744060571f7153e
Randall Leeds <tilgovi@hypothes.is>
2015-08-13 22:30:44 -0700

    Commit missing module
    Oops

---
+++ h/static/scripts/directive/window-scroll.js
@@ -0,0 +1,30 @@
+var angular = require('angular');
+
+module.exports = function () {
+  return {
+    link: function (scope, elem, attr) {
+      var active = true;
+      var html = elem.prop('ownerDocument').documentElement;
+      var view = elem.prop('ownerDocument').defaultView;
+
+      function onScroll() {
+        var clientHeight = html.clientHeight;
+        var scrollHeight = html.scrollHeight;
+        if (view.scrollY + clientHeight >= scrollHeight - clientHeight) {
+          if (active) {
+            active = false;
+            scope.$apply(attr.windowScroll);
+          }
+        } else {
+          active = true;
+        }
+      }
+
+      view.addEventListener('scroll', onScroll, false);
+
+      scope.$on('$destroy', function () {
+        view.removeEventListener('scroll', onScroll);
+      });
+    }
+  };
+};
