1be97c80a285422d8e6047c49ad2c6ede805d51b
Randall Leeds <tilgovi@hypothes.is>
2015-08-26 01:00:50 -0700

    Add missing __all__ declaration
    Conflicts:
    	h/api/groups/__init__.py

--- h/api/groups/__init__.py
+++ h/api/groups/__init__.py
@@ -2,3 +2,6 @@
 from h.api.groups.logic import set_group_if_reply
 from h.api.groups.auth import set_permissions
 from h.api.groups.auth import group_principals
+
+
+__all__ = ('set_group_if_reply', 'group_principals', 'set_permissions')
