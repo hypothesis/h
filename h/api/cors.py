5b2b8a6723802cbbd9c444b6a5c7a08d94324d0f
Randall Leeds <tilgovi@hypothes.is>
2015-08-25 19:35:41 -0700

    Make cors middleware into a view decorator
    In this way, it becomes possible to vary the policy by view.

--- h/api/cors.py
+++ h/api/cors.py
@@ -1,29 +1,45 @@
-from webob import Response
-from webob.dec import wsgify
-from webob.exc import HTTPBadRequest
+from pyramid.httpexceptions import HTTPBadRequest
 
 
-@wsgify.middleware
-def permit_cors(req,
-                app,
-                allow_credentials=False,
-                allow_headers=None,
-                allow_methods=None,
-                expose_headers=None,
-                max_age=86400):
+def policy(allow_credentials=False,
+           allow_headers=None,
+           allow_methods=None,
+           expose_headers=None,
+           max_age=86400):
     """
-    WSGI middleware that enables CORS support across an application.
+    View decorator that provides CORS support.
 
     CORS stands for "Cross-Origin Resource Sharing," and is a protocol
     implemented in browsers to allow safe dispatch of XMLHttpRequests across
     origins.
     """
+
+    def cors_decorator(wrapped):
+        def wrapper(context, request):
+            response = wrapped(context, request)
+            return set_cors_headers(request, response,
+                                    allow_credentials=allow_credentials,
+                                    allow_headers=allow_headers,
+                                    allow_methods=allow_methods,
+                                    expose_headers=expose_headers,
+                                    max_age=max_age)
+
+        return wrapper
+
+    return cors_decorator
+
+
+def set_cors_headers(request, response,
+                     allow_credentials=False,
+                     allow_headers=None,
+                     allow_methods=None,
+                     expose_headers=None,
+                     max_age=86400):
     # If the request is anything other than an OPTIONS request, we just
     # pass it through and add "A-C-A-O: *" to the response headers.
-    if req.method != 'OPTIONS':
-        resp = req.get_response(app)
-        resp.headers['Access-Control-Allow-Origin'] = '*'
-        return resp
+    if request.method != 'OPTIONS':
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
 
     # Otherwise, we're dealing with a CORS preflight request, which,
     # according to the spec:
@@ -31,12 +47,12 @@ def permit_cors(req,
     #  http://www.w3.org/TR/cors/#resource-preflight-requests
     #
     # ...MUST have an Origin header.
-    origin = req.headers.get('Origin')
+    origin = request.headers.get('Origin')
     if origin is None:
         raise HTTPBadRequest('CORS preflight request lacks Origin header.')
 
     # ...MUST have an Access-Control-Request-Method header.
-    request_method = req.headers.get('Access-Control-Request-Method')
+    request_method = request.headers.get('Access-Control-Request-Method')
     if request_method is None:
         raise HTTPBadRequest('CORS preflight request lacks '
                              'Access-Control-Request-Method header.')
@@ -50,8 +66,7 @@ def permit_cors(req,
     # Access-Control-Request-Method match up with the allowed headers and
     # methods, but there's no need to do this as we can simply return what is
     # allowed and the browser will do the rest.
-    resp = Response()
-    headers = resp.headers
+    headers = response.headers
     headers['Access-Control-Allow-Origin'] = origin
     headers['Access-Control-Allow-Methods'] = ', '.join(methods)
     headers['Access-Control-Max-Age'] = str(max_age)
@@ -65,4 +80,4 @@ def permit_cors(req,
     if expose_headers is not None:
         headers['Access-Control-Expose-Headers'] = ', '.join(expose_headers)
 
-    return resp
+    return response
