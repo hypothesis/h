fa17b41828c9c3049da0923dcbf8d954b2290208
Randall Leeds <tilgovi@hypothes.is>
2015-09-17 16:19:27 -0700

    Bump gevent version
    This should stop the logging that occurs when clients disconnect.
    https://github.com/gevent/gevent/pull/377

--- h/server.py
+++ h/server.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
+
 from gunicorn.workers.ggevent import GeventPyWSGIWorker, PyWSGIHandler
+from gunicorn.workers.ggevent import GeventResponse
 from ws4py.server.geventserver import WSGIServer, WebSocketWSGIHandler
 
 
@@ -17,6 +20,20 @@ class WSGIHandler(PyWSGIHandler, WebSocketWSGIHandler):
             self.environ.setdefault('ws4py.websocket', None)
 
         super(WSGIHandler, self).finalize_headers()
+
+    # Gunicorn does not handle gevent 1.1's wrapper around the logger yet.
+    def log_request(self):
+        start = datetime.fromtimestamp(self.time_start)
+        finish = datetime.fromtimestamp(self.time_finish)
+        response_time = finish - start
+        resp_headers = getattr(self, 'response_headers', {})
+        resp = GeventResponse(self.status, resp_headers, self.response_length)
+        if hasattr(self, 'headers'):
+            req_headers = [h.split(":", 1) for h in self.headers.headers]
+        else:
+            req_headers = []
+        self.server.log.logger.access(resp, req_headers, self.environ,
+                                      response_time)
 
 
 class Worker(GeventPyWSGIWorker):
