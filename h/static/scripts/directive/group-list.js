c650c1ae3ebd08a99097c6b7f86d4b48db674021
Robert Knight <robertknight@gmail.com>
2015-09-18 17:53:09 +0100

    Add inline share link to groups dropdown in top bar
     * Use an element directive for <group-list> for
       consistency with the <privacy> directive and because
       the directive has its own template.
    
     * Use flexbox for easier layout of items in the groups
       dropdown list.
    
     * Update the hover and selection states of items in the dropdown
       list as per the mocks.
    
     * Add descriptive titles to all links in the list
    
    Card 87

--- h/static/scripts/directive/group-list.js
+++ h/static/scripts/directive/group-list.js
@@ -1,17 +1,64 @@
 'use strict';
 
+var assert = require('assert');
+
+function GroupsListController($scope) {
+  $scope.expandedGroupId = undefined;
+
+  // used to keep the dropdown from closing when the user
+  // interacts with the inline link share pane within the groups list
+  $scope.stopClickPropagation = function($event) {
+    $event.stopPropagation();
+  }
+
+  // show the share link for the specified group or clear it if
+  // null
+  $scope.toggleShareLink = function (groupId) {
+    if (!groupId || $scope.expandedGroupId === groupId) {
+      $scope.expandedGroupId = undefined;
+    } else {
+      $scope.expandedGroupId = groupId;
+    }
+  };
+
+  $scope.shouldShowShareLink = function (groupId) {
+    return $scope.expandedGroupId === groupId;
+  }
+
+  $scope.linkForGroup = function (groupId) {
+    return $scope.baseURI + 'groups/' + groupId;
+  }
+}
+
 /**
  * @ngdoc directive
  * @name groupList
  * @restrict AE
  * @description Displays a list of groups of which the user is a member.
  */
-
 // @ngInject
 module.exports = function (groups) {
   return {
-    link: function (scope, elem, attrs) {
-      scope.groups = groups;
+    controller: ['$scope', GroupsListController],
+    link: function ($scope, elem, attrs) {
+      $scope.groups = groups;
+
+      // set the base URI used later to construct the sharing
+      // link for the group
+      $scope.baseURI = elem[0].ownerDocument.baseURI;
+
+      $scope.$watch('expandedGroupId', function (activeGroupId) {
+        if (activeGroupId) {
+          // wait for the share link field to be revealed and then select
+          // the link's text
+          setTimeout(function() {
+            var activeShareLinkField = elem[0].querySelector('.share-link-field[data-group-id=' + activeGroupId + ']');
+            assert(activeShareLinkField);
+            activeShareLinkField.focus();
+            activeShareLinkField.select();
+          }, 0);
+        }
+      });
     },
     restrict: 'AE',
     scope: {},
