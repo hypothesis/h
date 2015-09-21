hypothesis/h

commit cf410d0bd2127973739208ef94b2df59cc0b720e

Clean up socket, threading, mapper with routing
Move all the logic required to set up a clean environment for the
client views into the dependency resolution mechanism of ngRoute.

The annotation mapper gains an unloadAnnotations method to make the
isolated code cleaner.

Use angular-websocket to remove the need for our own socket class.

The AppController becomes much more sane.

h/static/scripts/app-controller.coffee
@@ -4,15 +4,11 @@ angular = require('angular')
 module.exports = class AppController
   this.$inject = [
     '$controller', '$document', '$location', '$route', '$scope', '$window',
-    'auth', 'drafts', 'features', 'identity',
-    'session', 'streamer', 'annotationUI',
-    'annotationMapper', 'threading'
+    'annotationUI', 'auth', 'drafts', 'features', 'identity', 'session'
   ]
   constructor: (
      $controller,   $document,   $location,   $route,   $scope,   $window,
-     auth,   drafts,   features,   identity,
-     session,   streamer,   annotationUI,
-     annotationMapper, threading
+     annotationUI,   auth,   drafts,   features,   identity,  session
   ) ->
     $controller('AnnotationUIController', {$scope})
 
@@ -30,43 +26,12 @@ module.exports = class AppController
 
     isFirstRun = $location.search().hasOwnProperty('firstrun')
 
-    streamerUrl = new URL('/ws', $document.prop('baseURI'))
-    streamerUrl.protocol = streamerUrl.protocol.replace('http', 'ws')
-    streamerUrl = streamerUrl.href
-
-    applyUpdates = (action, data) ->
-      # Update the application with new data from the websocket.
-      return unless data?.length
-      switch action
-        when 'create', 'update', 'past'
-          annotationMapper.loadAnnotations data
-        when 'delete'
-          for annotation in data
-            if a = threading.idTable[annotation.id]?.message
-              $scope.$emit('annotationDeleted', a)
-
-    streamer.onmessage = (data) ->
-      return if !data or data.type != 'annotation-notification'
-      action = data.options.action
-      payload = data.payload
-      applyUpdates(action, payload)
-      $scope.$digest()
-
     # App dialogs
     $scope.accountDialog = visible: false
     $scope.shareDialog = visible: false
 
     # Check to see if we are on the stream page so we can hide share button.
     $scope.isEmbedded = $window.top isnt $window
-
-    cleanupAnnotations = ->
-      # Clean up all the annotations
-      for id, container of $scope.threading.idTable when container.message
-        # Keep drafts. When logging out, drafts are already discarded.
-        if drafts.contains(container.message)
-          continue
-        else
-          $scope.$emit('annotationDeleted', container.message)
 
     identity.watch({
       onlogin: (identity) -> $scope.auth.user = auth.userid(identity)
@@ -95,13 +60,6 @@ module.exports = class AppController
         $scope.login()
       else
         $scope.accountDialog.visible = false
-
-      # Reopen the streamer.
-      streamer.close()
-      streamer.open($window.WebSocket, streamerUrl)
-
-      # Clean up annotations that should be removed.
-      cleanupAnnotations()
 
       # Reload the view if this is not the initial load.
       if oldVal isnt undefined
@@ -132,7 +90,3 @@ module.exports = class AppController
         unless angular.equals $location.search()['q'], query
           $location.search('q', query or null)
           annotationUI.clearSelectedAnnotations()
-
-    $scope.sort = name: 'Location'
-    $scope.threading = threading
-    $scope.threadRoot = $scope.threading?.root

h/static/scripts/stream-controller.coffee
@@ -37,10 +37,6 @@ module.exports = class StreamController
       if $routeParams.q isnt lastQuery
         $route.reload()
 
-    # XXX: Reset the threading service
-    threading.createIdTable([])
-    $scope.threadRoot = threading.root = mail.messageContainer()
-
     # Initialize the base filter
     streamFilter
       .resetFilter()
@@ -55,8 +51,8 @@ module.exports = class StreamController
     fetch(20)
 
     $scope.isStream = true
-
-    $scope.sort.name = 'Newest'
+    $scope.sort = name: 'Newest'
+    $scope.threadRoot = threading.root
 
     $scope.shouldShowThread = (container) -> true
 

h/static/scripts/widget-controller.coffee
@@ -4,13 +4,15 @@ angular = require('angular')
 module.exports = class WidgetController
   this.$inject = [
     '$scope', 'annotationUI', 'crossframe', 'annotationMapper',
-    'streamer', 'streamFilter', 'store'
+    'streamer', 'streamFilter', 'store', 'threading'
   ]
   constructor:   (
      $scope,   annotationUI, crossframe, annotationMapper,
-     streamer,   streamFilter,   store
+     streamer,   streamFilter,   store,   threading
   ) ->
     $scope.isStream = true
+    $scope.sort = name: 'Location'
+    $scope.threadRoot = threading.root
 
     @chunkSize = 200
     loaded = []

h/static/scripts/annotation-mapper.coffee
@@ -17,6 +17,14 @@ module.exports = [
       annotations = (new store.AnnotationResource(a) for a in annotations)
       $rootScope.$emit('annotationsLoaded', annotations)
 
+    unloadAnnotations: (annotations) ->
+      for annotation in annotations
+        container = threading.idTable[annotation.id]
+        if container?.message
+          if annotation isnt container.message
+            annotation = angular.copy(annotation, container.message)
+        $rootScope.$emit('annotationDeleted', annotation)
+
     createAnnotation: (annotation) ->
       annotation = new store.AnnotationResource(annotation)
       $rootScope.$emit('beforeAnnotationCreated', annotation)

h/static/scripts/annotation-viewer-controller.coffee
@@ -4,13 +4,15 @@ angular = require('angular')
 module.exports = class AnnotationViewerController
   this.$inject = [
     '$location', '$routeParams', '$scope',
-    'streamer', 'store', 'streamFilter', 'annotationMapper'
+    'streamer', 'store', 'streamFilter', 'annotationMapper', 'threading'
   ]
   constructor: (
      $location,   $routeParams,   $scope,
-     streamer,   store,   streamFilter,   annotationMapper
+     streamer,   store,   streamFilter,   annotationMapper,   threading
   ) ->
-    # Tells the view that these annotations are standalone
+    id = $routeParams.id
+
+    # Set up the viewer
     $scope.isStream = false
 
     # Provide no-ops until these methods are moved elsewere. They only apply
@@ -22,10 +24,9 @@ module.exports = class AnnotationViewerController
     $scope.search.update = (query) ->
       $location.path('/stream').search('q', query)
 
-    id = $routeParams.id
     store.AnnotationResource.read id: id, (annotation) ->
       annotationMapper.loadAnnotations([annotation])
-      $scope.threadRoot = children: [$scope.threading.getContainer(id)]
+      $scope.threadRoot = {children: threading.idTable[id]}
     store.SearchResource.get references: id, ({rows}) ->
       annotationMapper.loadAnnotations(rows)
 

h/static/scripts/app.coffee
@@ -1,12 +1,62 @@
 require('autofill-event')
 baseURI = require('document-base-uri')
 angular = require('angular')
+require('angular-websocket')
 require('angular-jwt')
 
 uuid = require('node-uuid')
+clientId = uuid.v4()
+
+socket = null
 
 resolve =
   store: ['store', (store) -> store.$promise]
+  streamer: [
+    '$websocket', 'annotationMapper'
+    ($websocket,   annotationMapper) ->
+      # Get the socket URL
+      url = new URL('/ws', baseURI)
+      url.protocol = url.protocol.replace('http', 'ws')
+
+      # Close any existing socket
+      socket?.close()
+
+      # Open the socket
+      socket = $websocket(url.href, [], {reconnectIfNotNormalClose: true})
+      socket.send(messageType: 'client_id', value: clientId)
+
+      # Listen for updates
+      socket.onMessage (event) ->
+        message = JSON.parse(event.data)
+        return if !message or message.type != 'annotation-notification'
+        action = message.options.action
+        annotations = message.payload
+        return unless annotations?.length
+        switch action
+          when 'create', 'update', 'past'
+            annotationMapper.loadAnnotations annotations
+          when 'delete'
+            annotationMapper.unloadAnnotations annotations
+
+      return socket
+  ]
+  threading: [
+    'annotationMapper', 'drafts', 'threading'
+    (annotationMapper,   drafts,   threading) ->
+      # Unload all the annotations
+      idTable = threading.idTable
+      annotations = (message for id, {message} of idTable when message)
+      annotationMapper.unloadAnnotations(annotations)
+
+      # Reset the threading root
+      threading.createIdTable([])
+      threading.root = mail.messageContainer()
+
+      # Thread all the drafts
+      threading.thread(drafts.all())
+
+      return threading
+  ]
 
 
 configureDocument = ['$provide', ($provide) ->
@@ -52,15 +102,11 @@ configureTemplates = ['$sceDelegateProvider', ($sceDelegateProvider) ->
 
 setupCrossFrame = ['crossframe', (crossframe) -> crossframe.connect()]
 
-setupHost = ['host', (host) -> ]
-
-setupStreamer = [
-  '$http', '$window', 'streamer'
-  ($http,   $window,   streamer) ->
-    clientId = uuid.v4()
-    streamer.clientId = clientId
-    $http.defaults.headers.common['X-Client-Id'] = clientId
+setupHttp = ['$http', ($http) ->
+  $http.defaults.headers.common['X-Client-Id'] = clientId
 ]
+
+setupHost = ['host', (host) -> ]
 
 setupFeatures = ['features', (features) -> features.fetch()]
 
@@ -74,6 +120,7 @@ module.exports = angular.module('h', [
   'ngRoute'
   'ngSanitize'
   'ngTagsInput'
+  'ngWebSocket'
   'toastr'
 ])
 
@@ -128,7 +175,6 @@ module.exports = angular.module('h', [
 .service('session', require('./session'))
 .service('store', require('./store'))
 .service('streamFilter', require('./stream-filter'))
-.service('streamer', require('./streamer'))
 .service('tags', require('./tags'))
 .service('time', require('./time'))
 .service('threading', require('./threading'))
@@ -148,5 +194,5 @@ module.exports = angular.module('h', [
 
 .run(setupFeatures)
 .run(setupCrossFrame)
-.run(setupStreamer)
+.run(setupHttp)
 .run(setupHost)

h/static/scripts/streamer.coffee
@@ -1,121 +1,0 @@
-ST_CONNECTING = 0
-ST_OPEN = 1
-ST_CLOSING = 2
-ST_CLOSED = 3
-
-###*
-# @ngdoc service
-# @name streamer
-#
-# @property {string} clientId A unique identifier for this client.
-#
-# @description
-# Provides access to the streamer websocket.
-###
-module.exports = class Streamer
-  constructor: ->
-    this.clientId = null
-
-    this.onopen = angular.noop
-    this.onclose = angular.noop
-    this.onmessage = angular.noop
-
-    this._failCount = 0
-    this._queue = []
-    this._state = ST_CLOSED
-
-  ###*
-  # @ngdoc method
-  # @name Streamer#open
-  #
-  # @param {Object} transport The transport class to create.
-  # @param {string} url The URL to which to connect.
-  # @param {Object|Array} protocols The protocol (or protocols) to use.
-  #
-  # @description
-  # Open the streamer websocket if it is not already open or connecting. Handles
-  # connection failures and sets up onmessage handlers.
-  ###
-  open: (transport, url, protocols) ->
-    if this._state == ST_OPEN || this._state == ST_CONNECTING
-      return
-
-    self = this
-
-    if protocols
-      this._sock = new transport(url, protocols)
-    else
-      this._sock = new transport(url)
-
-    this._state = ST_CONNECTING
-
-    this._sock.onopen = ->
-      self._state = ST_OPEN
-      self._failCount = 0
-
-      # Send the client id
-      if self.clientId
-        self.send(messageType: 'client_id', value: self.clientId)
-
-      # Give the application a chance to initialize the connection
-      self.onopen(name: 'open')
-      # Process queued messages
-      self._sendQueue()
-
-    this._sock.onclose = ->
-      self._state = ST_CLOSED
-      self._failCount++
-
-      reconnect = angular.bind(self, self.open, transport, url, protocols)
-      waitFor = backoff(self._failCount, 10)
-      setTimeout(reconnect, waitFor)
-
-    this._sock.onmessage = (msg) ->
-      self.onmessage(JSON.parse(msg.data))
-
-  ###*
-  # @ngdoc method
-  # @name Streamer#close
-  #
-  # @description
-  # Close the streamer socket.
-  ###
-  close: ->
-    if this._state == ST_CLOSING or this._state == ST_CLOSED
-      return
-
-    self = this
-
-    this._sock.onclose = ->
-      self._state = ST_CLOSED
-      self.onclose()
-
-    this._sock.close()
-    this._sock = null
-    this._state = ST_CLOSING
-
-  ###*
-  # @ngdoc method
-  # @name Streamer#send
-  #
-  # @param {Object} data Message data
-  #
-  # @description
-  # Send an object down the streamer websocket. This can be called before the
-  # socket is open: the message will be queued and delivered once a connection
-  # is established.
-  ###
-  send: (data) ->
-    if this._state == ST_OPEN
-      this._sock.send(JSON.stringify(data))
-    else
-      this._queue.push(data)
-
-  _sendQueue: ->
-    while msg = this._queue.shift()
-      this.send(msg)
-
-
-backoff = (index, max) ->
-  index = Math.min(index, max)
-  return 500 * Math.random() * (Math.pow(2, index) - 1)

h/static/scripts/test/annotation-mapper-test.coffee
@@ -71,6 +71,29 @@ describe 'annotationMapper', ->
       assert.called($rootScope.$emit)
       assert.calledWith($rootScope.$emit, 'annotationsLoaded', [])
 
+  describe '.unloadAnnotations()', ->
+    it 'triggers the annotationDeleted event', ->
+      sandbox.stub($rootScope, '$emit')
+      annotations = [{id: 1}, {id: 2}, {id: 3}]
+      annotationMapper.unloadAnnotations(annotations)
+      assert.called($rootScope.$emit)
+      assert.calledWith($rootScope.$emit, 'annotationDeleted', annotations[0])
+      assert.calledWith($rootScope.$emit, 'annotationDeleted', annotations[1])
+      assert.calledWith($rootScope.$emit, 'annotationDeleted', annotations[2])
+
+    it 'replaces the properties on the cached annotation with those from the deleted one', ->
+      sandbox.stub($rootScope, '$emit')
+      annotations = [{id: 1, url: 'http://example.com'}]
+      cached = {message: {id: 1, $$tag: 'tag1'}}
+      fakeThreading.idTable[1] = cached
+
+      annotationMapper.unloadAnnotations(annotations)
+      assert.called($rootScope.$emit)
+      assert.calledWith($rootScope.$emit, 'annotationDeleted', {
+        id: 1
+        url: 'http://example.com'
+      })
+
   describe '.createAnnotation()', ->
     it 'creates a new annotaton resource', ->
       ann = {}

commit 3cfed31537a0d231b109404a0a889280fe088a0d

Fix thread sorting
cf410d0 broke thread sorting by interfering with the (admittedly
arcane) sort apparatus.

There is a scope.watch in the AppController which is responsible for
filling the `predicate` field of the `sort` object when the `name` field
changes. In order for this to work, the controllers in child scopes
(namely StreamController or WidgetController) need to modify the `name`
property of the `sort` object in the parent scope, via prototypal
inheritance, rather than replacing the entire `sort` object.

h/static/scripts/app-controller.coffee
@@ -33,6 +33,9 @@ module.exports = class AppController
     # Check to see if we are on the stream page so we can hide share button.
     $scope.isEmbedded = $window.top isnt $window
 
+    # Default sort
+    $scope.sort = name: 'Location'
+
     identity.watch({
       onlogin: (identity) -> $scope.auth.user = auth.userid(identity)
       onlogout: -> $scope.auth.user = null

h/static/scripts/stream-controller.coffee
@@ -51,7 +51,7 @@ module.exports = class StreamController
     fetch(20)
 
     $scope.isStream = true
-    $scope.sort = name: 'Newest'
+    $scope.sort.name = 'Newest'
     $scope.threadRoot = threading.root
 
     $scope.shouldShowThread = (container) -> true

h/static/scripts/widget-controller.coffee
@@ -11,7 +11,6 @@ module.exports = class WidgetController
      streamer,   streamFilter,   store,   threading
   ) ->
     $scope.isStream = true
-    $scope.sort = name: 'Location'
     $scope.threadRoot = threading.root
 
     @chunkSize = 200