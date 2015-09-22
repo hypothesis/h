hypothesis/h

commit 70c9b9ba9bbf9384ef78a27a399c3bbb71cbbbf8

Split host / sidebar and clean up plugin loading

h/static/scripts/annotator/main.js
@@ -10,38 +10,30 @@ if (g.wgxpath) {
 // Applications
 Annotator.Guest = require('./guest');
 Annotator.Host = require('./host');
+Annotator.Sidebar = require('./sidebar');
+Annotator.PdfSidebar = require('./pdf-sidebar');
+
+// UI plugins
+Annotator.Plugin.BucketBar = require('./plugin/bucket-bar');
+Annotator.Plugin.Toolbar = require('./plugin/toolbar');
+
+// Document type plugins
+Annotator.Plugin.Pdf = require('./plugin/pdf');
+require('../vendor/annotator.document');  // Does not export the plugin :(
+
+// Selection plugins
+Annotator.Plugin.TextSelection = require('./plugin/textselection');
 
 // Cross-frame communication
 Annotator.Plugin.CrossFrame = require('./plugin/cross-frame');
-Annotator.Plugin.CrossFrame.Bridge = require('../bridge');
 Annotator.Plugin.CrossFrame.AnnotationSync = require('../annotation-sync');
+Annotator.Plugin.CrossFrame.Bridge = require('../bridge');
 Annotator.Plugin.CrossFrame.Discovery = require('../discovery');
-
-// Bucket bar
-require('./plugin/bucket-bar');
-
-// Toolbar
-require('./plugin/toolbar');
-
-// Creating selections
-require('./plugin/textselection');
 
 var docs = 'https://h.readthedocs.org/en/latest/hacking/customized-embedding.html';
 var options = {
-  app: jQuery('link[type="application/annotator+html"]').attr('href'),
-  BucketBar: {container: '.annotator-frame', scrollables: ['body']},
-  Toolbar: {container: '.annotator-frame'}
+  app: jQuery('link[type="application/annotator+html"]').attr('href')
 };
-
-// Document metadata plugins
-if (window.PDFViewerApplication) {
-  require('./plugin/pdf');
-  options.BucketBar.scrollables = ['#viewerContainer'];
-  options.PDF = {};
-} else {
-  require('../vendor/annotator.document');
-  options.Document = {};
-}
 
 if (window.hasOwnProperty('hypothesisConfig')) {
   if (typeof window.hypothesisConfig === 'function') {
@@ -53,7 +45,9 @@ if (window.hasOwnProperty('hypothesisConfig')) {
 
 Annotator.noConflict().$.noConflict(true)(function() {
   'use strict';
-  var Klass = Annotator.Host;
+  var Klass = window.PDFViewerApplication ?
+      Annotator.PdfSidebar :
+      Annotator.Sidebar;
   if (options.hasOwnProperty('constructor')) {
     Klass = options.constructor;
     delete options.constructor;

h/static/scripts/annotator/pdf-sidebar.coffee
@@ -1,0 +1,12 @@
+Sidebar = require('./sidebar')
+
+
+module.exports = class PdfSidebar extends Sidebar
+  options:
+    TextSelection: {}
+    Pdf: {}
+    BucketBar:
+      container: '.annotator-frame'
+      scrollables: ['#viewerContainer']
+    Toolbar:
+      container: '.annotator-frame'

h/static/scripts/annotator/plugin/pdf.coffee
@@ -2,7 +2,7 @@ extend = require('extend')
 Annotator = require('annotator')
 
 
-class PDF extends Annotator.Plugin
+module.exports = class Pdf extends Annotator.Plugin
   documentLoaded: null
   observer: null
   pdfViewer: null
@@ -105,7 +105,3 @@ class PDF extends Annotator.Plugin
 
     for annotation in refreshAnnotations
       annotator.anchor(annotation)
-
-Annotator.Plugin.PDF = PDF
-
-module.exports = PDF

h/static/scripts/annotator/guest.coffee
@@ -5,8 +5,6 @@ raf = require('raf')
 scrollIntoView = require('scroll-into-view')
 
 Annotator = require('annotator')
-Annotator.Plugin.BucketBar = BucketBar = require('./plugin/bucket-bar')
-Annotator.Plugin.CrossFrame = CrossFrame = require('./plugin/cross-frame')
 $ = Annotator.$
 
 highlighter = require('./highlighter')
@@ -33,11 +31,9 @@ module.exports = class Guest extends Annotator
     ".annotator-hl mouseover":           "onHighlightMouseover"
     ".annotator-hl mouseout":            "onHighlightMouseout"
 
-  # Plugin / Options configuration
   options:
-    TextHighlights: {}
+    Document: {}
     TextSelection: {}
-    clickToClose: true
 
   # Anchoring module
   anchoring: require('./anchoring/html')
@@ -131,24 +127,20 @@ module.exports = class Guest extends Annotator
 
   _setupWrapper: ->
     @wrapper = @element
-    if @options.clickToClose
-      @wrapper.on 'click', (event) =>
-        if !@selectedTargets?.length
-          this.hideFrame()
-    return this
+    this
 
   # These methods aren't used in the iframe-hosted configuration of Annotator.
-  _setupDynamicStyle: -> this
   _setupViewer: -> this
   _setupEditor: -> this
   _setupDocumentEvents: -> this
+  _setupDynamicStyle: -> this
 
   destroy: ->
     $('#annotator-dynamic-style').remove()
 
     @adder.remove()
 
-    @wrapper.find('.annotator-hl').each ->
+    @element.find('.annotator-hl').each ->
       $(this).contents().insertBefore(this)
       $(this).remove()
 
@@ -331,7 +323,7 @@ module.exports = class Guest extends Annotator
     else
       # Show the adder button
       @adder
-        .css(Annotator.Util.mousePosition(event, @wrapper[0]))
+        .css(Annotator.Util.mousePosition(event, @element[0]))
         .show()
 
     true
@@ -388,14 +380,6 @@ module.exports = class Guest extends Annotator
 
     @visibleHighlights = shouldShowHighlights
 
-  # Open the sidebar
-  showFrame: ->
-    @crossframe?.call('open')
-
-  # Close the sidebar
-  hideFrame: ->
-    @crossframe?.call('back')
-
   onAdderMouseup: (event) ->
     event.preventDefault()
     event.stopPropagation()
@@ -412,5 +396,4 @@ module.exports = class Guest extends Annotator
         this.createHighlight()
       when 'comment'
         this.createAnnotation()
-        this.showFrame()
     Annotator.Util.getGlobal().getSelection().removeAllRanges()

h/static/scripts/annotator/host.coffee
@@ -1,170 +1,36 @@
-raf = require('raf')
-
 Annotator = require('annotator')
 $ = Annotator.$
 
-Hammer = require('hammerjs')
-
 Guest = require('./guest')
-
-# Minimum width to which the frame can be resized.
-MIN_RESIZE = 280
 
 
 module.exports = class Host extends Guest
-  renderFrame: null
-  gestureState: null
-
   constructor: (element, options) ->
-    src = options.app
     if options.firstRun
-      # Allow options.app to contain query string params.
-      src = src + (if '?' in src then '&' else '?') + 'firstrun'
+      options.app += (if '?' in options.app then '&' else '?') + 'firstrun'
 
     # Create the iframe
     app = $('<iframe></iframe>')
     .attr('name', 'hyp_sidebar_frame')
     .attr('seamless', '')
-    .attr('src', src)
+    .attr('src', options.app)
 
     @frame = $('<div></div>')
     .css('display', 'none')
-    .addClass('annotator-frame annotator-outer annotator-collapsed')
+    .addClass('annotator-frame annotator-outer')
     .appendTo(element)
 
     super
-    this._addCrossFrameListeners()
 
     app.appendTo(@frame)
 
-    if options.firstRun
-      this.on 'panelReady', => this.showFrame(transition: false)
-
-    # Host frame dictates the toolbar options.
     this.on 'panelReady', =>
+      # Initialize tool state.
       this.setVisibleHighlights(!!options.showHighlights)
 
-      # Time to actually show the UI
+      # Show the UI
       @frame.css('display', '')
-
-    if @plugins.BucketBar?
-      this._setupGestures()
-      @plugins.BucketBar.element.on 'click', (event) =>
-        if @frame.hasClass 'annotator-collapsed'
-          this.showFrame()
 
   destroy: ->
     @frame.remove()
     super
-
-  showFrame: (options={transition: true}) ->
-    if options.transition
-      @frame.removeClass 'annotator-no-transition'
-    else
-      @frame.addClass 'annotator-no-transition'
-    @frame.css 'margin-left': "#{-1 * @frame.width()}px"
-    @frame.removeClass 'annotator-collapsed'
-
-    if @toolbar?
-      @toolbar.find('[name=sidebar-toggle]')
-      .removeClass('h-icon-chevron-left')
-      .addClass('h-icon-chevron-right')
-
-  hideFrame: ->
-    @frame.css 'margin-left': ''
-    @frame.removeClass 'annotator-no-transition'
-    @frame.addClass 'annotator-collapsed'
-
-    if @toolbar?
-      @toolbar.find('[name=sidebar-toggle]')
-      .removeClass('h-icon-chevron-right')
-      .addClass('h-icon-chevron-left')
-
-  _addCrossFrameListeners: ->
-    @crossframe.on('showFrame', this.showFrame.bind(this, null))
-    @crossframe.on('hideFrame', this.hideFrame.bind(this, null))
-
-  _initializeGestureState: ->
-    @gestureState =
-      initial: null
-      final: null
-
-  onPan: (event) =>
-    switch event.type
-      when 'panstart'
-        # Initialize the gesture state
-        this._initializeGestureState()
-        # Immadiate response
-        @frame.addClass 'annotator-no-transition'
-        # Escape iframe capture
-        @frame.css('pointer-events', 'none')
-        # Set origin margin
-        @gestureState.initial = parseInt(getComputedStyle(@frame[0]).marginLeft)
-
-      when 'panend'
-        # Re-enable transitions
-        @frame.removeClass 'annotator-no-transition'
-        # Re-enable iframe events
-        @frame.css('pointer-events', '')
-        # Snap open or closed
-        if @gestureState.final <= -MIN_RESIZE
-          this.showFrame()
-        else
-          this.hideFrame()
-        # Reset the gesture state
-        this._initializeGestureState()
-
-      when 'panleft', 'panright'
-        return unless @gestureState.initial?
-        # Compute new margin from delta and initial conditions
-        m = @gestureState.initial
-        d = event.deltaX
-        @gestureState.final = Math.min(Math.round(m + d), 0)
-        # Start updating
-        this._updateLayout()
-
-  onSwipe: (event) =>
-    switch event.type
-      when 'swipeleft'
-        this.showFrame()
-      when 'swiperight'
-        this.hideFrame()
-
-  _setupGestures: ->
-    $toggle = @toolbar.find('[name=sidebar-toggle]')
-
-    # Prevent any default gestures on the handle
-    $toggle.on('touchmove', (event) -> event.preventDefault())
-
-    # Set up the Hammer instance and handlers
-    mgr = new Hammer.Manager($toggle[0])
-    .on('panstart panend panleft panright', this.onPan)
-    .on('swipeleft swiperight', this.onSwipe)
-
-    # Set up the gesture recognition
-    pan = mgr.add(new Hammer.Pan({direction: Hammer.DIRECTION_HORIZONTAL}))
-    swipe = mgr.add(new Hammer.Swipe({direction: Hammer.DIRECTION_HORIZONTAL}))
-    swipe.recognizeWith(pan)
-
-    # Set up the initial state
-    this._initializeGestureState()
-
-    # Return this for chaining
-    this
-
-  # Schedule any changes needed to update the layout of the widget or page
-  # in response to interface changes.
-  _updateLayout: ->
-    # Only schedule one frame at a time
-    return if @renderFrame
-
-    # Schedule a frame
-    @renderFrame = raf =>
-      @renderFrame = null  # Clear the schedule
-
-      # Process the resize gesture
-      if @gestureState.final isnt @gestureState.initial
-        m = @gestureState.final
-        w = -m
-        @frame.css('margin-left', "#{m}px")
-        if w >= MIN_RESIZE then @frame.css('width', "#{w}px")

h/static/scripts/annotator/plugin/bucket-bar.coffee
@@ -42,7 +42,7 @@ scrollToClosest = (anchors, direction) ->
   scrollIntoView(next.highlights[0])
 
 
-class BucketBar extends Annotator.Plugin
+module.exports = class BucketBar extends Annotator.Plugin
   # svg skeleton
   html: """
         <div class="annotator-bucket-bar">
@@ -57,7 +57,7 @@ class BucketBar extends Annotator.Plugin
     gapSize: 60
 
     # Selectors for the scrollable elements on the page
-    scrollables: null
+    scrollables: ['body']
 
   # buckets of annotations that overlap
   buckets: []
@@ -293,6 +293,3 @@ class BucketBar extends Annotator.Plugin
 BucketBar.BUCKET_SIZE = BUCKET_SIZE
 BucketBar.BUCKET_NAV_SIZE = BUCKET_NAV_SIZE
 BucketBar.BUCKET_TOP_THRESHOLD = BUCKET_TOP_THRESHOLD
-
-# Export as a module
-module.exports = BucketBar

h/static/scripts/annotator/plugin/textselection.coffee
@@ -3,7 +3,7 @@ $ = Annotator.$
 
 
 # This plugin implements the UI code for creating text annotations
-class Annotator.Plugin.TextSelection extends Annotator.Plugin
+module.exports = class TextSelection extends Annotator.Plugin
 
   pluginInit: ->
     # Register the event handlers required for creating a selection

h/static/scripts/annotator/plugin/toolbar.coffee
@@ -1,3 +1,4 @@
+Annotator = require('annotator')
 $ = Annotator.$
 
 makeButton = (item) ->
@@ -10,7 +11,7 @@ makeButton = (item) ->
   button = $('<li></li>').append(anchor)
   return button[0]
 
-class Annotator.Plugin.Toolbar extends Annotator.Plugin
+module.exports = class Toolbar extends Annotator.Plugin
   HIDE_CLASS = 'annotator-hide'
 
   events:
@@ -35,9 +36,9 @@ class Annotator.Plugin.Toolbar extends Annotator.Plugin
           event.stopPropagation()
           collapsed = @annotator.frame.hasClass('annotator-collapsed')
           if collapsed
-            @annotator.showFrame()
+            @annotator.show()
           else
-            @annotator.hideFrame()
+            @annotator.hide()
     ,
       "title": "Hide Highlights"
       "class": "h-icon-visibility"
@@ -57,7 +58,7 @@ class Annotator.Plugin.Toolbar extends Annotator.Plugin
           event.preventDefault()
           event.stopPropagation()
           @annotator.createAnnotation()
-          @annotator.showFrame()
+          @annotator.show()
     ]
     @buttons = $(makeButton(item) for item in items)
 

commit 2897893bd9cf09ae60b558cb7d390ced2f35a8f7

Fix detection of PDF URIs
70c9b9b broke a number of features of the h client that were toggled on
for PDF documents, by renaming the Annotator PDF plugin from
`Annotator.Plugin.PDF` to `Annotator.Plugin.Pdf`.

While it's certainly an ugly side-effect of a straightforward rename,
the problem is that `Guest#getDocumentInfo` (to pick one example) used
`@plugins.PDF?` to determine whether to use the PDF metadata
(provided by the PDF.js `PDFViewerApplication`).

In the absence of PDF metadata, `getDocumentInfo` falls back to using
`window.location.href`, which explains the behaviour identified
in #2506.

Fixes #2506.

h/static/scripts/annotator/main.js
@@ -18,7 +18,7 @@ Annotator.Plugin.BucketBar = require('./plugin/bucket-bar');
 Annotator.Plugin.Toolbar = require('./plugin/toolbar');
 
 // Document type plugins
-Annotator.Plugin.Pdf = require('./plugin/pdf');
+Annotator.Plugin.PDF = require('./plugin/pdf');
 require('../vendor/annotator.document');  // Does not export the plugin :(
 
 // Selection plugins

h/static/scripts/annotator/pdf-sidebar.coffee
@@ -4,7 +4,7 @@ Sidebar = require('./sidebar')
 module.exports = class PdfSidebar extends Sidebar
   options:
     TextSelection: {}
-    Pdf: {}
+    PDF: {}
     BucketBar:
       container: '.annotator-frame'
       scrollables: ['#viewerContainer']

h/static/scripts/annotator/plugin/pdf.coffee
@@ -2,7 +2,7 @@ extend = require('extend')
 Annotator = require('annotator')
 
 
-module.exports = class Pdf extends Annotator.Plugin
+module.exports = class PDF extends Annotator.Plugin
   documentLoaded: null
   observer: null
   pdfViewer: null