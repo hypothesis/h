05e3785fc82848715a08f49088c9c97b0aa44041
Nick Stenning <nick@whiteink.com>
2015-09-23 16:58:22 +0200

    Translate the annotationMapper service to JavaScript
    In preparation for making some substantial changes to this piece of
    code, I'm translating it to JS.

---
+++ h/static/scripts/annotation-mapper.js
@@ -0,0 +1,74 @@
+'use strict';
+
+
+// Fetch the container object for the passed annotation from the threading
+// service, but only return it if it has an associated message.
+function getContainer(threading, annotation) {
+  var container = threading.idTable[annotation.id];
+  if (container === null || typeof container === 'undefined') {
+    return null;
+  }
+  // Also return null if the container has no message
+  if (!container.message) {
+    return null;
+  }
+  return container;
+}
+
+
+// Wraps the annotation store to trigger events for the CRUD actions
+// @ngInject
+function annotationMapper($rootScope, threading, store) {
+  function loadAnnotations(annotations) {
+    var loaded = [];
+
+    annotations.forEach(function (annotation) {
+      var container = getContainer(threading, annotation);
+      if (container !== null) {
+        angular.copy(annotation, container.message);
+        $rootScope.$emit('annotationUpdated', container.message);
+        return;
+      }
+
+      loaded.push(new store.AnnotationResource(annotation));
+    });
+
+    $rootScope.$emit('annotationsLoaded', loaded);
+  }
+
+  function unloadAnnotations(annotations) {
+    annotations.forEach(function (annotation) {
+      var container = getContainer(threading, annotation);
+      if (container !== null && annotation !== container.message) {
+        annotation = angular.copy(annotation, container.message);
+      }
+
+      $rootScope.$emit('annotationDeleted', annotation);
+    });
+  }
+
+  function createAnnotation(annotation) {
+    annotation = new store.AnnotationResource(annotation);
+    $rootScope.$emit('beforeAnnotationCreated', annotation);
+    return annotation;
+  }
+
+  function deleteAnnotation(annotation) {
+    return annotation.$delete({
+      id: annotation.id
+    }).then(function () {
+      $rootScope.$emit('annotationDeleted', annotation);
+      return annotation;
+    });
+  }
+
+  return {
+    loadAnnotations: loadAnnotations,
+    unloadAnnotations: unloadAnnotations,
+    createAnnotation: createAnnotation,
+    deleteAnnotation: deleteAnnotation
+  };
+}
+
+
+module.exports = annotationMapper;
