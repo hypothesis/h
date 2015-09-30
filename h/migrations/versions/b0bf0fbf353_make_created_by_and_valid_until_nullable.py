5957d3eac53ab7d797b73967549baa5f9ea7a49c
Sean Hammond <git@seanh.cc>
2015-09-30 15:15:08 +0100

    Fix a broken migration script
    When this script runs the created_by and valid_until columns will
    already exist, and any existing rows will contain NULL values for them,
    so we need to insert some default values into these existing rows before
    adding nullable=False constraints back to the columns.

--- h/migrations/versions/b0bf0fbf353_make_created_by_and_valid_until_nullable.py
+++ h/migrations/versions/b0bf0fbf353_make_created_by_and_valid_until_nullable.py
@@ -12,8 +12,11 @@ Create Date: 2015-09-30 15:14:03.050955
 revision = 'b0bf0fbf353'
 down_revision = 'ef3059e0396'
 
+import datetime
+
 from alembic import op
 import sqlalchemy as sa
+
 
 def upgrade():
     with op.batch_alter_table('activation') as batch_op:
@@ -27,9 +30,19 @@ def upgrade():
 
 def downgrade():
     with op.batch_alter_table('activation') as batch_op:
-        batch_op.alter_column('valid_until',
-                              existing_type=sa.DateTime,
-                              nullable=False)
+        batch_op.execute(
+            'UPDATE activation SET created_by = "web"')
+
         batch_op.alter_column('created_by',
                               existing_type=sa.Unicode(length=30),
+                              nullable=False)
+
+        in_three_days = datetime.datetime.now() + datetime.timedelta(days=3)
+        in_three_days = in_three_days.strftime('%Y-%m-%d %H:%M:%S')
+        batch_op.execute(
+            'UPDATE activation SET valid_until = "{in_three_days}"'.format(
+                in_three_days=in_three_days))
+
+        batch_op.alter_column('valid_until',
+                              existing_type=sa.DateTime,
                               nullable=False)
