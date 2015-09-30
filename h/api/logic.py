hypothesis/h

commit 11135fd4ea7478bf58fd6146092322163d4e5dc4

Remove remnants of annotator store auth
The consumer field is removed entirely because with authorization
disabled in annotator-store the field serves no purpose at present.

Claims used by the Annotator Auth plugin on the front end are removed
from the JWT since this client has been deprecated for some time.

The User and Consumer model stubs from annotator-store are removed
and the authenticated userid from Pyramid is passed to the logic
functions of h.api now.

h/api/logic.py
@@ -12,10 +12,10 @@ log = logging.getLogger(__name__)
 
 
 # These annotation fields are not to be set by the user.
-PROTECTED_FIELDS = ['created', 'updated', 'user', 'consumer', 'id']
+PROTECTED_FIELDS = ['created', 'updated', 'user', 'id']
 
 
-def create_annotation(fields, user):
+def create_annotation(fields, userid):
     """Create and store an annotation."""
     # Some fields are not to be set by the user, ignore them
     for field in PROTECTED_FIELDS:
@@ -23,16 +23,13 @@ def create_annotation(fields, user):
 
     # Create Annotation instance
     annotation = Annotation(fields)
-
-    annotation['user'] = user.id
-    annotation['consumer'] = user.consumer.key
+    annotation['user'] = userid
 
     # Save it in the database
     search_lib.prepare(annotation)
     annotation.save()
 
-    log.debug('Created annotation; user: %s, consumer key: %s',
-              annotation['user'], annotation['consumer'])
+    log.debug('Created annotation; user: %s', annotation['user'])
 
     return annotation
 
@@ -53,7 +50,7 @@ def _anonymize_deletes(annotation):
         annotation['permissions'][action] = filtered
 
 
-def update_annotation(annotation, fields, has_admin_permission):
+def update_annotation(annotation, fields, userid):
     """Update the given annotation with the given new fields.
 
     :raises RuntimeError: if the fields attempt to change the annotation's
@@ -66,13 +63,11 @@ def update_annotation(annotation, fields, has_admin_permission):
         fields.pop(field, None)
 
     # If the user is changing access permissions, check if it's allowed.
-    changing_permissions = (
-        'permissions' in fields and
-        fields['permissions'] != annotation.get('permissions', {})
-    )
-    if changing_permissions and not has_admin_permission:
-        raise RuntimeError(
-            _('Not authorized to change annotation permissions.'), 401)
+    permissions = annotation.get('permissions', {})
+    if fields.get('permissions', {}) != permissions:
+        if userid not in permissions.get('admin', []):
+            raise RuntimeError(
+                _('Not authorized to change annotation permissions.'), 401)
 
     if 'group' in fields and 'group' in annotation:
         if fields['group'] != annotation.get('group'):

h/api/test/logic_test.py
@@ -31,49 +31,37 @@ def test_create_annotation_pops_protected_fields(Annotation):
             'created': 'foo',
             'updated': 'foo',
             'user': 'foo',
-            'consumer': 'foo',
             'id': 'foo'
         },
-        user=mock.Mock())
+        userid='acct:annamaria@example.com')
 
-    for field in ('created', 'updated', 'user', 'consumer', 'id'):
+    for field in ('created', 'updated', 'user', 'id'):
         assert field not in Annotation.call_args[0][0]
 
 
 @create_annotation_fixtures
 def test_create_annotation_calls_Annotation(Annotation):
     fields = mock.MagicMock()
-    logic.create_annotation(fields, mock.Mock())
+    logic.create_annotation(fields, userid='acct:joe@example.org')
 
     Annotation.assert_called_once_with(fields)
 
 
 @create_annotation_fixtures
 def test_create_annotation_sets_user(Annotation):
-    """It should set the annotation's 'user' field to the user's id."""
-    user = mock.Mock()
+    """It should set the annotation's 'user' field to the userid."""
+    userid = 'acct:sofia@example.net'
     Annotation.return_value = _mock_annotation()
 
-    annotation = logic.create_annotation({}, user)
+    annotation = logic.create_annotation({}, userid)
 
-    assert annotation['user'] == user.id
-
-
-@create_annotation_fixtures
-def test_create_annotation_sets_consumer(Annotation):
-    """It should set the annotation's 'consumer' field to the consumer key."""
-    user = mock.Mock()
-    Annotation.return_value = _mock_annotation()
-
-    annotation = logic.create_annotation({}, user)
-
-    assert annotation['consumer'] == user.consumer.key
+    assert annotation['user'] == userid
 
 
 @create_annotation_fixtures
 def test_create_annotation_calls_prepare(Annotation, search_lib):
     """It should call prepare() once with the annotation."""
-    logic.create_annotation({}, mock.Mock())
+    logic.create_annotation({}, userid='acct:gustave@example.com')
 
     search_lib.prepare.assert_called_once_with(Annotation.return_value)
 
@@ -81,14 +69,15 @@ def test_create_annotation_calls_prepare(Annotation, search_lib):
 @create_annotation_fixtures
 def test_create_annotation_calls_save(Annotation):
     """It should call save() once."""
-    logic.create_annotation({}, mock.Mock())
+    logic.create_annotation({}, userid='acct:francois@example.com')
 
     Annotation.return_value.save.assert_called_once_with()
 
 
 @create_annotation_fixtures
 def test_create_annotation_returns_the_annotation(Annotation):
-    assert logic.create_annotation({}, mock.Mock()) == Annotation.return_value
+    result = logic.create_annotation({}, userid='acct:satyarupa@example.org')
+    assert result == Annotation.return_value
 
 
 @create_annotation_fixtures
@@ -97,7 +86,7 @@ def test_create_annotation_does_not_crash_if_annotation_has_no_group(
     assert 'group' not in Annotation.return_value
     fields = {}  # No group here either.
 
-    logic.create_annotation(fields, mock.Mock())
+    logic.create_annotation(fields, userid='acct:maya@example.net')
 
 
 @create_annotation_fixtures
@@ -117,7 +106,7 @@ def test_create_annotation_does_not_crash_if_annotations_parent_has_no_group(
     # And no group in the parent annotation either.
     Annotation.fetch.return_value = {}
 
-    logic.create_annotation(fields, mock.Mock())
+    logic.create_annotation(fields, userid='acct:filip@example.com')
 
 
 # The fixtures required to mock all of update_annotation()'s dependencies.
@@ -135,12 +124,11 @@ def test_update_annotation_does_not_pass_protected_fields_to_update():
             'created': 'foo',
             'updated': 'foo',
             'user': 'foo',
-            'consumer': 'foo',
             'id': 'foo'
         },
-        has_admin_permission=False)
+        userid='foo')
 
-    for field in ('created', 'updated', 'user', 'consumer', 'id'):
+    for field in ('created', 'updated', 'user', 'id'):
         assert field not in annotation.update.call_args[0][0]
 
 
@@ -148,31 +136,39 @@ def test_update_annotation_does_not_pass_protected_fields_to_update():
 def test_update_annotation_raises_if_non_admin_changes_perms():
     with pytest.raises(RuntimeError):
         logic.update_annotation(
-            _mock_annotation(),
-            fields={'permissions': 'changed'},
-            has_admin_permission=False)
+            _mock_annotation(permissions={}),
+            fields={'permissions': {'read': ['someone']}},
+            userid='alice')
 
 
 @update_annotation_fixtures
 def test_update_annotation_admins_can_change_permissions():
-    annotation = _mock_annotation(permissions='foo')
+    annotation = _mock_annotation(
+        permissions={'admin': ['alice']},
+        user='alice')
 
     logic.update_annotation(
         annotation,
         fields={'permissions': 'changed'},
-        has_admin_permission=True)
+        userid='alice')
 
     assert annotation['permissions'] == 'changed'
 
 
 @update_annotation_fixtures
 def test_update_annotation_non_admins_can_make_non_permissions_changes():
-    annotation = _mock_annotation(foo='bar')
+    annotation = _mock_annotation(
+        foo='bar',
+        permissions={'admin': ['alice']},
+        user='alice')
 
     logic.update_annotation(
         annotation,
-        fields={'foo': 'changed'},
-        has_admin_permission=False)
+        fields={
+            'foo': 'changed',
+            'permissions': {'admin': ['alice']},
+        },
+        userid='bob')
 
     assert annotation['foo'] == 'changed'
 
@@ -182,7 +178,7 @@ def test_update_annotation_calls_update():
     annotation = _mock_annotation()
     fields = {'foo': 'bar'}
 
-    logic.update_annotation(annotation, fields, False)
+    logic.update_annotation(annotation, fields, 'foo')
 
     annotation.update.assert_called_once_with(fields)
 
@@ -193,72 +189,72 @@ def test_update_annotation_user_cannot_change_group():
     fields = {'group': 'new'}
 
     with pytest.raises(RuntimeError):
-        logic.update_annotation(annotation, fields, False)
+        logic.update_annotation(annotation, fields, 'foo')
 
 
 @update_annotation_fixtures
 def test_update_annotation_removes_userid_from_permissions_if_deleted():
-    user = 'acct:fred@hypothes.is'
+    userid = 'acct:fred@hypothes.is'
     annotation = _mock_annotation(
         deleted=True,
-        user=user,
+        user=userid,
         permissions={
-            'admin': [user, 'someone else'],
-            'read': [user, 'someone else'],
+            'admin': [userid, 'someone else'],
+            'read': [userid, 'someone else'],
             'update': ['someone else'],
             'delete': ['someone else']
         }
     )
     fields = {
         'permissions': {
-            'update': [user],
-            'delete': [user]
+            'update': [userid],
+            'delete': [userid]
         }
     }
 
-    logic.update_annotation(annotation, fields, True)
+    logic.update_annotation(annotation, fields, userid)
 
     for action in annotation['permissions']:
-        assert user not in annotation['permissions'][action]
+        assert userid not in annotation['permissions'][action]
 
 
 @update_annotation_fixtures
 def test_update_annotation_does_not_remove_userid_if_not_deleted():
-    user = 'acct:fred@hypothes.is'
+    userid = 'acct:fred@hypothes.is'
     annotation = _mock_annotation(
         deleted=False,
-        user=user,
+        user=userid,
         permissions={
-            'admin': [user, 'someone else'],
-            'read': [user, 'someone else'],
+            'admin': [userid, 'someone else'],
+            'read': [userid, 'someone else'],
             'update': ['someone else'],
             'delete': ['someone else']
         }
     )
     fields = {
         'permissions': {
-            'update': [user],
-            'delete': [user]
+            'update': [userid],
+            'delete': [userid]
         }
     }
 
-    logic.update_annotation(annotation, fields, True)
+    logic.update_annotation(annotation, fields, userid)
 
     for action in annotation['permissions']:
-        assert user in annotation['permissions'][action]
+        assert userid in annotation['permissions'][action]
 
 
 @update_annotation_fixtures
 def test_update_annotation_if_deleted_does_not_remove_other_principals():
-    user = 'acct:fred@hypothes.is'
+    userid = 'acct:fred@hypothes.is'
     annotation = _mock_annotation(
         deleted=True,
-        user=user,
+        user=userid,
         permissions={
-            'admin': [user, 'someone else'],
-            'read': [user, 'someone else'],
-            'update': [user],
-            'delete': [user]
+            'admin': [userid, 'someone else'],
+            'read': [userid, 'someone else'],
+            'update': [userid],
+            'delete': [userid]
         }
     )
     fields = {
@@ -268,7 +264,7 @@ def test_update_annotation_if_deleted_does_not_remove_other_principals():
         }
     }
 
-    logic.update_annotation(annotation, fields, True)
+    logic.update_annotation(annotation, fields, userid)
 
     for action in annotation['permissions']:
         assert 'someone else' in annotation['permissions'][action]
@@ -278,7 +274,7 @@ def test_update_annotation_if_deleted_does_not_remove_other_principals():
 def test_update_annotation_calls_prepare(search_lib):
     annotation = _mock_annotation()
 
-    logic.update_annotation(annotation, {}, False)
+    logic.update_annotation(annotation, {}, 'foo')
 
     search_lib.prepare.assert_called_once_with(annotation)
 
@@ -287,7 +283,7 @@ def test_update_annotation_calls_prepare(search_lib):
 def test_update_annotation_calls_save():
     annotation = _mock_annotation()
 
-    logic.update_annotation(annotation, {}, False)
+    logic.update_annotation(annotation, {}, 'foo')
 
     annotation.save.assert_called_once_with()
 
@@ -297,7 +293,7 @@ def test_update_annotation_does_not_crash_if_annotation_has_no_group():
     annotation = _mock_annotation()
     assert 'group' not in annotation
 
-    logic.update_annotation(annotation, {}, False)
+    logic.update_annotation(annotation, {}, 'foo')
 
 
 @update_annotation_fixtures
@@ -316,7 +312,7 @@ def test_update_annotation_does_not_crash_if_annotations_parent_has_no_group(
     # And no group in the parent annotation either.
     Annotation.fetch.return_value = {}
 
-    logic.update_annotation(annotation, {}, False)
+    logic.update_annotation(annotation, {}, 'foo')
 
 
 # The fixtures required to mock all of delete_annotation()'s dependencies.

h/api/auth.py
@@ -2,20 +2,6 @@
 
 from pyramid import security
 
-from annotator.auth import Consumer, User
-
-
-def get_user(request):
-    """Create a User object for annotator-store."""
-    userid = request.unauthenticated_userid
-    if userid is not None:
-        for principal in request.effective_principals:
-            if principal.startswith('consumer:'):
-                key = principal[9:]
-                consumer = Consumer(key)
-                return User(userid, consumer, False)
-    return None
-
 
 def translate_annotation_principals(principals):
     """

h/api/db.py
@@ -28,10 +28,6 @@ def store_from_settings(settings):
     if 'es.compatibility' in settings:
         es.compatibility_mode = settings['es.compatibility']
 
-    # We want search results to be filtered according to their
-    # read-permissions, which is done in the store itself.
-    es.authorization_enabled = True
-
     return es
 
 

h/api/models.py
@@ -29,7 +29,6 @@ class Annotation(annotation.Annotation):
             },
         },
         'user': {'type': 'string', 'index': 'analyzed', 'analyzer': 'user'},
-        'consumer': {'type': 'string'},
         'target': {
             'properties': {
                 'source': {

h/api/test/views_test.py
@@ -129,7 +129,7 @@ def test_annotations_index_renders_results(search_lib):
 
 # The fixtures required to mock all of create()'s dependencies.
 create_fixtures = pytest.mark.usefixtures(
-    'get_user', 'logic', 'AnnotationEvent', 'search_lib')
+    'logic', 'AnnotationEvent', 'search_lib')
 
 
 @create_fixtures
@@ -145,43 +145,15 @@ def test_create_returns_error_if_parsing_json_fails():
 
 
 @create_fixtures
-def test_create_calls_logic(logic, get_user):
+def test_create_calls_create_annotation(logic):
     """It should call logic.create_annotation() appropriately."""
     request = mock.Mock()
 
     views.create(request)
 
     logic.create_annotation.assert_called_once_with(
-        fields=request.json_body, user=get_user.return_value)
-
-
-@create_fixtures
-def test_create_calls_create_annotation_once(logic):
-    """It should call logic.create_annotation() exactly once."""
-    request = mock.Mock()
-
-    views.create(request)
-
-    assert logic.create_annotation.call_count == 1
-
-
-@create_fixtures
-def test_create_passes_json_to_create_annotation(logic):
-    """It should pass the JSON from the request to create_annotation()."""
-    request = mock.Mock()
-
-    views.create(request)
-
-    assert logic.create_annotation.call_args[1]['fields'] == request.json_body
-
-
-@create_fixtures
-def test_create_passes_user_to_create_annotation(get_user, logic):
-    """It should pass the user from get_user() to logic.create_annotation()."""
-    views.create(mock.Mock())
-
-    assert logic.create_annotation.call_args[1]['user'] == (
-        get_user.return_value)
+        request.json_body,
+        userid=request.authenticated_userid)
 
 
 @create_fixtures
@@ -276,48 +248,16 @@ def test_update_returns_error_if_json_parsing_fails():
 
 
 @update_fixtures
-def test_update_calls_has_permission():
-    annotation = mock.Mock()
+def test_update_calls_update_annotation(logic):
+    context = mock.Mock()
     request = mock.Mock()
 
-    views.update(annotation, request)
+    views.update(context, request)
 
-    request.has_permission.assert_called_once_with('admin', annotation)
-
-
-@update_fixtures
-def test_update_calls_update_annotation_once(logic):
-    views.update(mock.Mock(), mock.Mock())
-
-    assert logic.update_annotation.call_count == 1
-
-
-@update_fixtures
-def test_update_passes_annotation_to_update_annotation(logic):
-    annotation = mock.Mock()
-
-    views.update(annotation, mock.Mock())
-
-    assert logic.update_annotation.call_args[0][0] == annotation.model
-
-
-@update_fixtures
-def test_update_passes_fields_to_update_annotation(logic):
-    request = mock.Mock()
-
-    views.update(mock.Mock(), request)
-
-    assert logic.update_annotation.call_args[0][1] == request.json_body
-
-
-@update_fixtures
-def test_update_passes_has_admin_permission_to_update_annotation(logic):
-    request = mock.Mock()
-
-    views.update(mock.Mock(), request)
-
-    assert logic.update_annotation.call_args[0][2] == (
-        request.has_permission.return_value)
+    logic.update_annotation.assert_called_once_with(
+        context.model,
+        request.json_body,
+        request.authenticated_userid)
 
 
 @update_fixtures
@@ -436,13 +376,6 @@ def AnnotationEvent(request):
 @pytest.fixture
 def Annotation(request):
     patcher = mock.patch('h.api.views.Annotation', autospec=True)
-    request.addfinalizer(patcher.stop)
-    return patcher.start()
-
-
-@pytest.fixture
-def get_user(request):
-    patcher = mock.patch('h.api.views.get_user', autospec=True)
     request.addfinalizer(patcher.stop)
     return patcher.start()
 

h/api/views.py
@@ -9,7 +9,6 @@ from pyramid.view import forbidden_view_config, notfound_view_config
 from pyramid.view import view_config
 
 from h.api import cors
-from h.api.auth import get_user
 from h.api.events import AnnotationEvent
 from h.api import search as search_lib
 from h.api import logic
@@ -147,10 +146,9 @@ def create(request):
                           'No JSON payload sent. Annotation not created.',
                           status_code=400)  # Client Error: Bad Request
 
-    user = get_user(request)
-
     # Create the annotation
-    annotation = logic.create_annotation(fields=fields, user=user)
+    annotation = logic.create_annotation(fields,
+                                         userid=request.authenticated_userid)
 
     # Notify any subscribers
     _publish_annotation_event(request, annotation, 'create')
@@ -183,12 +181,11 @@ def update(context, request):
                           'No JSON payload sent. Annotation not created.',
                           status_code=400)  # Client Error: Bad Request
 
-    # Check user's permissions
-    has_admin_permission = request.has_permission('admin', context)
-
     # Update and store the annotation
     try:
-        logic.update_annotation(annotation, fields, has_admin_permission)
+        logic.update_annotation(annotation,
+                                fields,
+                                userid=request.authenticated_userid)
     except RuntimeError as err:
         return _api_error(
             request,

h/auth.py
@@ -136,10 +136,6 @@ def groupfinder(userid, request):
     """
     principals = set()
 
-    if getattr(request, 'client', None) is not None:
-        consumer_group = 'consumer:{}'.format(request.client.client_id)
-        principals.add(consumer_group)
-
     user = models.User.get_by_userid(request.domain, userid)
     if user is None:
         return
@@ -193,14 +189,6 @@ def generate_signed_token(request):
         'exp': now + ttl,
         'iat': now,
     }
-
-    # bw compat for the Annotator Auth plugin
-    claims.update({
-        'consumerKey': request.client.client_id,
-        'issuedAt': now.isoformat(),
-        'ttl': int(ttl.total_seconds()),
-        'userId': request.user,
-    })
 
     claims.update(request.extra_credentials or {})
     return jwt.encode(claims, request.client.client_secret)

commit 95bdc76a8c19b891a639edb75174dedf5e01fd6f

Allow non-admins to change other fields when permissions absent
11135fd broke the ability to do partial updates without sending the
permissions field. This commit ensures this is tested and restores that
functionality.

h/api/logic.py
@@ -64,10 +64,13 @@ def update_annotation(annotation, fields, userid):
 
     # If the user is changing access permissions, check if it's allowed.
     permissions = annotation.get('permissions', {})
-    if fields.get('permissions', {}) != permissions:
-        if userid not in permissions.get('admin', []):
-            raise RuntimeError(
-                _('Not authorized to change annotation permissions.'), 401)
+    changing_permissions = (
+        'permissions' in fields and
+        fields['permissions'] != permissions
+    )
+    if changing_permissions and userid not in permissions.get('admin', []):
+        raise RuntimeError(
+            _('Not authorized to change annotation permissions.'), 401)
 
     if 'group' in fields and 'group' in annotation:
         if fields['group'] != annotation.get('group'):

h/api/test/logic_test.py
@@ -166,7 +166,6 @@ def test_update_annotation_non_admins_can_make_non_permissions_changes():
         annotation,
         fields={
             'foo': 'changed',
-            'permissions': {'admin': ['alice']},
         },
         userid='bob')
 