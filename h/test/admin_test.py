2cf3e894728083cc8a988c614e3e5494c531ccb9
Sean Hammond <git@seanh.cc>
2015-10-12 13:19:01 +0100

    Rename badge->blocklist on admin page

--- h/test/admin_test.py
+++ h/test/admin_test.py
@@ -465,73 +465,74 @@ def test_users_index_user_found(User):
                       "user": User.get_by_username.return_value}
 
 
-badge_index_fixtures = pytest.mark.usefixtures('models')
+blocklist_index_fixtures = pytest.mark.usefixtures('models')
 
 
-@badge_index_fixtures
-def test_badge_index_returns_all_blocklisted_urls(models):
-    assert admin.badge_index(Mock()) == {
+@blocklist_index_fixtures
+def test_blocklist_index_returns_all_blocklisted_urls(models):
+    assert admin.blocklist_index(Mock()) == {
         "uris": models.Blocklist.all.return_value}
 
 
-badge_add_fixtures = pytest.mark.usefixtures('models', 'badge_index')
+blocklist_add_fixtures = pytest.mark.usefixtures('models', 'blocklist_index')
 
 
-@badge_add_fixtures
-def test_badge_add_adds_uri_to_model(models):
+@blocklist_add_fixtures
+def test_blocklist_add_adds_uri_to_model(models):
     request = Mock(params={'add': 'test_uri'})
 
-    admin.badge_add(request)
+    admin.blocklist_add(request)
 
     models.Blocklist.assert_called_once_with(uri='test_uri')
     request.db.add.assert_called_once_with(models.Blocklist.return_value)
 
 
-@badge_add_fixtures
-def test_badge_add_returns_index(badge_index):
+@blocklist_add_fixtures
+def test_blocklist_add_returns_index(blocklist_index):
     request = Mock(params={'add': 'test_uri'})
 
-    assert admin.badge_add(request) == badge_index.return_value
+    assert admin.blocklist_add(request) == blocklist_index.return_value
 
 
-@badge_add_fixtures
-def test_badge_add_flashes_error_if_uri_already_blocked(models):
+@blocklist_add_fixtures
+def test_blocklist_add_flashes_error_if_uri_already_blocked(models):
     request = Mock(params={'add': 'test_uri'})
     models.Blocklist.side_effect = ValueError("test_error_message")
 
-    admin.badge_add(request)
+    admin.blocklist_add(request)
 
     assert not request.db.add.called
     request.session.flash.assert_called_once_with(
         "test_error_message", "error")
 
 
-@badge_add_fixtures
-def test_badge_add_returns_index_if_uri_already_blocked(models, badge_index):
+@blocklist_add_fixtures
+def test_blocklist_add_returns_index_if_uri_already_blocked(
+        models, blocklist_index):
     request = Mock(params={'add': 'test_uri'})
     models.Blocklist.side_effect = ValueError("test_error_message")
 
-    assert admin.badge_add(request) == badge_index.return_value
+    assert admin.blocklist_add(request) == blocklist_index.return_value
 
 
-badge_remove_fixtures = pytest.mark.usefixtures('models', 'badge_index')
+blocklist_remove_fixtures = pytest.mark.usefixtures('models', 'blocklist_index')
 
 
-@badge_remove_fixtures
-def test_badge_remove_deletes_model(models):
+@blocklist_remove_fixtures
+def test_blocklist_remove_deletes_model(models):
     request = Mock(params={'remove': 'test_uri'})
 
-    admin.badge_remove(request)
+    admin.blocklist_remove(request)
 
     models.Blocklist.get_by_uri.assert_called_once_with('test_uri')
     request.db.delete.assert_called_once_with(
         models.Blocklist.get_by_uri.return_value)
 
 
-@badge_remove_fixtures
-def test_badge_remove_returns_index(badge_index):
-    assert admin.badge_remove(Mock(params={'remove': 'test_uri'})) == (
-        badge_index.return_value)
+@blocklist_remove_fixtures
+def test_blocklist_remove_returns_index(blocklist_index):
+    assert admin.blocklist_remove(Mock(params={'remove': 'test_uri'})) == (
+        blocklist_index.return_value)
 
 
 @pytest.fixture
@@ -542,8 +543,8 @@ def models(request):
 
 
 @pytest.fixture
-def badge_index(request):
-    patcher = patch('h.admin.badge_index', autospec=True)
+def blocklist_index(request):
+    patcher = patch('h.admin.blocklist_index', autospec=True)
     request.addfinalizer(patcher.stop)
     return patcher.start()
 
