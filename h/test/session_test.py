91ea4510a37b5ba644934ced9cc4890b87427293
Robert Knight <robertknight@gmail.com>
2015-10-13 15:08:55 +0100

    Sort groups server-side by name, then by ID
     * Sort groups on the server in a useful order on the server
       rather than the client.
    
       This reduces the need for logic which is otherwise likely
       to be duplicated across clients.
    
     * Sort groups by name, _then by ID_. This ensures that if
       multiple groups have the same name they are sorted in
       a consistent order in the client.

---
+++ h/test/session_test.py
@@ -0,0 +1,29 @@
+import unittest
+
+from mock import Mock
+from mock import patch
+
+from h.session import model
+
+
+class FakeGroup():
+    def __init__(self, hashid, name):
+        self.hashid = hashid
+        self.name = name
+        self.slug = hashid
+
+
+@patch('h.models.User')
+def test_sorts_groups(User):
+    fake_user = Mock()
+    fake_user.groups = [
+        FakeGroup('c', 'Group A'),
+        FakeGroup('b', 'Group B'),
+        FakeGroup('a', 'Group B'),
+    ]
+    User.get_by_userid.return_value = fake_user
+    request = Mock()
+    session_model = model(request)
+
+    ids = [group['id'] for group in session_model['groups']]
+    assert ids == ['__world__', 'c', 'a', 'b']
