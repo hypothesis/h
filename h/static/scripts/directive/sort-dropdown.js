665bb7dc74e4b293b5a3cc8a1df71880cf6e837a
Robert Knight <robertknight@gmail.com>
2015-10-20 16:23:46 +0100

    Add API comments for dropdown menu directive
    T-91

--- h/static/scripts/directive/sort-dropdown.js
+++ h/static/scripts/directive/sort-dropdown.js
@@ -2,9 +2,15 @@ module.exports = function () {
   return {
     restrict: 'E',
     scope: {
+      /** The name of the currently selected sort criteria. */
       sortBy: '=',
+      /** A list of choices that the user can opt to sort by. */
       sortOptions: '=',
+      /** If true, the menu uses just an icon, otherwise
+       * it displays 'Sorted by {{sortBy}}'
+       */
       showAsIcon: '=',
+      /** Called when the user changes the current sort criteria. */
       onChangeSortBy: '&',
     },
     templateUrl: 'sort_dropdown.html',
