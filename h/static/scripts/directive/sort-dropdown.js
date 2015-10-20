7a58b9de6b19ff978980a2a2cc43b8ca26e11d8a
Robert Knight <robertknight@gmail.com>
2015-10-20 13:34:39 +0100

    Move sort control into top bar
    When the 'Groups' feature is enabled, move the sort
    menu into a dropdrown triggered via a button
    in the top bar.
    
    T-91

--- h/static/scripts/directive/sort-dropdown.js
+++ h/static/scripts/directive/sort-dropdown.js
@@ -4,6 +4,7 @@ module.exports = function () {
     scope: {
       sortBy: '=',
       sortOptions: '=',
+      showAsIcon: '=',
       onChangeSortBy: '&',
     },
     templateUrl: 'sort_dropdown.html',
