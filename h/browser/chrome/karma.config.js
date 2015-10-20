9cb69953748a62e8a4ef83df25231f671b453cf3
Robert Knight <robertknight@gmail.com>
2015-10-20 10:33:59 +0100

    Add target to build extension tests in watch mode
    For more efficient development add a target which starts
    the Chrome extension tests in auto-watch mode.

--- h/browser/chrome/karma.config.js
+++ h/browser/chrome/karma.config.js
@@ -77,7 +77,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // start these browsers
@@ -87,6 +87,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
+    singleRun: false
   });
 };
