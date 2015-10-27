70f3c8adad7bf0030b29b4d97e7d8c524a28db16
Robert Knight <robertknight@gmail.com>
2015-10-27 15:55:52 +0000

    Wait for feature flag data before loading sidebar view
    Feature flags can affect the initial search query when
    the app loads. eg. If the groups feature is enabled then
    we will by default load annotations for the most recently
    used group, otherwise we'll load public annotations.
    
    Therefore, wait for the feature flags to be retrieved
    before loading the view.
    
    The feature flags endpoint is very fast so the behavior
    was correct most of the time previously but occassionally
    the app would load and fetch Public annotations before
    the feature data was available which would have resulted
    in fetching group annotations.
    
    At the same time, this clears the feature flag data
    when the current user changes and fixes an issue where
    switching from a non-groups enabled account to a groups-enabled
    account did not immediately update the UI.
    
    Fixes #2675

--- h/static/scripts/test/features-test.js
+++ h/static/scripts/test/features-test.js
@@ -41,71 +41,86 @@ describe('h:features', function () {
     return handler;
   }
 
-  it('fetch should retrieve features data', function () {
-    defaultHandler();
-    features.fetch();
-    $httpBackend.flush();
+  describe('fetch', function() {
+    it('should retrieve features data', function () {
+      defaultHandler();
+      features.fetch();
+      $httpBackend.flush();
+      assert.equal(features.flagEnabled('foo'), true);
+    });
+
+    it('should return a promise', function () {
+      defaultHandler();
+      features.fetch().then(function () {
+        assert.equal(features.flagEnabled('foo'), true);
+      });
+      $httpBackend.flush();
+    });
+
+    it('should not explode for errors fetching features data', function () {
+      defaultHandler().respond(500, "ASPLODE!");
+      var handler = sinon.stub();
+      features.fetch().then(handler);
+      $httpBackend.flush();
+      assert.calledOnce(handler);
+    });
+
+    it('should only send one request at a time', function () {
+      defaultHandler();
+      features.fetch();
+      features.fetch();
+      $httpBackend.flush();
+    });
   });
 
-  it('fetch should not explode for errors fetching features data', function () {
-    defaultHandler().respond(500, "ASPLODE!");
-    features.fetch();
-    $httpBackend.flush();
-  });
+  describe('flagEnabled', function () {
+    it('should retrieve features data', function () {
+      defaultHandler();
+      features.flagEnabled('foo');
+      $httpBackend.flush();
+    });
 
-  it('fetch should only send one request at a time', function () {
-    defaultHandler();
-    features.fetch();
-    features.fetch();
-    $httpBackend.flush();
-  });
+    it('should return false initially', function () {
+      defaultHandler();
+      var result = features.flagEnabled('foo');
+      $httpBackend.flush();
 
-  it('flagEnabled should retrieve features data', function () {
-    defaultHandler();
-    features.flagEnabled('foo');
-    $httpBackend.flush();
-  });
+      assert.isFalse(result);
+    });
 
-  it('flagEnabled should return false initially', function () {
-    defaultHandler();
-    var result = features.flagEnabled('foo');
-    $httpBackend.flush();
+    it('should return flag values when data is loaded', function () {
+      defaultHandler();
+      features.fetch();
+      $httpBackend.flush();
 
-    assert.isFalse(result);
-  });
+      var foo = features.flagEnabled('foo');
+      assert.isTrue(foo);
 
-  it('flagEnabled should return flag values when data is loaded', function () {
-    defaultHandler();
-    features.fetch();
-    $httpBackend.flush();
+      var bar = features.flagEnabled('bar');
+      assert.isFalse(bar);
+    });
 
-    var foo = features.flagEnabled('foo');
-    assert.isTrue(foo);
+    it('should return false for unknown flags', function () {
+      defaultHandler();
+      features.fetch();
+      $httpBackend.flush();
 
-    var bar = features.flagEnabled('bar');
-    assert.isFalse(bar);
-  });
+      var baz = features.flagEnabled('baz');
+      assert.isFalse(baz);
+    });
 
-  it('flagEnabled should return false for unknown flags', function () {
-    defaultHandler();
-    features.fetch();
-    $httpBackend.flush();
+    it('should trigger a new fetch after cache expiry', function () {
+      var clock = sandbox.useFakeTimers();
 
-    var baz = features.flagEnabled('baz');
-    assert.isFalse(baz);
-  });
+      defaultHandler();
+      features.flagEnabled('foo');
+      $httpBackend.flush();
 
-  it('flagEnabled should trigger a new fetch after cache expiry', function () {
-    var clock = sandbox.useFakeTimers();
+      clock.tick(301 * 1000);
 
-    defaultHandler();
-    features.flagEnabled('foo');
-    $httpBackend.flush();
-
-    clock.tick(301 * 1000);
-
-    defaultHandler();
-    features.flagEnabled('foo');
-    $httpBackend.flush();
+      defaultHandler();
+      features.flagEnabled('foo');
+      $httpBackend.flush();
+    });
   });
 });
