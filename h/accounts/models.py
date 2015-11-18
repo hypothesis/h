d03fd3628d990ec1167078f220bff0d8ee8d77e4
Sean Hammond <git@seanh.cc>
2015-11-18 15:59:54 +0100

    2to3: Use h._compat not pyramid.compat

--- h/accounts/models.py
+++ h/accounts/models.py
@@ -4,13 +4,13 @@ import random
 import string
 
 import cryptacular.bcrypt
-from pyramid.compat import text_type
 import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.sql import expression
 
 from h.db import Base
+from h._compat import text_type
 
 CRYPT = cryptacular.bcrypt.BCRYPTPasswordManager()
 USERNAME_MIN_LENGTH = 3
