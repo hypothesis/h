b9cd58da0258c1e4afd39b2f7a4df3c389aa3397
Nick Stenning <nick@whiteink.com>
2015-11-20 16:18:36 +0100

    Use the database session directly
    One nice side effect of faster database tests through the use of
    PostgreSQL SAVEPOINTS is that we can simply prepare every test to use
    the database with no noticeable slowdown.
    
    We no longer need to explicitly use the `db_session` test fixture to
    access the database session.

--- h/badge/test/models_test.py
+++ h/badge/test/models_test.py
@@ -1,60 +1,60 @@
 # -*- coding: utf-8 -*-
 import pytest
-import mock
 
+from h import db
 from h.badge import models
 
 
-def test_cannot_add_same_uri_twice(db_session):
-    db_session.add(models.Blocklist(uri="test_uri"))
-    db_session.flush()
+def test_cannot_add_same_uri_twice():
+    db.Session.add(models.Blocklist(uri="test_uri"))
+    db.Session.flush()
 
     with pytest.raises(ValueError):
         models.Blocklist(uri="test_uri")
 
 
-def test_get_by_uri_returns_model(db_session):
+def test_get_by_uri_returns_model():
     model = models.Blocklist(uri="test_uri")
-    db_session.add(model)
-    db_session.flush()
+    db.Session.add(model)
+    db.Session.flush()
 
     assert models.Blocklist.get_by_uri("test_uri") == model
 
 
-def test_get_by_uri_returns_None_if_no_match(db_session):
+def test_get_by_uri_returns_None_if_no_match():
     model = models.Blocklist(uri="test_uri")
-    db_session.add(model)
-    db_session.flush()
+    db.Session.add(model)
+    db.Session.flush()
 
     assert models.Blocklist.get_by_uri("another_uri") is None
 
 
-def test_all(db_session):
+def test_all():
     uris = [
         models.Blocklist(uri="first"),
         models.Blocklist(uri="second"),
         models.Blocklist(uri="third")
     ]
     for uri in uris:
-        db_session.add(uri)
-    db_session.flush()
+        db.Session.add(uri)
+    db.Session.flush()
 
     assert models.Blocklist.all() == uris
 
 
-def test_is_blocked(db_session):
-    db_session.add(models.Blocklist(uri=u"http://example.com"))
-    db_session.add(models.Blocklist(uri=u"http://example.com/bar"))
-    db_session.flush()
+def test_is_blocked():
+    db.Session.add(models.Blocklist(uri=u"http://example.com"))
+    db.Session.add(models.Blocklist(uri=u"http://example.com/bar"))
+    db.Session.flush()
 
     assert models.Blocklist.is_blocked(u"http://example.com")
     assert models.Blocklist.is_blocked(u"http://example.com/bar")
     assert not models.Blocklist.is_blocked(u"http://example.com/foo")
 
 
-def test_is_blocked_with_wildcards(db_session):
-    db_session.add(models.Blocklist(uri="%//example.com%"))
-    db_session.flush()
+def test_is_blocked_with_wildcards():
+    db.Session.add(models.Blocklist(uri="%//example.com%"))
+    db.Session.flush()
 
     assert models.Blocklist.is_blocked("http://example.com/")
     assert models.Blocklist.is_blocked("http://example.com/bar")
--- h/groups/test/models_test.py
+++ h/groups/test/models_test.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 import pytest
 
+from h import db
 from h.groups import models
 from h.test import factories
 
 
-def test_init(db_session):
+def test_init():
     name = "My Hypothesis Group"
     user = factories.User()
 
     group = models.Group(name=name, creator=user)
-    db_session.add(group)
-    db_session.flush()
+    db.Session.add(group)
+    db.Session.flush()
 
     assert group.id
     assert group.name == name
@@ -22,58 +23,58 @@ def test_init(db_session):
     assert group.members == [user]
 
 
-def test_with_short_name(db_session):
+def test_with_short_name():
     """Should raise ValueError if name shorter than 4 characters."""
     with pytest.raises(ValueError):
         models.Group(name="abc", creator=factories.User())
 
 
-def test_with_long_name(db_session):
+def test_with_long_name():
     """Should raise ValueError if name longer than 25 characters."""
     with pytest.raises(ValueError):
         models.Group(name="abcdefghijklmnopqrstuvwxyz",
                      creator=factories.User())
 
 
-def test_slug(db_session):
+def test_slug():
     name = "My Hypothesis Group"
     user = factories.User()
 
     group = models.Group(name=name, creator=user)
-    db_session.add(group)
-    db_session.flush()
+    db.Session.add(group)
+    db.Session.flush()
 
     assert group.slug == "my-hypothesis-group"
 
 
-def test_repr(db_session):
+def test_repr():
     name = "My Hypothesis Group"
     user = factories.User()
 
     group = models.Group(name=name, creator=user)
-    db_session.add(group)
-    db_session.flush()
+    db.Session.add(group)
+    db.Session.flush()
 
     assert repr(group) == "<Group: my-hypothesis-group>"
 
 
-def test_get_by_id_when_id_does_exist(db_session):
+def test_get_by_id_when_id_does_exist():
     name = "My Hypothesis Group"
     user = factories.User()
 
     group = models.Group(name=name, creator=user)
-    db_session.add(group)
-    db_session.flush()
+    db.Session.add(group)
+    db.Session.flush()
 
     assert models.Group.get_by_id(group.id) == group
 
 
-def test_get_by_id_when_id_does_not_exist(db_session):
+def test_get_by_id_when_id_does_not_exist():
     name = "My Hypothesis Group"
     user = factories.User()
 
     group = models.Group(name=name, creator=user)
-    db_session.add(group)
-    db_session.flush()
+    db.Session.add(group)
+    db.Session.flush()
 
     assert models.Group.get_by_id(23) is None
