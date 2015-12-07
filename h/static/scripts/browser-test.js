1f190e1e48a025ae8b3f39dd65b221d4d5c38d61
Robert Knight <robertknight@gmail.com>
2015-12-07 07:52:37 +0000

    Re-implement the logic for showing the browser-appropriate installer
     * Re-implement logic for showing browser-appropriate installation
       option
    
     * Tweak the border styles on the proxy link form

--- h/static/scripts/browser-test.js
+++ h/static/scripts/browser-test.js
@@ -1,14 +1,23 @@
 // TODO - Make this a CommonJS module and move it into
 // the site JS?
-if (window.chrome !== undefined) {
-  var elements = document.getElementsByClassName(
-      "unhide-in-chrome");
-  for (var i=0; i<elements.length; i++) {
-    elements[i].classList.remove("hidden");
-  }
-  elements = document.getElementsByClassName(
-      "hide-in-chrome");
-  for (var i=0; i<elements.length; i++) {
-    elements[i].classList.add("hidden");
-  }
+var env = {
+  browserIsChrome: typeof window.chrome !== 'undefined',
+  browserIsOther: typeof window.chrome === 'undefined',
+};
+
+function hypenate(key) {
+  // 'camelCase' -> 'camel-case'
+  return key.replace(/[a-z]+/g, '$&-').toLowerCase().slice(0, -1);
 }
+
+Object.keys(env).forEach(function (key) {
+  var selector = '.js-' + hypenate(key);
+  var elements = document.querySelectorAll(selector);
+  for (var i = 0; i < elements.length; i++) {
+    if (env[key]) {
+      elements[i].classList.remove('is-hidden');
+    } else {
+      elements[i].classList.add('is-hidden');
+    }
+  }
+});
