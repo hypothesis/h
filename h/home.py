2f5410a7aa5bcafe55bfaeac2b063c670b0b051b
Robert Knight <robertknight@gmail.com>
2015-12-07 07:52:42 +0000

    Add missing RSS link and avoid duplicating Chrome extension link
    Expose the RSS link and Chrome extension links in the template.

--- h/home.py
+++ h/home.py
@@ -6,17 +6,22 @@ from pyramid import view
                   request_method='GET',
                   renderer='h:templates/home.html.jinja2')
 def index(context, request):
+    config = {
+        "chrome_ext_link": "https://chrome.google.com/webstore/detail/bjfhmglciegochdpefhhlphglcehbmek",
+        "rss_feed_url": "https://hypothes.is/feed",
+    }
+
     if request.authenticated_user:
         username = request.authenticated_user.username
         user_profile_link = (
             request.route_url("stream")
             + "?q=user:{username}".format(username=username))
-        return {
+        config.update({
             "username": username,
-            "user_profile_link": user_profile_link
-        }
-    else:
-        return {}
+            "user_profile_link": user_profile_link,
+        })
+
+    return config
 
 
 def includeme(config):
