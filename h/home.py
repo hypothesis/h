b7d47af6cee0e7f1dd8a9bdf0037a7ec86c5f9fd
Nick Stenning <nick@whiteink.com>
2015-12-11 18:42:42 +0100

    Simplify h.home and use a real feature flag for homepage

--- h/home.py
+++ h/home.py
@@ -2,83 +2,26 @@
 from pyramid import view
 
 
-def user_info(request):
-    if request.authenticated_user:
-        username = request.authenticated_user.username
-        user_profile_link = (
-            request.route_url("stream")
-            + "?q=user:{username}".format(username=username))
-        return {
-            "username": username,
-            "user_profile_link": user_profile_link,
-        }
-    else:
-        return {}
-
-
 @view.view_config(route_name='index',
                   request_method='GET',
                   renderer='h:templates/old-home.html.jinja2')
 def index(context, request):
-    return user_info(request)
+    context = {}
 
+    if request.authenticated_user:
+        username = request.authenticated_user.username
+        context['username'] = username
+        context['user_profile_link'] = (
+            request.route_url("stream") +
+            "?q=user:{username}".format(username=username)
+        )
 
-def trim_snippet(snippet):
-    MAX_SNIPPET_LENGTH = 190
-    return snippet[0:MAX_SNIPPET_LENGTH] + u"\u2026"
+    if request.feature('new_homepage'):
+        request.override_renderer = 'h:templates/home.html.jinja2'
 
-
-# recent blog posts on the H blog from 7/12/15 for the initial
-# implementation of the new homepage design. This will be followed
-# up by a proper system for fetching posts and caching them.
-RECENT_NEWS_ITEMS = [{
-    "title": "A Coalition of over 40 Scholarly Publishers",
-    "link": "https://hypothes.is/blog/a-coalition-of-over-40-scholarly-publishers/",
-    "date": "December 1, 2015",
-    "snippet": trim_snippet("""
-Today we're announcing a coalition of over 40 scholarly publishers, platforms,
-libraries and technology partners that share the goal of building an open
-conversation layer over all knowledge. Over the next several years this coalition
-will be working together to define, design and implement a common framework for
-scholarly collaboration from peer-review through post-publication discussion
-""")
-},{
-    "title": "Undergrad Shannon Griffiths on Using Hypothesis in the Classroom",
-    "link": "https://hypothes.is/blog/undergrad-shannon-griffiths-on-using-hypothesis-in-the-classroom/",
-    "date": "November 30, 2015",
-    "snippet": trim_snippet("""
-This blog was written and published by Shannon Griffiths, an English major at
-Plymouth State University. Her professor, Robin DeRosa, is using Hypothesis in
-several of her classes this term. Check out her Open Anthology of
-Earlier American Literature, hosted on PressBooks and
-annotated using Hypothesis by her undergraduates.""")
-},{
-    "title": "Hypothes.is at Society for Neuroscience",
-    "link": "https://hypothes.is/blog/hypothes-is-at-society-for-neuroscience/",
-    "date": "November 19, 2015",
-    "snippet": trim_snippet("""
-At long last, I'm able to sit down and summarize my thoughts and experiences on
-Hypothes.is at the Society for Neuroscience meeting in Chicago, Oct 17-21st.
-First of all, a hearty, gigantic thank you to my colleagues at the
-Neuroscience Information Framework, a Hypothes.is partner
-""")
-}]
-
-
-@view.view_config(route_name='new_index',
-                  request_method='GET',
-                  renderer='h:templates/home.html.jinja2')
-def new_index(context, request):
-    config = {
-        "chrome_ext_link": "https://chrome.google.com/webstore/detail/bjfhmglciegochdpefhhlphglcehbmek",
-        "rss_feed_url": "https://hypothes.is/feed",
-        "news_items": RECENT_NEWS_ITEMS,
-    }
-    config.update(user_info(request))
-    return config
+    return context
 
 
 def includeme(config):
     config.add_route('index', '/')
-    config.add_route('new_index', '/new-homepage')
     config.scan(__name__)
