c2ebb29dfc44a46daef5738ec31a3b59c65b6860
Alice Wyan <alice@finitud.org>
2015-12-22 16:49:21 +0000

    Add migration to make user.password_updated column non-nullable
    This should be run after the rest of the code in this branch has been
    deployed to production, otherwise the production environment will break.

---
+++ h/migrations/versions/43e7c4ed2fd7_make_user_password_updated_non_nullable.py
@@ -0,0 +1,22 @@
+"""make user password_updated non-nullable
+
+Revision ID: 43e7c4ed2fd7
+Revises: 530268a1937c
+Create Date: 2015-12-22 15:48:14.867487
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '43e7c4ed2fd7'
+down_revision = '530268a1937c'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.alter_column('user', 'password_updated', nullable=False)
+
+
+def downgrade():
+    op.alter_column('user', 'password_updated', nullable=True)
