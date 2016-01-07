d8246dbb0229ea07d48111df05a172bed9f60593
Sean Hammond <git@seanh.cc>
2016-01-07 15:13:18 +0000

    Fix test code styles
    - Remove an unused import
    - Use new-style classes
    - Fix import styles to follow Google Python Style Guide
    - Fix test names to include the name of the function being tested
    - Fix patches to use autospec=True, this catches some integration errors that
      wouldn't be caught otherwise.
    - Insert blank lines between the three stages of the test function
      recipe: setup, call the method under test, assertions.

--- h/test/session_test.py
+++ h/test/session_test.py
@@ -1,43 +1,42 @@
-import unittest
-
 import pytest
-from mock import Mock
-from mock import patch
+import mock
 
-from h.session import model
+from h import session
 
 
-class FakeGroup():
+class FakeGroup(object):
     def __init__(self, pubid, name):
         self.pubid = pubid
         self.name = name
         self.slug = pubid
 
 
-@patch('h.models.User')
-def test_sorts_groups(User):
-    fake_user = Mock()
+@mock.patch('h.models.User', autospec=True)
+def test_model_sorts_groups(User):
+    fake_user = mock.Mock()
     fake_user.groups = [
         FakeGroup('c', 'Group A'),
         FakeGroup('b', 'Group B'),
         FakeGroup('a', 'Group B'),
     ]
-    request = Mock(authenticated_user=fake_user)
-    session_model = model(request)
+    request = mock.Mock(authenticated_user=fake_user)
+    session_model = session.model(request)
 
     ids = [group['id'] for group in session_model['groups']]
+
     assert ids == ['__world__', 'c', 'a', 'b']
 
 
-@patch('h.session.features')
-def test_includes_features(features, fake_user):
+@mock.patch('h.session.features', autospec=True)
+def test_model_includes_features(features, fake_user):
     feature_dict = {
         'feature_one': True,
         'feature_two': False,
     }
-    features.all = Mock(return_value=feature_dict)
-    request = Mock(authenticated_user=fake_user)
-    assert model(request)['features'] == feature_dict
+    features.all = mock.Mock(return_value=feature_dict)
+    request = mock.Mock(authenticated_user=fake_user)
+
+    assert session.model(request)['features'] == feature_dict
 
 
 @pytest.mark.parametrize(
@@ -76,6 +75,6 @@ def test_model_show_sidebar_tutorial(
 
 @pytest.fixture
 def fake_user():
-    fake_user = Mock()
+    fake_user = mock.Mock()
     fake_user.groups = []
     return fake_user
