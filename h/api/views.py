hypothesis/h

commit f810d98a0ad2f879a41aa6b09ad3ec762f6f5619

Rename h.api.logic to h.api.storage and update views
This commit moves the core persistence functions that are currently in
`h.api.logic` to `h.api.storage` -- a more descriptively named module --
and updates the view functions accordingly.

This commit also makes the following changes:

- AnnotationEvents are no longer fired for annotation 'read' events.
  Nothing uses these.
- `h.api.resources` has been simplified. Annotation resource objects are
  no longer created when the underlying annotation does not exist.

h/api/views.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 
-"""HTTP/REST API for interacting with the annotation store."""
+"""
+HTTP/REST API for storage and retrieval of annotation data.
+
+This module contains the views which implement our REST API, mounted by default
+at ``/api``. Currently, the endpoints are limited to:
+
+- basic CRUD (create, read, update, delete) operations on annotations
+- annotation search
+- a handful of authentication related endpoints
+
+It is worth noting up front that in general, authorization for requests made to
+each endpoint is handled outside of the body of the view functions. In
+particular, requests to the CRUD API endpoints are protected by the Pyramid
+authorization system. You can find the mapping between annotation "permissions"
+objects and Pyramid ACLs in :mod:`h.api.resources`.
+"""
 
 import logging
 
@@ -12,8 +27,8 @@ from pyramid.view import view_config
 from h.api import cors
 from h.api.events import AnnotationEvent
 from h.api import search as search_lib
-from h.api import logic
 from h.api import schemas
+from h.api import storage
 from h.api.resources import Annotation
 from h.api.resources import Annotations
 from h.api.resources import Root
@@ -173,13 +188,11 @@ def annotations_index(request):
 
 @api_config(context=Annotations, request_method='POST', permission='create')
 def create(request):
-    """Read the POSTed JSON-encoded annotation and persist it."""
+    """Create an annotation from the POST payload."""
     schema = schemas.CreateAnnotationSchema(request)
     appstruct = schema.validate(_json_payload(request))
+    annotation = storage.create_annotation(appstruct)
 
-    annotation = logic.create_annotation(appstruct)
-
-    # Notify any subscribers
     _publish_annotation_event(request, annotation, 'create')
     return annotation
 
@@ -187,44 +200,27 @@ def create(request):
 @api_config(context=Annotation, request_method='GET', permission='read')
 def read(context, request):
     """Return the annotation (simply how it was stored in the database)."""
-    annotation = context.model
-
-    # Notify any subscribers
-    _publish_annotation_event(request, annotation, 'read')
-
-    return annotation
+    return context.model
 
 
 @api_config(context=Annotation, request_method='PUT', permission='update')
 def update(context, request):
-    """Update the fields we received and store the updated version."""
-    annotation = context.model
-    schema = schemas.UpdateAnnotationSchema(request, annotation)
+    """Update the specified annotation with data from the PUT payload."""
+    schema = schemas.UpdateAnnotationSchema(request, annotation=context.model)
     appstruct = schema.validate(_json_payload(request))
+    annotation = storage.update_annotation(context.id, appstruct)
 
-    # Update and store the annotation
-    logic.update_annotation(annotation, appstruct)
-
-    # Notify any subscribers
     _publish_annotation_event(request, annotation, 'update')
     return annotation
 
 
 @api_config(context=Annotation, request_method='DELETE', permission='delete')
 def delete(context, request):
-    """Delete the annotation permanently."""
-    annotation = context.model
+    """Delete the specified annotation."""
+    storage.delete_annotation(context.id)
 
-    logic.delete_annotation(annotation)
-
-    # Notify any subscribers
-    _publish_annotation_event(request, annotation, 'delete')
-
-    # Return a confirmation
-    return {
-        'id': annotation['id'],
-        'deleted': True,
-    }
+    _publish_annotation_event(request, {'id': context.id}, 'delete')
+    return {'id': context.id, 'deleted': True}
 
 
 def _json_payload(request):

h/api/test/views_test.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-"""Defines unit tests for h.api.views."""
 
 import mock
 import pytest
@@ -7,17 +6,6 @@ import pytest
 from pyramid import testing
 
 from h.api import views
-
-
-def _mock_annotation(**kwargs):
-    """Return a mock h.api.resources.Annotation object."""
-    annotation = mock.MagicMock()
-    annotation.model = model = mock.MagicMock()
-    model.__getitem__.side_effect = kwargs.__getitem__
-    model.__setitem__.side_effect = kwargs.__setitem__
-    model.get.side_effect = kwargs.get
-    model.__contains__.side_effect = kwargs.__contains__
-    return annotation
 
 
 def test_error_not_found_sets_status_code():
@@ -119,7 +107,6 @@ def test_access_token_returns_create_token_response():
     assert response_data == request.create_token_response.return_value
 
 
-# The fixtures required to mock all of annotator_token()'s dependencies.
 annotator_token_fixtures = pytest.mark.usefixtures('access_token')
 
 
@@ -166,9 +153,9 @@ def test_annotations_index_returns_search_results(search_lib):
     assert result == search_lib.search.return_value
 
 
-# The fixtures required to mock all of create()'s dependencies.
-create_fixtures = pytest.mark.usefixtures(
-    'logic', 'AnnotationEvent', 'schemas')
+create_fixtures = pytest.mark.usefixtures('AnnotationEvent',
+                                          'schemas',
+                                          'storage')
 
 
 @create_fixtures
@@ -184,15 +171,15 @@ def test_create_raises_if_json_parsing_fails():
 
 
 @create_fixtures
-def test_create_calls_create_annotation(logic, schemas):
-    """It should call logic.create_annotation() appropriately."""
+def test_create_calls_create_annotation(storage, schemas):
+    """It should call storage.create_annotation() appropriately."""
     request = mock.Mock()
     schema = schemas.CreateAnnotationSchema.return_value
     schema.validate.return_value = {'foo': 123}
 
     views.create(request)
 
-    logic.create_annotation.assert_called_once_with({'foo': 123})
+    storage.create_annotation.assert_called_once_with({'foo': 123})
 
 
 @create_fixtures
@@ -206,43 +193,38 @@ def test_create_calls_validator(schemas):
 
 
 @create_fixtures
-def test_create_inits_AnnotationEvent_once(AnnotationEvent):
-    views.create(mock.Mock())
-
-    assert AnnotationEvent.call_count == 1
-
-
-@create_fixtures
-def test_create_event(AnnotationEvent, logic):
+def test_create_event(AnnotationEvent, storage):
     request = mock.Mock()
-    annotation = logic.create_annotation.return_value
+    annotation = storage.create_annotation.return_value
     event = AnnotationEvent.return_value
 
     views.create(request)
 
-    AnnotationEvent.assert_called_once_with == (request, annotation, 'create')
+    AnnotationEvent.assert_called_once_with(request, annotation, 'create')
     request.registry.notify.assert_called_once_with(event)
 
 
 @create_fixtures
-def test_create_returns_annotation(logic):
+def test_create_returns_annotation(storage):
     request = mock.Mock()
+
     result = views.create(request)
 
-    assert result == logic.create_annotation.return_value
+    assert result == storage.create_annotation.return_value
 
 
 def test_read_returns_annotation():
     context = mock.Mock()
     request = mock.Mock()
+
     result = views.read(context, request)
 
     assert result == context.model
 
 
-# The fixtures required to mock all of update()'s dependencies.
-update_fixtures = pytest.mark.usefixtures(
-    'logic', 'Annotation', 'schemas')
+update_fixtures = pytest.mark.usefixtures('AnnotationEvent',
+                                          'schemas',
+                                          'storage')
 
 
 @update_fixtures
@@ -259,16 +241,17 @@ def test_update_raises_if_json_parsing_fails():
 
 @update_fixtures
 def test_update_calls_validator(schemas):
+    context = mock.Mock()
     request = mock.Mock()
     schema = schemas.UpdateAnnotationSchema.return_value
 
-    views.update(mock.Mock(), request)
+    views.update(context, request)
 
     schema.validate.assert_called_once_with(request.json_body)
 
 
 @update_fixtures
-def test_update_calls_update_annotation(logic, schemas):
+def test_update_calls_update_annotation(storage, schemas):
     context = mock.Mock()
     request = mock.Mock()
     schema = schemas.UpdateAnnotationSchema.return_value
@@ -276,117 +259,72 @@ def test_update_calls_update_annotation(logic, schemas):
 
     views.update(context, request)
 
-    logic.update_annotation.assert_called_once_with(context.model,
-                                                    {'foo': 123})
+    storage.update_annotation.assert_called_once_with(context.id, {'foo': 123})
 
 
 @update_fixtures
-def test_update_event(AnnotationEvent):
+def test_update_returns_annotation(storage):
+    context = mock.Mock()
     request = mock.Mock()
-    annotation = mock.Mock()
+
+    result = views.update(context, request)
+
+    assert result == storage.update_annotation.return_value
+
+
+@update_fixtures
+def test_update_event(AnnotationEvent, storage):
+    context = mock.Mock()
+    request = mock.Mock()
     event = AnnotationEvent.return_value
-    views.update(annotation, request)
-    AnnotationEvent.assert_called_once_with(request, annotation.model,
-                                            'update')
+    annotation = storage.update_annotation.return_value
+
+    views.update(context, request)
+
+    AnnotationEvent.assert_called_once_with(request, annotation, 'update')
     request.registry.notify.assert_called_once_with(event)
 
 
-@update_fixtures
-def test_update_returns_annotation():
-    context = mock.Mock()
-    request = mock.Mock()
-    result = views.update(context, request)
-
-    assert result == context.model
-
-
-# The fixtures required to mock all of delete()'s dependencies.
-delete_fixtures = pytest.mark.usefixtures('AnnotationEvent', 'logic')
+delete_fixtures = pytest.mark.usefixtures('AnnotationEvent', 'storage')
 
 
 @delete_fixtures
-def test_delete_calls_delete_annotation(logic):
-    annotation = mock.MagicMock()
+def test_delete_calls_delete_annotation(storage):
+    context = mock.Mock()
     request = mock.Mock()
 
-    views.delete(annotation, request)
+    views.delete(context, request)
 
-    logic.delete_annotation.assert_called_once_with(annotation.model)
+    storage.delete_annotation.assert_called_once_with(context.id)
 
 
 @delete_fixtures
 def test_delete_event(AnnotationEvent):
-    annotation = _mock_annotation(id='foo', group='test-group')
-    request = mock.Mock(effective_principals=['group:test-group'])
+    context = mock.Mock()
+    request = mock.Mock()
     event = AnnotationEvent.return_value
 
-    views.delete(annotation, request)
+    views.delete(context, request)
 
-    AnnotationEvent.assert_called_once_with(request, annotation.model, 'delete')
+    AnnotationEvent.assert_called_once_with(request,
+                                            {'id': context.id},
+                                            'delete')
     request.registry.notify.assert_called_once_with(event)
 
 
 @delete_fixtures
-def test_delete_returns_id():
-    annotation = _mock_annotation(id='foo', group='test-group')
+def test_delete_returns_object():
+    context = mock.Mock()
+    request = mock.Mock()
 
-    response_data = views.delete(
-        annotation, mock.Mock(effective_principals=['group:test-group']))
+    result = views.delete(context, request)
 
-    assert response_data['id'] == annotation.model['id']
-
-
-@delete_fixtures
-def test_delete_returns_deleted():
-    response_data = views.delete(
-        _mock_annotation(id='foo', group='test-group'),
-        mock.Mock(effective_principals=['group:test-group']))
-
-    assert response_data['deleted'] is True
-
-
-@delete_fixtures
-def test_delete_does_not_crash_if_annotation_has_no_group():
-    annotation = _mock_annotation(id='foo')
-    assert 'group' not in annotation
-
-    views.delete(
-        annotation,
-        mock.Mock(effective_principals=['group:test-group']))
-
-
-@pytest.fixture
-def search_lib(request):
-    patcher = mock.patch('h.api.views.search_lib', autospec=True)
-    request.addfinalizer(patcher.stop)
-    return patcher.start()
+    assert result == {'id': context.id, 'deleted': True}
 
 
 @pytest.fixture
 def AnnotationEvent(request):
     patcher = mock.patch('h.api.views.AnnotationEvent', autospec=True)
-    request.addfinalizer(patcher.stop)
-    return patcher.start()
-
-
-@pytest.fixture
-def Annotation(request):
-    patcher = mock.patch('h.api.views.Annotation', autospec=True)
-    request.addfinalizer(patcher.stop)
-    return patcher.start()
-
-
-@pytest.fixture
-def logic(request):
-    patcher = mock.patch('h.api.views.logic', autospec=True)
-    request.addfinalizer(patcher.stop)
-    return patcher.start()
-
-
-@pytest.fixture
-def _publish_annotation_event(request):
-    patcher = mock.patch('h.api.views._publish_annotation_event',
-                         autospec=True)
     request.addfinalizer(patcher.stop)
     return patcher.start()
 
@@ -401,5 +339,27 @@ def access_token(request):
 @pytest.fixture
 def schemas(request):
     patcher = mock.patch('h.api.views.schemas', autospec=True)
+    request.addfinalizer(patcher.stop)
+    return patcher.start()
+
+
+@pytest.fixture
+def search_lib(request):
+    patcher = mock.patch('h.api.views.search_lib', autospec=True)
+    request.addfinalizer(patcher.stop)
+    return patcher.start()
+
+
+@pytest.fixture
+def storage(request):
+    patcher = mock.patch('h.api.views.storage', autospec=True)
+    request.addfinalizer(patcher.stop)
+    return patcher.start()
+
+
+@pytest.fixture
+def _publish_annotation_event(request):
+    patcher = mock.patch('h.api.views._publish_annotation_event',
+                         autospec=True)
     request.addfinalizer(patcher.stop)
     return patcher.start()

h/api/logic.py
@@ -1,31 +1,0 @@
-# -*- coding: utf-8 -*-
-import logging
-
-from h import i18n
-
-from h.api.models import Annotation
-from h.api import search as search_lib
-
-
-_ = i18n.TranslationString
-log = logging.getLogger(__name__)
-
-
-def create_annotation(fields):
-    """Create and store an annotation."""
-    annotation = Annotation(fields)
-    search_lib.prepare(annotation)
-    annotation.save()
-
-    return annotation
-
-
-def update_annotation(annotation, fields):
-    """Update the given annotation with the given new fields."""
-    annotation.update(fields)
-    search_lib.prepare(annotation)
-    annotation.save()
-
-
-def delete_annotation(annotation):
-    annotation.delete()

h/api/resources.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from pyramid.decorator import reify
 from pyramid.security import Allow, Deny
 from pyramid.security import Authenticated, Everyone
 
 from h.api import auth
-from h.api import models
+from h.api import storage
 
 
 class Resource(dict):
@@ -25,32 +24,19 @@ class Resource(dict):
         self[name] = obj
 
 
-class Collection(Resource):
-    """
-    A collection of other resources.
-    """
-
-    def __getitem__(self, key):
-        if key not in self and callable(self.factory):
-            self.add(key, self.factory(key))
-        return super(Collection, self).__getitem__(key)
-
-    def factory(self, key):
-        raise NotImplementedError
-
-
 class Annotation(Resource):
     """
     A Resource representing an annotation.
     """
+    def __init__(self, id, model):
+        self.id = id
+        self.model = model
 
     def __acl__(self):
         acl = []
 
-        model = self.model
-
         # Convert annotator-store roles to pyramid principals
-        for action, roles in model.get('permissions', {}).items():
+        for action, roles in self.model.get('permissions', {}).items():
             principals = auth.translate_annotation_principals(roles)
 
             for principal in principals:
@@ -60,16 +46,8 @@ class Annotation(Resource):
 
         return acl
 
-    @reify
-    def model(self):
-        if 'id' in self:
-            instance = models.Annotation.fetch(self['id'])
-            return instance or models.Annotation(id=self['id'])
-        else:
-            return models.Annotation()
 
-
-class Annotations(Collection):
+class Annotations(Resource):
     """
     A collection of Annotation resources.
     """
@@ -97,8 +75,20 @@ class Annotations(Collection):
 
         return [(Allow, 'group:' + group, 'create'), deny]
 
-    def factory(self, key):
-        return Annotation(id=key)
+    def __getitem__(self, id):
+        """
+        Fetch Annotation subresource, if it exists.
+        """
+        model = storage.fetch_annotation(id)
+        if model is None:
+            raise KeyError()
+        resource = self.factory(id, model)
+        resource.__parent__ = self
+        resource.__name__ = id
+        return resource
+
+    def factory(self, id, model):
+        return Annotation(id, model)
 
 
 class Root(Resource):

h/api/storage.py
@@ -1,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Annotation storage API.
+
+This module provides the core API with access to basic persistence functions
+for storing and retrieving annotations. Data passed to these functions is
+assumed to be validated.
+"""
+
+from h.api import models
+from h.api import search
+
+
+def fetch_annotation(id):
+    """
+    Fetch the annotation with the given id.
+
+    :param id: the annotation id
+    :type id: str
+
+    :returns: the annotation, if found, or None.
+    :rtype: dict, NoneType
+    """
+    return models.Annotation.fetch(id)
+
+
+def create_annotation(data):
+    """
+    Create an annotation from passed data.
+
+    :param data: a dictionary of annotation properties
+    :type data: dict
+
+    :returns: the created annotation
+    :rtype: dict
+    """
+    annotation = models.Annotation(data)
+
+    # FIXME: this should happen when indexing, not storing.
+    search.prepare(annotation)
+
+    annotation.save()
+    return annotation
+
+
+def update_annotation(id, data):
+    """
+    Update the annotation with the given id from passed data.
+
+    This executes a partial update of the annotation identified by `id` using
+    the passed data.
+
+    :param id: the annotation id
+    :type id: str
+    :param data: a dictionary of annotation properties
+    :type data: dict
+
+    :returns: the updated annotation
+    :rtype: dict
+    """
+    annotation = models.Annotation.fetch(id)
+    annotation.update(data)
+
+    # FIXME: this should happen when indexing, not storing.
+    search.prepare(annotation)
+
+    annotation.save()
+    return annotation
+
+
+def delete_annotation(id):
+    """
+    Delete the annotation with the given id.
+
+    :param id: the annotation id
+    :type id: str
+    """
+    annotation = models.Annotation.fetch(id)
+    annotation.delete()

h/api/test/logic_test.py
@@ -1,111 +1,0 @@
-# -*- coding: utf-8 -*-
-import pytest
-import mock
-
-from h.api import logic
-
-
-def _mock_annotation(**kwargs):
-    """Return a mock h.api.models.Annotation object."""
-    annotation = mock.MagicMock()
-    annotation.__getitem__.side_effect = kwargs.__getitem__
-    annotation.__setitem__.side_effect = kwargs.__setitem__
-    annotation.get.side_effect = kwargs.get
-    annotation.__contains__.side_effect = kwargs.__contains__
-    annotation.update.side_effect = kwargs.update
-    annotation.pop.side_effect = kwargs.pop
-    return annotation
-
-
-# The fixtures required to mock all of create_annotation()'s dependencies.
-create_annotation_fixtures = pytest.mark.usefixtures(
-    'Annotation', 'search_lib')
-
-
-@create_annotation_fixtures
-def test_create_annotation_calls_Annotation(Annotation):
-    fields = mock.MagicMock()
-    logic.create_annotation(fields)
-
-    Annotation.assert_called_once_with(fields)
-
-
-@create_annotation_fixtures
-def test_create_annotation_calls_prepare(Annotation, search_lib):
-    """It should call prepare() once with the annotation."""
-    logic.create_annotation({})
-
-    search_lib.prepare.assert_called_once_with(Annotation.return_value)
-
-
-@create_annotation_fixtures
-def test_create_annotation_calls_save(Annotation):
-    """It should call save() once."""
-    logic.create_annotation({})
-
-    Annotation.return_value.save.assert_called_once_with()
-
-
-@create_annotation_fixtures
-def test_create_annotation_returns_the_annotation(Annotation):
-    result = logic.create_annotation({})
-    assert result == Annotation.return_value
-
-
-# The fixtures required to mock all of update_annotation()'s dependencies.
-update_annotation_fixtures = pytest.mark.usefixtures('search_lib')
-
-
-@update_annotation_fixtures
-def test_update_annotation_calls_update():
-    annotation = _mock_annotation()
-    fields = {'foo': 'bar'}
-
-    logic.update_annotation(annotation, fields)
-
-    annotation.update.assert_called_once_with(fields)
-
-
-@update_annotation_fixtures
-def test_update_annotation_calls_prepare(search_lib):
-    annotation = _mock_annotation()
-
-    logic.update_annotation(annotation, {})
-
-    search_lib.prepare.assert_called_once_with(annotation)
-
-
-@update_annotation_fixtures
-def test_update_annotation_calls_save():
-    annotation = _mock_annotation()
-
-    logic.update_annotation(annotation, {})
-
-    annotation.save.assert_called_once_with()
-
-
-# The fixtures required to mock all of delete_annotation()'s dependencies.
-delete_annotation_fixtures = pytest.mark.usefixtures()
-
-
-@delete_annotation_fixtures
-def test_delete_annotation_calls_delete():
-    annotation = mock.MagicMock()
-
-    logic.delete_annotation(annotation)
-
-    annotation.delete.assert_called_once_with()
-
-
-@pytest.fixture
-def Annotation(request):
-    patcher = mock.patch('h.api.logic.Annotation', autospec=True)
-    request.addfinalizer(patcher.stop)
-    return patcher.start()
-
-
-@pytest.fixture
-def search_lib(request):
-    patcher = mock.patch('h.api.logic.search_lib', autospec=True)
-    request.addfinalizer(patcher.stop)
-    return patcher.start()

h/api/test/resources_test.py
@@ -8,56 +8,51 @@ from h.api import resources
 class TestAnnotationPermissions(object):
 
     def test_principal(self):
-        resource = resources.Annotation()
-        resource.__name__ = 'foo'
-        annotation = resource.model
-        annotation['permissions'] = {
-            'read': ['saoirse'],
-        }
+        resource = resources.Annotation('foo', {
+            'permissions': {
+                'read': ['saoirse'],
+            }
+        })
         actual = resource.__acl__()
         expect = [(security.Allow, 'saoirse', 'read')]
         assert actual == expect
 
     def test_deny_system_role(self):
-        resource = resources.Annotation()
-        resource.__name__ = 'foo'
-        annotation = resource.model
-        annotation['permissions'] = {
-            'read': [security.Everyone],
-        }
+        resource = resources.Annotation('foo', {
+            'permissions': {
+                'read': [security.Everyone],
+            }
+        })
         actual = resource.__acl__()
         expect = []
         assert actual == expect
 
     def test_group(self):
-        resource = resources.Annotation()
-        resource.__name__ = 'foo'
-        annotation = resource.model
-        annotation['permissions'] = {
-            'read': ['group:lulapalooza'],
-        }
+        resource = resources.Annotation('foo', {
+            'permissions': {
+                'read': ['group:lulapalooza'],
+            }
+        })
         actual = resource.__acl__()
         expect = [(security.Allow, 'group:lulapalooza', 'read')]
         assert actual == expect
 
     def test_group_world(self):
-        resource = resources.Annotation()
-        resource.__name__ = 'foo'
-        annotation = resource.model
-        annotation['permissions'] = {
-            'read': ['group:__world__'],
-        }
+        resource = resources.Annotation('foo', {
+            'permissions': {
+                'read': ['group:__world__'],
+            }
+        })
         actual = resource.__acl__()
         expect = [(security.Allow, security.Everyone, 'read')]
         assert actual == expect
 
     def test_group_authenticated(self):
-        resource = resources.Annotation()
-        resource.__name__ = 'foo'
-        annotation = resource.model
-        annotation['permissions'] = {
-            'read': ['group:__authenticated__'],
-        }
+        resource = resources.Annotation('foo', {
+            'permissions': {
+                'read': ['group:__authenticated__'],
+            }
+        })
         actual = resource.__acl__()
         expect = [(security.Allow, security.Authenticated, 'read')]
         assert actual == expect

h/resources.py
@@ -23,8 +23,8 @@ class Annotation(api.Annotation):
 
 
 class Annotations(api.Annotations):
-    def factory(self, key):
-        return Annotation(id=key)
+    def factory(self, id, model):
+        return Annotation(id, model)
 
 
 class Stream(Resource):

commit e45b9cecee430a147c3092870ba1d16040999cf1

Fix a bug in processing annotation deletions
f810d98 introduced a bug where annotation deletions did not propagate
through the streamer code correctly. This is because the streamer code
(fairly) assumes that it receives a complete annotation object from the
queue. f810d98 incorrectly modified ths view function causing only the
id (and no other fields) to be queued.

h/api/views.py
@@ -219,7 +219,13 @@ def delete(context, request):
     """Delete the specified annotation."""
     storage.delete_annotation(context.id)
 
-    _publish_annotation_event(request, {'id': context.id}, 'delete')
+    # N.B. We publish the original model (including all the original annotation
+    # fields) so that queue subscribers have context needed to decide how to
+    # process the delete event. For example, the streamer needs to know the
+    # target URLs of the deleted annotation in order to know which clients to
+    # forward the delete event to.
+    _publish_annotation_event(request, context.model, 'delete')
+
     return {'id': context.id, 'deleted': True}
 
 

h/api/test/views_test.py
@@ -306,9 +306,7 @@ def test_delete_event(AnnotationEvent):
 
     views.delete(context, request)
 
-    AnnotationEvent.assert_called_once_with(request,
-                                            {'id': context.id},
-                                            'delete')
+    AnnotationEvent.assert_called_once_with(request, context.model, 'delete')
     request.registry.notify.assert_called_once_with(event)
 
 