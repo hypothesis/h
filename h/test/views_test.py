87dc1285a96dcee8a70637f97bfd2ec707df7084
Robert Knight <robertknight@gmail.com>
2016-01-20 18:57:43 +0000

    Decouple sidebar HTML and embed JS template from webassets and Pyramid
    To enable building of the extension and client app without needing
    to set up the website, remove all dependencies of the sidebar HTML
    and embed JS rendering from Pyramid
    
     * Extract rendering of the client assets into a separate module
       with no Pyramid dependencies
    
     * Replace the use of the webassets 'assets' Jinja2 extension and
       references to Pyramid objects in app.html and embed.js
    
     * Refactor Angular template <script> tag rendering to reduce
       duplication
    
     * Adapt buildext.py to render app.html and embed.js without going through Pyramid
    
     * Re-add Google Analytics tracking ID to app HTML template context
    
       For base.html.jinja2, the Google Analytics tracking ID is
       added by a renderer global in subscribers.py.
    
       Since app.html.jinja2 is rendered outside of this context,
       we need to pass in the tracking ID explicitly.
    
     * ga_create_options has been renamed to ga_cookie_domain
       and changed to be an unquoted string rather than a list
       of parameters since that is all we are using it for at the moment.
    
     * Explicitly disable Google Analytics tracking for the extension
    
       It was previously the case that when building the extension,
       the Google Analytics tracking ID was never included.
    
       I'm unsure whether this was intentional or not, so for the moment,
       explicitly pass in the tracking ID as None to make it more obvious
       that tracking is not being used.

--- h/test/views_test.py
+++ h/test/views_test.py
@@ -13,30 +13,35 @@ import pytest
 from h import views
 
 
+def _dummy_request():
+    request = testing.DummyRequest()
+    request.webassets_env = mock.MagicMock()
+    request.route_url = mock.MagicMock()
+    request.sentry = mock.MagicMock()
+    return request
+
+
 class TestAnnotationView(unittest.TestCase):
 
-    def test_og_document(self):
+    @patch('h.client.render_app_html')
+    def test_og_document(self, render_app_html):
+        render_app_html.return_value = '<html></html>'
         annotation = {'id': '123', 'user': 'foo'}
         annotation['document'] = {'title': 'WikiHow — How to Make a  ☆Starmap☆'}
         context = mock.MagicMock(model=annotation)
-        request = testing.DummyRequest()
-        result = views.annotation(context, request)
-        assert isinstance(result, dict)
+        request = _dummy_request()
+        views.annotation(context, request)
+        args, kwargs = render_app_html.call_args
         test = lambda d: 'foo' in d['content'] and 'Starmap' in d['content']
-        assert any(test(d) for d in result['meta_attrs'])
+        assert any(test(d) for d in kwargs['extra']['meta_attrs'])
 
-    def test_og_no_document(self):
+    @patch('h.client.render_app_html')
+    def test_og_no_document(self, render_app_html):
+        render_app_html.return_value = '<html></html>'
         annotation = {'id': '123', 'user': 'foo'}
         context = mock.MagicMock(model=annotation)
-        request = testing.DummyRequest()
-        result = views.annotation(context, request)
-        assert isinstance(result, dict)
+        request = _dummy_request()
+        views.annotation(context, request)
+        args, kwargs = render_app_html.call_args
         test = lambda d: 'foo' in d['content']
-        assert any(test(d) for d in result['meta_attrs'])
-
-
-@pytest.fixture(autouse=True)
-def app_config(request):
-    patcher = mock.patch('h.views.app_config', autospec=True)
-    patcher.start()
-    request.addfinalizer(patcher.stop)
+        assert any(test(d) for d in kwargs['extra']['meta_attrs'])
