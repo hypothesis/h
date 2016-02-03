6607a47e28230601c8981a3096bcbcab67823444
Robert Knight <robertknight@gmail.com>
2016-02-03 15:29:26 +0000

    Fix client-side router on site
    The JS for the group share page failed to load
    because of an incorrect comparison of the result
    of String.match() against a number and also
    because of an attempt to load Bootstrap
    unnecessarily on this pages.
    
    This commit fixes the issue and prepares us for
    adding new pages in future by:
    
     * Adding a client-side micro-router to simplify running
       page-specific JS
    
     * Only loading the Bootstrap components on pages that
       actually need it.
    
    Fixes #2867

--- h/static/scripts/share-group-form.js
+++ h/static/scripts/share-group-form.js
@@ -1,4 +1,7 @@
 function ShareGroupFormController(element) {
+  if (!element.querySelector('.is-member-of-group')) {
+    return;
+  }
   var shareLink = element.querySelector('.js-share-link');
   shareLink.focus();
   shareLink.select();
