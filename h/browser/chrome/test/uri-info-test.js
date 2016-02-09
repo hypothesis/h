d57f7ec887c7a8a46cbd168deb6e17893afe33b9
Sean Hammond <git@seanh.cc>
2016-02-09 19:11:07 +0000

    Lint uri-info-test.js
    Make jscs happy with uri-info-test.js.

--- h/browser/chrome/test/uri-info-test.js
+++ h/browser/chrome/test/uri-info-test.js
@@ -1,12 +1,14 @@
+'use strict';
+
 var uriInfo = require('../lib/uri-info');
 var settings = require('./settings.json');
 
 var toResult = require('../../../static/scripts/test/promise-util').toResult;
 
-describe('UriInfo.query', function() {
+describe('UriInfo.query', function () {
   var badgeURL = settings.apiUrl + '/badge';
 
-  beforeEach(function() {
+  beforeEach(function () {
     sinon.stub(window, 'fetch').returns(
       Promise.resolve(
         new window.Response('{"total": 1}', {
@@ -19,23 +21,25 @@ describe('UriInfo.query', function() {
     sinon.stub(console, 'error');
   });
 
-  afterEach(function() {
+  afterEach(function () {
     window.fetch.restore();
     console.error.restore();
   });
 
-  it('sends the correct XMLHttpRequest', function() {
+  it('sends the correct XMLHttpRequest', function () {
     return uriInfo.query('tabUrl').then(function () {
       assert.equal(fetch.callCount, 1);
-      assert.deepEqual(fetch.lastCall.args, [badgeURL + "?uri=tabUrl"]);
+      assert.deepEqual(fetch.lastCall.args, [badgeURL + '?uri=tabUrl']);
     });
   });
 
-  it('urlencodes the URL appropriately', function() {
-    return toResult(uriInfo.query("http://foo.com?bar=baz qüx"))
+  it('urlencodes the URL appropriately', function () {
+    return toResult(uriInfo.query('http://foo.com?bar=baz qüx'))
       .then(function () {
       assert.equal(fetch.callCount, 1);
-      assert.deepEqual(fetch.lastCall.args, [badgeURL + "?uri=http%3A%2F%2Ffoo.com%3Fbar%3Dbaz+q%C3%BCx"]);
+      assert.deepEqual(
+        fetch.lastCall.args,
+        [badgeURL + '?uri=http%3A%2F%2Ffoo.com%3Fbar%3Dbaz+q%C3%BCx']);
     });
   });
 
@@ -46,7 +50,7 @@ describe('UriInfo.query', function() {
   ];
 
   INVALID_RESPONSES.forEach(function (response) {
-    it("returns an error if the server's JSON is invalid", function() {
+    it('returns an error if the server\'s JSON is invalid', function () {
       fetch.returns(
         Promise.resolve(
           new window.Response(
