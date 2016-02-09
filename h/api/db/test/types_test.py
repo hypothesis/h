b7f387bb087ceaa12cd3e1413dc3cf7a03d3e250
chdorner <christof@chdorner.com>
2016-02-09 19:20:59 +0100

    Add an AnnotationSelectorJSONB type for SQLAlchemy
    Some annotations (mostly coming from PDFs) include NULL bytes in
    the prefix/exact/suffix fields of a TextQuoteSelector. Postgres
    does not allow having NULL bytes in columns that are stored as
    text, which includes JSONB.
    
    Since we want to avoid escaping NULL bytes for all JSONB columns,
    which could be done through a custom JSON serializer in SQLAlchemy,
    this AnnotationSelectorJSONB type is only escaping the NULL bytes
    where we know they can happen.
    
    If we get NULL bytes in other fields we will see exceptions and can
    then react accordingly.

--- h/api/db/test/types_test.py
+++ h/api/db/test/types_test.py
@@ -6,7 +6,7 @@ from sqlalchemy.dialects.postgresql import dialect
 
 from h.api.db import types
 
-FIXTURES = [
+UUID_FIXTURES = [
     # 16 byte UUIDv4s
     ('OHFscuRvTmiwpOHWfrbzxw', '38716c72e46f4e68b0a4e1d67eb6f3c7'),
     ('JCfVA8bARx6wdjQq-vp9VA', '2427d503c6c0471eb076342afafa7d54'),
@@ -21,7 +21,7 @@ FIXTURES = [
     (None, None),
 ]
 
-FIXTURES_INVALID = [
+UUID_FIXTURES_INVALID = [
     # Incorrect type
     123,
     [1, 2, 3],
@@ -38,20 +38,74 @@ FIXTURES_INVALID = [
 ]
 
 
-@pytest.mark.parametrize('app,db', FIXTURES)
-def test_serialize(app, db):
+@pytest.mark.parametrize('app,db', UUID_FIXTURES)
+def test_uuid_serialize(app, db):
     t = types.URLSafeUUID()
     assert t.process_bind_param(app, dialect) == db
 
 
-@pytest.mark.parametrize('data', FIXTURES_INVALID)
-def test_serialize_non_base64_data(data):
+@pytest.mark.parametrize('data', UUID_FIXTURES_INVALID)
+def test_uuid_serialize_non_base64_data(data):
     t = types.URLSafeUUID()
     with pytest.raises(types.InvalidUUID):
         t.process_bind_param(data, dialect)
 
 
-@pytest.mark.parametrize('app,db', FIXTURES)
-def test_deserialize(app, db):
+@pytest.mark.parametrize('app,db', UUID_FIXTURES)
+def test_uuid_deserialize(app, db):
     t = types.URLSafeUUID()
     assert t.process_result_value(db, dialect) == app
+
+
+def test_annotation_selector_serialize():
+    t = types.AnnotationSelectorJSONB()
+    selectors = [{
+        'type': 'TextQuoteSelector',
+        'prefix': u'\u0000Lorem ipsum ',
+        'exact': u'dolor sit amet,\u0000 ',
+        'suffix': u'consectetur\u0000 adipiscing elit.'
+    }]
+
+    value = t.process_bind_param(selectors, dialect)
+    assert value[0]['prefix'] == u'\\u0000Lorem ipsum '
+    assert value[0]['exact'] == 'dolor sit amet,\\u0000 '
+    assert value[0]['suffix'] == 'consectetur\\u0000 adipiscing elit.'
+
+
+def test_annotation_selector_serialize_missing_text_quote_selector():
+    t = types.AnnotationSelectorJSONB()
+    selectors = [{
+        'type': 'RangeSelector',
+        'startContainer': '/div[1]/div[2]',
+        'endContainer': '/div[1]/div[3]',
+        'startOffset': 39,
+        'endoffset': 1
+    }]
+    assert t.process_bind_param(selectors, dialect) == selectors
+
+
+def test_annotation_selector_deserialize():
+    t = types.AnnotationSelectorJSONB()
+    selectors = [{
+        'type': 'TextQuoteSelector',
+        'prefix': u'\\u0000Lorem ipsum ',
+        'exact': u'dolor sit amet,\\u0000 ',
+        'suffix': u'consectetur\\u0000 adipiscing elit.'
+    }]
+
+    value = t.process_result_value(selectors, dialect)
+    assert value[0]['prefix'] == u'\u0000Lorem ipsum '
+    assert value[0]['exact'] == u'dolor sit amet,\u0000 '
+    assert value[0]['suffix'] == u'consectetur\u0000 adipiscing elit.'
+
+
+def test_annotation_selector_deserialize_missing_text_quote_selector():
+    t = types.AnnotationSelectorJSONB()
+    selectors = [{
+        'type': 'RangeSelector',
+        'startContainer': '/div[1]/div[2]',
+        'endContainer': '/div[1]/div[3]',
+        'startOffset': 39,
+        'endoffset': 1
+    }]
+    assert t.process_result_value(selectors, dialect) == selectors
