b425991739be7f42f08a38a537b4483f5b3fde58
chdorner <christof@chdorner.com>
2016-02-10 20:02:06 +0100

    Copy SQLAlchemy naming convention to api.db
    So that indices and constraints will have the same names when
    being auto-created in development mode.

--- h/api/db/__init__.py
+++ h/api/db/__init__.py
@@ -1,10 +1,33 @@
 # -*- coding: utf-8 -*-
 
 from annotator import es
+from sqlalchemy import MetaData
 from sqlalchemy.ext import declarative
 
 
-Base = declarative.declarative_base()  # pylint: disable=invalid-name
+# Create a default metadata object with naming conventions for indexes and
+# constraints. This makes changing such constraints and indexes with alembic
+# after creation much easier. See:
+#
+#   http://docs.sqlalchemy.org/en/latest/core/constraints.html#configuring-constraint-naming-conventions
+#
+# The migrations we run with alembic use the naming convention from
+# :py:mod:`h.db`. In order that auto-created tables (i.e. those created by
+# `Base.metadata.create_all()`) are compatible with these naming conventions,
+# we need to specify them here, too, as `h.api` has its own global metadata
+# object.
+#
+# N.B. This must be kept in sync with the naming conventions in :py:mod:`h.db`.
+#
+metadata = MetaData(naming_convention={
+    "ix": "ix__%(column_0_label)s",
+    "uq": "uq__%(table_name)s__%(column_0_name)s",
+    "ck": "ck__%(table_name)s__%(constraint_name)s",
+    "fk": "fk__%(table_name)s__%(column_0_name)s__%(referred_table_name)s",
+    "pk": "pk__%(table_name)s"
+})
+
+Base = declarative.declarative_base(metadata=metadata)  # pylint: disable=invalid-name
 
 
 def use_session(session, base=Base):
