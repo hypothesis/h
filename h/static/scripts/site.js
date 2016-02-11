hypothesis/h

commit f7b519daf7569937eca08259a35eccd4faccd5e9

Don't autofocus the group share link
Unfortunately, on group pages for groups who have annotated lots of
documents, this results in a rather confusing jump to the bottom of the
page.

Fixes #2917.

h/static/scripts/site.js
@@ -24,10 +24,6 @@ page('/groups/new', function () {
   new CreateGroupFormController(document.body);
 });
 
-page('/groups/:id/:name', function () {
-  new ShareGroupFormController(document.body);
-});
-
 document.addEventListener('DOMContentLoaded', function () {
   page.start();
 });

h/static/scripts/share-group-form.js
@@ -1,10 +1,0 @@
-function ShareGroupFormController(element) {
-  if (!element.querySelector('.is-member-of-group')) {
-    return;
-  }
-  var shareLink = element.querySelector('.js-share-link');
-  shareLink.focus();
-  shareLink.select();
-}
-
-module.exports = ShareGroupFormController;

h/templates/groups/share.html.jinja2
@@ -43,7 +43,7 @@
               belonging to the group.</li>
           <li>Invite anyone to join the group using the link below:</li>
         </ol>
-        <input class="share-link-field js-share-link" value="{{ group_url }}">
+        <input class="share-link-field" value="{{ group_url }}">
         <a href="//twitter.com/intent/tweet?url={{ group_url }}"
            target="_blank"
            title="Tweet link"

commit 2b10317afdaef7a8367959244272ee0b255bc8b8

Remove unused/broken require in site.js
I broke this in f7b519d, it seems.

h/static/scripts/site.js
@@ -9,7 +9,6 @@ var page = require('page');
 var CreateGroupFormController = require('./create-group-form');
 var DropdownMenuController = require('./dropdown-menu');
 var InstallerController = require('./installer-controller');
-var ShareGroupFormController = require('./share-group-form');
 
 // setup components
 new DropdownMenuController(document);