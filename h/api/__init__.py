f71fd7bf534e24df41ea679f578e59b5adbf133b
Nick Stenning <nick@whiteink.com>
2016-02-16 15:02:54 +0100

    Don't include h.features from h.api
    The h.api package shouldn't reference modules in the parent package, and
    this is currently a no-op. The features module is already included in
    the application bootstrap in h.app.

--- h/api/__init__.py
+++ h/api/__init__.py
@@ -5,7 +5,6 @@ def includeme(config):
     config.add_route('api', '/', factory='h.api.resources.create_root')
     config.add_route('token', '/token')
 
-    config.include('h.features')
     config.include('h.api.db')
     config.include('h.api.search')
     config.include('h.api.views')
