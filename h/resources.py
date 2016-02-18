f8c4db5966013e80e04b66cc23ec4052d0810ce5
Nick Stenning <nick@whiteink.com>
2016-02-18 17:28:08 +0000

    Simplify root ACL
    This ACL simplifies dramatically if we allow role.Admin users all
    permissions -- after all, why are they admins if not to have ALL THE
    POWER?

--- h/resources.py
+++ h/resources.py
@@ -1,23 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from pyramid.security import Allow
+from pyramid.security import ALL_PERMISSIONS
+from pyramid.security import DENY_ALL
 
 from h.auth import role
 
 
 class Root(object):
     __acl__ = [
-        (Allow, role.Admin, 'admin_index'),
         (Allow, role.Staff, 'admin_index'),
-        (Allow, role.Admin, 'admin_features'),
-        (Allow, role.Admin, 'admin_nipsa'),
-        (Allow, role.Admin, 'admin_admins'),
-        (Allow, role.Admin, 'admin_staff'),
-        (Allow, role.Admin, 'admin_users'),
-        (Allow, role.Staff, 'admin_users'),
-        (Allow, role.Admin, 'admin_badge'),
-        (Allow, role.Admin, 'admin_groups'),
         (Allow, role.Staff, 'admin_groups'),
+        (Allow, role.Staff, 'admin_users'),
+        (Allow, role.Admin, ALL_PERMISSIONS),
+        DENY_ALL
     ]
 
     def __init__(self, request):
