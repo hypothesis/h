hypothesis/h

commit 13eb923efd58ba148229696a63500bcab5c669f8

Add a group membership validation check at create time
This is currently enforced by an ACL on the `Annotations` resource in
`h.api`, but in view of some refactoring I have planned, I want to move
the enforcement of a check that the user can only create annotations in
groups they are a member of into the validation layer.

When the refactoring is complete, this will result in an improved error
message if a user tries to create an annotation in a group they're not a
member of via the API -- they'll get an explicit explanation of what
they have done wrong rather than a 404.

h/api/schemas.py
@@ -101,6 +101,15 @@ class CreateAnnotationSchema(object):
         # Set the annotation user field to the request user.
         appstruct['user'] = self.request.authenticated_userid
 
+        # Assert that the user has permission to create an annotation in the
+        # group they've asked to create one in.
+        if 'group' in appstruct:
+            group_principal = 'group:{}'.format(appstruct['group'])
+            if group_principal not in self.request.effective_principals:
+                raise ValidationError('group: ' +
+                                      _('You may not create annotations in '
+                                        'groups you are not a member of!'))
+
         return appstruct
 
 

h/api/test/schemas_test.py
@@ -91,6 +91,44 @@ def test_createannotationschema_ignores_input_user(data, authn_policy):
     assert result == {'user': 'acct:jeanie@example.com'}
 
 
+@pytest.mark.parametrize('data,effective_principals,ok', [
+    # No group supplied
+    ({}, [], True),
+
+    # World group
+    ({'group': '__world__'}, [], False),
+    ({'group': '__world__'}, ['group:__world__'], True),
+
+    # Other group
+    ({'group': 'abcdef'}, [], False),
+    ({'group': 'abcdef'}, ['group:__world__'], False),
+    ({'group': 'abcdef'}, ['group:__world__', 'group:abcdef'], True),
+])
+def test_createannotationschema_rejects_annotations_to_other_groups(data,
+                                                                    effective_principals,
+                                                                    ok,
+                                                                    authn_policy):
+    """
+    A user cannot create an annotation in a group they're not a member of.
+
+    If a group is specified in the annotation, then reject the creation if the
+    relevant group principal is not present in the request's effective
+    principals.
+    """
+    authn_policy.effective_principals.return_value = effective_principals
+    request = testing.DummyRequest()
+    schema = schemas.CreateAnnotationSchema(request)
+
+    if ok:
+        result = schema.validate(data)
+        assert result.get('group') == data.get('group')
+
+    else:
+        with pytest.raises(schemas.ValidationError) as exc:
+            schema.validate(data)
+        assert exc.value.message.startswith('group:')
+
+
 @pytest.mark.parametrize('data', [
     {},
     {'foo': 'bar'},

commit 06d9a4aff68b652405ed7b6c48a0e83fc6994142

Fix major oversight preventing annotation creation
13eb923 introduced a pretty spectacular bug preventing anyone from
creating annotations in the "Public" bucket. Annotations created in this
virtual group have a `group` field of `__world__`, but that corresponds
to a principal of `pyramid.security.Everyone`.

h/api/schemas.py
@@ -5,6 +5,7 @@ import copy
 import jsonschema
 from jsonschema.exceptions import best_match
 from pyramid import i18n
+from pyramid import security
 
 _ = i18n.TranslationStringFactory(__package__)
 
@@ -104,7 +105,10 @@ class CreateAnnotationSchema(object):
         # Assert that the user has permission to create an annotation in the
         # group they've asked to create one in.
         if 'group' in appstruct:
-            group_principal = 'group:{}'.format(appstruct['group'])
+            if appstruct['group'] == '__world__':
+                group_principal = security.Everyone
+            else:
+                group_principal = 'group:{}'.format(appstruct['group'])
             if group_principal not in self.request.effective_principals:
                 raise ValidationError('group: ' +
                                       _('You may not create annotations in '

h/api/test/schemas_test.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 import mock
 from pyramid import testing
+from pyramid import security
 import pytest
 
 from h.api import schemas
@@ -97,12 +98,12 @@ def test_createannotationschema_ignores_input_user(data, authn_policy):
 
     # World group
     ({'group': '__world__'}, [], False),
-    ({'group': '__world__'}, ['group:__world__'], True),
+    ({'group': '__world__'}, [security.Everyone], True),
 
     # Other group
     ({'group': 'abcdef'}, [], False),
-    ({'group': 'abcdef'}, ['group:__world__'], False),
-    ({'group': 'abcdef'}, ['group:__world__', 'group:abcdef'], True),
+    ({'group': 'abcdef'}, [security.Everyone], False),
+    ({'group': 'abcdef'}, [security.Everyone, 'group:abcdef'], True),
 ])
 def test_createannotationschema_rejects_annotations_to_other_groups(data,
                                                                     effective_principals,