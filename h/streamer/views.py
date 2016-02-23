0e70a5227f3bbb3864075f9c8ea63220abc83aeb
Nick Stenning <nick@whiteink.com>
2016-02-23 11:54:32 +0000

    Improve streamer exception views
    Currently the exception views for the streamer are inconsistent and
    unhelpful. This commit normalises the response format for all unexpected
    events.
    
    Also, add stats collection for these events so we can see how often they
    occur.

--- h/streamer/views.py
+++ h/streamer/views.py
@@ -2,12 +2,16 @@
 
 from pyramid import httpexceptions
 from pyramid.config import aslist
+from pyramid.view import forbidden_view_config
+from pyramid.view import notfound_view_config
+from pyramid.view import view_config
 from ws4py.exc import HandshakeError
 from ws4py.server.wsgiutils import WebSocketWSGIApplication
 
 from h.streamer import websocket
 
 
+@view_config(route_name='ws')
 def websocket_view(request):
     # WebSockets can be opened across origins and send cookies. To prevent
     # scripts on other sites from using this socket, ensure that the Origin
@@ -21,12 +25,56 @@ def websocket_view(request):
     return request.get_response(app)
 
 
-def bad_handshake(exc, request):
-    raise httpexceptions.HTTPBadRequest()
+@notfound_view_config(renderer='json')
+def notfound(exc, request):
+    request.response.status_code = 404
+    request.stats.incr('streamer.error.not_found')
+    return {
+        'ok': False,
+        'error': 'not_found',
+        'reason': 'These are not the droids you are looking for.',
+    }
+
+
+@forbidden_view_config(renderer='json')
+def forbidden(exc, request):
+    request.response.status_code = 403
+    request.stats.incr('streamer.error.forbidden')
+    return {
+        'ok': False,
+        'error': 'forbidden',
+        'reason': 'You are not allowed here. Are you connecting from an '
+                  'allowed origin?',
+    }
+
+
+@view_config(context=HandshakeError, renderer='json')
+def error_badhandshake(exc, request):
+    request.response.status_code = 400
+    request.stats.incr('streamer.error.bad_handshake')
+    return {
+        'ok': False,
+        'error': 'bad_handshake',
+        'reason': 'Handshake failed. Are you a WebSocket client?',
+    }
+
+
+@view_config(context=Exception, renderer='json')
+def error(context, request):
+    request.response.status_code = 500
+    request.sentry.captureException()
+    request.stats.incr('streamer.error.server_error')
+    if request.debug:
+        raise
+    return {
+        'ok': False,
+        'error': 'server_error',
+        'reason': 'An unexpected error occurred and has been reported.',
+    }
 
 
 def includeme(config):
     settings = config.registry.settings
     settings['origins'] = aslist(settings.get('origins', ''))
-    config.add_view(websocket_view, route_name='ws')
-    config.add_view(bad_handshake, context=HandshakeError)
+
+    config.scan(__name__)
