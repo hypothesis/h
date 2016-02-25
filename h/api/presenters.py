b86dada4867915e646040fceb63bd502fcf44f40
chdorner <christof@chdorner.com>
2016-02-25 14:02:26 +0100

    Add deep_merge_dict util function
    Which merges two dictionaries recursively.

--- h/api/presenters.py
+++ h/api/presenters.py
@@ -3,6 +3,20 @@
 Presenters for API data.
 """
 
+import collections
+
 
 def utc_iso8601(datetime):
     return datetime.strftime('%Y-%m-%dT%H:%M:%S.%f+00:00')
+
+
+def deep_merge_dict(a, b):
+    """Recursively merges dict `b` into dict `a`."""
+
+    for k, v in b.items():
+        if isinstance(v, collections.Mapping):
+            if k not in a or not isinstance(a[k], dict):
+                a[k] = dict()
+            deep_merge_dict(a[k], v)
+        else:
+            a[k] = v
--- h/api/test/presenters_test.py
+++ h/api/test/presenters_test.py
@@ -2,7 +2,7 @@
 
 import datetime
 
-from h.api.presenters import utc_iso8601
+from h.api.presenters import utc_iso8601, deep_merge_dict
 
 
 def test_utc_iso8601():
@@ -13,6 +13,21 @@ def test_utc_iso8601():
 def test_utc_iso8601_ignores_timezone():
     t = datetime.datetime(2016, 2, 24, 18, 03, 25, 7685, Berlin())
     assert utc_iso8601(t) == '2016-02-24T18:03:25.007685+00:00'
+
+
+def test_deep_merge_dict():
+    a = {'foo': 1, 'bar': 2, 'baz': {'foo': 3, 'bar': 4}}
+    b = {'bar': 8, 'baz': {'bar': 6, 'qux': 7}, 'qux': 15}
+    deep_merge_dict(a, b)
+
+    assert a == {
+        'foo': 1,
+        'bar': 8,
+        'baz': {
+            'foo': 3,
+            'bar': 6,
+            'qux': 7},
+        'qux': 15}
 
 
 class Berlin(datetime.tzinfo):
