ceb6bc09093aaa9eb7726de4a7f89c361f4c7bd2
Sean Hammond <git@seanh.cc>
2016-03-03 11:10:02 +0000

    Fix syntax error in gulpfile.js
    It looks like backticks ` were used instead of ', resulting in:
    
        /.../h/gulpfile.js:33
            throw new Error(`Environment variable ${key} is not set`);
                            ^
        SyntaxError: Unexpected token ILLEGAL

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ function isSASSFile(file) {
 
 function getEnv(key) {
   if (!process.env.hasOwnProperty(key)) {
-    throw new Error(`Environment variable ${key} is not set`);
+    throw new Error('Environment variable ${key} is not set');
   }
   return process.env[key];
 }
