63e74d81327e622dc211f25bb6f79573e6ad5c2a
Sean Hammond <git@seanh.cc>
2016-03-22 13:15:55 +0000

    Tidy up imports
    One import per line, alphabetical order.

--- h/api/models/__init__.py
+++ h/api/models/__init__.py
@@ -12,9 +12,11 @@ functions in `h.api.storage`.
 """
 
 from h.api.models.annotation import Annotation
-from h.api.models.document import Document, DocumentMeta, DocumentURI
 from h.api.models.document import create_or_update_document_meta
 from h.api.models.document import create_or_update_document_uri
+from h.api.models.document import Document
+from h.api.models.document import DocumentMeta
+from h.api.models.document import DocumentURI
 
 __all__ = (
     'Annotation',
