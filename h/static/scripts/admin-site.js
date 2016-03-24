7dbdef24121a4cebe9fc31666cefe1301d1e4e1d
chdorner <christof@chdorner.com>
2016-03-24 18:22:23 +0100

    Configure sentry for admin JS bundle
    So we get reports about JS errors in the admin bundle.
    
    This is particularily important since we introduced a bug in the past
    that didn't confirm the users delete form which could have led to
    deleting users and all their data without a confirmation dialog.

--- h/static/scripts/admin-site.js
+++ h/static/scripts/admin-site.js
@@ -1,2 +1,10 @@
+'use strict';
+
+// configure error reporting
+var settings = require('./settings')(document);
+if (settings.raven) {
+  require('./raven').init(settings.raven);
+}
+
 window.$ = window.jQuery = require('jquery');
 require('bootstrap');
