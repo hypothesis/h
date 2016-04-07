d41206709028b93e73350eef50fb3587af7dd9b1
Sean Hammond <git@seanh.cc>
2016-04-07 15:11:39 +0100

    Add a new Elasticsearch index
    Add a second Elasticsearch index named "hypothesis" by default. This is
    intended to be used as the new Elasticsearch index that will be filled
    from annotation data in the new PostgreSQL database.
    
    The legacy index named "annotator" by default is still present and
    everything still reads from and writes to that index.
    
    This commit adds:
    
    - The config settings for the new index
    - The code to create the index on application startup if it doesn't exist
    - The code to read (i.e. search) from the new index when the 'postgres'
      feature flag is on
    
    This commit does not add:
    
    - Any code to write to the new index.
      The new search index is always empty for now.
      Later commits will add a worker process that writes changes to the new index.
    
    Config settings
    ---------------
    
    Rename the existing es.index configuration setting for the now legacy
    Elasticsearch database to legacy.es.index.
    
    Use the original name es.index for the new Elasticsearch index.
    
    The new Elasticsearch index uses the same host (the es.host setting) and
    other settings as the legacy one (only the index name can be set
    differently for each index). This means that we don't need to make any
    changes to the config settings in our deployment code - it'll just
    continue to use the same Elasticsearch host and other settings, and the
    legacy index's name will default to "annotator" and the new index's name
    will default to "hypothesis", and the new index will simply be
    automatically created in the existing Elasticsearch host when this code
    is deployed.
    
    Details:
    
    * The es.index setting becomes legacy.es.index.
      The default value is "annotator" as before.
    
    * A new es.index setting is added whose default value is "hypothesis".
    
    * Other Elasticsearch settings do not get legacy copies, rather the
      single setting is used for both the legacy and the new Elasticsearch:
      es.host, es.compatibility_mode, es.client_poolsize and es.client_timeout.
    
    request.es and request.legacy_es
    --------------------------------
    
    request.es is now an Elasticsearch client for the new search index _if_
    the 'postgres' feature flag is on, but remains a client for the old
    index if the feature flag is off. So searches will continue to go to the
    old search index, then will go to the new search index when the feature
    flag is turned on.
    
    The new request.legacy_es is always an Elasticsearch client for the old
    search index, regardless of whether the 'postgres' feature flag is on or
    off. All code that _writes_ to the search index has been changed to use
    this, as we always want to write to the old search index regardless of
    whether the feature flag is on or off.
    
    For example when creating, updating or deleting annotations with the
    postgres feature flag on these changes will first be written to
    PostgreSQL then to the legacy Elasticsearch index. If the feature flag
    is off the changes will be written to the legacy Elasticsearch index
    only.
    
    Nothing writes to the new Elasticsearch index yet. Later commits will
    add a worker process that writes changes to the new index.
    
    NIPSA worker
    ------------
    
    The NIPSA worker always writes to the legacy Elasticsearch index.
    There's no support for NIPSA with the new index yet.
    
    annotool
    --------
    
    The `hypothesis annotool` command always writes to the legacy
    Elasticsearch index. There's no support for annotool with the new index
    yet.
    
    reindex
    -------
    
    The `hypothesis reindex` command always writes to the legacy
    Elasticsearch index. There's no support for reindexing with the new index
    yet.
    
    scripts/migrate.py
    ------------------
    
    The migration script has been changed to use request.legacy_es so that
    it always uses the legacy Elasticsearch index, as it should.

--- h/api/search/__init__.py
+++ h/api/search/__init__.py
@@ -12,6 +12,7 @@ __all__ = ('search',)
 
 
 def _get_client(settings):
+    """Return a client for the Elasticsearch index."""
     host = settings['es.host']
     index = settings['es.index']
     kwargs = {}
@@ -23,10 +24,36 @@ def _get_client(settings):
     return Client(host, index, **kwargs)
 
 
+def _legacy_get_client(settings):
+    """Return a client for the legacy Elasticsearch index."""
+    host = settings['es.host']
+    index = settings['legacy.es.index']
+    kwargs = {}
+    kwargs['timeout'] = settings.get('es.client_timeout', 10)
+
+    if 'es.client_poolsize' in settings:
+        kwargs['maxsize'] = settings['es.client_poolsize']
+
+    return Client(host, index, **kwargs)
+
+
+def _get_client_or_legacy_client(request):
+    """
+    Return the Elasticsearch client.
+
+    Returns a client for either the new Elasticsearch index (if the 'postgres'
+    feature flag is on) or the legacy index (if the flag is off).
+
+    """
+    func = _get_client if request.feature('postgres') else _legacy_get_client
+    return func(request.registry.settings)
+
+
 def includeme(config):
     settings = config.registry.settings
     settings.setdefault('es.host', 'http://localhost:9200')
-    settings.setdefault('es.index', 'annotator')
+    settings.setdefault('es.index', 'hypothesis')
+    settings.setdefault('legacy.es.index', 'annotator')
 
     # Allow users of this module to register additional search filter and
     # search matcher factories.
@@ -40,11 +67,23 @@ def includeme(config):
     # Add a property to all requests for easy access to the elasticsearch
     # client. This can be used for direct or bulk access without having to
     # reread the settings.
-    config.add_request_method(lambda r: _get_client(r.registry.settings),
+
+    # request.legacy_es is always a client for the legacy Elasticsearch index,
+    # regardless of whether the 'postgres' feature flag is on.
+    # This should be used to write to the legacy search index.
+    config.add_request_method(
+        lambda r: _legacy_get_client(r.registry.settings),
+        name='legacy_es',
+        reify=True)
+
+    # request.es is a client for either the new or the legacy Elasticsearch
+    # index, depending on the 'postgres' feature flaf.
+    # This should always be used to read from the search index.
+    config.add_request_method(_get_client_or_legacy_client,
                               name='es',
                               reify=True)
 
     # If requested, automatically configure the index
     if asbool(settings.get('h.search.autoconfig', False)):
-        client = _get_client(settings)
-        configure_index(client)
+        configure_index(_get_client(settings))
+        configure_index(_legacy_get_client(settings))
