f6c7555476dce5bd429f36fb36045e15be672f7d
chdorner <christof@chdorner.com>
2016-04-07 20:14:25 +0200

    Replace usage of the `page` node package with our own

--- h/static/scripts/admin-site.js
+++ h/static/scripts/admin-site.js
@@ -9,14 +9,10 @@ if (settings.raven) {
 window.$ = window.jQuery = require('jquery');
 require('bootstrap');
 
-var page = require('page');
+var page = require('./page');
 
 var AdminUsersController = require('./admin-users');
 
 page('/admin/users', function() {
   new AdminUsersController(document.body, window);
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-  page.start();
 });
