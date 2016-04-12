962abce7d18986a9c4ab68959ef637c5a2d62bdc
Nick Stenning <nick@whiteink.com>
2016-04-12 23:24:58 +0200

    Move features client into h.features.client module
    This moves the features client into its own module and substantially
    simplifies the testing of this component.
    
    The logic of returning only valid current features is already
    implemented in the model class, so the Client doesn't need to duplicate
    this functionality. As a result, the interaction between the client and
    the model is reduced to a single "fetcher" callable, which is injected
    for ease of testing.

--- h/features/__init__.py
+++ h/features/__init__.py
@@ -1,97 +1,16 @@
 # -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
-import logging
 import os
 
-from pyramid import events
 import transaction
 
-from h.auth import role
+from h.features import client
 from h.features import models
 
-log = logging.getLogger(__name__)
 
-
-class UnknownFeatureError(Exception):
-    pass
-
-
-class Client(object):
-    """
-    Determine if the named feature is enabled for the current request.
-    If the feature has no override in the database, it will default to
-    False. Features must be documented, and an UnknownFeatureError will be
-    thrown if an undocumented feature is interrogated.
-    """
-
-    def __init__(self, request):
-        self.request = request
-        self._cache = {}
-
-    def __call__(self, name):
-        return self.enabled(name)
-
-    def load(self):
-        """Loads the feature flag states into the internal cache."""
-        self._cache = {f.name: self._state(f) for f in models.Feature.all()}
-
-    def enabled(self, name):
-        """
-        Determine if the named feature is enabled for the current request.
-
-        If the feature has no override in the database, it will default to
-        False. Features must be documented, and an UnknownFeatureError will be
-        thrown if an undocumented feature is interrogated.
-
-        When the internal cache is empty, it will automatically load the
-        feature flags from the database first.
-        """
-        if not self._cache:
-            self.load()
-
-        if name not in self._cache:
-            raise UnknownFeatureError(
-                '{0} is not a valid feature name'.format(name))
-
-        return self._cache[name]
-
-    def all(self):
-        """
-        Returns a dict mapping feature flag names to enabled states
-        for the user associated with a given request.
-
-        When the internal cache is empty, it will automatically load the
-        feature flags from the database first.
-        """
-        if not self._cache:
-            self.load()
-
-        return self._cache
-
-    def clear(self):
-        self._cache = {}
-
-    def _state(self, feature):
-        # Features that don't exist in the database are off.
-        if feature is None:
-            return False
-        # Features that are on for everyone are on.
-        if feature.everyone:
-            return True
-        # Features that are on for admin are on if the current user is an
-        # admin.
-        if feature.admins and role.Admin in self.request.effective_principals:
-            return True
-        # Features that are on for staff are on if the current user is a staff
-        # member.
-        if feature.staff and role.Staff in self.request.effective_principals:
-            return True
-        return False
-
-
-@events.subscriber(events.ApplicationCreated)
-def remove_old_flags_on_boot(event):
+def _remove_old_flags_on_boot(event):
     """Remove old feature flags from the database on startup."""
     # Skip this if we're in a script, not actual app startup. See the comment
     # in h.script:main for an explanation.
@@ -105,7 +24,8 @@ def remove_old_flags_on_boot(event):
 def includeme(config):
     config.include('h.features.views')
 
-    config.add_request_method(Client, name='feature', reify=True)
-    config.add_route('features_status', '/app/features')
+    config.add_request_method(client.Client, name='feature', reify=True)
+    config.add_subscriber(_remove_old_flags_on_boot,
+                          'pyramid.events.ApplicationCreated')
 
-    config.scan(__name__)
+    config.add_route('features_status', '/app/features')
