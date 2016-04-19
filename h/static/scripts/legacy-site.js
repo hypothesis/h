d6fe7b8b24f745a6b3a0eabdb8b70e68b8faabe7
chdorner <christof@chdorner.com>
2016-04-19 14:14:57 +0200

    Move JS code on old home page into legacy-site.js
    `legacy-site.js` is only loaded on the old home page, so we can safely
    add it to that existing JS file.

--- h/static/scripts/legacy-site.js
+++ h/static/scripts/legacy-site.js
@@ -1,2 +1,15 @@
+'use strict';
+
 window.$ = window.jQuery = require('jquery');
 require('bootstrap');
+
+if (window.chrome !== undefined) {
+  var elements = document.getElementsByClassName('unhide-in-chrome');
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.remove('hidden');
+  }
+  elements = document.getElementsByClassName('hide-in-chrome');
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.add('hidden');
+  }
+}
