d3a852fbd44ba3ad6e34758de61e31aa5e62e872
Robert Knight <robertknight@gmail.com>
2016-05-09 09:55:02 +0100

    Support PDFs in Firefox WebExtension
    Add support for detecting the PDF.js-based viewer in the Firefox
    extension.
    
    Also add a comment about a common confusion when examining tabs
    displaying PDFs in Google Chrome. See
    https://github.com/hypothesis/h/issues/3293#issuecomment-217834556

--- h/browser/chrome/lib/detect-content-type.js
+++ h/browser/chrome/lib/detect-content-type.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * Returns the type of content in the current document,
  * currently either 'PDF' or 'HTML'.
@@ -10,19 +12,48 @@
  * of content in embedded viewers where that differs from the tab's
  * main URL.
  */
-function detectContentType() {
-  // check if this is the Chrome PDF viewer
-  var chromePDFPluginSelector =
-    'embed[type="application/pdf"][name="plugin"]';
-  if (document.querySelector(chromePDFPluginSelector)) {
-    return {
-      type: 'PDF',
-    };
-  } else {
-    return {
-      type: 'HTML',
-    };
+function detectContentType(document_) {
+  document_ = document_ || document;
+
+  function detectChromePDFViewer() {
+    // When viewing a PDF in Chrome, the viewer consists of a top-level
+    // document with an <embed> tag, which in turn instantiates an inner HTML
+    // document providing the PDF viewer UI plus another <embed> tag which
+    // instantiates the native PDF renderer.
+    //
+    // The selector below matches the <embed> tag in the top-level document. To
+    // see this document, open the inspector on a PDF viewer tab with
+    // Ctrl+Shift+I rather than right-clicking on the viewport and selecting the
+    // 'Inspect' option which will instead show the _inner_ document.
+    if (!!document_.querySelector('embed[type="application/pdf"][name="plugin"]')) {
+      return {type: 'PDF'};
+    }
   }
+
+  function detectFirefoxPDFViewer() {
+    // The Firefox PDF viewer is an instance of PDF.js.
+    //
+    // The Firefox PDF plugin specifically can be detected via the <base>
+    // tag it includes, which can be done from a content script (which runs
+    // in an isolated JS world from the page's own scripts).
+    //
+    // Generic PDF.js detection can be done by looking for the
+    // `window.PDFViewerApplication` object. This however requires running JS
+    // code in the same JS context as the page's own code.
+    if (document_.baseURI.indexOf('resource://pdf.js') === 0) {
+      return {type: 'PDF'};
+    }
+  }
+
+  var detectFns = [detectChromePDFViewer, detectFirefoxPDFViewer];
+  for (var i = 0; i < detectFns.length; i++) {
+    var typeInfo = detectFns[i]();
+    if (typeInfo) {
+      return typeInfo;
+    }
+  }
+
+  return {type: 'HTML'};
 }
 
 module.exports = detectContentType;
