67cc66a6c02ae77742e4cb4ed1e7ac9a08e071c1
Robert Knight <robertknight@gmail.com>
2016-05-12 18:44:25 +0100

    Only filter annotations (not replies) against selection
    Only annotations, not replies can be selected, so only filter top-level
    annotations against the selection map.
    
    This resulted in an issue where 'View N more in conversation' being
    shown where 'N' was the number of replies when selecting an annotation
    with replies in the page.
    
    See https://github.com/hypothesis/h/pull/3285#issuecomment-218734287

--- h/static/scripts/build-thread.js
+++ h/static/scripts/build-thread.js
@@ -210,14 +210,6 @@ function hasVisibleChildren(thread) {
   });
 }
 
-
-function hasSelectedChildren(thread, selected) {
-  return thread.children.some(function (child) {
-    return selected.indexOf(child.id) !== -1 ||
-           hasSelectedChildren(child, selected);
-  });
-}
-
 /**
  * Default options for buildThread()
  */
@@ -279,15 +271,21 @@ function buildThread(annotations, opts) {
     if (opts.forceVisible && opts.forceVisible.indexOf(id(annotation)) !== -1) {
       return true;
     }
-    if (opts.selected.length > 0 &&
-        opts.selected.indexOf(id(annotation)) === -1) {
-      return false;
-    }
     if (opts.filterFn && !opts.filterFn(annotation)) {
       return false;
     }
     return true;
   };
+
+  // When there is a selection, only include top-level threads (annotations)
+  // that are selected
+  if (opts.selected.length > 0) {
+    thread = Object.assign({}, thread, {
+      children: thread.children.filter(function (child) {
+        return opts.selected.indexOf(child.id) !== -1;
+      }),
+    });
+  }
 
   // Set the visibility of threads based on whether they match
   // the current search filter
@@ -315,8 +313,7 @@ function buildThread(annotations, opts) {
 
     return Object.assign({}, thread, {
       collapsed: thread.collapsed &&
-                 !hasUnfilteredChildren &&
-                 !hasSelectedChildren(thread, opts.selected)
+                 !hasUnfilteredChildren,
     });
   });
 
