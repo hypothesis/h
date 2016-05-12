ad5085488392ee283d103707257edd740fd7ee5b
Alice Wyan <finitud@users.noreply.github.com>
2016-05-12 11:01:02 +0100

    Feature flags cohorts: Add Cohort model (#3309)
    Add a model and a join table for feature cohorts: groups of users who will (soon) be able to be opted in to specific feature flags.

--- h/features/models.py
+++ h/features/models.py
@@ -7,6 +7,7 @@ import logging
 import sqlalchemy as sa
 
 from h.db import Base
+from h.db import mixins
 
 log = logging.getLogger(__name__)
 
@@ -105,3 +106,37 @@ class Feature(Base):
 
     def __repr__(self):
         return '<Feature {f.name} everyone={f.everyone}>'.format(f=self)
+
+
+class FeatureCohort(Base, mixins.Timestamps):
+    __tablename__ = 'featurecohort'
+
+    id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+    name = sa.Column(sa.UnicodeText(), nullable=False, index=True)
+
+    # Cohort membership
+    members = sa.orm.relationship('User',
+                                  secondary='featurecohort_user',
+                                  backref='cohorts')
+
+    def __init__(self, name):
+        self.name = name
+
+
+FEATURECOHORT_USER_TABLE = sa.Table(
+    'featurecohort_user', Base.metadata,
+    sa.Column('id',
+              sa.Integer,
+              nullable=False,
+              autoincrement=True,
+              primary_key=True),
+    sa.Column('cohort_id',
+              sa.Integer,
+              sa.ForeignKey('featurecohort.id'),
+              nullable=False),
+    sa.Column('user_id',
+              sa.Integer,
+              sa.ForeignKey('user.id'),
+              nullable=False),
+    sa.UniqueConstraint('cohort_id', 'user_id'),
+)
