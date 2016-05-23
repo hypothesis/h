1e4acd60403be0740523b31492f72ae60a43a49f
Sean Hammond <git@seanh.cc>
2016-05-23 18:32:03 +0100

    Fix a Sphinx warning
    Exclude pattern in Sphinx config got out if date when file was moved.

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,7 @@ release = '0.0.2'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'hacking/services.rst']
+exclude_patterns = ['_build', 'developing/services.rst']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
