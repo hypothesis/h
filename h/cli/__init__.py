7afe3a77e3395538c567b8a6a1a31a06e3649fbc
chdorner <christof@chdorner.com>
2016-05-24 15:59:00 +0200

    Remove legacy_reindex CLI command
    Which is not used anymore after the switch to Postgres is completed.

--- h/cli/__init__.py
+++ h/cli/__init__.py
@@ -19,7 +19,6 @@ SUBCOMMANDS = (
     'h.cli.commands.celery.celery',
     'h.cli.commands.devserver.devserver',
     'h.cli.commands.initdb.initdb',
-    'h.cli.commands.legacy_reindex.legacy_reindex',
     'h.cli.commands.migrate.migrate',
     'h.cli.commands.reindex.reindex',
 )
