24c88a508ba4c27c0bae890ba5adf49c19331a9b
Alice Wyan <alice@finitud.org>
2016-06-01 18:56:17 +0100

    Fix features cohort permissions

--- h/admin/views/cohorts.py
+++ h/admin/views/cohorts.py
@@ -10,7 +10,7 @@ from h import paginator
 @view_config(route_name='admin_cohorts',
              request_method='GET',
              renderer='h:templates/admin/cohorts.html.jinja2',
-             permission='admin_cohorts')
+             permission='admin_features')
 @paginator.paginate
 def cohorts_index(context, request):
     return models.FeatureCohort.query.order_by(models.FeatureCohort.created.desc())
@@ -20,7 +20,7 @@ def cohorts_index(context, request):
              request_method='POST',
              request_param='add',
              renderer='h:templates/admin/cohorts.html.jinja2',
-             permission='admin_cohorts')
+             permission='admin_features')
 def cohorts_add(request):
     """Create a new feature cohort."""
     cohort_name = request.params['add']
