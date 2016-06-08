cd63c0fc8510e3dcb8b5de8b4f0c89ddef4380b4
Alice Wyan <alice@finitud.org>
2016-06-08 12:32:39 +0100

    Improve test coverage and test cases for feature cohorts

--- tests/h/admin/views/features_test.py
+++ tests/h/admin/views/features_test.py
@@ -5,9 +5,8 @@ from pyramid.testing import DummyRequest
 import pytest
 
 from h import db
+from h import models
 from h.admin.views import features as views
-
-from .... import factories
 
 
 class DummyFeature(object):
@@ -71,57 +70,112 @@ def test_features_save_checks_csrf_token(Feature, check_csrf_token):
     check_csrf_token.assert_called_with(request)
 
 
-def test_cohorts_index_when_no_cohorts():
+def test_cohorts_index_without_cohorts():
     req = DummyRequest(db=db.Session)
     result = views.cohorts_index({}, req)
     assert result["results"] == []
 
 
-def test_new_cohort_creates_cohort_with_no_members():
+def test_cohorts_index_with_cohorts():
     req = DummyRequest(db=db.Session)
-    req.params['add'] = 'cohort'
-    result = views.cohorts_add(req)
-    assert isinstance(result, httpexceptions.HTTPSeeOther)
+    cohort1 = models.FeatureCohort(name='cohort1')
+    cohort2 = models.FeatureCohort(name='cohort2')
+    db.Session.add(cohort1)
+    db.Session.add(cohort2)
+    db.Session.flush()
 
     result = views.cohorts_index({}, req)
-    assert len(result["results"]) == 1
+    assert len(result["results"]) == 2
 
-    cohort = result["results"][0]
+
+def test_cohorts_add_creates_cohort_with_no_members():
+    req = DummyRequest(db=db.Session)
+    req.params['add'] = 'cohort'
+    views.cohorts_add(req)
+
+    result = req.db.query(models.FeatureCohort).filter_by(name='cohort').all()
+    assert len(result) == 1
+
+    cohort = result[0]
     assert cohort.name == "cohort"
     assert len(cohort.members) == 0
 
 
-def create_cohort(req):
-    req.params['add'] = 'cohort'
-    views.cohorts_add(req)
-    return views.cohorts_index({}, req)['results'][0]
+def test_cohorts_edit_add_user():
+    req = DummyRequest(db=db.Session)
+    user = models.User(username='benoit',
+                       password='mandelbrot',
+                       email='benoit@example.com')
+    cohort = models.FeatureCohort(name='FractalCohort')
 
+    db.Session.add(user)
+    db.Session.add(cohort)
+    db.Session.flush()
 
-def add_user_to_cohort(req, user, cohort):
     req.matchdict['id'] = cohort.id
     req.params['add'] = user.username
     views.cohorts_edit_add(req)
 
+    assert len(cohort.members) == 1
+    assert cohort.members[0].username == user.username
 
-def test_cohort_edit_add_and_remove_user():
+
+def test_cohorts_edit_remove_user():
     req = DummyRequest(db=db.Session)
-    user = factories.User()
-    db.Session.add(user)
-    cohort = create_cohort(req)
-    add_user_to_cohort(req, user, cohort)
+    user = models.User(username='benoit',
+                       password='mandelbrot',
+                       email='benoit@example.com')
+    cohort = models.FeatureCohort(name='FractalCohort')
+    cohort.members.append(user)
 
-    req.matchdict['id'] = cohort.id
-    res = views.cohorts_edit({}, req)
-    assert len(res['members']) == 1
-    assert res['members'][0].username == user.username
+    db.Session.add(user)
+    db.Session.add(cohort)
+    db.Session.flush()
+
+    assert len(cohort.members) == 1
 
     req.matchdict['id'] = cohort.id
     req.params['remove'] = user.username
     views.cohorts_edit_remove(req)
 
+    assert len(cohort.members) == 0
+
+
+def test_cohorts_edit_with_no_users():
+    req = DummyRequest(db=db.Session)
+    cohort = models.FeatureCohort(name='FractalCohort')
+    db.Session.add(cohort)
+    db.Session.flush()
+
     req.matchdict['id'] = cohort.id
-    res = views.cohorts_edit({}, req)
-    assert len(res['members']) == 0
+    result = views.cohorts_edit({}, req)
+
+    assert result['cohort'].id == cohort.id
+    assert len(result['cohort'].members) == 0
+
+
+def test_cohorts_edit_with_users():
+    req = DummyRequest(db=db.Session)
+    cohort = models.FeatureCohort(name='FractalCohort')
+    user1 = models.User(username='benoit',
+                        password='mandelbrot',
+                        email='benoit@example.com')
+    user2 = models.User(username='emily',
+                        password='noether',
+                        email='emily@example.com')
+    cohort.members.append(user1)
+    cohort.members.append(user2)
+
+    db.Session.add(user1)
+    db.Session.add(user2)
+    db.Session.add(cohort)
+    db.Session.flush()
+
+    req.matchdict['id'] = cohort.id
+    result = views.cohorts_edit({}, req)
+
+    assert result['cohort'].id == cohort.id
+    assert len(result['cohort'].members) == 2
 
 
 @pytest.fixture(autouse=True)
