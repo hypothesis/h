aa85f7db27e307298cc40620dad9765b84a3ee51
Nick Stenning <nick@whiteink.com>
2016-06-15 22:03:13 +0200

    Add some timers to the streamer code
    So we have some idea how long message processing is taking.
    
    I've had to change where the defaults for the StatsdClient host and port
    are set so that the tests don't need to include `h.stats`.

--- h/stats.py
+++ h/stats.py
@@ -5,14 +5,11 @@ __all__ = ('get_client',)
 
 
 def get_client(settings):
-    return statsd.StatsClient(host=settings.get('statsd.host'),
-                              port=settings.get('statsd.port'))
+    return statsd.StatsClient(host=settings.get('statsd.host', 'localhost'),
+                              port=settings.get('statsd.port', 8125))
 
 
 def includeme(config):
-    config.registry.settings.setdefault('statsd.host', 'localhost')
-    config.registry.settings.setdefault('statsd.port', 8125)
-
     # Allow easy access to a statsd client as `request.stats`
     config.add_request_method(lambda r: get_client(r.registry.settings),
                               name='stats',
