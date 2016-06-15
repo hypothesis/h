54ca2e00a66aea72e9ac9e3139a7a0a981f5da39
Robert Knight <robertknight@gmail.com>
2016-06-15 08:14:19 +0100

    Make the standalone annotation page work for replies
    When displaying a standalone annotation page for replies, display the
    entire conversation thread which that reply is in, including the
    original annotation.
    
    Previously standalone annotation pages for replies were broken because
    only the reply itself was fetched, the the existence of the top-level
    annotation was inferred from the reply's `references` field but because
    it was missing, a blank "Message not available" card was shown instead.
    
    Showing the entire thread fixes this problem and also allows the user to
    see the complete context of the conversation.
    
    Also refactor the tests to be less tied to the implementation details of
    the class and expressed more in terms of what the view does from a
    user's point of view.
    
    Fixes #3367

--- h/static/scripts/annotation-viewer-controller.js
+++ h/static/scripts/annotation-viewer-controller.js
@@ -2,6 +2,28 @@
 
 var angular = require('angular');
 
+/**
+ * Fetch all annotations in the same thread as `id`.
+ *
+ * @return Promise<Array<Annotation>>
+ */
+function fetchThread(store, id) {
+  var annot;
+  return store.AnnotationResource.get({id: id}).$promise.then(function (annot) {
+    if (annot.references && annot.references.length) {
+      // This is a reply, fetch the top-level annotation
+      return store.AnnotationResource.get({id: annot.references[0]}).$promise;
+    } else {
+      return annot;
+    }
+  }).then(function (annot_) {
+    annot = annot_;
+    return store.SearchResource.get({references: annot.id}).$promise;
+  }).then(function (searchResult) {
+    return [annot].concat(searchResult.rows);
+  });
+}
+
 // @ngInject
 function AnnotationViewerController (
   $location, $routeParams, $scope,
@@ -33,20 +55,27 @@ function AnnotationViewerController (
     annotationUI.setCollapsed(id, collapsed);
   };
 
-  store.AnnotationResource.get({ id: id }, function (annotation) {
-    annotationMapper.loadAnnotations([annotation]);
+  this.ready = fetchThread(store, id).then(function (annots) {
+    annotationMapper.loadAnnotations(annots);
+
+    var topLevelID = annots.filter(function (annot) {
+      return (annot.references || []).length === 0;
+    })[0];
+
+    if (!topLevelID) {
+      return;
+    }
+
+    streamFilter
+      .setMatchPolicyIncludeAny()
+      .addClause('/references', 'one_of', topLevelID, true)
+      .addClause('/id', 'equals', topLevelID, true);
+    streamer.setConfig('filter', { filter: streamFilter.getFilter() });
+
+    annots.forEach(function (annot) {
+      annotationUI.setCollapsed(annot.id, false);
+    });
   });
-
-  store.SearchResource.get({ references: id }, function (data) {
-    annotationMapper.loadAnnotations(data.rows);
-  });
-
-  streamFilter
-    .setMatchPolicyIncludeAny()
-    .addClause('/references', 'one_of', id, true)
-    .addClause('/id', 'equals', id, true);
-
-  streamer.setConfig('filter', { filter: streamFilter.getFilter() });
 }
 
 module.exports = AnnotationViewerController;
