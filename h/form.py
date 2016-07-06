1db5882a7a4629da484f7a434a733b1a7b12dabd
Sean Hammond <git@snhmnd.fastmail.com>
2016-07-06 14:25:55 +0100

    Escape user input in Deform forms
    Replace deform_jinja2.jinja2_renderer_factory() with our own Deform
    Jinja2 renderer function which uses our existing Jinja2 environment
    (which escapes user input because it has auto-escaping enabled).'
    
    deform_jinja2.jinja2_renderer_factory() does not escape user text
    rendered in form fields - it doesn't turn on Jinja2's autoescaping, and
    there's no way for a caller of this function to turn on autoescaping.
    
    Rather than manually escape every instance of user text in our Deform
    Jinja2 templates, replace deform_jinja2.jinja2_renderer_factory() with
    our own renderer function that uses our existing Jinja2 environment that
    has autoescaping enabled.

--- h/form.py
+++ h/form.py
@@ -7,6 +7,10 @@ form templates in preference to the defaults. Uses `deform_jinja2` to provide
 the fallback templates in Jinja2 format, which we can then extend and modify as
 necessary.
 """
+import deform
+import jinja2
+import pyramid_jinja2
+from pyramid.path import AssetResolver
 
 
 SEARCH_PATHS = (
@@ -15,16 +19,53 @@ SEARCH_PATHS = (
 )
 
 
-def init():
-    from deform import Form
-    from deform_jinja2 import jinja2_renderer_factory
-    from deform_jinja2.translator import PyramidTranslator
+class Jinja2Renderer(object):
+    """
+    An alternate Deform renderer that uses Jinja2 to render templates.
 
-    renderer = jinja2_renderer_factory(search_paths=SEARCH_PATHS,
-                                       translator=PyramidTranslator())
+    We supply our own Jinja2 renderer because deform_jinja2's one doesn't
+    support autoescaping.
+    """
 
-    Form.set_default_renderer(renderer)
+    def __init__(self, base_env):
+        """
+        Return a new callable Jinja2Renderer object.
+
+        :param base_env: the initial Jinja2 environment that this renderer's
+            Jinja2 environment will be created as an overlay of
+        :type base_env: :py:class:`jinja2.Environment`
+        """
+        self._base_env = base_env
+        self._env = None
+
+    def _get_env(self):
+        """
+        Return this renderer's Jinja2 environment.
+
+        Initialize the environment if it hasn't been initialized already.
+        """
+        if not self._env:
+            resolver = AssetResolver()
+            searchpath = [resolver.resolve(path).abspath()
+                          for path in SEARCH_PATHS]
+            loader = pyramid_jinja2.SmartAssetSpecLoader(searchpath)
+            self._env = self._base_env.overlay(loader=loader)
+        return self._env
+
+    def __call__(self, template_name, **kwargs):
+        """Return the given template rendered with the given keyword args."""
+        if not template_name.endswith('.jinja2'):
+            template_name += '.jinja2'
+
+        template = self._get_env().get_template(template_name)
+
+        return jinja2.Markup(template.render(**kwargs))
 
 
-def includeme(_):
-    init()
+def initialize_deform_renderer(config):
+    deform.Form.set_default_renderer(
+        Jinja2Renderer(config.get_jinja2_environment()))
+
+
+def includeme(config):
+    config.action(None, initialize_deform_renderer, args=[config])
