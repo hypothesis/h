db5c058eb42148b59bca6ce948cb1338f5d79dfc
Robert Knight <robertknight@gmail.com>
2016-07-28 14:41:44 +0100

    Expose feature flags to deform templates (#3637)
    Add a mechanism to customize the rendering of form controls by deform
    based on properties of the request, such as the active feature flags.
    
     * Add a `create_form` factory function on the request which returns
       a deform.Form that is configured to use a renderer which exposes the
       request's feature flags as a `feature` callable in the context
    
     * Replace all direct uses of `deform.Form` with `request.create_form`
    
     * Remove global override for deform.Form renderer as it is now unused

--- h/form.py
+++ h/form.py
@@ -13,6 +13,8 @@ import pyramid_jinja2
 from pyramid.path import AssetResolver
 
 
+ENVIRONMENT_KEY = 'h.form.jinja2_environment'
+
 SEARCH_PATHS = (
     'h:templates/deform/',
     'deform_jinja2:bootstrap_templates/',
@@ -20,52 +22,73 @@ SEARCH_PATHS = (
 
 
 class Jinja2Renderer(object):
-    """
-    An alternate Deform renderer that uses Jinja2 to render templates.
+    """An alternate Deform renderer that uses Jinja2 to render templates."""
 
-    We supply our own Jinja2 renderer because deform_jinja2's one doesn't
-    support autoescaping.
-    """
-
-    def __init__(self, base_env):
+    def __init__(self, env, system=None):
         """
         Return a new callable Jinja2Renderer object.
 
-        :param base_env: the initial Jinja2 environment that this renderer's
-            Jinja2 environment will be created as an overlay of
-        :type base_env: :py:class:`jinja2.Environment`
-        """
-        self._base_env = base_env
-        self._env = None
+        :param env: the Jinja2 environment used by this renderer
+        :type env: :py:class:`jinja2.Environment`
 
-    def _get_env(self):
+        :param system: a dictionary of system renderer globals
+        :type system: dict
         """
-        Return this renderer's Jinja2 environment.
-
-        Initialize the environment if it hasn't been initialized already.
-        """
-        if not self._env:
-            resolver = AssetResolver()
-            searchpath = [resolver.resolve(path).abspath()
-                          for path in SEARCH_PATHS]
-            loader = pyramid_jinja2.SmartAssetSpecLoader(searchpath)
-            self._env = self._base_env.overlay(autoescape=True, loader=loader)
-        return self._env
+        self._env = env
+        self._system = system if system is not None else {}
 
     def __call__(self, template_name, **kwargs):
-        """Return the given template rendered with the given keyword args."""
+        """Render the named template with the passed keywords as context."""
         if not template_name.endswith('.jinja2'):
             template_name += '.jinja2'
 
-        template = self._get_env().get_template(template_name)
+        template = self._env.get_template(template_name)
+        context = self._system.copy()
+        context.update(kwargs)
 
-        return jinja2.Markup(template.render(**kwargs))
-
-
-def initialize_deform_renderer(config):
-    deform.Form.set_default_renderer(
-        Jinja2Renderer(config.get_jinja2_environment()))
+        return jinja2.Markup(template.render(context))
 
 
-def includeme(config):
-    config.action(None, initialize_deform_renderer, args=[config])
+def create_environment(base):
+    """
+    Create a Jinja2 environment for rendering forms.
+
+    Creates an overlay environment based on the passed `base` environment that
+    is suitable for rendering the Deform templates.
+    """
+    # Build a template loader based on SEARCH_PATHS
+    resolver = AssetResolver()
+    searchpath = [resolver.resolve(path).abspath() for path in SEARCH_PATHS]
+    loader = pyramid_jinja2.SmartAssetSpecLoader(searchpath)
+
+    # Make an overlay environment from the main Jinja2 environment. See:
+    #
+    #   http://jinja.pocoo.org/docs/dev/api/#jinja2.Environment.overlay
+    return base.overlay(autoescape=True, loader=loader)
+
+
+def create_form(request, *args, **kwargs):
+    """
+    Create a :py:class:`deform.Form` instance for this request.
+
+    This request method creates a :py:class:`deform.Form` object which (by
+    default) will use the renderer configured in the :py:mod:`h.form` module.
+    """
+    env = request.registry[ENVIRONMENT_KEY]
+    renderer = Jinja2Renderer(env, {
+        'feature': request.feature,
+    })
+    kwargs.setdefault('renderer', renderer)
+
+    return deform.Form(*args, **kwargs)
+
+
+def configure_environment(config):  # pragma: no cover
+    """Configure the form template environment and store it in the registry."""
+    base = config.get_jinja2_environment()
+    config.registry[ENVIRONMENT_KEY] = create_environment(base)
+
+
+def includeme(config):  # pragma: no cover
+    config.action(None, configure_environment, args=(config,))
+    config.add_request_method(create_form)
