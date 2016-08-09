4cfcf9ea1596a7ba2e999394cc42ef237956ed11
chdorner <christof@chdorner.com>
2016-08-09 13:19:03 +0200

    Ignore block and inline math when rendering Markdown

--- src/memex/markdown.py
+++ src/memex/markdown.py
@@ -10,6 +10,45 @@ import mistune
 markdown = None
 
 
+class MathMarkdown(mistune.Markdown):
+    def output_block_math(self):
+        return self.renderer.block_math(self.token['text'])
+
+
+class MathInlineLexer(mistune.InlineLexer):
+    def __init__(self, *args, **kwargs):
+        super(MathInlineLexer, self).__init__(*args, **kwargs)
+        self.rules.inline_math = re.compile(r'\\\((.*?)\\\)', re.DOTALL)
+        self.default_rules.insert(0, 'inline_math')
+
+    def output_inline_math(self, m):
+        return self.renderer.inline_math(m.group(1))
+
+
+class MathBlockLexer(mistune.BlockLexer):
+    def __init__(self, *args, **kwargs):
+        super(MathBlockLexer, self).__init__(*args, **kwargs)
+        self.rules.block_math = re.compile(r'^\$\$(.*?)\$\$', re.DOTALL)
+        self.default_rules.insert(0, 'block_math')
+
+    def parse_block_math(self, m):
+        self.tokens.append({
+            'type': 'block_math',
+            'text': m.group(1)
+        })
+
+
+class MathRenderer(mistune.Renderer):
+    def __init__(self, **kwargs):
+        super(MathRenderer, self).__init__(**kwargs)
+
+    def block_math(self, text):
+        return '<p>$$%s$$</p>\n' % text
+
+    def inline_math(self, text):
+        return '\\(%s\\)' % text
+
+
 def render(text):
     if text is not None:
         render = _get_markdown()
@@ -20,5 +59,8 @@ def render(text):
 def _get_markdown():
     global markdown
     if markdown is None:
-        markdown = mistune.Markdown(escape=True)
+        markdown = MathMarkdown(renderer=MathRenderer(),
+                                inline=MathInlineLexer,
+                                block=MathBlockLexer,
+                                escape=True)
     return markdown
--- tests/memex/markdown_test.py
+++ tests/memex/markdown_test.py
@@ -8,3 +8,13 @@ from memex import markdown
 def test_render_renders_markdown():
     actual = markdown.render('_emphasis_ **bold**')
     assert '<p><em>emphasis</em> <strong>bold</strong></p>\n' == actual
+
+
+def test_render_ignores_math_block():
+    actual = markdown.render('$$1 + 1 = 2$$')
+    assert '<p>$$1 + 1 = 2$$</p>\n' == actual
+
+
+def test_render_ignores_inline_match():
+    actual = markdown.render('Foobar \(1 + 1 = 2\)')
+    assert '<p>Foobar \(1 + 1 = 2\)</p>\n' == actual
