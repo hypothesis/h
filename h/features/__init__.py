6b849da5d613203c7d5a241b2b27abc318d5f11f
Sean Hammond <git@snhmnd.fastmail.com>
2016-08-10 16:39:23 +0100

    Load feature flags on NewRequest

--- h/features/__init__.py
+++ h/features/__init__.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 
 import os
 
+from pyramid.events import NewRequest
+
 from h import db
 from h.features import client
 from h.features import models
@@ -32,6 +34,17 @@ def includeme(config):
     config.include('h.features.views')
 
     config.add_request_method(Client, name='feature', reify=True)
+
+    # Load the feature flags from the database at the beginning of each request.
+    # This prevents sqlalchemy DetachedInstanceErrors that can occur if the
+    # feature flags client tries to load the feature flags later on and the
+    # database session has already been closed.
+    # This is done on NewRequest to make sure that things like
+    # request.effective_principals are already setup, so that we get the correct
+    # feature flags.
+    config.add_subscriber(lambda event: event.request.feature.load(),
+                          NewRequest)
+
     config.add_subscriber(_remove_old_flags_on_boot,
                           'pyramid.events.ApplicationCreated')
 
