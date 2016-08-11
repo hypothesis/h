f2e9572fb7a2942920881fb00bc568945401527b
Robert Knight <robertknight@gmail.com>
2016-08-11 16:21:46 +0100

    Add a helper for generating SVG icons for use on the website (#3675)
    The helper generates icons using inline SVG, following an approach
    described at https://github.com/blog/2112-delivering-octicons-with-svg
    
    This gives us a nice way to do vector icons which can be customized
    using CSS.
    
    In order for icons to be colorable using CSS, they must set the 'fill'
    or 'stroke' properties of paths to 'currentColor'

--- h/jinja_extensions.py
+++ h/jinja_extensions.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import datetime
+from functools import partial
 import json
+import re
+
+try:
+    from xml.etree import cElementTree as ElementTree
+except ImportError:
+    from xml.etree import ElementTree
 
 from jinja2 import Markup
 from jinja2.ext import Extension
+
+SVG_NAMESPACE_URI = 'http://www.w3.org/2000/svg'
 
 
 class Filters(Extension):
@@ -31,3 +40,51 @@ def human_timestamp(timestamp, now=datetime.datetime.utcnow):
 def to_json(value):
     """Convert a dict into a JSON string"""
     return Markup(json.dumps(value))
+
+
+class SvgIcon(Extension):
+
+    """
+    Setup helpers for rendering icons.
+    """
+
+    def __init__(self, environment):
+        super(SvgIcon, self).__init__(environment)
+
+        def read_icon(name):
+            return open('build/images/icons/{}.svg'.format(name)).read()
+
+        environment.globals['svg_icon'] = partial(svg_icon, read_icon)
+
+
+def svg_icon(loader, name, css_class=''):
+    """
+    Return inline SVG markup for an icon.
+
+    This is a helper for generating inline SVGs for rendering icons in HTML
+    that can be customized via CSS.
+    See https://github.com/blog/2112-delivering-octicons-with-svg
+
+    :param loader: Callable accepting an icon name and returning XML markup for
+                   the SVG.
+    :param name: The name of the SVG file to render
+    :param css_class: CSS class attribute for the returned `<svg>` element
+    """
+
+    # Register SVG as the default namespace. This avoids a problem where
+    # ElementTree otherwise serializes SVG elements with an 'ns0' namespace (eg.
+    # '<ns0:svg>...') and browsers will not render the result as SVG.
+    # See http://stackoverflow.com/questions/8983041
+    ElementTree.register_namespace('', SVG_NAMESPACE_URI)
+    root = ElementTree.fromstring(loader(name))
+
+    if css_class:
+        root.set('class', css_class)
+
+    # If the SVG has its own title, ignore it in favor of the title attribute
+    # of the <svg> or its containing element, which is usually a link.
+    title_el = root.find('{{{}}}title'.format(SVG_NAMESPACE_URI))
+    if title_el is not None:
+        root.remove(title_el)
+
+    return Markup(ElementTree.tostring(root))
