7d3270238060466cc4f4702572008e5b4c5a00a0
Nick Stenning <nick@whiteink.com>
2016-08-15 18:19:58 +0200

    Add migration to fill in user authority column
    Add a migration to fill in the user authority for old data. This is
    executed as a single UPDATE query and the value 'hypothes.is' is
    hard-coded.
    
    Even with ~50K rows in the table this is pretty fast and so it's
    unlikely we'll need to do anything more intelligent.

---
+++ h/migrations/versions/2e2cc6a0c521_fill_in_user_authority_column.py
@@ -0,0 +1,26 @@
+"""
+Fill in user authority column
+
+Revision ID: 2e2cc6a0c521
+Revises: f48100c9af86
+Create Date: 2016-08-15 18:13:08.372479
+"""
+
+from __future__ import unicode_literals
+
+import sqlalchemy as sa
+from alembic import op
+
+revision = '2e2cc6a0c521'
+down_revision = 'f48100c9af86'
+
+user_table = sa.table('user', sa.Column('authority', sa.UnicodeText()))
+
+
+def upgrade():
+    op.execute(user_table.update().values(authority='hypothes.is'))
+
+
+def downgrade():
+    pass
+
