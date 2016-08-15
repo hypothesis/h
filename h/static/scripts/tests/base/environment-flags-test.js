4e9915421879c0a4a71f25f66fd31ab3cd01e962
Robert Knight <robertknight@gmail.com>
2016-08-15 14:57:02 +0100

    Add a mechanism for adjusting element styling depending on browser capabilities
    This adds a simple system and CSS helpers for adjusting the styling and
    behavior of elements depending on whether the client is JS-capable and
    whether scripts successfully loaded or not.
    
     - Add an inline script to the <head> element of every page which sets
       `env-$flag` classes on the <html> element to indicate support for
       running scripts (.env-js-capable) and touch support (.env-touch)
    
     - Add mixin which supports defining CSS selectors which only apply if
       the browser is JS capable and scripts successfully loaded

---
+++ h/static/scripts/tests/base/environment-flags-test.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var EnvironmentFlags = require('../../base/environment-flags');
+
+var TIMEOUT_DELAY = 10000;
+
+describe('EnvironmentFlags', function () {
+  var clock;
+  var el;
+  var flags;
+
+  beforeEach(function () {
+    el = document.createElement('div');
+    flags = new EnvironmentFlags(el);
+    clock = sinon.useFakeTimers();
+  });
+
+  afterEach(function () {
+    clock.restore();
+  });
+
+  describe('#init', function () {
+    it('should mark document as JS capable on load', function () {
+      flags.init();
+      assert.isTrue(el.classList.contains('env-js-capable'));
+    });
+
+    it('should mark JS load as failed after a timeout', function () {
+      flags.init();
+      clock.tick(TIMEOUT_DELAY);
+      assert.isTrue(el.classList.contains('env-js-timeout'));
+    });
+  });
+
+  describe('#ready', function () {
+    it('should prevent JS load timeout flag from being set', function () {
+      flags.init();
+      flags.ready();
+      clock.tick(TIMEOUT_DELAY);
+      assert.isFalse(el.classList.contains('env-js-timeout'));
+    });
+
+    it('should clear timeout flag if already set', function () {
+      flags.init();
+      clock.tick(TIMEOUT_DELAY);
+      flags.ready();
+      assert.isFalse(el.classList.contains('env-js-timeout'));
+    });
+  });
+
+  describe('#set', function () {
+    it('should add a flag if `on` is true', function () {
+      flags.set('shiny-feature', true);
+      assert.isTrue(el.classList.contains('env-shiny-feature'));
+    });
+
+    it('should remove a flag if `on` is false', function () {
+      flags.set('shiny-feature', false);
+      assert.isFalse(el.classList.contains('env-shiny-feature'));
+    });
+  });
+
+  describe('#get', function () {
+    it('should return true if the flag is set', function () {
+      flags.set('shiny-feature', true);
+      assert.isTrue(flags.get('shiny-feature'));
+    });
+
+    it('should return false if the flag is set', function () {
+      assert.isFalse(flags.get('shiny-feature'));
+    });
+  });
+});
