ba0c4288b6e8b589a1727f931b39a7221bb0bacb
Sean Hammond <seanh@users.noreply.github.com>
2016-08-23 12:16:06 +0100

    Add a docstring to h.models (#3756)

--- h/models.py
+++ h/models.py
@@ -1,4 +1,22 @@
 # -*- coding: utf-8 -*-
+"""
+A module into which all ORM classes are imported.
+
+To avoid circular imports almost all code should import ORM classes from this
+module rather than importing them directly,
+``from h import models`` rather than ``from h.foo import models``
+
+This is a convenience - you can just import this one module and all of the
+ORM classes will be defined, instead of having to import every models module
+individually.
+
+For example when testing ORM classes the test module for ``h.foo.models.Bar``
+can't just import ``h.foo.models``, it would also need to import the models
+module for each database table that ``Bar`` has a (direct or indirect) foreign
+key to. So for convenience the test module can instead just do
+``from h import models`` and have all ORM classes be defined.
+
+"""
 
 from h.accounts import models as accounts_models
 from h.auth import models as auth_models
