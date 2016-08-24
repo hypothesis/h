c2e480b3e1529e4c4c78bf948a3408f9f9bca35b
Nick Stenning <nick@whiteink.com>
2016-08-24 16:53:59 +0100

    Correct typo in debug logging

--- h/views/main.py
+++ h/views/main.py
@@ -111,7 +111,7 @@ def _configure_noisy_session_logging(request):
 
     @event.listens_for(request.db, 'after_flush')
     def after_flush(sess, flush_context):
-        log.info('after_attach sess=%r flush_context=%r', sess, flush_context)
+        log.info('after_flush sess=%r flush_context=%r', sess, flush_context)
 
     @event.listens_for(request.db, 'before_commit')
     def before_commit(sess):
