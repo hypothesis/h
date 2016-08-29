a0745333a0d9d462634dd606161a7e0d0586f905
Nick Stenning <nick@whiteink.com>
2016-08-29 11:04:34 +0200

    Remove tm.activate_hook
    Although any bug was eliminated by the previous commit, we can also just
    remove the transaction manager `activate_hook`. If the requests in
    question don't actually use the database, it provides no performance
    gain -- indeed, it's intended more for things such as websockets or
    long-polling endpoints that want to manage their own transaction
    lifecycle.

--- h/app.py
+++ h/app.py
@@ -24,12 +24,6 @@ def in_debug_mode(request):
     return asbool(request.registry.settings.get('pyramid.debug_all'))
 
 
-def tm_activate_hook(request):
-    if request.path.startswith(('/assets/', '/_debug_toolbar/')):
-        return False
-    return True
-
-
 def create_app(global_config, **settings):
     """
     Create the h WSGI application.
@@ -84,7 +78,6 @@ def includeme(config):
     config.add_settings({
         "tm.attempts": 3,
         "tm.manager_hook": lambda request: transaction.TransactionManager(),
-        "tm.activate_hook": tm_activate_hook,
         "tm.annotate_user": False,
     })
     config.include('pyramid_tm')
