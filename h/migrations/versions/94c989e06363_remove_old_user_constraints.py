9371b243885680f91fc7beb6c42cdd9d88fb5240
Nick Stenning <nick@whiteink.com>
2016-09-08 16:28:54 +0200

    Add schema migrations to update user uniqueness constraints
    Add two migrations to update the uniqueness constraints on the user
    table.
    
    The first, b0e1a12de5e8, renames the existing constraints, and
    then adds new constraints in a way that won't interfere with table
    writes. This is a reversible migration
    
    The second, 94c989e06363, removes the old single-column constraints that
    were renamed by the first migration.

---
+++ h/migrations/versions/94c989e06363_remove_old_user_constraints.py
@@ -0,0 +1,28 @@
+"""
+Remove old user constraints
+
+These were created in b0e1a12de5e8 in order to avoid making that migration
+irreversible. This is the irreversible part.
+
+Revision ID: 94c989e06363
+Revises: b0e1a12de5e8
+Create Date: 2016-09-08 16:21:25.444258
+"""
+
+from __future__ import unicode_literals
+
+from alembic import op
+
+
+revision = '94c989e06363'
+down_revision = 'b0e1a12de5e8'
+
+
+def upgrade():
+    op.drop_constraint('uq__user__email_old', 'user')
+    op.drop_constraint('uq__user__uid_old', 'user')
+    op.drop_constraint('uq__user__username_old', 'user')
+
+
+def downgrade():
+    pass
