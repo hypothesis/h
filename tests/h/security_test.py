0204664259dbf6f23d129c53163838731c2fb359
Nick Stenning <nick@whiteink.com>
2016-09-08 13:03:26 +0200

    Add a utility function for generating secure tokens
    Adds a token_urlsafe utility function to `h.security` for generating
    cryptographically random secure tokens.
    
    The implementation is modelled heavily on the implementation of the
    function of the same name in Python 3.6's `secrets` module.

--- tests/h/security_test.py
+++ tests/h/security_test.py
@@ -3,10 +3,31 @@
 from __future__ import unicode_literals
 
 from passlib.context import CryptContext
+from hypothesis import strategies as st
+from hypothesis import given
 
+from h._compat import text_type
 from h.security import password_context
+from h.security import token_urlsafe
 
 
 def test_password_context():
     assert isinstance(password_context, CryptContext)
     assert len(password_context.schemes()) > 0
+
+
+@given(nbytes=st.integers(min_value=1, max_value=64))
+def test_token_urlsafe(nbytes):
+    tok = token_urlsafe(nbytes)
+
+    # Should be text
+    assert isinstance(tok, text_type)
+    # Always at least nbytes of data
+    assert len(tok) > nbytes
+
+
+def test_token_urlsafe_no_args():
+    tok = token_urlsafe()
+
+    assert isinstance(tok, text_type)
+    assert len(tok) > 32
