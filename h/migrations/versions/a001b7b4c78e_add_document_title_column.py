c328d742129f64cc6e1842f8624992c581df8cb9
chdorner <christof@chdorner.com>
2016-09-12 12:03:01 +0200

    Add migration for Document.title column

---
+++ h/migrations/versions/a001b7b4c78e_add_document_title_column.py
@@ -0,0 +1,24 @@
+"""
+Add document title column
+
+Revision ID: a001b7b4c78e
+Revises: 94c989e06363
+Create Date: 2016-09-12 11:59:35.296908
+"""
+
+from __future__ import unicode_literals
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = 'a001b7b4c78e'
+down_revision = '94c989e06363'
+
+
+def upgrade():
+    op.add_column('document', sa.Column('title', sa.UnicodeText()))
+
+
+def downgrade():
+    op.drop_column('document', 'title')
