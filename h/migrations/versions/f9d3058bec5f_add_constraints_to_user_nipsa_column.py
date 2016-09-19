41b98768ded92b1bc921d3370ac44a3bf7b1e52d
Nick Stenning <nick@whiteink.com>
2016-09-19 13:11:06 +0200

    Add model constraints to user NIPSA column
    Adds a database schema migration to add the NOT NULL constraint and the
    server default specified in the User model.

---
+++ h/migrations/versions/f9d3058bec5f_add_constraints_to_user_nipsa_column.py
@@ -0,0 +1,25 @@
+"""
+Add constraints to user NIPSA column
+
+Revision ID: f9d3058bec5f
+Revises: b7117b569f8b
+Create Date: 2016-09-19 13:07:58.098068
+"""
+
+from __future__ import unicode_literals
+
+import sqlalchemy as sa
+from alembic import op
+
+
+revision = 'f9d3058bec5f'
+down_revision = 'b7117b569f8b'
+
+
+def upgrade():
+    op.alter_column('user', 'nipsa', nullable=False)
+    op.alter_column('user', 'nipsa', server_default=sa.sql.expression.false())
+
+def downgrade():
+    op.alter_column('user', 'nipsa', nullable=True)
+    op.alter_column('user', 'nipsa', server_default=None)
