07d155fcc771631e4063e488685c6d7c019ace10
Robert Knight <robertknight@gmail.com>
2016-09-21 12:05:36 +0100

    Add client helper for performing XHR submissions
    Add a `submitForm()` function which handles submitting a form on the
    client and returning a promise that resolves to the re-rendered form and
    status code, or the error if form submission fails.
    
    The modern `fetch()` API is used rather than XHR since this provides a
    more convenient Promise-based interface. It is polyfilled for older
    browsers.

---
+++ h/static/scripts/util/submit-form.js
@@ -0,0 +1,73 @@
+'use strict';
+
+/**
+ * @typedef SubmitError
+ * @property {number} status - HTTP status code. 400 if form submission failed
+ *           due to a validation error or a different 4xx or 5xx code if it
+ *           failed for other reasons.
+ * @property {string} [form] - HTML markup for the form containing validation
+ *           error messages if submission failed with a 400 status.
+ * @property {string} [reason] - The status message if form submission failed
+ *           for reasons other than a validation error.
+ */
+
+/**
+ * Exception thrown if form submission fails.
+ *
+ * @property {SubmitError} params - Describes why submission failed. These
+ *           properties are exposed on the FormSubmitError instance.
+ */
+class FormSubmitError extends Error {
+  constructor(message, params) {
+    super(message);
+    Object.assign(this, params);
+  }
+}
+
+/**
+ * @typedef {Object} SubmitResult
+ * @property {number} status - Always 200
+ * @property {string} form - The HTML markup for the re-rendered form
+ */
+
+/**
+ * Submit a form using the Fetch API and return the markup for the re-rendered
+ * version of the form.
+ *
+ * @param {HTMLFormElement} formEl - The `<form>` to submit
+ * @return {Promise<SubmitResult>} A promise which resolves when the form
+ *         submission completes or rejects with a FormSubmitError if the server
+ *         rejects the submission due to a validation error or the network
+ *         request fails.
+ */
+function submitForm(formEl, fetch = window.fetch) {
+  var response;
+  return fetch(formEl.action, {
+    body: new FormData(formEl),
+    credentials: 'same-origin',
+    method: 'POST',
+    headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+    },
+  }).then(response_ => {
+    response = response_;
+    return response.text();
+  }).then(body => {
+    var { status } = response;
+    switch (status) {
+    case 200:
+      return {status, form: body};
+    case 400:
+      throw new FormSubmitError('Form validation failed', {
+        status, form: body,
+      });
+    default:
+      throw new FormSubmitError('Form submission failed', {
+        status,
+        reason: response.statusText,
+      });
+    }
+  });
+}
+
+module.exports = submitForm;
