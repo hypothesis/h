2c692560f5c2f833ffc65470b4d47548676ffd01
Robert Knight <robertknight@gmail.com>
2016-09-21 17:59:39 +0100

    Consistency fixes for SignupFormController (#3902)
    Refactor SignupFormController to follow the pattern of other controllers
    in inheriting from the Controller base class.
    
     * Fix lint errors about unused variables and missing semis
    
     * Use template string in test
    
     * Remove sendEvent() helper which is not required in PhantomJS 2.x

--- h/static/scripts/tests/controllers/signup-form-controller-test.js
+++ h/static/scripts/tests/controllers/signup-form-controller-test.js
@@ -1,39 +1,29 @@
 'use strict';
 
-var SignupFormController = require('../../controllers/signup-form-controller')
+var SignupFormController = require('../../controllers/signup-form-controller');
 
-// helper to dispatch a native event to an element
-function sendEvent(element, eventType) {
-  // createEvent() used instead of Event constructor
-  // for PhantomJS compatibility
-  var event = document.createEvent('Event');
-  event.initEvent(eventType, true /* bubbles */, true /* cancelable */);
-  element.dispatchEvent(event);
-}
+var TEMPLATE = `
+  <form class="js-signup-form">
+    <input type="submit" class="js-signup-btn">
+  </form>
+  `;
 
-describe('SignupFormController', function() {
+describe('SignupFormController', function () {
   var element;
-  var template;
   var form;
   var submitBtn;
 
-  before(function () {
-    template = '<form class="js-signup-form">' +
-               '<input type="submit" class="js-signup-btn">' +
-               '</form>'
-  });
-
   beforeEach(function () {
     element = document.createElement('div');
-    element.innerHTML = template;
+    element.innerHTML = TEMPLATE;
     form = element.querySelector('.js-signup-form');
     submitBtn = element.querySelector('.js-signup-btn');
   });
 
   it('disables the submit button on form submit', function () {
-    var controller = new SignupFormController(form);
+    new SignupFormController(form);
     assert.isFalse(submitBtn.disabled);
-    sendEvent(form, 'submit');
+    form.dispatchEvent(new Event('submit'));
     assert.isTrue(submitBtn.disabled);
   });
 });
