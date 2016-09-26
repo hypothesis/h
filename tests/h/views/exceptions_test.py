40189dab5d5de0d217775d8e2d73f5fcb6c4ab9a
Nick Stenning <nick@whiteink.com>
2016-09-26 12:44:49 +0200

    Extract API exception views into h/views/api_exceptions.py
    This commit extracts the exception views for the API and JSON requests
    into its own view module, h/views/api_exceptions.py
    
    Changes include:
    
    - Updating the "reason" supplied for a 403/404 to be a human-readable
      string rather than "not_found" as before. This makes the response
      consistent with other JSON error responses.
    - Adding a "status" field to the response for an uncaught exception when
      the request specifies "Accept: application/json". This makes the
      response consistent with other JSON error responses.
    - Adding an exception view to catch deliberately thrown APIErrors. This
      is not currently used.
    - Tests added for both application and API exception views.
    - We rely on a `config.scan(...)` of the whole `h.views` package rather
      than having to define an `includeme` in each view module.

---
+++ tests/h/views/exceptions_test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from h.views.exceptions import notfound, error
+
+
+def test_notfound_view(pyramid_request):
+    result = notfound(pyramid_request)
+
+    assert pyramid_request.response.status_int == 404
+    assert result == {}
+
+
+def test_error_view(patch, pyramid_request):
+    handle_exception = patch('h.views.exceptions.handle_exception')
+
+    result = error(pyramid_request)
+
+    handle_exception.assert_called_once_with(pyramid_request)
+    assert result == {}
