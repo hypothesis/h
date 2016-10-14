fbb84394a7a47d0bd5153114583c07ac57497bd5
Ricky Han <rickylqhan@gmail.com>
2016-10-14 07:14:25 +0000

    new changes

--- h/views/main.py
+++ h/views/main.py
@@ -57,6 +57,7 @@ def robots(context, request):
 @view_config(route_name='stream')
 @view_config(route_name='stream_login')
 @view_config(route_name='stream_logout')
+@view_config(route_name='stream_forgot')
 def stream(context, request):
     atom = request.route_url('stream_atom')
     rss = request.route_url('stream_rss')
