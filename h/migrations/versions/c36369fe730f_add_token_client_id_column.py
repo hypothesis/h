177c4e0fceca754cf91e3046e714d877154e0224
chdorner <christof@chdorner.com>
2016-10-19 15:48:45 +0200

    Add migration for token.authclient_id column
    This way we can track which authclient is responsible for which tokens,
    how many active users an authclient has, etc.
    
    It is also useful for the future when everybody will be able to create
    authclients, because when an authclient gets deleted all tokens need to
    be removed to make sure that they can't be used anymore for making
    requests.

---
+++ h/migrations/versions/c36369fe730f_add_token_client_id_column.py
@@ -0,0 +1,30 @@
+"""
+Add token.client_id column
+
+Revision ID: c36369fe730f
+Revises: e15e47228c43
+Create Date: 2016-10-19 15:24:13.387546
+"""
+
+from __future__ import unicode_literals
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+
+revision = 'c36369fe730f'
+down_revision = 'e15e47228c43'
+
+
+def upgrade():
+    op.add_column('token', sa.Column(
+        'authclient_id',
+        postgresql.UUID(),
+        sa.ForeignKey('authclient.id', ondelete='cascade'),
+        nullable=True,
+    ))
+
+
+def downgrade():
+    op.drop_column('token', 'authclient_id')
