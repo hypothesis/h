3b9b7e3400e7e2da8ce1d334e5d819c9316afe29
chdorner <christof@chdorner.com>
2016-10-26 17:25:48 +0200

    Add h JSONSchema like memex's
    This is a base class for other schemas that act on JSON data and adhere
    to the jsonschema standard.

---
+++ h/schemas.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""Classes for validating data passed to views."""
+
+from __future__ import unicode_literals
+
+import copy
+import jsonschema
+from jsonschema.exceptions import best_match
+
+
+class ValidationError(Exception):
+    pass
+
+
+class JSONSchema(object):
+    """
+    Validate data according to a Draft 4 JSON Schema.
+
+    Inherit from this class and override the `schema` class property with a
+    valid JSON schema.
+    """
+
+    schema = {}
+
+    def __init__(self):
+        self.validator = jsonschema.Draft4Validator(self.schema)
+
+    def validate(self, data):
+        """
+        Validate `data` according to the current schema.
+
+        :param data: The data to be validated
+        :return: valid data
+        :raises ValidationError: if the data is invalid
+        """
+        # Take a copy to ensure we don't modify what we were passed.
+        appstruct = copy.deepcopy(data)
+        error = best_match(self.validator.iter_errors(appstruct))
+        if error is not None:
+            raise ValidationError(_format_jsonschema_error(error))
+        return appstruct
+
+
+def _format_jsonschema_error(error):
+    """Format a :py:class:`jsonschema.ValidationError` as a string."""
+    if error.path:
+        dotted_path = '.'.join([str(c) for c in error.path])
+        return '{path}: {message}'.format(path=dotted_path,
+                                          message=error.message)
+    return error.message
