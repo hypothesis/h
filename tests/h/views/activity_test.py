0093d58ee8f7c75f2577dfb44fdb1f3e8a7a80ef
Sean Hammond <git@snhmnd.fastmail.com>
2016-11-03 17:16:56 +0000

    Fix test import

--- tests/h/views/activity_test.py
+++ tests/h/views/activity_test.py
@@ -5,8 +5,7 @@ import pytest
 import mock
 from pyramid import httpexceptions
 
-from h.views.activity import PAGE_SIZE
-from h.views.activity import search
+from h.views import activity
 
 
 @pytest.mark.usefixtures('paginate', 'query')
@@ -15,24 +14,24 @@ class TestSearch(object):
         pyramid_request.feature.flags['search_page'] = False
 
         with pytest.raises(httpexceptions.HTTPNotFound):
-            search(pyramid_request)
+            activity.search(pyramid_request)
 
     def test_it_checks_for_redirects(self, pyramid_request, query):
-        search(pyramid_request)
+        activity.search(pyramid_request)
 
         query.check_url.assert_called_once_with(pyramid_request,
                                                 query.extract.return_value)
 
     def test_it_executes_a_search_query(self, pyramid_request, query):
-        search(pyramid_request)
+        activity.search(pyramid_request)
 
         query.execute.assert_called_once_with(pyramid_request,
                                               query.extract.return_value,
-                                              page_size=PAGE_SIZE)
+                                              page_size=activity.PAGE_SIZE)
 
     def test_it_allows_to_specify_the_page_size(self, pyramid_request, query):
         pyramid_request.params['page_size'] = 100
-        search(pyramid_request)
+        activity.search(pyramid_request)
 
         query.execute.assert_called_once_with(pyramid_request,
                                               query.extract.return_value,
@@ -40,15 +39,15 @@ class TestSearch(object):
 
     def test_it_uses_default_page_size_when_value_is_a_string(self, pyramid_request, query):
         pyramid_request.params['page_size'] = 'foobar'
-        search(pyramid_request)
+        activity.search(pyramid_request)
 
         query.execute.assert_called_once_with(pyramid_request,
                                               query.extract.return_value,
-                                              page_size=PAGE_SIZE)
+                                              page_size=activity.PAGE_SIZE)
 
     def test_it_uses_passed_in_page_size_for_pagination(self, pyramid_request, paginate):
         pyramid_request.params['page_size'] = 100
-        search(pyramid_request)
+        activity.search(pyramid_request)
 
         paginate.assert_called_once_with(pyramid_request,
                                          mock.ANY,
