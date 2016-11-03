b7ae11c5c5e2861b12d996ed9b686f993dbf3521
chdorner <christof@chdorner.com>
2016-11-03 15:33:06 +0100

    Allow to specify authority with user admin cli command

--- tests/h/cli/commands/user_test.py
+++ tests/h/cli/commands/user_test.py
@@ -10,7 +10,7 @@ from h.cli.commands import user as user_cli
 class TestAdminCommand(object):
     def test_it_adds_admin(self, cli, cliconfig, non_admin_user, db_session):
         result = cli.invoke(user_cli.admin,
-                            ['--on', non_admin_user.username],
+                            [u'--on', non_admin_user.username],
                             obj=cliconfig)
 
         assert result.exit_code == 0
@@ -28,9 +28,34 @@ class TestAdminCommand(object):
         user = db_session.query(models.User).get(non_admin_user.id)
         assert user.admin
 
+    def test_it_adds_admin_with_specific_authority(self, cli, cliconfig, non_admin_user, db_session):
+        non_admin_user.authority = u'partner.org'
+        db_session.flush()
+
+        result = cli.invoke(user_cli.admin,
+                            [u'--authority', u'partner.org', non_admin_user.username],
+                            obj=cliconfig)
+
+        assert result.exit_code == 0
+
+        user = db_session.query(models.User).get(non_admin_user.id)
+        assert user.admin
+
     def test_it_removes_admin(self, cli, cliconfig, admin_user, db_session):
         result = cli.invoke(user_cli.admin,
-                            ['--off', admin_user.username],
+                            [u'--off', admin_user.username],
+                            obj=cliconfig)
+
+        assert result.exit_code == 0
+
+        user = db_session.query(models.User).get(admin_user.id)
+        assert not user.admin
+
+    def test_it_removes_admin_with_specific_authority(self, cli, cliconfig, admin_user, db_session):
+        admin_user.authority = u'partner.org'
+
+        result = cli.invoke(user_cli.admin,
+                            [u'--off', u'--authority', u'partner.org', admin_user.username],
                             obj=cliconfig)
 
         assert result.exit_code == 0
@@ -41,6 +66,17 @@ class TestAdminCommand(object):
     def test_it_errors_when_user_could_not_be_found(self, cli, cliconfig, non_admin_user, db_session):
         result = cli.invoke(user_cli.admin,
                             ['bogus_%s' % non_admin_user.username],
+                            obj=cliconfig)
+
+        assert result.exit_code == 1
+        user = db_session.query(models.User).get(non_admin_user.id)
+        assert not user.admin
+
+    def test_it_errors_when_user_with_specific_authority_could_not_be_found(
+            self, cli, cliconfig, non_admin_user, db_session):
+
+        result = cli.invoke(user_cli.admin,
+                            ['--authority', 'foo.com', non_admin_user.username],
                             obj=cliconfig)
 
         assert result.exit_code == 1
