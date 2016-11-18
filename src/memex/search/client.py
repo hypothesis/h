ecd7111a1b91f0a849c2d72e4b0ead32d4b0132f
Nick Stenning <nick@whiteink.com>
2016-11-18 14:27:13 +0100

    Add methods for managing aliased indices to Client
    Adds get_aliased_index and update_aliased_index methods to the
    Elasticsearch client which can be used to interrogate and update index
    aliases for the open search index.

--- src/memex/search/client.py
+++ src/memex/search/client.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import elasticsearch
+from elasticsearch import Elasticsearch
+from elasticsearch.exceptions import NotFoundError
+
+__all__ = ('Client',)
 
 
 class Client(object):
@@ -15,13 +18,45 @@ class Client(object):
     :param index: index name
     """
 
-    class t(object):
+    class t(object):  # noqa
         """Document types"""
         annotation = 'annotation'
         document = 'document'
 
     def __init__(self, host, index, **kwargs):
         self.index = index
-        self.conn = elasticsearch.Elasticsearch([host],
-                                                verify_certs=True,
-                                                **kwargs)
+        self.conn = Elasticsearch([host], verify_certs=True, **kwargs)
+
+    def get_aliased_index(self):
+        """
+        Fetch the name of the underlying index.
+
+        Returns ``None`` if the index is not aliased or does not exist.
+        """
+        try:
+            result = self.conn.indices.get_alias(name=self.index)
+        except NotFoundError:  # no alias with that name
+            return None
+        if len(result) > 1:
+            raise RuntimeError("We don't support managing aliases that "
+                               "point to multiple indices at the moment!")
+        return result.keys()[0]
+
+    def update_aliased_index(self, new_target):
+        """
+        Update the alias to point to a new target index.
+
+        Will raise `RuntimeError` if the index is not aliased or does not
+        exist.
+        """
+        old_target = self.get_aliased_index()
+        if old_target is None:
+            raise RuntimeError("Cannot update aliased index for index that "
+                               "is not already aliased.")
+
+        self.conn.indices.update_aliases(body={
+            'actions': [
+                {'add': {'index': new_target, 'alias': self.index}},
+                {'remove': {'index': old_target, 'alias': self.index}},
+            ],
+        })
