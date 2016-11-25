b7163ac1c8195907e69fb55524885d960cdf627a
chdorner <christof@chdorner.com>
2016-11-25 14:41:46 +0100

    Add migration for `group.authority` column

---
+++ h/migrations/versions/3acf258322d5_add_authority_column_to_group.py
@@ -0,0 +1,24 @@
+"""
+Add authority column to group
+
+Revision ID: 3acf258322d5
+Revises: c36369fe730f
+Create Date: 2016-11-24 16:33:40.238726
+"""
+
+from __future__ import unicode_literals
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = '3acf258322d5'
+down_revision = 'c36369fe730f'
+
+
+def upgrade():
+    op.add_column('group', sa.Column('authority', sa.UnicodeText(), nullable=True))
+
+
+def downgrade():
+    op.drop_column('group', 'authority')
