adcff37b2071455e099ca2b98822f1b36f1075ac
Sean Hammond <git@snhmnd.fastmail.com>
2016-12-02 12:55:50 +0000

    Add HasFeatureFlagPredicate
    Allows for Pyramid view config to select one view or another depending
    on whether a given feature flag is on or off.

--- tests/h/views/predicates_test.py
+++ tests/h/views/predicates_test.py
@@ -3,6 +3,31 @@ import mock
 from h.views import predicates
 
 
+class TestHasFeatureFlagPredicate(object):
+
+    def test_text(self):
+        predicate = predicates.HasFeatureFlagPredicate('foo',
+                                                       mock.sentinel.config)
+
+        assert predicate.text() == 'has_feature_flag = foo'
+
+    def test_phash(self):
+        predicate = predicates.HasFeatureFlagPredicate('foo',
+                                                       mock.sentinel.config)
+
+        assert predicate.phash() == 'has_feature_flag = foo'
+
+    def test__call__(self):
+        request = mock.Mock(spec_set=['feature'])
+        predicate = predicates.HasFeatureFlagPredicate('bar',
+                                                       mock.sentinel.config)
+
+        result = predicate(mock.sentinel.context, request)
+
+        request.feature.assert_called_once_with('bar')
+        assert result == request.feature.return_value
+
+
 class TestHasPermissionPredicate(object):
 
     def test_text(self):
