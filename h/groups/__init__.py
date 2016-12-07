ef4145975d155f1a74bd068bcf36d6c185d7deb4
chdorner <christof@chdorner.com>
2016-12-07 17:09:06 +0100

    Implement h group write permission function
    This is our implementation of the function that checks if the currently
    logged-in user is allowed to create an annotation in the given group.
    
    This already takes the new group access flags, specifically
    `writeable_by` into account. There are ideas floating around that our
    "__world__" group should be a stored group as well, until then there is
    a special implementation for when the specified group is "__world__"
    where we check that the user has the same authority as
    `request.auth_domain`.

--- h/groups/__init__.py
+++ h/groups/__init__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from h.groups.auth import annotation_group_write_permitted
+
 
 def includeme(config):
+    config.set_memex_group_write_permitted(annotation_group_write_permitted)
+
     config.register_service_factory('.services.groups_factory', name='groups')
