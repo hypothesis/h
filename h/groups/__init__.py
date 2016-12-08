05fe2f852ad3b937e39df17428d299cbe4a43845
chdorner <christof@chdorner.com>
2016-12-08 17:48:08 +0100

    Set memex groupfinder to `h.groups.util.fetch_group`
    This will ensure that we return a correct Group model to memex and it
    can then in the next commit change the annotation creation code to check
    with the Pyramid ACL system if the current user is allowed to write to
    the given group.

--- h/groups/__init__.py
+++ h/groups/__init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from h.groups import util
+
 
 def includeme(config):
     config.register_service_factory('.services.groups_factory', name='groups')
+    config.memex_set_groupfinder('h.groups.util.fetch_group')
