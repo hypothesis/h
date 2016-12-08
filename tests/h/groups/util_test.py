c0ac7657004f3eb3e76a1dcbf25eeec8cd835806
chdorner <christof@chdorner.com>
2016-12-08 18:03:10 +0100

    Add WorldGroup value object
    This is so that we can properly authorize the __world__ group. The h
    groupfinder that we hook into memex will return an instance of this
    WorldGroup class when the given id is "__world__".

--- tests/h/groups/util_test.py
+++ tests/h/groups/util_test.py
@@ -2,7 +2,19 @@
 
 from __future__ import unicode_literals
 
+from pyramid import security
+
 from h.groups import util
+
+
+def test_world_group_acl():
+    group = util.WorldGroup('example.com')
+
+    assert group.__acl__() == [
+        (security.Allow, security.Everyone, 'read'),
+        (security.Allow, 'authority:example.com', 'write'),
+        security.DENY_ALL,
+    ]
 
 
 class TestFetchGroup(object):
@@ -22,3 +34,7 @@ class TestFetchGroup(object):
         result = util.fetch_group(pyramid_request, group.pubid)
         # make sure this call doesn't raise
         result.__acl__()
+
+    def test_it_returns_correct_group_for_world_group(self, pyramid_request):
+        group = util.fetch_group(pyramid_request, '__world__')
+        assert isinstance(group, util.WorldGroup)
