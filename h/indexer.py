167a42963c1843946223cfb501825b5c01aa90a7
chdorner <christof@chdorner.com>
2016-12-19 19:42:41 +0100

    Store new index name setting during reindex
    The indexer workers can then read this setting and make sure to write to
    both the old and current indices.

--- h/indexer.py
+++ h/indexer.py
@@ -13,6 +13,8 @@ from memex.search.index import BatchIndexer
 
 log = logging.getLogger(__name__)
 
+SETTING_NEW_INDEX = u'reindex.new_index'
+
 
 def reindex(session, es, request):
     """Reindex all annotations into a new index, and update the alias."""
@@ -20,20 +22,31 @@ def reindex(session, es, request):
     if get_aliased_index(es) is None:
         raise RuntimeError('cannot reindex if current index is not aliased')
 
+    settings = request.find_service(name='settings')
+
     new_index = configure_index(es)
-    indexer = BatchIndexer(session, es, request, target_index=new_index, op_type='create')
 
-    errored = indexer.index()
-    if errored:
-        log.debug('failed to index {} annotations, retrying...'.format(
-            len(errored)))
-        errored = indexer.index(errored)
+    try:
+        settings.put(SETTING_NEW_INDEX, new_index)
+        request.tm.commit()
+
+        indexer = BatchIndexer(session, es, request, target_index=new_index, op_type='create')
+
+        errored = indexer.index()
         if errored:
-            log.warn('failed to index {} annotations: {!r}'.format(
-                len(errored),
-                errored))
+            log.debug('failed to index {} annotations, retrying...'.format(
+                len(errored)))
+            errored = indexer.index(errored)
+            if errored:
+                log.warn('failed to index {} annotations: {!r}'.format(
+                    len(errored),
+                    errored))
 
-    update_aliased_index(es, new_index)
+        update_aliased_index(es, new_index)
+
+    finally:
+        settings.delete(SETTING_NEW_INDEX)
+        request.tm.commit()
 
 
 def subscribe_annotation_event(event):
--- tests/h/indexer_test.py
+++ tests/h/indexer_test.py
@@ -11,60 +11,61 @@ from h import indexer
 @pytest.mark.usefixtures('BatchIndexer',
                          'configure_index',
                          'get_aliased_index',
-                         'update_aliased_index')
+                         'update_aliased_index',
+                         'settings_service')
 class TestReindex(object):
-    def test_sets_op_type_to_create(self, es, BatchIndexer):
-        indexer.reindex(mock.sentinel.session, es, mock.sentinel.request)
+    def test_sets_op_type_to_create(self, pyramid_request, es, BatchIndexer):
+        indexer.reindex(mock.sentinel.session, es, pyramid_request)
 
         _, kwargs = BatchIndexer.call_args
         assert kwargs['op_type'] == 'create'
 
-    def test_indexes_annotations(self, es, batchindexer):
+    def test_indexes_annotations(self, pyramid_request, es, batchindexer):
         """Should call .index() on the batch indexer instance."""
-        indexer.reindex(mock.sentinel.session, es, mock.sentinel.request)
+        indexer.reindex(mock.sentinel.session, es, pyramid_request)
 
         batchindexer.index.assert_called_once_with()
 
-    def test_retries_failed_annotations(self, es, batchindexer):
+    def test_retries_failed_annotations(self, pyramid_request, es, batchindexer):
         """Should call .index() a second time with any failed annotation IDs."""
         batchindexer.index.return_value = ['abc123', 'def456']
 
-        indexer.reindex(mock.sentinel.session, es, mock.sentinel.request)
+        indexer.reindex(mock.sentinel.session, es, pyramid_request)
 
         assert batchindexer.index.mock_calls == [
             mock.call(),
             mock.call(['abc123', 'def456']),
         ]
 
-    def test_creates_new_index(self, es, configure_index, matchers):
+    def test_creates_new_index(self, pyramid_request, es, configure_index, matchers):
         """Creates a new target index."""
-        indexer.reindex(mock.sentinel.session, es, mock.sentinel.request)
+        indexer.reindex(mock.sentinel.session, es, pyramid_request)
 
         configure_index.assert_called_once_with(es)
 
-    def test_passes_new_index_to_indexer(self, es, configure_index, BatchIndexer):
+    def test_passes_new_index_to_indexer(self, pyramid_request, es, configure_index, BatchIndexer):
         """Pass the name of the new index as target_index to indexer."""
         configure_index.return_value = 'hypothesis-abcd1234'
 
-        indexer.reindex(mock.sentinel.session, es, mock.sentinel.request)
+        indexer.reindex(mock.sentinel.session, es, pyramid_request)
 
         _, kwargs = BatchIndexer.call_args
         assert kwargs['target_index'] == 'hypothesis-abcd1234'
 
-    def test_updates_alias_when_reindexed(self, es, configure_index, update_aliased_index):
+    def test_updates_alias_when_reindexed(self, pyramid_request, es, configure_index, update_aliased_index):
         """Call update_aliased_index on the client with the new index name."""
         configure_index.return_value = 'hypothesis-abcd1234'
 
-        indexer.reindex(mock.sentinel.session, es, mock.sentinel.request)
+        indexer.reindex(mock.sentinel.session, es, pyramid_request)
 
         update_aliased_index.assert_called_once_with(es, 'hypothesis-abcd1234')
 
-    def test_does_not_update_alias_if_indexing_fails(self, es, batchindexer, update_aliased_index):
+    def test_does_not_update_alias_if_indexing_fails(self, pyramid_request, es, batchindexer, update_aliased_index):
         """Don't call update_aliased_index if index() fails..."""
         batchindexer.index.side_effect = RuntimeError('fail')
 
         try:
-            indexer.reindex(mock.sentinel.session, es, mock.sentinel.request)
+            indexer.reindex(mock.sentinel.session, es, pyramid_request)
         except RuntimeError:
             pass
 
@@ -75,6 +76,26 @@ class TestReindex(object):
 
         with pytest.raises(RuntimeError):
             indexer.reindex(mock.sentinel.session, es, mock.sentinel.request)
+
+    def test_stores_new_index_name_in_settings(self, pyramid_request, es, settings_service, configure_index):
+        configure_index.return_value = 'hypothesis-abcd1234'
+
+        indexer.reindex(mock.sentinel.session, es, pyramid_request)
+
+        settings_service.put.assert_called_once_with(indexer.SETTING_NEW_INDEX, 'hypothesis-abcd1234')
+
+    def test_deletes_index_name_setting(self, pyramid_request, es, settings_service):
+        indexer.reindex(mock.sentinel.session, es, pyramid_request)
+
+        settings_service.delete.assert_called_once_with(indexer.SETTING_NEW_INDEX)
+
+    def test_deletes_index_name_setting_when_exception_raised(self, pyramid_request, es, settings_service, batchindexer):
+        batchindexer.index.side_effect = RuntimeError('boom!')
+
+        with pytest.raises(RuntimeError):
+            indexer.reindex(mock.sentinel.session, es, pyramid_request)
+
+        settings_service.delete.assert_called_once_with(indexer.SETTING_NEW_INDEX)
 
     @pytest.fixture
     def BatchIndexer(self, patch):
@@ -106,6 +127,17 @@ class TestReindex(object):
         mock_es.index = 'hypothesis'
         mock_es.t.annotation = 'annotation'
         return mock_es
+
+    @pytest.fixture
+    def settings_service(self, pyramid_config):
+        service = mock.Mock()
+        pyramid_config.register_service(service, name='settings')
+        return service
+
+    @pytest.fixture
+    def pyramid_request(self, pyramid_request):
+        pyramid_request.tm = mock.Mock()
+        return pyramid_request
 
 
 @pytest.mark.usefixtures('add_annotation', 'delete_annotation')
