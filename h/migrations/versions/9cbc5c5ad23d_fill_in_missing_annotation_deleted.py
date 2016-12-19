a3ae332a5b9e028c8cb9a287a094cbf82fad9bd6
Nick Stenning <nick@whiteink.com>
2016-12-19 14:34:15 +0000

    Add migration to fill in annotation deleted rows

---
+++ h/migrations/versions/9cbc5c5ad23d_fill_in_missing_annotation_deleted.py
@@ -0,0 +1,28 @@
+"""
+Fill in missing Annotation.deleted
+
+Revision ID: 9cbc5c5ad23d
+Revises: 5bfdfde681ea
+Create Date: 2016-12-19 12:41:25.269188
+"""
+
+from __future__ import unicode_literals
+
+from alembic import op
+import sqlalchemy as sa
+
+revision = '9cbc5c5ad23d'
+down_revision = '5bfdfde681ea'
+
+annotation = sa.table('annotation', sa.column('deleted', sa.Boolean))
+
+
+def upgrade():
+    op.execute(annotation
+               .update()
+               .where(annotation.c.deleted == None)
+               .values(deleted=False))
+
+
+def downgrade():
+    pass
