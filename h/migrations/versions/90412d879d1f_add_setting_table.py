ed20e653678eb25e635c64874c3922badeccb2c2
Christof Dorner <christof@chdorner.com>
2016-12-19 14:29:31 +0100

    Add migration for new `setting` table (#4243)

---
+++ h/migrations/versions/90412d879d1f_add_setting_table.py
@@ -0,0 +1,37 @@
+"""
+Add `setting` table
+
+Revision ID: 90412d879d1f
+Revises: 8ae9d103551f
+Create Date: 2016-12-19 13:29:39.933771
+"""
+
+from __future__ import unicode_literals
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = '90412d879d1f'
+down_revision = '8ae9d103551f'
+
+
+def upgrade():
+    op.create_table('setting',
+                    sa.Column('key',
+                              sa.UnicodeText(),
+                              primary_key=True),
+                    sa.Column('value',
+                              sa.UnicodeText()),
+                    sa.Column('created',
+                              sa.DateTime,
+                              server_default=sa.func.now(),
+                              nullable=False),
+                    sa.Column('updated',
+                              sa.DateTime,
+                              server_default=sa.func.now(),
+                              nullable=False))
+
+
+def downgrade():
+    op.drop_table('setting')
