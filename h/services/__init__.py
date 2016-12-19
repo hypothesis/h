10b2bd1cf7b55116b828d335c94780c8d7d7bd6b
chdorner <christof@chdorner.com>
2016-12-19 19:18:43 +0100

    Add settings service
    For easy access to get/put/delete settings.

--- h/services/__init__.py
+++ h/services/__init__.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 
 """Service definitions that handle business logic."""
+
+from __future__ import unicode_literals
+
+
+def includeme(config):
+    config.register_service_factory('.settings.settings_factory', name='settings')
---
+++ h/services/settings.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from h.models import Setting
+
+
+class SettingsService(object):
+    """A service for fetching and manipulating settings."""
+
+    def __init__(self, session):
+        """
+        Create a new settings service.
+
+        :param session: the SQLAlchemy session object
+        """
+        self.session = session
+
+    def get(self, key):
+        setting = self._fetch(key)
+
+        if setting is None:
+            return None
+
+        return setting.value
+
+    def put(self, key, value):
+        setting = self._fetch(key)
+
+        if setting is None:
+            setting = Setting(key=key)
+
+        setting.value = value
+        self.session.add(setting)
+
+    def delete(self, key):
+        setting = self._fetch(key)
+
+        if setting is not None:
+            self.session.delete(setting)
+
+    def _fetch(self, key):
+        return self.session.query(Setting).get(key)
+
+
+def settings_factory(context, request):
+    """Return a SettingsService instance for the passed context and request."""
+    return SettingsService(session=request.db)
