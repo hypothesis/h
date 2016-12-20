8fae22237404bd46ebc6eaa08b9b643823a34f6d
chdorner <christof@chdorner.com>
2016-12-20 13:22:46 +0100

    Add and delete from current and new index during reindex
    This is the final piece that allows us to re-index into a new index
    without any downtime.

--- h/tasks/indexer.py
+++ h/tasks/indexer.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from h.celery import celery
+from h.indexer.reindexer import SETTING_NEW_INDEX
 
 from memex import storage
 from memex.search.index import index
@@ -13,7 +14,27 @@ def add_annotation(id_):
     if annotation:
         index(celery.request.es, annotation, celery.request)
 
+        # If a reindex is running at the moment, add annotation to the new index
+        # as well.
+        future_index = _current_reindex_new_name(celery.request)
+        if future_index is not None:
+            index(celery.request.es, annotation, celery.request,
+                  target_index=future_index)
+
 
 @celery.task
 def delete_annotation(id_):
     delete(celery.request.es, id_)
+
+    # If a reindex is running at the moment, delete annotation from the
+    # new index as well.
+    future_index = _current_reindex_new_name(celery.request)
+    if future_index is not None:
+        delete(celery.request.es, id_, target_index=future_index)
+
+
+def _current_reindex_new_name(request):
+    settings = celery.request.find_service(name='settings')
+    new_index = settings.get(SETTING_NEW_INDEX)
+
+    return new_index
