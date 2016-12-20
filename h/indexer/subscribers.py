d6abf1e9d347b6e13d377f326caab83677370eda
chdorner <christof@chdorner.com>
2016-12-20 12:34:41 +0100

    Split h.indexer into a package
    To resolve a circular import in a future commit. `h.tasks.indexer` needs
    to import the `SETTING_NEW_INDEX` constant, but `h.indexer` needed to
    import `h.tasks.indexer` for the event subscriber.

---
+++ h/indexer/subscribers.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from h.tasks.indexer import add_annotation, delete_annotation
+
+
+def subscribe_annotation_event(event):
+    if event.action in ['create', 'update']:
+        add_annotation.delay(event.annotation_id)
+    elif event.action == 'delete':
+        delete_annotation.delay(event.annotation_id)
