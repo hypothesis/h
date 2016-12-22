8f0826f051d637565f6989d85e8bf01b744f2e45
Nick Stenning <nick@whiteink.com>
2016-12-22 17:00:46 +0000

    Move reset password email generation into h.emails
    In preparation for providing a custom HTML template for the reset
    password email, this commit moves the generation of the reset password
    email into h.emails, along with our other emails.

--- h/emails/__init__.py
+++ h/emails/__init__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from h.emails import reply_notification, signup
+from h.emails import reply_notification, reset_password, signup
 
-__all__ = ('reply_notification', 'signup')
+__all__ = (
+    'reply_notification',
+    'reset_password',
+    'signup',
+)
