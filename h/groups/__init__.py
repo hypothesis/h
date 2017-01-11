0d6510b52a6b038a28f3c534ce41c82fad0d4ee6
chdorner <christof@chdorner.com>
2017-01-11 16:47:44 +0100

    Remove h groupfinder
    This is not being used anymore, but we use a groupfinder service now.

--- h/groups/__init__.py
+++ h/groups/__init__.py
@@ -2,5 +2,4 @@
 
 
 def includeme(config):
-    config.memex_set_groupfinder('h.groups.util.fetch_group')
     config.memex_add_search_filter('h.groups.search.GroupAuthFilter')
--- tests/h/groups/util_test.py
+++ tests/h/groups/util_test.py
@@ -15,26 +15,3 @@ def test_world_group_acl():
         (security.Allow, 'authority:example.com', 'write'),
         security.DENY_ALL,
     ]
-
-
-class TestFetchGroup(object):
-    def test_it_returns_the_correct_group(self, pyramid_request, factories):
-        groups = [factories.Group() for _ in xrange(3)]
-        assert util.fetch_group(pyramid_request, groups[1].pubid) == groups[1]
-
-    def test_it_returns_none_when_group_missing(self, pyramid_request, factories):
-        # create a group
-        factories.Group()
-
-        assert util.fetch_group(pyramid_request, 'bogus') is None
-
-    def test_it_returns_an_object_implementing_acl(self, pyramid_request, factories):
-        group = factories.Group()
-
-        result = util.fetch_group(pyramid_request, group.pubid)
-        # make sure this call doesn't raise
-        result.__acl__()
-
-    def test_it_returns_correct_group_for_world_group(self, pyramid_request):
-        group = util.fetch_group(pyramid_request, '__world__')
-        assert isinstance(group, util.WorldGroup)
