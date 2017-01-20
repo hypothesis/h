6a5d52e8ec5e7dea57c5cd38756ff6730fdde4ce
chdorner <christof@chdorner.com>
2017-01-20 11:15:04 +0100

    Remove unused has_permission view predicate

--- h/views/predicates.py
+++ h/views/predicates.py
@@ -17,22 +17,5 @@ class HasFeatureFlagPredicate(object):
         return request.feature(self.feature_flag)
 
 
-class HasPermissionPredicate(object):
-    """True if the request has the given permission on the context object."""
-
-    def __init__(self, permission, config):
-        self.permission = permission
-
-    def text(self):
-        return 'has_permission = {permission}'.format(
-            permission=self.permission)
-
-    phash = text
-
-    def __call__(self, context, request):
-        return request.has_permission(self.permission)
-
-
 def includeme(config):
     config.add_view_predicate('has_feature_flag', HasFeatureFlagPredicate)
-    config.add_view_predicate('has_permission', HasPermissionPredicate)
