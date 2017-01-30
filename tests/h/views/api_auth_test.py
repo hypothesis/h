988b4acccc9261df006b949eb00d756eb78ddd69
Sean Hammond <git@snhmnd.fastmail.com>
2017-01-30 11:32:09 +0000

    Use factories to create tokens in tests

--- tests/h/views/api_auth_test.py
+++ tests/h/views/api_auth_test.py
@@ -5,7 +5,6 @@ from __future__ import unicode_literals
 import mock
 import pytest
 
-from h import models
 from h.services.oauth import oauth_service_factory, TOKEN_TTL
 from h.services.user import user_service_factory
 from h.exceptions import OAuthTokenError
@@ -29,8 +28,8 @@ class TestAccessToken(object):
         oauth_service.create_token.assert_called_once_with(
             mock.sentinel.user, mock.sentinel.authclient)
 
-    def test_it_returns_an_oauth_compliant_response(self, pyramid_request, oauth_service):
-        token = models.Token()
+    def test_it_returns_an_oauth_compliant_response(self, factories, pyramid_request, oauth_service):
+        token = factories.Token()
         oauth_service.create_token.return_value = token
 
         response = views.access_token(pyramid_request)
@@ -39,15 +38,15 @@ class TestAccessToken(object):
         assert response['token_type'] == 'bearer'
         assert response['expires_in'] == TOKEN_TTL.total_seconds()
 
-    def test_it_returns_the_refresh_token_if_the_token_has_one(self, pyramid_request, oauth_service):
-        token = models.Token()
+    def test_it_returns_the_refresh_token_if_the_token_has_one(self, factories, pyramid_request, oauth_service):
+        token = factories.Token()
         refresh_token = token.refresh_token = 'test_refresh_token'
         oauth_service.create_token.return_value = token
 
         assert views.access_token(pyramid_request)['refresh_token'] == refresh_token
 
-    def test_it_does_not_returns_the_refresh_token_if_the_token_does_not_have_one(self, pyramid_request, oauth_service):
-        token = models.Token()
+    def test_it_does_not_returns_the_refresh_token_if_the_token_does_not_have_one(self, factories, pyramid_request, oauth_service):
+        token = factories.Token()
         oauth_service.create_token.return_value = token
 
         assert 'refresh_token' not in views.access_token(pyramid_request)
