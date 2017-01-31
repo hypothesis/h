125c23b4c3c36b3d9fb5b58985f7bb272cf56ac7
Sean Hammond <git@snhmnd.fastmail.com>
2017-01-31 13:20:38 +0000

    Add functional tests for OAuth tokens

---
+++ tests/functional/test_oauth.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+"""Functional tests for getting and using OAuth 2 access and refresh tokens."""
+
+from __future__ import unicode_literals
+
+import datetime
+
+import jwt
+import pytest
+
+
+@pytest.mark.functional
+class TestOAuth(object):
+    def test_getting_an_access_token(self, app, authclient, userid):
+        """Test using grant tokens and access tokens."""
+        # Test using a grant token to get an access token.
+        response = self.get_access_token(app, authclient, userid)
+        access_token = response['access_token']
+
+        # Test that you can use the access token to authorize with the API.
+        app.get(
+            '/api/profile',
+            headers={'Authorization': str('Bearer {}'.format(access_token))},
+        )
+
+    def test_request_fails_if_access_token_wrong(self, app, authclient,
+                                                 userid):
+        app.get(
+            '/api/profile',
+            headers={'Authorization': str('Bearer wrong')},
+            status=404,
+        )
+
+    def test_request_fails_if_access_token_expired(self, app, authclient,
+                                                   db_session, factories,
+                                                   userid):
+        token = factories.Token(
+            expires=datetime.datetime.utcnow() - datetime.timedelta(hours=1))
+        token = token.value
+        db_session.commit()
+
+        app.get(
+            '/api/profile',
+            headers={'Authorization': str('Bearer {}'.format(token))},
+            status=404,
+        )
+
+    def test_using_a_refresh_token(self, app, authclient, userid):
+        """Get a new access token by POSTing a refresh token to /api/token."""
+        # Start by getting an access token and refresh token.
+        response = self.get_access_token(app, authclient, userid)
+        old_access_token = response['access_token']
+        refresh_token = response['refresh_token']
+
+        # Use the refresh token to get a new access token.
+        response = app.post(
+            '/api/token',
+            {
+                'grant_type': 'refresh_token',
+                'refresh_token': refresh_token,
+            }
+        )
+        new_access_token = response.json_body['access_token']
+
+        # Test that the new access token works.
+        app.get(
+            '/api/profile',
+            headers={
+                'Authorization': str('Bearer {}'.format(new_access_token)),
+            },
+        )
+
+        # Test that the old access token still works, too.
+        app.get(
+            '/api/profile',
+            headers={
+                'Authorization': str('Bearer {}'.format(old_access_token)),
+            },
+        )
+
+    def test_refresh_token_request_fails_if_refresh_token_wrong(self, app,
+                                                                authclient,
+                                                                userid):
+        app.post(
+            '/api/token',
+            {
+                'grant_type': 'refresh_token',
+                'refresh_token': 'wrong',
+            },
+            status=400,
+        )
+
+    def test_refresh_token_request_fails_if_token_expired(self, app,
+                                                          authclient,
+                                                          db_session,
+                                                          factories,
+                                                          userid):
+        token = factories.Token(
+            expires=datetime.datetime.utcnow() - datetime.timedelta(hours=1))
+        refresh_token = token.refresh_token
+        db_session.commit()
+
+        app.post(
+            '/api/token',
+            {
+                'grant_type': 'refresh_token',
+                'refresh_token': refresh_token
+            },
+            status=400,
+        )
+
+    def test_you_cannot_use_a_refresh_token_to_authenticate_api_requests(
+            self, app, authclient, userid):
+        response = self.get_access_token(app, authclient, userid)
+        refresh_token = response['refresh_token']
+
+        app.get(
+            '/api/profile',
+            headers={'Authorization': str('Bearer {}'.format(refresh_token))},
+            status=404,
+        )
+
+    def get_access_token(self, app, authclient, userid):
+        """Get an access token by POSTing a grant token to /api/token."""
+        response = app.post(
+            '/api/token',
+            {
+                'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+                'assertion': jwt.encode({'iss': authclient.id,
+                                         'aud': 'localhost',
+                                         'sub': userid,
+                                         }, authclient.secret)},
+        )
+        return response.json_body
+
+    @pytest.fixture
+    def authclient(self, db_session, factories):
+        authclient = factories.AuthClient()
+        db_session.commit()
+        return authclient
+
+    @pytest.fixture
+    def userid(self, db_session, factories):
+        user = factories.User()
+        db_session.commit()
+        return user.userid
