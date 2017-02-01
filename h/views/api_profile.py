a3d45a4a4abb5b7b6f899eade5186820340a545c
chdorner <christof@chdorner.com>
2017-02-01 18:27:05 +0100

    Change /api/profile hypermedia name to profile.read
    We will soon have an endpoint that allows the client to update a users
    preferences that lives at `PATCH /api/profile`, so we should change the
    path of the `GET /api/profile` to allow for the new endpoint.

--- h/views/api_profile.py
+++ h/views/api_profile.py
@@ -11,7 +11,7 @@ from memex.views import api_config
 @api_config(route_name='api.profile',
             request_method='GET',
             effective_principals=security.Authenticated,
-            link_name='profile',
+            link_name='profile.read',
             description="Fetch the user's profile")
 def profile(request):
     return h_session.profile(request)
