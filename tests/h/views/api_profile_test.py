8ec4123a1939d9ae5add5c5c851547698159124c
chdorner <christof@chdorner.com>
2017-02-01 14:22:26 +0100

    Refactor profile endpoint tests into test class

--- tests/h/views/api_profile_test.py
+++ tests/h/views/api_profile_test.py
@@ -7,12 +7,12 @@ import pytest
 from h.views import api_profile
 
 
-def test_profile_view_proxies_to_session(session_profile, pyramid_request):
-    session_profile.return_value = {'foo': 'bar'}
-    result = api_profile.profile(pyramid_request)
-    assert result == {'foo': 'bar'}
+class TestProfile(object):
+    def test_profile_view_proxies_to_session(self, session_profile, pyramid_request):
+        session_profile.return_value = {'foo': 'bar'}
+        result = api_profile.profile(pyramid_request)
+        assert result == {'foo': 'bar'}
 
-
-@pytest.fixture
-def session_profile(patch):
-    return patch('h.session.profile')
+    @pytest.fixture
+    def session_profile(self, patch):
+        return patch('h.session.profile')
