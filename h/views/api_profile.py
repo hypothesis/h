b066dba1d98b1819a9da06e6aae3408971bbf48b
chdorner <christof@chdorner.com>
2017-02-01 16:19:49 +0100

    Add endpoint for updating user preferences

--- h/views/api_profile.py
+++ h/views/api_profile.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 from pyramid import security
 
 from h import session as h_session
+from h.exceptions import APIError
 from memex.views import api_config
 
 
@@ -14,4 +15,21 @@ from memex.views import api_config
             link_name='profile.read',
             description="Fetch the user's profile")
 def profile(request):
+    return h_session.profile(request)
+
+
+@api_config(route_name='api.profile',
+            request_method='PATCH',
+            effective_principals=security.Authenticated,
+            link_name='profile.update',
+            description="Update a user's preferences")
+def update_preferences(request):
+    preferences = request.json_body.get('preferences', {})
+
+    svc = request.find_service(name='user')
+    try:
+        svc.update_preferences(request.authenticated_user, **preferences)
+    except TypeError as e:
+        raise APIError(e.message, status_code=400)
+
     return h_session.profile(request)
--- tests/h/views/api_profile_test.py
+++ tests/h/views/api_profile_test.py
@@ -2,8 +2,10 @@
 
 from __future__ import unicode_literals
 
+import mock
 import pytest
 
+from h.exceptions import APIError
 from h.views import api_profile
 
 
@@ -13,6 +15,53 @@ class TestProfile(object):
         result = api_profile.profile(pyramid_request)
         assert result == {'foo': 'bar'}
 
+
+@pytest.mark.usefixtures('user_service', 'session_profile')
+class TestUpdatePreferences(object):
+    def test_updates_preferences(self, pyramid_request, user, user_service):
+        pyramid_request.json_body = {'preferences': {'show_sidebar_tutorial': True}}
+
+        api_profile.update_preferences(pyramid_request)
+
+        user_service.update_preferences.assert_called_once_with(
+                user, show_sidebar_tutorial=True)
+
+    def test_handles_invalid_preferences_error(self, pyramid_request, user_service):
+        user_service.update_preferences.side_effect = TypeError('uh oh, wrong prefs')
+
+        with pytest.raises(APIError) as exc:
+            api_profile.update_preferences(pyramid_request)
+
+        assert exc.value.message == 'uh oh, wrong prefs'
+
+    def test_handles_missing_preferences_payload(self, pyramid_request):
+        pyramid_request.json_body = {'foo': 'bar'}
+
+        # should not raise
+        api_profile.update_preferences(pyramid_request)
+
+    def test_returns_session_profile(self, pyramid_request, session_profile):
+        result = api_profile.update_preferences(pyramid_request)
+
+        assert result == session_profile.return_value
+
     @pytest.fixture
-    def session_profile(self, patch):
-        return patch('h.session.profile')
+    def pyramid_request(self, pyramid_request, user):
+        pyramid_request.authenticated_user = user
+        pyramid_request.json_body = {}
+        return pyramid_request
+
+    @pytest.fixture
+    def user(self, factories):
+        return factories.User.build()
+
+    @pytest.fixture
+    def user_service(self, pyramid_config):
+        svc = mock.Mock()
+        pyramid_config.register_service(svc, name='user')
+        return svc
+
+
+@pytest.fixture
+def session_profile(patch):
+    return patch('h.session.profile')
