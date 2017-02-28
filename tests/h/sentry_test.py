bddcd007af26d134c5c2926661449c18c7a4428c
Nick Stenning <nick@whiteink.com>
2017-02-28 13:37:31 +0100

    Add environment name to Sentry client
    Skyliner provides the ENV environment variable for our application,
    which contains either "prod" or "qa" depending on the environment.
    
    Sentry supports partitioning reports based on the environment within a
    single application. Adding this metadata to the `Client` allows us to
    migrate QA and Prod onto a single application in Sentry.

---
+++ tests/h/sentry_test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import pytest
+
+from h.sentry import get_client
+
+
+class TestGetClient(object):
+
+    @pytest.mark.parametrize('settings,env', [
+        ({}, 'dev'),
+        ({'h.env': 'qa'}, 'qa'),
+        ({'h.env': 'prod'}, 'prod'),
+    ])
+    def test_set_environment(self, settings, env):
+        client = get_client(settings)
+
+        assert client.environment == env
