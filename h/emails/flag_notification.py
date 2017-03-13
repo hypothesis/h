9ad86deb344063d63451de06d4ba1651a69741bf
Sheetal Umesh Kumar <sheetaluk@Sheetals-MacBook-Pro.local>
2017-03-13 17:28:02 -0400

    Generate an email notification when an annotation is flagged and send to the group's administrator.
    Fixes https://github.com/hypothesis/product-backlog/issues/180

---
+++ h/emails/flag_notification.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from pyramid.renderers import render
+
+from h.i18n import TranslationString as _
+
+
+def generate(request, email, incontext_link):
+    """
+    Generate an email to notify the group admin when a group member flags an annotation.
+
+    :param request: the current request
+    :type request: pyramid.request.Request
+    :param email: the group admin's email address
+    :type email: text
+    :param incontext_link: the direct link to the flagged annotation
+    :type incontext_link: text
+
+    :returns: a 4-element tuple containing: recipients, subject, text, html
+    """
+    context = {
+        'incontext_link': incontext_link,
+    }
+
+    subject = _('An annotation has been flagged')
+
+    text = render('h:templates/emails/flag_notification.txt.jinja2',
+                  context,
+                  request=request)
+    html = render('h:templates/emails/flag_notification.html.jinja2',
+                  context,
+                  request=request)
+
+    return [email], subject, text, html
