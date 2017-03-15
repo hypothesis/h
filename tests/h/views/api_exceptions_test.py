d500adbfaaf372e0a52a42a6f31f845da539207b
chdorner <christof@chdorner.com>
2017-03-15 10:24:49 +0100

    Render proper JSON API error on schemas.ValidationError
    This should also fix the 500 error when somebody tries to call `POST
    /api/users` with the wrong arguments, which is still a long outstanding
    bug.

--- tests/h/views/api_exceptions_test.py
+++ tests/h/views/api_exceptions_test.py
@@ -3,11 +3,12 @@
 from __future__ import unicode_literals
 
 from h.exceptions import APIError
-from h.views.api_exceptions import api_notfound, api_error, json_error
+from h.schemas import ValidationError
+from h.views import api_exceptions as views
 
 
 def test_api_notfound_view(pyramid_request):
-    result = api_notfound(pyramid_request)
+    result = views.api_notfound(pyramid_request)
 
     assert pyramid_request.response.status_int == 404
     assert result['status'] == 'failure'
@@ -17,17 +18,27 @@ def test_api_notfound_view(pyramid_request):
 def test_api_error_view(pyramid_request):
     context = APIError(message='asplosions!', status_code=418)
 
-    result = api_error(context, pyramid_request)
+    result = views.api_error(context, pyramid_request)
 
     assert pyramid_request.response.status_code == 418
     assert result['status'] == 'failure'
     assert result['reason'] == 'asplosions!'
 
 
+def test_api_validation_error(pyramid_request):
+    context = ValidationError('missing required userid')
+
+    result = views.api_validation_error(context, pyramid_request)
+
+    assert pyramid_request.response.status_code == 400
+    assert result['status'] == 'failure'
+    assert result['reason'] == 'missing required userid'
+
+
 def test_json_error_view(patch, pyramid_request):
     handle_exception = patch('h.views.api_exceptions.handle_exception')
 
-    result = json_error(pyramid_request)
+    result = views.json_error(pyramid_request)
 
     handle_exception.assert_called_once_with(pyramid_request)
     assert result['status'] == 'failure'
