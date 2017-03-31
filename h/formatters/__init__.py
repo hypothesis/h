02ee256ce452d891335a319948ba8e29c3805c4d
David Thompson <dthompson@hypothes.is>
2017-03-31 18:26:37 +0100

    Add a formatter to show moderator-specific info
    As it currently stands, all this formatter shows is the total number of
    times an annotation has been flagged, but there might be more
    information in the future, hence the `moderator` parent key. It fairly
    closely follows the pattern set out in the `AnnotationFlagFormatter`
    class.

--- h/formatters/__init__.py
+++ h/formatters/__init__.py
@@ -4,8 +4,10 @@ from __future__ import unicode_literals
 
 from h.formatters.annotation_flag import AnnotationFlagFormatter
 from h.formatters.annotation_hidden import AnnotationHiddenFormatter
+from h.formatters.annotation_moderation import AnnotationModerationFormatter
 
 __all__ = (
     'AnnotationFlagFormatter',
     'AnnotationHiddenFormatter',
+    'AnnotationModerationFormatter',
 )
