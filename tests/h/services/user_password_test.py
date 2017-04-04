e25ce7fd56c59e0b30bd31eb6bb0c96527918135
Nick Stenning <nick@whiteink.com>
2017-04-04 11:45:22 +0200

    Add a UserPasswordService for managing user passwords (#4468)
    This commit adds a service which has responsibility for checking (and
    updating) user passwords. This is in preparation for removing that
    functionality from the user model.
    
    This is desirable primarily because it makes it much easier to make our
    tests fast. At the moment, many tests which interact with User model
    instances end up computing the 12-round bcrypt hash of passwords, which
    is *very* slow.
    
    By encapsulating the password checking functionality in a service of its
    own we can limit the performance impact of password checking in tests to
    just this service.

---
+++ tests/h/services/user_password_test.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import pytest
+from passlib.context import CryptContext
+
+from h.security import password_context
+from h.services.user_password import UserPasswordService
+
+
+class TestUserPasswordService(object):
+    def test_check_password_false_with_null_password(self, svc, user):
+        assert not svc.check_password(user, 'anything')
+
+    def test_check_password_false_with_empty_password(self, svc, user):
+        user._password = ''
+
+        assert not svc.check_password(user, '')
+
+    def test_check_password_true_with_matching_password(self, svc, user):
+        svc.update_password(user, 's3cr37')
+
+        assert svc.check_password(user, 's3cr37')
+
+    def test_check_password_false_with_incorrect_password(self, svc, user):
+        svc.update_password(user, 's3cr37')
+
+        assert not svc.check_password(user, 'somethingelse')
+
+    def test_check_password_validates_old_style_passwords(self, svc, user):
+        user.salt = 'somesalt'
+        # Generated with passlib.hash.bcrypt.encrypt('foobar' + 'somesalt', rounds=4)
+        user._password = '$2a$04$zDQnlV/YBG.ju2i14V15p.5nWYL52ZBqjGsBWgLAisGkEJw812BHy'
+
+        assert not svc.check_password(user, 'somethingelse')
+        assert svc.check_password(user, 'foobar')
+
+    def test_check_password_upgrades_old_style_passwords(self, hasher, svc, user):
+        user.salt = 'somesalt'
+        # Generated with passlib.hash.bcrypt.encrypt('foobar' + 'somesalt', rounds=4)
+        user._password = '$2a$04$zDQnlV/YBG.ju2i14V15p.5nWYL52ZBqjGsBWgLAisGkEJw812BHy'
+
+        svc.check_password(user, 'foobar')
+
+        assert user.salt is None
+        assert not hasher.needs_update(user._password)
+
+    def test_check_password_only_upgrades_when_password_is_correct(self, hasher, svc, user):
+        user.salt = 'somesalt'
+        # Generated with passlib.hash.bcrypt.encrypt('foobar' + 'somesalt', rounds=4)
+        user._password = '$2a$04$zDQnlV/YBG.ju2i14V15p.5nWYL52ZBqjGsBWgLAisGkEJw812BHy'
+
+        svc.check_password(user, 'donkeys')
+
+        assert user.salt is not None
+        assert hasher.needs_update(user._password)
+
+    def test_check_password_works_after_upgrade(self, svc, user):
+        user.salt = 'somesalt'
+        # Generated with passlib.hash.bcrypt.encrypt('foobar' + 'somesalt', rounds=4)
+        user._password = '$2a$04$zDQnlV/YBG.ju2i14V15p.5nWYL52ZBqjGsBWgLAisGkEJw812BHy'
+
+        svc.check_password(user, 'foobar')
+
+        assert svc.check_password(user, 'foobar')
+
+    def test_check_password_upgrades_new_style_passwords(self, hasher, svc, user):
+        # Generated with passlib.hash.bcrypt.encrypt('foobar', rounds=4, ident='2b')
+        user._password = '$2b$04$L2j.vXxlLt9JJNHHsy0EguslcaphW7vssSpHbhqCmf9ECsMiuTd1y'
+
+        svc.check_password(user, 'foobar')
+
+        assert not hasher.needs_update(user._password)
+
+    def test_updating_password_unsets_salt(self, svc, user):
+        user.salt = 'somesalt'
+        user._password = 'whatever'
+
+        svc.update_password(user, 'flibble')
+
+        assert user.salt is None
+        assert svc.check_password(user, 'flibble')
+
+    def test_uses_global_password_context_by_default(self):
+        svc = UserPasswordService()
+        assert svc.hasher == password_context
+
+    @pytest.fixture
+    def hasher(self):
+        # Use a much faster hasher for testing purposes. DO NOT use as few as
+        # 5 rounds of bcrypt in production code under ANY CIRCUMSTANCES.
+        return CryptContext(schemes=['bcrypt'],
+                            bcrypt__ident='2b',
+                            bcrypt__min_rounds=5,
+                            bcrypt__max_rounds=5)
+
+    @pytest.fixture
+    def svc(self, hasher):
+        svc = UserPasswordService()
+        svc.hasher = hasher
+        return svc
+
+    @pytest.fixture
+    def user(self, factories):
+        return factories.User.build()
