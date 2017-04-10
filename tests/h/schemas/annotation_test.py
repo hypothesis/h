8e4abdcd13125b5a6f63657877441c38b25a66df
chdorner <christof@chdorner.com>
2017-04-10 14:42:35 +0200

    Add the 'moderation' field to the set of protected fields
    Since fcc0257, "moderation" is included in the JSON rendering of
    annotation objects. This field is only available for group
    administrators, and should be ignored by the annotation update
    endpoint.
    
    We may also need a separate migration to clean up any "moderation"
    values that got into annotation "extras" fields.

--- tests/h/schemas/annotation_test.py
+++ tests/h/schemas/annotation_test.py
@@ -204,6 +204,7 @@ class TestCreateUpdateAnnotationSchema(object):
         'links',
         'flagged',
         'hidden',
+        'moderation',
     ])
     def test_it_removes_protected_fields(self, pyramid_request, validate, field):
         data = {}
