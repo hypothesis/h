41903f5f6b23f5622fa018dc359f6073077c4d7a
Sean Hammond <git@snhmnd.fastmail.com>
2017-05-22 14:34:19 +0100

    Fix a broken test
    `db_session.add(g1, g2)` actually only adds `g1` to the session (`g2` is
    the optional `warn=True` args to `add()`). Use `db_session.add_all()`
    instead to add multiple objects to the session at once.
    
    This test was still passing because sqlalchemy was still adding `g2` to
    the session indirectly somehow.

--- tests/h/models/groups_test.py
+++ tests/h/models/groups_test.py
@@ -64,7 +64,7 @@ def test_created_by(db_session, factories):
     group_1 = models.Group(name=name_1, authority='foobar.com', creator=user)
     group_2 = models.Group(name=name_2, authority='foobar.com', creator=user)
 
-    db_session.add(group_1, group_2)
+    db_session.add_all([group_1, group_2])
     db_session.flush()
 
     assert models.Group.created_by(db_session, user).all() == [group_1, group_2]
