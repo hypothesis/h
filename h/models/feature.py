57a0d39de88fbf026f7856608e395d6c2820adf2
Robert Knight <robertknight@gmail.com>
2017-06-26 12:17:04 +0100

    Re-sort feature flag list.

--- h/models/feature.py
+++ h/models/feature.py
@@ -19,11 +19,10 @@ FEATURES = {
                     "client?"),
     'homepage_redirects': "Enable homepage redirects (for WordPress migration)?",
     'orphans_tab': "Show the orphans tab to separate anchored and unanchored annotations?",
+    'overlay_highlighter': "Use the new overlay highlighter?",
     'search_for_doi': "Use DOI metadata when searching for annotations on the current page?",
     'total_shared_annotations': "Show the total number of shared annotations for users and groups?",
-    'overlay_highlighter': "Use the new overlay highlighter?",
     'view-switcher': 'Use the new-style [Annotations|Page Notes|Orphans] view switcher control at the top of the sidebar',
-
 }
 
 # Once a feature has been fully deployed, we remove the flag from the codebase.
