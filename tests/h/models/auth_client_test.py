d562cc77d028cc7c048a8023dbc465e69dc45805
chdorner <christof@chdorner.com>
2017-07-26 11:32:29 +0200

    Check that authclients using authz code grant type have redirect_uri

--- tests/h/models/auth_client_test.py
+++ tests/h/models/auth_client_test.py
@@ -3,13 +3,29 @@ from __future__ import unicode_literals
 
 import pytest
 
+from sqlalchemy.exc import IntegrityError
+
 from h.models import AuthClient
+from h.models.auth_client import GrantType
 
 
 class TestAuthClient(object):
 
     def test_has_id(self, client):
         assert client.id
+
+    def test_does_not_allow_empty_redirect_uri_for_authz_code_grant(self, factories, db_session):
+        client = factories.AuthClient.build(grant_type=GrantType.authorization_code, redirect_uri=None)
+        db_session.add(client)
+
+        with pytest.raises(IntegrityError):
+            db_session.flush()
+
+    @pytest.mark.parametrize('grant_type', [GrantType.client_credentials, GrantType.jwt_bearer, GrantType.password])
+    def test_allows_empty_redirect_uri_for_other_grant(self, factories, db_session, grant_type):
+        client = factories.AuthClient.build(grant_type=grant_type, redirect_uri=None)
+        db_session.add(client)
+        db_session.flush()
 
     @pytest.fixture
     def client(self, db_session):
