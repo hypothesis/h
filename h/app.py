3eab29b89d047a7f7d2960383368fc5d6b502334
David Thompson <dthompson@hypothes.is>
2017-08-25 12:19:53 +0100

    Remove references to an old Annotator auth header
    Our client no longer uses this header[1], so we no longer need to
    explicitly allow it in our CORS policy or accept it as an authorisation
    header.
    
    [1]: https://github.com/hypothesis/client/commit/63c442fd936749d8e84e

--- h/app.py
+++ h/app.py
@@ -53,7 +53,6 @@ def includeme(config):
     config.add_tween('h.tweens.conditional_http_tween_factory', under=EXCVIEW)
     config.add_tween('h.tweens.redirect_tween_factory')
     config.add_tween('h.tweens.csrf_tween_factory')
-    config.add_tween('h.tweens.auth_token')
     config.add_tween('h.tweens.security_header_tween_factory')
 
     config.add_request_method(in_debug_mode, 'debug', reify=True)
