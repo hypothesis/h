f3e1c4f239ec50ae0f1817b262a31daab2996b30
Benjamin Goering <bengoering@gmail.com>
2017-10-16 13:19:19 -0700

    Add h.cli groups join --user ---group

--- h/cli/commands/groups.py
+++ h/cli/commands/groups.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import click
+from h.models import User, Group
 
 
 @click.group()
@@ -10,8 +11,10 @@ def groups():
 
 @groups.command('add-publisher-group')
 @click.option('--name', prompt=True, help="The name of the group")
-@click.option('--authority', prompt=True, help="The authority which the group is associated with")
-@click.option('--creator', prompt=True, help="The username of the group's creator")
+@click.option('--authority', prompt=True,
+              help="The authority which the group is associated with")
+@click.option('--creator', prompt=True,
+              help="The username of the group's creator")
 @click.pass_context
 def add_publisher_group(ctx, name, authority, creator):
     """
@@ -25,8 +28,10 @@ def add_publisher_group(ctx, name, authority, creator):
 
 @groups.command('add-public-group')
 @click.option('--name', prompt=True, help="The name of the group")
-@click.option('--authority', prompt=True, help="The authority which the group is associated with")
-@click.option('--creator', prompt=True, help="The username of the group's creator")
+@click.option('--authority', prompt=True,
+              help="The authority which the group is associated with")
+@click.option('--creator', prompt=True,
+              help="The username of the group's creator")
 @click.pass_context
 def add_public_group(ctx, name, authority, creator):
     """
@@ -48,5 +53,33 @@ def _create_group(type, ctx, name, authority, creator):
     group_svc = request.find_service(name='group')
     group_svc.create(name=name, authority=authority, userid=creator_userid,
                      type_=type)
+
+    request.tm.commit()
+
+
+@groups.command('join')
+@click.option('--user', prompt=True, help="username of user to add to a group")
+@click.option('--authority', prompt=True,
+              help="authority of user to add to group")
+@click.option('--group', prompt=True, help="pubid of group to add to")
+@click.pass_context
+def join(ctx, user, authority, group):
+    """
+    join a user to a group
+    """
+    group_id = group
+    request = ctx.obj['bootstrap']()
+
+    user = User.get_by_username(request.db, user, authority)
+    if not user:
+        raise ValueError('Could not find user {0}@{1}'.format(user, authority))
+
+    group = request.db.query(Group).filter_by(pubid=group_id).one_or_none()
+    if not group:
+        raise ValueError(
+            'Could not find group with pubid={0}'.format(group_id))
+
+    groups_service = request.find_service(name='group')
+    groups_service.member_join(group, user.userid)
 
     request.tm.commit()
