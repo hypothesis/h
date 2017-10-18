7d8053c914864f1ee666188028a8156bdb87b4b8
Benjamin Goering <bengoering@gmail.com>
2017-10-18 13:48:33 -0700

    Admin Create Group form/tests are driven by new h.services.group GROUP_TYPES constant

--- tests/functional/test_admin_groups.py
+++ tests/functional/test_admin_groups.py
@@ -4,19 +4,22 @@ from __future__ import unicode_literals
 
 import pytest
 
+from h.services.group import GROUP_TYPES
+
 
 @pytest.mark.functional
 class TestAdminGroupCreate(object):
     """Tests for the /admin/groups/create page."""
 
-    def test_can_create_group(self, app, admin_user_and_password):
+    @pytest.mark.parametrize('group_type', GROUP_TYPES.keys())
+    def test_can_create_group(self, app, admin_user_and_password, group_type):
         admin_user, admin_user_password = admin_user_and_password
         app = self._login(app, admin_user.username, admin_user_password)
         res = app.get('/admin/groups/create')
         create_group_form = res.forms['admin-group-create-form']
         create_group_form['name'] = 'Public Group for Test'
         create_group_form['description'] = 'description of awesome group'
-        create_group_form['group_type'].select('public')
+        create_group_form['group_type'].select(group_type)
         form_submit_res = create_group_form.submit().follow()
         assert form_submit_res.text.startswith('<!DOCTYPE html>')
 
