b5f381edd6f9b358e87c3424e10e7bb0f1965b4e
Benjamin Goering <bengoering@gmail.com>
2017-10-18 12:48:25 -0700

    Admin Group Create form creates group

--- tests/functional/test_admin_groups.py
+++ tests/functional/test_admin_groups.py
@@ -7,18 +7,18 @@ import pytest
 
 @pytest.mark.functional
 class TestAdminGroupCreate(object):
-    """Tests for the /account/settings page."""
+    """Tests for the /admin/groups/create page."""
 
     def test_can_create_group(self, app, admin_user_and_password):
         admin_user, admin_user_password = admin_user_and_password
         app = self._login(app, admin_user.username, admin_user_password)
         res = app.get('/admin/groups/create')
         create_group_form = res.forms['admin-group-create-form']
-        create_group_form['name'] = 'Public Group From TestAdminGroupCreate'
+        create_group_form['name'] = 'Public Group for Test'
         create_group_form['description'] = 'description of awesome group'
         create_group_form['group_type'].select('public')
         form_submit_res = create_group_form.submit().follow()
-        # assert res.text.startswith('<!DOCTYPE html>')
+        assert form_submit_res.text.startswith('<!DOCTYPE html>')
 
     def _login(self, app, username, password):
         res = app.get('/login')
