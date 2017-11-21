8873af92ea55aa4308e9b0d7fc14897d5440e738
David Thompson <dthompson@hypothes.is>
2017-11-21 13:08:35 +0000

    First stab at an API-based top-level importer
    This is the barest minimum I could think of to get something that looks
    API-compatible. It's not going to work yet, at least until we can pass
    in an auth token and a group ID, and it completely ignores the problems
    of ordering annotations into an order that respects replies, and reply
    ID lookup, but it's a start.

--- h/cli/commands/import.py
+++ h/cli/commands/import.py
@@ -16,5 +16,29 @@ def import_annotations(annotation_file):
     click.echo('{} annotations to import'.format(len(raw_annotations)))
 
     schema = AnnotationImportSchema()
+    validated_annotations = [schema.validate(ann) for ann in raw_annotations]
 
-    [schema.validate(ann) for ann in raw_annotations]
+    top_level = [a for a in validated_annotations if a['motivation'] == 'commenting']
+    replies = [a for a in validated_annotations if a['motivation'] == 'replying']
+    click.echo('{} top-level annotations'.format(len(top_level)))
+    click.echo('{} replies'.format(len(replies)))
+
+    group_id = 'foobangwibble'
+
+    shared_permissions = {
+            'read': ['group:{}'.format(group_id)],
+    }
+
+    def top_level_payload(annotation):
+        return {
+                'group': group_id,
+                'permissions': shared_permissions,
+                'references': [],
+                'tags': [],
+                'target': [],
+                'text': annotation['body'][0]['value'],
+                'uri': annotation['target'],
+        }
+
+    for annotation in top_level:
+        click.echo(json.dumps(top_level_payload(annotation), indent=2))
