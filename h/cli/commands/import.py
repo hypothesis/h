b6a2b4d88c437a73e358faf479bf38226c69911e
David Thompson <dthompson@hypothes.is>
2017-11-23 16:52:34 +0000

    Echo debug messages to STDERR
    This frees up STDOUT for a mapping of import IDs to annotation IDs.

--- h/cli/commands/import.py
+++ h/cli/commands/import.py
@@ -7,6 +7,10 @@ import click
 from h.schemas.annotation_import import AnnotationImportSchema
 
 
+def err_echo(message):
+    click.echo(message, err=True)
+
+
 @click.command('import')
 @click.argument('annotation_file', type=click.File('rb'))
 def import_annotations(annotation_file):
@@ -14,17 +18,16 @@ def import_annotations(annotation_file):
     with annotation_file:
         raw_annotations = json.load(annotation_file)
 
-    click.echo('{} annotations to import'.format(len(raw_annotations)))
+    err_echo('{} annotations to import'.format(len(raw_annotations)))
 
     schema = AnnotationImportSchema()
     validated_annotations = [schema.validate(ann) for ann in raw_annotations]
 
     top_level = [a for a in validated_annotations if a['motivation'] == 'commenting']
     replies = [a for a in validated_annotations if a['motivation'] == 'replying']
-    click.echo('{} top-level annotations'.format(len(top_level)))
-    click.echo('{} replies'.format(len(replies)))
-
     group_id = 'foobangwibble'
+    err_echo('{} top-level annotations'.format(len(top_level)))
+    err_echo('{} replies'.format(len(replies)))
 
     shared_permissions = {
             'read': ['group:{}'.format(group_id)],
@@ -42,4 +45,4 @@ def import_annotations(annotation_file):
         }
 
     for annotation in top_level:
-        click.echo(json.dumps(top_level_payload(annotation), indent=2))
+        err_echo(json.dumps(top_level_payload(annotation), indent=2))
