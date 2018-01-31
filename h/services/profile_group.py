9a5f63b6aefc197ca0665b2d91e86a929b1d8daf
Lyza Danger Gardner <lyza@lyza.com>
2018-01-31 14:00:58 -0500

    Add placeholders for `type`, `scoped`, `urls`  properties
    Refactor tests, as well, for more sanity

--- h/services/profile_group.py
+++ h/services/profile_group.py
@@ -7,7 +7,7 @@ class ProfileGroupService(object):
 
     """A service for retrieving groups associated with users."""
 
-    def __init__(self, session, open_group_finder):
+    def __init__(self, session, route_url, open_group_finder):
         """
         Create a new profile_groups service.
 
@@ -15,6 +15,7 @@ class ProfileGroupService(object):
         :param open_group_finder: a callable for finding open groups
         """
         self.session = session
+        self.route_url = route_url
         self.open_group_finder = open_group_finder
 
     def all(self, user=None, authority=None):
@@ -30,7 +31,21 @@ class ProfileGroupService(object):
         return [self._group_model(group) for group in groups]
 
     def _group_model(self, group):
-        model = {'name': group.name, 'id': group.pubid, 'public': group.is_public}
+        model = {
+          'name': group.name,
+          'id': group.pubid,
+          'public': group.is_public,
+          'scoped': False,  # TODO
+          'type': 'open' if group.is_public else 'private',  # TODO
+          'urls': {}
+        }
+        if not group.is_public:
+            # `url` legacy property support
+            model['url'] = self.route_url('group_read',
+                                          pubid=group.pubid,
+                                          slug=group.slug)
+            # `urls` are the future
+            model['urls']['group'] = model['url']
         return model
 
 
@@ -38,4 +53,5 @@ def profile_groups_factory(context, request):
     """Return a ProfileGroupService instance for the passed context and request."""
     auth_group_svc = request.find_service(name='authority_group')
     return ProfileGroupService(session=request.db,
+                               route_url=request.route_url,
                                open_group_finder=auth_group_svc.public_groups)
