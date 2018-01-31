7abbf375a44cdfe6b86746ba1c96536e62af335e
Hannah Stepanek <hmstepanek@hypothes.is>
2018-01-31 10:14:52 -0800

    configuring ES logging filter
    Added elastic search logging filter configuration to filter out
    all ReadTimeout warnings.

--- h/config.py
+++ h/config.py
@@ -17,6 +17,7 @@ from h.settings import (
     database_url,
     mandrill_settings,
 )
+from h.search.logging_filters import FilterExceptions
 
 __all__ = ('configure',)
 
@@ -134,5 +135,9 @@ def configure(environ=None, settings=None):
         if settings['debug_query'] == 'trace':
             level = logging.DEBUG
         logging.getLogger('sqlalchemy.engine').setLevel(level)
+
+    # Add ES logging filter to filter out ReadTimeout warnings
+    es_logger = logging.getLogger('elasticsearch')
+    es_logger.addFilter(FilterExceptions(("requests.exceptions.ReadTimeout","WARNING")))
 
     return Configurator(settings=settings)
