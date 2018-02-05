8f346f30e6938d03a904a4d8cd8b8725ea5e0eff
Robert Knight <robertknight@gmail.com>
2018-02-05 15:26:04 +0000

    Fix N806 lint warning
    ALL_CAPS var names are only permitted outside functions.

--- h/accounts/util.py
+++ h/accounts/util.py
@@ -44,9 +44,9 @@ def validate_orcid(orcid):
     Returns the normalized ORCID if successfully parsed or raises a ValueError
     otherwise.
     """
-    ORCID_REGEX = '\A[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{3}[0-9X]\Z'
+    orcid_regex = '\A[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{3}[0-9X]\Z'
 
-    if not re.match(ORCID_REGEX, orcid):
+    if not re.match(orcid_regex, orcid):
         raise ValueError('The format of this ORCID is incorrect'.format(orcid))
 
     if _orcid_checksum_digit(orcid[:-1]) != orcid[-1:]:
