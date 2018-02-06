b4dcf3faeb99b211d0fe1753c661100d5e7dee56
Sean Hammond <git@snhmnd.fastmail.com>
2018-02-06 14:38:26 +0000

    Factor out some code duplication

--- h/services/group.py
+++ h/services/group.py
@@ -37,23 +37,19 @@ class GroupService(object):
 
         :returns: the created group
         """
-        creator = self.user_fetcher(userid)
-        group = Group(name=name,
-                      authority=creator.authority,
-                      creator=creator,
-                      description=description,
-                      joinable_by=JoinableBy.authority,
-                      readable_by=ReadableBy.members,
-                      writeable_by=WriteableBy.members,
-                      )
+        group = self._create(name=name,
+                             userid=userid,
+                             description=description,
+                             joinable_by=JoinableBy.authority,
+                             readable_by=ReadableBy.members,
+                             writeable_by=WriteableBy.members,
+                             )
+        group.members.append(group.creator)
 
-        group.members.append(creator)
-
-        self.session.add(group)
+        # Flush the DB to generate group.pubid before publish()ing it.
         self.session.flush()
 
         self.publish('group-join', group.pubid, group.creator.userid)
-
         return group
 
     def create_open_group(self, name, userid, description=None):
@@ -68,17 +64,13 @@ class GroupService(object):
 
         :returns: the created group
         """
-        creator = self.user_fetcher(userid)
-        group = Group(name=name,
-                      authority=creator.authority,
-                      creator=creator,
-                      description=description,
-                      joinable_by=None,
-                      readable_by=ReadableBy.world,
-                      writeable_by=WriteableBy.authority,
-                      )
-        self.session.add(group)
-        return group
+        return self._create(name=name,
+                            userid=userid,
+                            description=description,
+                            joinable_by=None,
+                            readable_by=ReadableBy.world,
+                            writeable_by=WriteableBy.authority,
+                            )
 
     def member_join(self, group, userid):
         """Add `userid` to the member list of `group`."""
@@ -127,6 +119,20 @@ class GroupService(object):
             return []
 
         return [g.pubid for g in self.session.query(Group.pubid).filter_by(creator=user)]
+
+    def _create(self, name, userid, description, joinable_by, readable_by, writeable_by):
+        """Create a group and save it to the DB."""
+        creator = self.user_fetcher(userid)
+        group = Group(name=name,
+                      authority=creator.authority,
+                      creator=creator,
+                      description=description,
+                      joinable_by=joinable_by,
+                      readable_by=readable_by,
+                      writeable_by=writeable_by,
+                      )
+        self.session.add(group)
+        return group
 
 
 def groups_factory(context, request):
