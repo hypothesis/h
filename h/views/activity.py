aed0f71d4f11248e213b97106d86ac61c5b9fccf
Hannah Stepanek <hmstepanek@hypothes.is>
2018-02-07 17:55:05 -0800

    Enable activity pages for open groups
    Enabled members to be a None type and added 'admins'. If members
    is None it indicates that the group has no concept of members and
    is an "open" group. Admins looks just like members but is simply
    a list of the creator-this will probably be expanded at some point
    to be an actual list of admins but until then it's a list of one.
    
    Commit includes the following requirements:
    - # of public annotations made in that group
    - Name of the group admins (so that H users know who created
      and controls that group)
    - Share link for group (so that H users can share the group)
    - Open Groups won't appear in the groups dropdown (for now)
    - H users can search for a group in the search box, and it will
      return any corresponding groups (using existing search
      rules - future changes to open group discoverability in
      search will be handled in another ticket).
    - Open groups should have their own groups landing page
      (like private groups currently do - i.e.
      https://hypothes.is/groups/8M8734qW/artis-group)
    - Open groups pages are visible to logged out users.
    - Changed "Shared Annotations" to "Annotations" as it's really
      all annotations in the group.
    - Changed "Top tags" to "Tags" as it appears to be all tags.
    - Added a creator tag next to the user who is the creator of
      the group.
    - If private groupi; shows Members and "Invite new members"
      link. If open group; shows Moderators and "Share group" link.

--- h/views/activity.py
+++ h/views/activity.py
@@ -106,9 +106,10 @@ class GroupSearchController(SearchController):
 
         result['opts'] = {'search_groupname': self.group.name}
 
-        if self.request.user not in self.group.members:
+        # if the group has a concept of members (aka joinable_by is not None)
+        # and the user is not in the list of members
+        if self.group.joinable_by and (self.request.user not in self.group.members):
             return result
-
         def user_annotation_count(aggregation, userid):
             for user in aggregation:
                 if user['user'] == userid:
@@ -116,23 +117,39 @@ class GroupSearchController(SearchController):
             return 0
 
         q = query.extract(self.request)
+        members = None
+        moderators = None
         users_aggregation = result['search_results'].aggregations.get('users', [])
-        members = [{'username': u.username,
-                    'userid': u.userid,
-                    'count': user_annotation_count(users_aggregation,
-                                                   u.userid),
-                    'faceted_by': _faceted_by_user(self.request,
-                                                   u.username,
-                                                   q)}
-                   for u in self.group.members]
-        members = sorted(members, key=lambda k: k['username'].lower())
+        # if the group has a concept of members provide list of member info
+        # else provide list of moderators instead
+        if self.group.joinable_by:
+            members = [{'username': u.username,
+                        'userid': u.userid,
+                        'count': user_annotation_count(users_aggregation,
+                                                       u.userid),
+                        'faceted_by': _faceted_by_user(self.request,
+                                                       u.username,
+                                                       q)}
+                       for u in self.group.members]
+            members = sorted(members, key=lambda k: k['username'].lower())
+        else:
+            # anticipating that [self.group.creator] will change
+            # to an actual list of moderators at some point
+            moderators = [{'username': u.username,
+                           'userid': u.userid,
+                           'count': user_annotation_count(users_aggregation,
+                                                          u.userid),
+                           'faceted_by': _faceted_by_user(self.request,
+                                                          u.username,
+                                                          q)}
+                          for u in [self.group.creator]]
+            moderators = sorted(moderators, key=lambda k: k['username'].lower())
 
         group_annotation_count = self.request.find_service(name='annotation_stats').group_annotation_count(self.group.pubid)
 
         result['stats'] = {
             'annotation_count': group_annotation_count,
         }
-
         result['group'] = {
             'created': self.group.created.strftime('%B, %Y'),
             'description': self.group.description,
@@ -142,8 +159,9 @@ class GroupSearchController(SearchController):
                                           pubid=self.group.pubid,
                                           slug=self.group.slug),
             'members': members,
+            'moderators': moderators,
+            'creator': self.group.creator.userid,
         }
-
         if self.request.has_permission('admin', self.group):
             result['group_edit_url'] = self.request.route_url(
                 'group_edit', pubid=self.group.pubid)
