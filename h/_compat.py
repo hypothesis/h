7bf8948a36e486bb0f366e85fc04e5f7c08bdbcd
Robert Knight <robertknight@gmail.com>
2018-02-09 12:42:08 +0000

    Fix incorrect compatibility export of `xrange`
    The Python 2 / 3 cases were the wrong way round. xrange was removed in
    Python 3.

--- h/_compat.py
+++ h/_compat.py
@@ -25,11 +25,11 @@ PY2 = sys.version_info[0] == 2
 if not PY2:
     text_type = str
     string_types = (str,)
-    xrange = xrange
+    xrange = range
 else:
     text_type = unicode  # noqa
     string_types = (str, unicode)  # noqa
-    xrange = range
+    xrange = xrange
 
 try:
     import ConfigParser as configparser
