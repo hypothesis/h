27dd6484b678ab5d224db2ccecd5ddfd6e69eb38
Hannah Stepanek <hmstepanek@hypothes.is>
2018-03-02 15:33:56 -0800

    Fix hypothesis authority value in logo dict

--- h/models/group.py
+++ h/models/group.py
@@ -130,7 +130,8 @@ class Group(Base, mixins.Timestamps):
     def logo(self):
         """Returns logo icon if one exists for this groups authority else
         returns None."""
-        return {'biopub.hypothes.is': 'biopub-logo', 'hypothesis': 'logo'}.get(self.authority)
+        return {'biopub.hypothes.is': 'biopub-logo',
+                'hypothes.is': 'logo'}.get(self.authority)
 
     def __acl__(self):
         terms = []
