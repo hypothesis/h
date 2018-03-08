79d91cddf2a971c761d6f5a2e561da29a8245658
Hannah Stepanek <hmstepanek@hypothes.is>
2018-03-08 13:05:37 -0800

    Add organization model
    Organization is a group of groups. Currently this is a very simple
    implementation with attributes logo, name, pubid, and authority.
    The name is the name of the orgnaization, the logo is expected to
    be an svg, the pubid is a unique public identifier to be used in
    urls, and authority is the authority of the group. A cap was put
    on the size of the logo in chars simply to prevent a malliciously
    large upload. It also verifies the logo is an svg and is
    htmlparseable.

---
+++ tests/h/models/organization_test.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import pytest
+from h import models
+
+
+def test_init_sets_given_attributes():
+    logo = """<svg width="100" height="100">
+            <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
+            </svg>"""
+    organization = models.Organization(
+        name='My organization',
+        logo=logo,
+        authority='example.com',
+        )
+
+    assert organization.name == 'My organization'
+    assert organization.logo == logo
+    assert organization.authority == 'example.com'
+
+
+def test_null_logo():
+    organization = models.Organization(name="My Organization")
+
+    assert organization.name == 'My Organization'
+    assert organization.logo is None
+
+
+def test_slug():
+    organization = models.Organization(name="My Organization")
+
+    assert organization.slug == 'my-organization'
+
+
+def test_too_short_name_raises_value_error():
+    with pytest.raises(ValueError):
+        models.Organization(name="")
+
+
+def test_too_long_name_raises_value_error():
+    with pytest.raises(ValueError):
+        models.Organization(name="abcdefghijklmnopqrstuvwxyz")
+
+
+def test_too_long_logo_raises_value_error():
+    with pytest.raises(ValueError):
+        models.Organization(logo='<svg>{}</svg>'.format("abcdefghijklmnopqrstuvwxyz" * 400))
+
+
+def test_malformed_logo_raises_value_error():
+    with pytest.raises(ValueError):
+        models.Organization(logo='<svg>/svg>')
+
+
+def test_non_svg_logo_raises_value_error():
+    with pytest.raises(ValueError):
+        models.Organization(logo='<h>This is not a svg</h>')
+
+
+def test_repr(db_session, factories):
+    organization = models.Organization(name='My Organization', authority='example.com')
+    db_session.add(organization)
+    db_session.flush()
+
+    assert organization.id is not None
+    assert organization.pubid is not None
+    assert repr(organization) == "<Organization: my-organization>"
