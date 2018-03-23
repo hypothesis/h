ec2409eab79e765131da3d6adae2d2d712cad176
Sean Hammond <git@snhmnd.fastmail.com>
2018-03-23 18:49:13 +0000

    Add organization logo view

---
+++ h/views/organizations.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from pyramid.view import view_config
+
+
+@view_config(route_name='organization_logo', request_method='GET', renderer='svg', http_cache=600)
+def organization_logo(logo, request):
+    return logo
