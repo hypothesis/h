7555de224313e564741fc2169749a22abb7f6253
Sean Hammond <git@snhmnd.fastmail.com>
2018-03-23 17:38:31 +0000

    Add an SVGRenderer

--- h/renderers.py
+++ h/renderers.py
@@ -8,5 +8,31 @@ import pyramid.renderers
 json_sorted_factory = pyramid.renderers.JSON(sort_keys=True)
 
 
+class SVGRenderer(object):
+    """
+    A renderer for SVG image files.
+
+    A view callable can use this renderer and just return a string of SVG
+    (u"<svg> ... </svg>") for the body of the response:
+
+        @view_config(renderer="svg", ...)
+        def my_svg_image_view(request):
+            ...
+            return u"<svg> ... </svg>"
+
+    The response will be rendered as an SVG image response with the correct
+    Content-Type etc so that browsers will render the image.
+
+    """
+    def __init__(self, info):
+        pass
+
+    def __call__(self, value, system):
+        response = system['request'].response
+        response.content_type = 'image/svg+xml'
+        return value
+
+
 def includeme(config):
     config.add_renderer(name='json_sorted', factory=json_sorted_factory)
+    config.add_renderer(name='svg', factory=SVGRenderer)
--- tests/h/renderers_test.py
+++ tests/h/renderers_test.py
@@ -4,7 +4,11 @@ from __future__ import unicode_literals
 
 from collections import OrderedDict
 
+import mock
+import pytest
+
 from h.renderers import json_sorted_factory
+from h.renderers import SVGRenderer
 
 
 class TestSortedJSONRenderer(object):
@@ -17,3 +21,34 @@ class TestSortedJSONRenderer(object):
         result = renderer(data, system={})
 
         assert result == '{"bar": 1, "baz": 5, "foo": "bang"}'
+
+
+class TestSVGRenderer(object):
+    def test_it_sets_the_content_type(self, pyramid_request, system, svg_renderer):
+        svg_renderer(mock.sentinel.svg_content, system)
+
+        assert pyramid_request.response.content_type == 'image/svg+xml'
+
+    def test_it_returns_the_svg_content(self, system, svg_renderer):
+        # It returns the actual SVG content (u"<svg> ... </svg>") (actually
+        # just whatever the view callable that's using the renderer returned)
+        # as the body of the response for Pyramid to render.
+        assert svg_renderer(mock.sentinel.svg_content, system) == mock.sentinel.svg_content
+
+    @pytest.fixture
+    def system(self, pyramid_request):
+        """
+        Return a fake Pyramid `system` dict.
+
+        Returns a fake of the `system` dict that Pyramid passes to renderers
+        when it calls them. It's a dict containing a bunch of Pyramid stuff,
+        for example the current request.
+
+        """
+        return {
+            'request': pyramid_request,
+        }
+
+    @pytest.fixture
+    def svg_renderer(self):
+        return SVGRenderer(mock.sentinel.info)
