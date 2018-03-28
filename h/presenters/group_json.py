6d23b2b67376e7785f0cd927d9066943513018a8
Lyza Danger Gardner <lyza@lyza.com>
2018-03-28 10:15:22 -0400

    Refactor class to not pass attributes around as params
    Clean up method signatures to use `self.group` instead of taking
    a `group` arg

--- h/presenters/group_json.py
+++ h/presenters/group_json.py
@@ -10,28 +10,29 @@ class GroupJSONPresenter(object):
         self.group = group
         self._links_svc = links_svc
 
-    def asdict(self):
-        return self._model(self.group)
-
-    def _model(self, group):
-        model = {
-          'name': group.name,
-          'id': group.pubid,
-          'organization': '',  # unexexpanded org; no link available yet, so empty string by default
-          'public': group.is_public,  # DEPRECATED: TODO: remove from client
-          'scoped': True if group.scopes else False,
-          'type': group.type
-        }
-        model = self._inject_urls(group, model)
+    def asdict(self, expand=[]):
+        model = self._model()
+        self._inject_urls(model)
         return model
 
-    def _inject_urls(self, group, model):
+    def _model(self):
+        model = {
+          'name': self.group.name,
+          'id': self.group.pubid,
+          'organization': '',  # unexexpanded org; no link available yet, so empty string by default
+          'public': self.group.is_public,  # DEPRECATED: TODO: remove from client
+          'scoped': True if self.group.scopes else False,
+          'type': self.group.type
+        }
+        return model
+
+    def _inject_urls(self, model):
         model['links'] = {}
         model['urls'] = {}  # DEPRECATED TODO: remove from client
         if not self._links_svc:
             return model
 
-        model['links'] = self._links_svc.get_all(group)
+        model['links'] = self._links_svc.get_all(self.group)
         model['urls'] = model['links']  # DEPRECATED TODO: remove from client
         if 'html' in model['links']:
             # DEPRECATED TODO: remove from client
@@ -46,5 +47,5 @@ class GroupsJSONPresenter(object):
         self.groups = groups
         self._links_svc = links_svc
 
-    def asdicts(self):
-        return [GroupJSONPresenter(group, self._links_svc).asdict() for group in self.groups]
+    def asdicts(self, expand=[]):
+        return [GroupJSONPresenter(group, self._links_svc).asdict(expand=expand) for group in self.groups]
