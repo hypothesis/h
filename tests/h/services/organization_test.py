c503fb33ddc6f63b364a65f45c063a406e38fbf1
Hannah Stepanek <hmstepanek@hypothes.is>
2018-04-03 07:51:11 -0700

    Add organization service (#4921)
    * Add organization service for creating orgs
    
    Add an organization service that creates organizations. This will
    be used by the cli to create an organization.
    
    * Add tests for organization service
    
    * fixup! Add tests for organization service

---
+++ tests/h/services/organization_test.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import pytest
+
+from h.models import Organization
+from h.services.organization import OrganizationService
+from h.services.organization import organization_factory
+
+
+class TestOrganizationService(object):
+    def test_create_returns_organization(self, service):
+        organization = service.create(
+            name='Organization',
+            authority='publisher.com',
+            logo='<svg>H</svg>')
+
+        assert isinstance(organization, Organization)
+
+    def test_create_with_default_logo_returns_organization(self, service):
+        organization = service.create(
+            name='Organization',
+            authority='publisher.com')
+
+        assert isinstance(organization, Organization)
+        assert organization.logo is None
+
+    @pytest.fixture
+    def service(self, db_session):
+        return OrganizationService(db_session)
+
+
+class TestOrganizationsFactory(object):
+    def test_returns_organizations_service(self, pyramid_request):
+        svc = organization_factory(pyramid_request)
+
+        assert isinstance(svc, OrganizationService)
+
+    def test_provides_request_db_as_session(self, pyramid_request):
+        svc = organization_factory(pyramid_request)
+
+        assert svc.session == pyramid_request.db
