c93a48efd9d43d9fb3b5872fce71851aa174fdf8
Hannah Stepanek <hmstepanek@hypothes.is>
2018-05-23 22:00:40 -0700

    Remove unused Env,Deprecated, & mandrill_setting
    Remove the implementation of these now unused classes and function
    and thier tests. These were all replaced with SettingsManager.

--- h/settings.py
+++ h/settings.py
@@ -91,56 +91,6 @@ class SettingsManager(object):
                                        type_.__name__))
 
 
-class DeprecatedSetting(object):
-    """A wrapper for deprecated settings which emits appropriate warnings."""
-
-    def __init__(self, setting, message):
-        self.setting = setting
-        self.message = message
-
-        # Test seam
-        self.warn = log.warn
-
-    def __call__(self, environ):
-        result = self.setting(environ)
-        if result is not None:
-            self.warn(self.warning)
-        return result
-
-    @property
-    def warning(self):
-        return 'use of {s} is deprecated: {m}'.format(s=self.setting,
-                                                      m=self.message)
-
-
-class EnvSetting(object):
-
-    """An (optionally typed) setting read from an environment variable."""
-
-    def __init__(self, setting, varname, type=None):
-        self.setting = setting
-        self.varname = varname
-        if type is not None:
-            self.type = type
-        else:
-            self.type = str
-
-    def __call__(self, environ):
-        if self.varname in environ:
-            try:
-                value = self.type(environ[self.varname])
-            except ValueError:
-                raise SettingError('error parsing environment variable '
-                                   '{varname}={value!r} as {typename}'.format(
-                                       varname=self.varname,
-                                       typename=self.type.__name__,
-                                       value=environ[self.varname]))
-            return {self.setting: value}
-
-    def __str__(self):
-        return 'environment variable {name}'.format(name=self.varname)
-
-
 def database_url(url):
     """Parse a string as a Heroku-style database URL."""
     # Heroku database URLs start with postgres://, which is an old and
@@ -149,14 +99,3 @@ def database_url(url):
     if url.startswith('postgres://'):
         url = 'postgresql+psycopg2://' + url[len('postgres://'):]
     return url
-
-
-def mandrill_settings(environ):
-    if 'MANDRILL_USERNAME' in environ and 'MANDRILL_APIKEY' in environ:
-        return {
-            'mail.username': environ['MANDRILL_USERNAME'],
-            'mail.password': environ['MANDRILL_APIKEY'],
-            'mail.host': 'smtp.mandrillapp.com',
-            'mail.port': 587,
-            'mail.tls': True,
-        }
--- tests/h/settings_test.py
+++ tests/h/settings_test.py
@@ -10,69 +10,8 @@ from h.settings import (SettingsManager,
                         database_url)
 
 
-class FakeSetting(object):
-    def __init__(self, result):
-        self.result = result
-
-    def __call__(self, environ):
-        return self.result
-
-    def __str__(self):
-        return 'fake setting'
-
-
-class TestDeprecatedSetting(object):
-    def test_emits_warnings_when_child_setting_is_used(self):
-        func = settings.DeprecatedSetting(FakeSetting(result={'foo': 'bar'}),
-                                          message='what to do instead')
-        func.warn = mock.Mock(spec_set=[])
-
-        result = func({})
-
-        assert result == {'foo': 'bar'}
-        func.warn.assert_called_once_with('use of fake setting is '
-                                          'deprecated: what to do instead')
-
-    def test_emits_no_warnings_when_unused(self):
-        func = settings.DeprecatedSetting(FakeSetting(result=None),
-                                          message='what to do instead')
-        func.warn = mock.Mock(spec_set=[])
-
-        result = func({})
-
-        assert result is None
-        assert not func.warn.called
-
-
 def asutf8(string):
     return string.encode()
-
-
-@pytest.mark.parametrize('setting,varname,type,environ,expected', (
-    # Should return None when the env var in question isn't set
-    ('foo', 'FOO', None, {}, None),
-
-    # Should return the setting as a string when the env var is set
-    ('foo', 'FOO', None, {'FOO': 'bar'}, {'foo': 'bar'}),
-    ('foo.bar', 'FOO', None, {'FOO': 'baz'}, {'foo.bar': 'baz'}),
-
-    # Should coerce the result using the passed type
-    ('foo', 'FOO', asutf8, {'FOO': 'bar'}, {'foo': b'bar'}),
-    ('app_port', 'PORT', int, {'PORT': '123'}, {'app_port': 123}),
-))
-def test_env_setting(setting, varname, type, environ, expected):
-    func = settings.EnvSetting(setting, varname, type)
-
-    result = func(environ)
-
-    assert result == expected
-
-
-def test_env_setting_returns_nicer_error_for_type_failure():
-    func = settings.EnvSetting('port', 'PORT', type=int)
-
-    with pytest.raises(settings.SettingError):
-        func({'PORT': 'notanint'})
 
 
 def test_database_url():
@@ -80,26 +19,6 @@ def test_database_url():
     expected = 'postgresql+psycopg2://postgres:1234/database'
 
     assert database_url(url) == expected
-
-
-def test_mandrill_settings():
-    environ = {
-        'MANDRILL_USERNAME': 'foobar',
-        'MANDRILL_APIKEY': 'wibble',
-    }
-    expected = {
-        'mail.username': 'foobar',
-        'mail.password': 'wibble',
-        'mail.host': 'smtp.mandrillapp.com',
-        'mail.port': 587,
-        'mail.tls': True,
-    }
-
-    assert settings.mandrill_settings(environ) == expected
-
-
-def test_mandrill_settings_unset():
-    assert settings.mandrill_settings({}) is None
 
 
 class TestSettingsManager(object):
