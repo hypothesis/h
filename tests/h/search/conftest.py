0739a909072a903385d0a3eba436a8c4e84d534e
Sean Hammond <git@snhmnd.fastmail.com>
2018-05-24 17:05:50 +0100

    Use group_service fixture in all search tests
    Enable the group_service fixture automatically for all search tests. A
    lot of search tests need this fixture, so avoid them all having to
    enable it one by one.

--- tests/h/search/conftest.py
+++ tests/h/search/conftest.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import mock
 import pytest
 
 import h.search.index
+from h.services.group import GroupService
+
+
+@pytest.fixture(autouse=True)
+def group_service(pyramid_config):
+    group_service = mock.create_autospec(GroupService, instance=True, spec_set=True)
+    group_service.groupids_readable_by.return_value = ["__world__"]
+    pyramid_config.register_service(group_service, name="group")
+    return group_service
 
 
 @pytest.fixture
