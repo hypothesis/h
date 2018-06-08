e19e90d0fd8037a78129b2c4f84ee36f9ff299dd
Lyza Danger Gardner <lyza@lyza.com>
2018-06-08 15:08:26 -0700

    Change field to ARRAY of strings
    ARRAY with ENUM no worky in SQLAlchemy, so sad

--- h/migrations/versions/ec178dfd2098_add_motivations_field_to_annotation_.py
+++ h/migrations/versions/ec178dfd2098_add_motivations_field_to_annotation_.py
@@ -13,33 +13,13 @@ from sqlalchemy.dialects import postgresql
 revision = "ec178dfd2098"
 down_revision = "178270e3ee58"
 
-
-class Motivation(enum.Enum):
-    assessing = "assessing"
-    bookmarking = "bookmarking"
-    classifying = "classifying"
-    commenting = "commenting"
-    describing = "describing"
-    editing = "editing"
-    highlighting = "highlighting"
-    identifying = "identifying"
-    linking = "linking"
-    moderating = "moderating"
-    questioning = "questioning"
-    replying = "replying"
-    tagging = "tagging"
-
-motivation_type = sa.Enum(Motivation, name="annotation_motivation")
-
 def upgrade():
-    # Create the motivation_type type in db
-    motivation_type.create(op.get_bind())
-    # Add an Array column whose elements must be a motivation_type
+    # Add an Array column
+    # Ideally, the items should be enum'd, but SQLAlchemy does not support
+    # ARRAY with ENUM; validation is at model level
     op.add_column("annotation", sa.Column('motivations',
-                    postgresql.ARRAY(motivation_type),
-                    server_default=sa.text('ARRAY[]::annotation_motivation[]')
+                    postgresql.ARRAY(sa.UnicodeText, zero_indexes=True)
                   ))
 
 def downgrade():
     op.drop_column("annotation", "motivations")
-    motivation_type.drop(op.get_bind())
