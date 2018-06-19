cc5baf4204b12588dbb1ce63bfc279f1e4b9b9ea
Sean Hammond <git@snhmnd.fastmail.com>
2018-06-19 13:02:28 +0100

    Reduce size of try block
    It's best to keep as little code as possible inside a try block.

--- h/traversal/roots.py
+++ h/traversal/roots.py
@@ -124,21 +124,20 @@ class AuthClientRoot(object):
     def __getitem__(self, client_id):
         try:
             client = self.request.db.query(AuthClient).filter_by(id=client_id).one()
-
-            # Add the default root factory to this resource's lineage so that the
-            # default ACL is applied. This is needed so that permissions required by
-            # auth client admin views (e.g. the "admin_oauthclients" permission) are
-            # granted to admin users.
-            #
-            # For details on how ACLs work see the docs for Pyramid's
-            # ACLAuthorizationPolicy:
-            # https://docs.pylonsproject.org/projects/pyramid/en/latest/api/authorization.html
-            client.__parent__ = Root(self.request)
-
-            return client
         except:  # noqa: E722
             # No such client found or not a valid UUID.
             raise KeyError()
+
+        # Add the default root factory to this resource's lineage so that the default
+        # ACL is applied. This is needed so that permissions required by auth client
+        # admin views (e.g. the "admin_oauthclients" permission) are granted to admin
+        # users.
+        #
+        # For details on how ACLs work see the docs for Pyramid's ACLAuthorizationPolicy:
+        # https://docs.pylonsproject.org/projects/pyramid/en/latest/api/authorization.html
+        client.__parent__ = Root(self.request)
+
+        return client
 
 
 class OrganizationRoot(object):
