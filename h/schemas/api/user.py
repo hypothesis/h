518fe2baa3d2cf115ef5820040cab0633dcc29de
Lyza Danger Gardner <lyza@lyza.com>
2018-06-28 15:31:10 -0400

    Create new module for user API schemas

---
+++ h/schemas/api/user.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from h.models.user import (
+    DISPLAY_NAME_MAX_LENGTH,
+    EMAIL_MAX_LENGTH,
+    USERNAME_MAX_LENGTH,
+    USERNAME_MIN_LENGTH,
+)
+from h.schemas.base import JSONSchema
+
+
+class CreateUserAPISchema(JSONSchema):
+    """Validate a user JSON object."""
+
+    schema = {
+        'type': 'object',
+        'properties': {
+            'authority': {
+                'type': 'string',
+                'format': 'hostname',
+            },
+            'username': {
+                'type': 'string',
+                'minLength': USERNAME_MIN_LENGTH,
+                'maxLength': USERNAME_MAX_LENGTH,
+                'pattern': '^[A-Za-z0-9._]+$',
+            },
+            'email': {
+                'type': 'string',
+                'format': 'email',
+                'maxLength': EMAIL_MAX_LENGTH,
+            },
+            'display_name': {
+                'type': 'string',
+                'maxLength': DISPLAY_NAME_MAX_LENGTH,
+            },
+        },
+        'required': [
+            'authority',
+            'username',
+            'email',
+        ],
+    }
+
+
+class UpdateUserAPISchema(JSONSchema):
+    """Validate a user JSON object."""
+
+    schema = {
+        'type': 'object',
+        'properties': {
+            'email': {
+                'type': 'string',
+                'format': 'email',
+                'maxLength': EMAIL_MAX_LENGTH,
+            },
+            'display_name': {
+                'type': 'string',
+                'maxLength': DISPLAY_NAME_MAX_LENGTH,
+            },
+        },
+    }
