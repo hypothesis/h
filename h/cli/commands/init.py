4836ff89972772a618f043f1236e32398a811a5f
Robert Knight <robertknight@gmail.com>
2018-06-28 14:08:25 +0100

    Initialize the Elasticsearch 6 cluster in the `init` CLI command
    Initialize the ES 6 cluster with the ES 6 mappings when the `init` CLI
    command is invoked.

--- h/cli/commands/init.py
+++ h/cli/commands/init.py
@@ -19,6 +19,7 @@ def init(ctx):
 
     _init_db(request.registry.settings)
     _init_search(request.registry.settings)
+    _init_search_es6(request.registry.settings)
 
 
 def _init_db(settings):
@@ -39,4 +40,11 @@ def _init_search(settings):
     client = search.get_client(settings)
 
     log.info("initializing search index")
+    search.init(client)
+
+
+def _init_search_es6(settings):
+    client = search.get_es6_client(settings)
+
+    log.info("initializing ES6 search index")
     search.init(client)
