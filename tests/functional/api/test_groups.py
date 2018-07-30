40877744397c7538a887728307cd454bcbce8707
Lyza Danger Gardner <lyza@lyza.com>
2018-07-30 13:09:00 -0400

    Add single functional test for creating a private group
    - Move groups-related API tests into an `api` subdirectory

--- tests/functional/api/test_groups.py
+++ tests/functional/api/test_groups.py
@@ -8,6 +8,21 @@ import pytest
 @pytest.mark.functional
 class TestGroupAPI(object):
 
+    def test_create_group(self, app, user_with_token):
+        """Test a request to create a private group through the API"""
+
+        user, token = user_with_token
+        headers = {'Authorization': str('Bearer {}'.format(token.value))}
+
+        group = {
+            'name': 'My Group'
+        }
+
+        res = app.post_json('/api/groups', group, headers=headers)
+
+        assert res.status_code == 200
+        assert res.json['name'] == 'My Group'
+
     def test_leave_group(self, app, group, group_member_with_token):
         """Test a request to leave a group through the API."""
 
@@ -44,3 +59,12 @@ def group_member_with_token(group_member, db_session, factories):
     db_session.add(token)
     db_session.commit()
     return (group_member, token)
+
+
+@pytest.fixture
+def user_with_token(db_session, factories):
+    user = factories.User()
+    token = factories.DeveloperToken(userid=user.userid)
+    db_session.add(token)
+    db_session.commit()
+    return (user, token)
