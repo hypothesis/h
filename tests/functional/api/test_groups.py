3596a9359f977140ebf0705dd654197989814de8
Lyza Danger Gardner <lyza@lyza.com>
2018-07-30 13:36:24 -0400

    Expand and reorganize groups API functional tests

--- tests/functional/api/test_groups.py
+++ tests/functional/api/test_groups.py
@@ -6,25 +6,34 @@ import pytest
 
 
 @pytest.mark.functional
-class TestGroupAPI(object):
+class TestCreateGroup(object):
 
-    def test_create_group(self, app, user_with_token):
-        """Test a request to create a private group through the API"""
-
-        user, token = user_with_token
-        headers = {'Authorization': str('Bearer {}'.format(token.value))}
-
+    def test_it_returns_http_200_with_valid_payload(self, app, token_auth_header):
         group = {
             'name': 'My Group'
         }
 
-        res = app.post_json('/api/groups', group, headers=headers)
+        res = app.post_json('/api/groups', group, headers=token_auth_header)
 
         assert res.status_code == 200
-        assert res.json['name'] == 'My Group'
 
-    def test_leave_group(self, app, group, group_member_with_token):
-        """Test a request to leave a group through the API."""
+    def test_it_returns_http_400_with_invalid_payload(self, app, token_auth_header):
+        group = {}
+
+        res = app.post_json('/api/groups', group, headers=token_auth_header, expect_errors=True)
+
+        assert res.status_code == 400
+
+    @pytest.fixture
+    def token_auth_header(self, user_with_token):
+        user, token = user_with_token
+        return {'Authorization': str('Bearer {}'.format(token.value))}
+
+
+@pytest.mark.functional
+class TestRemoveMember(object):
+
+    def test_it_removes_authed_user_from_group(self, app, group, group_member_with_token):
 
         group_member, token = group_member_with_token
         headers = {'Authorization': str('Bearer {}'.format(token.value))}
