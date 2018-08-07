46a552f025865b4119c48e0a27b419e0ee2f538c
Hannah Stepanek <hmstepanek@hypothes.is>
2018-08-06 17:07:25 -0700

    Remove es filter syntax for es1
    Remove the conditional statment that changes the filter syntax
    depending on the elasticsearch version since we are now only running
    es6.
    
    This also means the tests can be removed.
    
    Fix the old tests to reflect the changes to the filter.

--- tests/h/search/query_test.py
+++ tests/h/search/query_test.py
@@ -45,34 +45,6 @@ class TestBuilder(object):
     def test_it_ignores_unknown_sort_fields(self, search):
         search.run({"sort": "no_such_field"})
 
-    def test_it_uses_old_filter_syntax_for_es1(self):
-        # TODO - Remove this test once ES 1 support is dropped.
-
-        builder = Builder(es_version=(1, 7, 0))
-        filter_ = AuthorityFilter("default")
-        params = {}
-        builder.append_filter(filter_)
-
-        query = builder.build(params)["query"]
-
-        assert query == {"filtered": {"filter": {"and": [filter_(params)]},
-                                      "query": {"match_all": {}}}}
-
-    def test_it_uses_new_filter_syntax_for_es6(self):
-        # TODO - This test will be obsolete once the tests which execute
-        # searches against Elasticsearch are running against ES 6.
-
-        builder = Builder(es_version=(6, 2, 0))
-        filter_ = AuthorityFilter("default")
-        params = {}
-        builder.append_filter(filter_)
-
-        query = builder.build(params)["query"]
-
-        assert query == {"bool": {"filter": [filter_(params)],
-                                  "must": []}}
-
-
 class TestTopLevelAnnotationsFilter(object):
 
     def test_it_filters_out_replies_but_leaves_annotations_in(self, Annotation, search):
