db29f246b2033dcd3efe8e1b61924c038fbdb6d9
Lyza Danger Gardner <lyza@lyza.com>
2018-08-30 14:05:25 -0400

    Update `h.services.user` for `request.default_authority`

--- tests/h/services/user_test.py
+++ tests/h/services/user_test.py
@@ -170,7 +170,7 @@ class TestUserServiceFactory(object):
     def test_provides_request_authority_as_default_authority(self, pyramid_request):
         svc = user_service_factory(None, pyramid_request)
 
-        assert svc.default_authority == pyramid_request.authority
+        assert svc.default_authority == pyramid_request.default_authority
 
     def test_provides_request_db_as_session(self, pyramid_request):
         svc = user_service_factory(None, pyramid_request)
