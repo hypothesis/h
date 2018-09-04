162c33c0430fefcfaa77505e8c9838bc1b2c671c
Hannah Stepanek <hmstepanek@hypothes.is>
2018-09-04 11:18:11 -0700

    Change append_filter to append_modifier

--- h/activity/query.py
+++ h/activity/query.py
@@ -165,8 +165,8 @@ def fetch_annotations(session, ids):
 @newrelic.agent.function_trace()
 def _execute_search(request, query, page_size):
     search = Search(request, stats=request.stats)
-    search.append_filter(AuthorityFilter(authority=request.authority))
-    search.append_filter(TopLevelAnnotationsFilter())
+    search.append_modifier(AuthorityFilter(authority=request.authority))
+    search.append_modifier(TopLevelAnnotationsFilter())
     for agg in aggregations_for(query):
         search.append_aggregation(agg)
 
