c5e1549662aa86aaad676e13c8c238e8126afad5
Robert Knight <robertknight@gmail.com>
2018-09-05 12:40:58 +0100

    Validate query parameters in search feed views
    Enforce that only recognized arguments are passed from query parameters
    to `Search.run`, the same as we do for the search API.
    
    Also add minimal functional tests for the RSS feed views.

---
+++ tests/functional/test_feeds.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import pytest
+
+
+@pytest.mark.functional
+def test_atom_feed(app):
+    app.get('/stream.atom')
+
+
+@pytest.mark.functional
+def test_rss_feed(app):
+    app.get('/stream.rss')
