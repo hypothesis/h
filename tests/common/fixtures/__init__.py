2d19b651f2b4a0bd820a2e8942b4d5d3c7b5a511
Robert Knight <robertknight@gmail.com>
2018-09-06 12:12:38 +0100

    Remove code that establishes elasticsearch_dsl's default connection
    We are using elasticsearch_dsl for generating and executing search
    queries in app code and tests, but we've opted to stick with the
    existing method of establishing a connection to Elasticsearch rather
    than using elasticsearch_dsl's default connection.
    
    The existing method of creating a connection and exposing it via
    `request.es` is convenient to work with in tests, since it can be easily
    mocked. It is also more consistent with how we set up and expose
    connections to other external services such as statsd or the database
    session.

--- tests/common/fixtures/__init__.py
+++ tests/common/fixtures/__init__.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from tests.common.fixtures.elasticsearch import es_client, es_connect
+from tests.common.fixtures.elasticsearch import es_client
 from tests.common.fixtures.elasticsearch import init_elasticsearch
 
 
 __all__ = (
     "es_client",
-    "es_connect",
     "init_elasticsearch",
 )
