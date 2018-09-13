a83e95cfc6d37de6a8ea264899c0c3a02148e2b7
Sheetal Umesh Kumar <sheetaluk@hypothes.is>
2018-09-13 11:34:06 -0400

    Add a field called hidden to an annotation document at the time of indexing.
    This new field is set when all children of an annotation AND the
    annotation itself, have been moderated.
    We will then use this field to determine which annotations are displayed
    and which ones are filtered out.
    
    See https://github.com/hypothesis/product-backlog/issues/583

--- tests/h/presenters/annotation_searchindex_test.py
+++ tests/h/presenters/annotation_searchindex_test.py
@@ -8,13 +8,15 @@ import mock
 import pytest
 
 from h.presenters.annotation_searchindex import AnnotationSearchIndexPresenter
+from h.services.annotation_moderation import AnnotationModerationService
 
 
-@pytest.mark.usefixtures('DocumentSearchIndexPresenter')
+@pytest.mark.usefixtures('DocumentSearchIndexPresenter', 'moderation_service', 'thread_ids')
 class TestAnnotationSearchIndexPresenter(object):
 
-    def test_asdict(self, DocumentSearchIndexPresenter):
-        annotation = mock.Mock(
+    def test_asdict(self, DocumentSearchIndexPresenter, pyramid_request, thread_ids):
+
+        annotation = mock.MagicMock(
             id='xyz123',
             created=datetime.datetime(2016, 2, 24, 18, 3, 25, 768),
             updated=datetime.datetime(2016, 2, 29, 10, 24, 5, 564),
@@ -27,11 +29,11 @@ class TestAnnotationSearchIndexPresenter(object):
             shared=True,
             target_selectors=[{'TestSelector': 'foobar'}],
             references=['referenced-id-1', 'referenced-id-2'],
-            thread_ids=['thread-id-1', 'thread-id-2'],
+            thread_ids=thread_ids,
             extra={'extra-1': 'foo', 'extra-2': 'bar'})
         DocumentSearchIndexPresenter.return_value.asdict.return_value = {'foo': 'bar'}
 
-        annotation_dict = AnnotationSearchIndexPresenter(annotation).asdict()
+        annotation_dict = AnnotationSearchIndexPresenter(annotation, pyramid_request).asdict()
 
         assert annotation_dict == {
             'authority': 'hypothes.is',
@@ -51,22 +53,101 @@ class TestAnnotationSearchIndexPresenter(object):
                         'selector': [{'TestSelector': 'foobar'}]}],
             'document': {'foo': 'bar'},
             'references': ['referenced-id-1', 'referenced-id-2'],
-            'thread_ids': ['thread-id-1', 'thread-id-2'],
+            'thread_ids': thread_ids,
+            'hidden': False,
         }
 
-    def test_it_copies_target_uri_normalized_to_target_scope(self):
-        annotation = mock.Mock(
+    def test_it_copies_target_uri_normalized_to_target_scope(self, pyramid_request):
+        annotation = mock.MagicMock(
             userid='acct:luke@hypothes.is',
             target_uri_normalized='http://example.com/normalized',
             extra={})
 
-        annotation_dict = AnnotationSearchIndexPresenter(annotation).asdict()
+        annotation_dict = AnnotationSearchIndexPresenter(annotation, pyramid_request).asdict()
 
         assert annotation_dict['target'][0]['scope'] == [
             'http://example.com/normalized']
+
+    def test_it_marks_annotation_hidden_when_it_and_all_children_are_moderated(self,
+                                                                               pyramid_request,
+                                                                               moderation_service,
+                                                                               thread_ids):
+        annotation = mock.MagicMock(
+            userid='acct:luke@hypothes.is',
+            thread_ids=thread_ids)
+
+        moderation_service.hidden.return_value = True
+        moderation_service.all_hidden.return_value = thread_ids
+
+        annotation_dict = AnnotationSearchIndexPresenter(annotation, pyramid_request).asdict()
+
+        assert annotation_dict['hidden'] is True
+
+    def test_it_does_not_mark_annotation_hidden_when_it_is_not_moderated(self,
+                                                                         pyramid_request,
+                                                                         moderation_service,
+                                                                         thread_ids):
+        annotation = mock.MagicMock(
+            userid='acct:luke@hypothes.is',
+            thread_ids=thread_ids)
+
+        moderation_service.all_hidden.return_value = thread_ids
+
+        annotation_dict = AnnotationSearchIndexPresenter(annotation, pyramid_request).asdict()
+
+        assert annotation_dict['hidden'] is False
+
+    def test_it_does_not_mark_annotation_hidden_when_children_are_not_moderated(self,
+                                                                                pyramid_request,
+                                                                                moderation_service,
+                                                                                thread_ids):
+        annotation = mock.MagicMock(
+            userid='acct:luke@hypothes.is',
+            thread_ids=thread_ids)
+
+        moderation_service.all_hidden.return_value = thread_ids[1:]
+        moderation_service.hidden.return_value = True
+
+        annotation_dict = AnnotationSearchIndexPresenter(annotation, pyramid_request).asdict()
+
+        assert annotation_dict['hidden'] is False
+
+    def test_it_does_not_mark_annotation_hidden_when_not_moderated_and_no_replies(self, pyramid_request):
+        thread_ids = []
+        annotation = mock.MagicMock(
+            userid='acct:luke@hypothes.is',
+            thread_ids=thread_ids)
+
+        annotation_dict = AnnotationSearchIndexPresenter(annotation, pyramid_request).asdict()
+
+        assert annotation_dict['hidden'] is False
+
+    def test_it_marks_annotation_hidden_when_moderated_and_no_replies(self, pyramid_request, moderation_service):
+        annotation = mock.MagicMock(userid='acct:luke@hypothes.is')
+
+        moderation_service.hidden.return_value = True
+
+        annotation_dict = AnnotationSearchIndexPresenter(annotation, pyramid_request).asdict()
+
+        assert annotation_dict['hidden'] is True
 
     @pytest.fixture
     def DocumentSearchIndexPresenter(self, patch):
         class_ = patch('h.presenters.annotation_searchindex.DocumentSearchIndexPresenter')
         class_.return_value.asdict.return_value = {}
         return class_
+
+
+@pytest.fixture
+def moderation_service(pyramid_config):
+    svc = mock.create_autospec(AnnotationModerationService, spec_set=True, instance=True)
+    svc.all_hidden.return_value = []
+    svc.hidden.return_value = False
+    pyramid_config.register_service(svc, name='annotation_moderation')
+    return svc
+
+
+@pytest.fixture
+def thread_ids():
+    # Annotation reply ids are referred to as thread_ids in our code base.
+    return ['thread-id-1', 'thread-id-2']
