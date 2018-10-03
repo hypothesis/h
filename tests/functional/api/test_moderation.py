a6357fbf6b5a1169660b68425ba0573514476e3e
Lyza Danger Gardner <lyza@lyza.com>
2018-10-03 14:48:02 -0400

    Add rudimentary functional tests for moderation API (DELETE)

--- tests/functional/api/test_moderation.py
+++ tests/functional/api/test_moderation.py
@@ -66,6 +66,59 @@ class TestPutHide(object):
         assert res.status_code == 404
 
 
+@pytest.mark.functional
+class TestDeleteHide(object):
+
+    def test_it_returns_http_204_for_group_creator(self,
+                                                   app,
+                                                   group_annotation,
+                                                   user_with_token):
+        user, token = user_with_token
+        headers = {'Authorization': str('Bearer {}'.format(token.value))}
+
+        res = app.delete('/api/annotations/{id}/hide'.format(id=group_annotation.id),
+                                                             headers=headers)
+
+        # The creator of a group has moderation rights over the annotations in that group
+        assert res.status_code == 204
+
+    def test_it_returns_http_404_if_annotation_is_in_world_group(self,
+                                                                 app,
+                                                                 world_annotation,
+                                                                 user_with_token):
+        user, token = user_with_token
+        headers = {'Authorization': str('Bearer {}'.format(token.value))}
+
+        res = app.delete('/api/annotations/{id}/hide'.format(id=world_annotation.id),
+                                                             headers=headers,
+                                                             expect_errors=True)
+        # The current user does not have moderation rights on the world group
+        assert res.status_code == 404
+
+    def test_it_returns_http_404_if_no_authn(self,
+                                             app,
+                                             group_annotation):
+
+        res = app.delete('/api/annotations/{id}/hide'.format(id=group_annotation.id),
+                                                             expect_errors=True)
+
+        assert res.status_code == 404
+
+    def test_it_returns_http_404_if_annotation_is_private(self,
+                                                          app,
+                                                          private_group_annotation,
+                                                          user_with_token):
+
+        user, token = user_with_token
+        headers = {'Authorization': str('Bearer {}'.format(token.value))}
+
+        res = app.delete('/api/annotations/{id}/hide'.format(id=private_group_annotation.id),
+                                                             headers=headers,
+                                                             expect_errors=True)
+        # private annotations cannot be moderated
+        assert res.status_code == 404
+
+
 @pytest.fixture
 def user(db_session, factories):
     user = factories.User()
