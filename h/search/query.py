f895098f80735010a09c222a1bdd315c8e13fbc1
Hannah Stepanek <hmstepanek@hypothes.is>
2018-10-15 17:24:08 -0700

    Disallow underscore wildcard in domain (#5372)
    * Replace ? with _ in error message
    
    * Dissallow _ wildcards in domain
    
    * Add note about spec support for _'s in domain
    
    * fixup: which spec
    
    * fixup: add example python url to doc string
    
    * fixup: remove trailing whitespaces
    
    * fixup: doc why wildcards at begining of url aren't performant

--- h/search/query.py
+++ h/search/query.py
@@ -22,21 +22,25 @@ def wildcard_uri_is_valid(wildcard_uri):
     """
     Return True if uri contains wildcards in appropriate places, return False otherwise.
 
-    *'s are not permitted in the scheme or netloc. _'s are not permitted in the scheme.
+    *'s and _'s are not permitted in the scheme or netloc aka:
+        scheme://netloc/path;parameters?query#fragment.
+
+    If a wildcard is near the begining of a url, elasticsearch will find a large portion of the
+    annotations because it is based on luncene which searches from left to right. In order to
+    avoid the performance implications of having such a large initial search space, wildcards are
+    not allowed in the begining of the url.
     """
     if "*" not in wildcard_uri and "_" not in wildcard_uri:
         return False
 
+    # Note: according to the URL spec _'s are allowed in the domain so this may be
+    # something that needs to be supported at a later date.
     normalized_uri = urlparse.urlparse(wildcard_uri)
-    if not normalized_uri.scheme or "*" in normalized_uri.netloc:
+    if (not normalized_uri.scheme or
+            "*" in normalized_uri.netloc or
+            "_" in normalized_uri.netloc):
         return False
 
-    # If a wildcard comes before the port aka: http://localhost:_3000 the request for the
-    # port will fail with a ValueError: invalid literal for int() with base 10: '_3000'.
-    try:
-        normalized_uri.port
-    except ValueError:
-        return False
     return True
 
 
