09bc73ba0ed8d47e32629b74a0430ee305dee157
dwolfe <buckofalltrades@gmail.com>
2018-12-06 09:50:45 -0800

    Fix linting errors

--- h/static/scripts/controllers/alert-close-button-controller.js
+++ h/static/scripts/controllers/alert-close-button-controller.js
@@ -9,11 +9,9 @@ class AlertCloseButtonController extends Controller {
   constructor(element, options) {
     super(element, options);
 
-    const window_ = options.window || window;
-
-    element.addEventListener('click', (event) => {
+    element.addEventListener('click', () => {
       let target = this.element.parentNode;
-      while (!target.classList.contains("alert")) {
+      while (!target.classList.contains('alert')) {
         target = target.parentNode;
       }
       target.remove();
