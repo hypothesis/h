1373cbdbf9bc0f6d07c43a76b7f25de8a44249fc
Hannah Stepanek <hmstepanek@hypothes.is>
2018-12-06 13:16:45 -0800

    Make h.search.util for search uri helpers
    Move wildcard_uri_is_valid into h.search.util.
    Add add_default_scheme into h.search.util.
    
    Add and move tests to tests.h.search.util_tests

---
+++ h/search/util.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import re
+from h._compat import urlparse
+
+
+def wildcard_uri_is_valid(wildcard_uri):
+    """
+    Return True if uri contains wildcards in appropriate places, return False otherwise.
+
+    *'s and _'s are not permitted in the scheme or netloc aka:
+        scheme://netloc/path;parameters?query#fragment.
+
+    If a wildcard is near the begining of a url, elasticsearch will find a large portion of the
+    annotations because it is based on luncene which searches from left to right. In order to
+    avoid the performance implications of having such a large initial search space, wildcards are
+    not allowed in the begining of the url.
+    """
+    if "*" not in wildcard_uri and "_" not in wildcard_uri:
+        return False
+
+    # Note: according to the URL spec _'s are allowed in the domain so this may be
+    # something that needs to be supported at a later date.
+    normalized_uri = urlparse.urlparse(wildcard_uri)
+    if (not normalized_uri.scheme
+            or "*" in normalized_uri.netloc
+            or "_" in normalized_uri.netloc):
+        return False
+
+    return True
+
+
+def add_default_scheme(uri):
+    """
+    In order to not prepend an extra http:// in cases where
+    there may be wildcard characters in the scheme, add _ and *
+    to the list of valid scheme characters.
+    For example: ht*://example.com w/o allowing wildcards
+        would be http://ht*//example.com which is intuitively wrong.
+    Note: This will not add a scheme to server:port uri's.
+    """
+    # See https://tools.ietf.org/html/rfc3986#section-3.1
+    uri_scheme_pattern = "[a-zA-Z_/*][a-zA-Z0-9+-._/*]*:"
+
+    if re.match(uri_scheme_pattern, uri):
+        return uri
+
+    return "http://" + uri
---
+++ tests/h/search/util_test.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import pytest
+from h.search import util
+
+
+@pytest.mark.parametrize('wildcard_uri,expected', [
+    ("htt*://bar.com", False),
+    ("*http://bar.com", False),
+    ("http_://bar.com", False),
+    ("http://bar_com*", False),
+    ("_http://bar.com", False),
+    ("http://localhost:3000*", False),
+    ("http://localhost:_3000", False),
+    ("http://bar.com_foo=baz", False),
+    ("http://example.com_", False),
+    ("http://bar*.com", False),
+    ("file://*", False),
+    ("https://foo.com", False),
+    ("http://foo.com*", False),
+    ("http://foo.com/*", True),
+    ("urn:*", True),
+    ("doi:10.101_", True),
+    ("http://example.com/__/", True),
+])
+def test_identifies_wildcard_uri_is_valid(wildcard_uri, expected):
+    assert util.wildcard_uri_is_valid(wildcard_uri) == expected
+
+
+@pytest.mark.parametrize('uri,expected', [
+    ("htt*://bar.com", "htt*://bar.com"),
+    ("*bar.com", "http://*bar.com"),
+    ("*http://bar.com", "*http://bar.com"),
+    ("http_://bar.com", "http_://bar.com"),
+    ("http://bar_com*", "http://bar_com*"),
+    ("server:9000", "server:9000"),
+    ("http://localhost:3000*", "http://localhost:3000*"),
+    ("file://*", "file://*"),
+    ("urn:*", "urn:*"),
+    ("doi:10.101_", "doi:10.101_"),
+])
+def test_add_default_scheme(uri, expected):
+    assert util.add_default_scheme(uri) == expected
