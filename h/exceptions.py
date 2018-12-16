93859e448f94eaae37ad294709fa0170334c75b7
Sean Hammond <git@snhmnd.fastmail.com>
2018-12-16 13:29:39 +0000

    Tidy up h/exceptions.py
    Just some formatting and docstrings.

--- h/exceptions.py
+++ h/exceptions.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 """Exceptions raised by the h application."""
 
 from __future__ import unicode_literals
@@ -10,7 +9,6 @@ from h.i18n import TranslationString as _  # noqa: N813
 
 
 class APIError(httpexceptions.HTTPError):
-
     """A problem handling an API request."""
 
     def __init__(self, message, status_code=500):
@@ -18,22 +16,18 @@ class APIError(httpexceptions.HTTPError):
 
 
 class ConflictError(httpexceptions.HTTPConflict):
-    """
-    Exception raised if client request represents a duplicate of an
-    existing resource.
-    """
+    """An API request represents a duplicate of an existing resource."""
 
     def __init__(self, message=_("Conflict")):
         super(ConflictError, self).__init__(message)
 
 
 class OAuthTokenError(httpexceptions.HTTPUnauthorized):
-
     """
-    Exception raised when an OAuth token request failed.
+    An OAuth token request failure.
 
-    This specifically handles OAuth errors which have a type (``message``) and
-    a description (``description``).
+    Raising an exception of this class causes an error response that has
+    a type (``message``) and a description (``description``).
     """
 
     def __init__(self, message, type_):
@@ -42,11 +36,7 @@ class OAuthTokenError(httpexceptions.HTTPUnauthorized):
 
 
 class PayloadError(httpexceptions.HTTPBadRequest):
-
-    """
-    Exception raised for API requests made with missing/invalid
-    payloads.
-    """
+    """An API request has a missing or invalid payload."""
 
     def __init__(self):
         message = _("Expected a valid JSON payload, but none was found!")
