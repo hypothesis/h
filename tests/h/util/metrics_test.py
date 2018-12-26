a803a125cb7e7c0756076af5efe1b9d1b512e93f
Hannah Stepanek <hmstepanek@hypothes.is>
2018-12-26 08:31:17 -0800

    Reformat tests

--- tests/h/util/metrics_test.py
+++ tests/h/util/metrics_test.py
@@ -9,32 +9,37 @@ from h.util import metrics
 
 class TestRecordSearchQueryParams(object):
     def test_records_parameters(self, newrelic_agent):
-        params = MultiDict(tag="tagsvalue",
-                           _separate_replies=True,
-                           url="urlvalue",
-                           bad="unwanted")
+        params = MultiDict(
+            tag="tagsvalue", _separate_replies=True, url="urlvalue", bad="unwanted"
+        )
         metrics.record_search_query_params(params, True)
         newrelic_agent.current_transaction().add_custom_parameters.assert_called_once_with(
-            [("es_url", "urlvalue"),
-             ("es_tag", "tagsvalue"),
-             ("es__separate_replies", True)])
+            [
+                ("es_url", "urlvalue"),
+                ("es_tag", "tagsvalue"),
+                ("es__separate_replies", True),
+            ]
+        )
 
     def test_does_not_record_separate_replies_if_false(self, newrelic_agent):
         params = MultiDict({})
         metrics.record_search_query_params(params, False)
-        newrelic_agent.current_transaction().add_custom_parameters.assert_called_once_with([])
+        newrelic_agent.current_transaction().add_custom_parameters.assert_called_once_with(
+            []
+        )
 
     def test_does_not_record_parameters_if_no_transaction(self, newrelic_agent):
         newrelic_agent.current_transaction.return_value = None
-        params = MultiDict(tag="tagsvalue",
-                           _separate_replies=True,
-                           url="urlvalue",
-                           bad="unwanted")
+        params = MultiDict(
+            tag="tagsvalue", _separate_replies=True, url="urlvalue", bad="unwanted"
+        )
         metrics.record_search_query_params(params, True)
 
     @pytest.fixture
     def newrelic_agent(self, newrelic_agent):
-        newrelic_agent.current_transaction.return_value.add_custom_parameters = mock.Mock()
+        newrelic_agent.current_transaction.return_value.add_custom_parameters = (
+            mock.Mock()
+        )
         return newrelic_agent
 
 
