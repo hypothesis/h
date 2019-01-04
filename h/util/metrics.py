1290aa80f3f0deef2a16fc9b5add2594f9a93f07
Hannah Stepanek <hmstepanek@hypothes.is>
2019-01-04 08:03:38 -0800

    Cleanup metrics (#5473)
    * Use one function to record search query params
    
    Previously there were two functions to record search query params,
    one of them was for activity pages, the other for the api. The activity
    pages use a subset of the params used by the api thus the api function
    contains the superset. Instead of having two separate functions called
    in each view, have one function that is called in
    h/search/core.py:Search.run.
    
    Also instead of wrapping the newrelic add_custom_parameters, call it
    directly in the function where it is used and mock newrelic.agent in
    the tests. This will simplify the code and reduce abstraction improving
    readability.
    
    NewRelic provides an add_custom_parameters function that accepts multiple
    parameters at once. The advantage of using this is internally get current
    transaction won't be called each time we add a new parameter which will be
    a slight performance improvement over calling add_custom_parameter multiple
    times.
    
    * Add check for if current transaction exists
    
    Occasionally on startup there is a race condition where the current transaction
    may not exist and current_transaction may return None.
    If the current transaction is None then there is no add_custom_parameters
    function to call and parameters should not be added.
    
    * Make separate_replies key a separate input param
    
    Since separate_replies is actualy a config setting rather than part
    of the search query params inside the Search class, pass it in as a
    separate argument to avoid adding logic into the Search class. The
    logic required to add it as part of the params would modify the params
    multidict which is a little dangerous since if done incorrectly could
    lead to a different search query.
    
    * Reformat tests
    
    * Minor tweaks based on code review feedback
    
    - Cleanup docstrings
    - Break up test
    - Rename tests
    - Cleanup comments

--- h/util/metrics.py
+++ h/util/metrics.py
@@ -3,24 +3,20 @@ from __future__ import unicode_literals
 import newrelic.agent
 
 
-def send_metric(name, metric):
-    """Send a metric to New Relic.
-
-    :arg name: The key name. Only the first 255 characters are retained
-    :type name: str
-    :arg metric: The string value to add to the current transaction
-        Only the first 255 characters are retained.
-    :type metric: str, int, float, bool
+def record_search_query_params(params, separate_replies):
     """
-    newrelic.agent.add_custom_parameter(name, metric)
+    Send search params to New Relic as "attributes" on each transaction.
 
+    Only the first 255 characters of the value are retained and values must
+    be str, int, float, or bool types.
 
-def record_search_query_param_usage(params, separate_replies):
-    """Send search request params to New Relic as metrics.
+    Disclaimer: If there are multiple values for a single key, only
+    submit the first value to New Relic as there is no way of submitting
+    multiple values for a single attribute.
 
     :arg params: the request params to record
     :type params: webob.multidict.MultiDict
-    :arg separate_replies: Records usage of _separate_replies parameter in the search
+    :arg separate_replies: the value of the separate_replies search setting
     :type separate_replies: bool
     """
     keys = [
@@ -40,46 +36,18 @@ def record_search_query_param_usage(params, separate_replies):
         # Record usage of wildcard feature.
         "wildcard_uri",
     ]
+    # The New Relic Query Language does not permit _ at the begining
+    # and offset is a reserved key word.
+    params = [("es_{}".format(k), params[k]) for k in keys if k in params]
+
     # Record usage of _separate_replies which will help distinguish client calls
     # for loading the sidebar annotations from other API calls.
-    send_metric("es__separate_replies", separate_replies)
+    if separate_replies:
+        params.append(("es__separate_replies", separate_replies))
 
-    for k in keys:
-        if k in params:
-            # The New Relic Query Language does not permit _ at the begining
-            # and offset is a reserved key word.
-            send_metric("es_{}".format(k), params[k])
-
-
-def record_search_api_usage_metrics(params):
-    """Send search API request params to New Relic as metrics.
-
-    :arg params: the request params to record
-    :type params: webob.multidict.MultiDict
-    """
-    keys = [
-        # Record usage of inefficient offset and it's alternative search_after.
-        "offset",
-        "search_after",
-        "sort",
-        # Record usage of url/uri (url is an alias of uri).
-        "url",
-        "uri",
-        # Record usage of tags/tag (tags is an alias of tag).
-        "tags",
-        "tag",
-        # Record usage of _separate_replies which will help distinguish client calls
-        # for loading the sidebar annotations from other api calls.
-        "_separate_replies",
-        # Record group and user-these help in identifying slow queries.
-        "group",
-        "user",
-        # Record usage of wildcard feature.
-        "wildcard_uri",
-    ]
-
-    for k in keys:
-        if k in params:
-            # The New Relic Query Language does not permit _ at the begining
-            # and offset is a reserved key word.
-            send_metric("es_{}".format(k), params[k])
+    # On startup, there is a race condition in New Relic where there may not be
+    # a transaction. If there isn't, current_transaction will return None in which
+    # case we can't record params.
+    current_transaction = newrelic.agent.current_transaction()
+    if current_transaction:
+        newrelic.agent.current_transaction().add_custom_parameters(params)
--- tests/h/util/metrics_test.py
+++ tests/h/util/metrics_test.py
@@ -1,47 +1,51 @@
 from __future__ import unicode_literals
 
 import pytest
-from mock import call
+import mock
 from webob.multidict import MultiDict
 
 from h.util import metrics
 
 
-class TestSendMetric(object):
-    def test_send_metric(self, newrelic):
-        metrics.send_metric("testkey", "testvalue")
-        newrelic.agent.add_custom_parameter.assert_called_once_with(
-            "testkey", "testvalue"
+class TestRecordSearchQueryParams(object):
+    def test_it_passes_parameters_to_newrelic(self, newrelic_agent):
+        params = MultiDict(tag="tagsvalue", _separate_replies=True, url="urlvalue")
+        metrics.record_search_query_params(params, True)
+        newrelic_agent.current_transaction().add_custom_parameters.assert_called_once_with(
+            [
+                ("es_url", "urlvalue"),
+                ("es_tag", "tagsvalue"),
+                ("es__separate_replies", True),
+            ]
         )
 
-    @pytest.fixture(autouse=True)
-    def newrelic(self, patch):
-        return patch("h.util.metrics.newrelic")
+    def test_it_does_not_pass_unrecognized_parameters_to_newrelic(self, newrelic_agent):
+        params = MultiDict(bad="unwanted")
+        metrics.record_search_query_params(params, True)
+        newrelic_agent.current_transaction().add_custom_parameters.assert_called_once_with(
+            [("es__separate_replies", True)]
+        )
 
+    def test_it_does_not_record_separate_replies_if_False(self, newrelic_agent):
+        params = MultiDict({})
+        metrics.record_search_query_params(params, False)
+        newrelic_agent.current_transaction().add_custom_parameters.assert_called_once_with(
+            []
+        )
 
-@pytest.mark.usefixtures("send_metric")
-class TestRecordSearchApiUsageMetrics(object):
-    def test_records_parameters(self, send_metric):
-        params = MultiDict(tags="tagsvalue", url="urlvalue", bad="unwanted")
-        metrics.record_search_api_usage_metrics(params)
-        assert send_metric.call_args_list == [
-            call("es_url", "urlvalue"),
-            call("es_tags", "tagsvalue"),
-        ]
+    def test_it_does_not_record_parameters_if_no_transaction(self, newrelic_agent):
+        newrelic_agent.current_transaction.return_value = None
+        params = MultiDict(tag="tagsvalue")
+        metrics.record_search_query_params(params, True)
 
-
-@pytest.mark.usefixtures("send_metric")
-class TestRecordSearchQueryParamUsage(object):
-    def test_records_parameters(self, send_metric):
-        params = MultiDict(tags="tagsvalue", url="urlvalue", bad="unwanted")
-        metrics.record_search_query_param_usage(params, True)
-        assert send_metric.call_args_list == [
-            call("es__separate_replies", True),
-            call("es_url", "urlvalue"),
-            call("es_tags", "tagsvalue"),
-        ]
+    @pytest.fixture
+    def newrelic_agent(self, newrelic_agent):
+        newrelic_agent.current_transaction.return_value.add_custom_parameters = (
+            mock.Mock()
+        )
+        return newrelic_agent
 
 
 @pytest.fixture
-def send_metric(patch):
-    return patch("h.util.metrics.send_metric")
+def newrelic_agent(patch):
+    return patch("h.util.metrics.newrelic.agent")
