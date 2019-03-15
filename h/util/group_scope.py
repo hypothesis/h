20716829cae46c3731f2f2cb75446275f9405bd3
Lyza Danger Gardner <lyza@lyza.com>
2019-03-15 15:20:17 -0400

    Update scope-matching functions

--- h/util/group_scope.py
+++ h/util/group_scope.py
@@ -18,6 +18,25 @@ def match(uri, scopes):
     return scope in scopes
 
 
+def uri_in_scope(uri, scopes):
+    """
+    Does the URI match any of the scope patterns?
+
+    Return True if the URI matches one or more patterns in scopes (if the
+    URI string begins with any of the scope strings)
+
+    :arg uri: URI string in question
+    :arg scopes: List of URIs that define scope
+    :type scopes: list(str)
+    :rtype: bool
+    """
+    scope_matches = [scope for scope in scopes if uri.startswith(scope)]
+
+    if len(scope_matches):
+        return True
+    return False
+
+
 # TODO: This concept no longer makes sense with more granular scoping. There is
 # no equivalent 1:1 uri <-> scope relationship. Remove this function soon.
 def uri_scope(uri):
@@ -25,9 +44,9 @@ def uri_scope(uri):
     Return the scope for a given URI
 
     Parse a scope from a URI string. Presently a scope is an origin, so this
-    proxies to _parse_origin.
+    proxies to parse_origin.
     """
-    return _parse_origin(uri)
+    return parse_origin(uri)
 
 
 def uri_to_scope(uri):
@@ -41,7 +60,7 @@ def uri_to_scope(uri):
     # A URL with no origin component will result in a `None` value for
     # origin, while a URL with no path component will result in an empty
     # string for path.
-    origin = _parse_origin(uri)
+    origin = parse_origin(uri)
     path = _parse_path(uri) or None
     return (origin, path)
 
@@ -54,7 +73,7 @@ def _parse_path(uri):
     return parsed[2]
 
 
-def _parse_origin(uri):
+def parse_origin(uri):
     """
     Return the origin of a URI or None if empty or invalid.
 
@@ -62,7 +81,10 @@ def _parse_origin(uri):
     Return ``<scheme> + '://' + <host> + <port>``
     for a URI.
 
+    This can return None if no valid origin can be extracted from ``uri``
+
     :param uri: URI string
+    :rtype: str or None
     """
 
     if uri is None:
--- tests/h/util/group_scope_test.py
+++ tests/h/util/group_scope_test.py
@@ -28,6 +28,50 @@ def test_it_parses_scope_from_uri(uri, expected_scope):
     assert scope == expected_scope
 
 
+class TestURIInScope(object):
+    @pytest.mark.parametrize(
+        "uri,in_origin,in_path,in_other",
+        [
+            ("https://www.foo.com", True, False, False),
+            ("http://foo.com", False, False, False),
+            ("http://www.foo.com/bar/qux.html", True, True, True),
+            ("http://www.foo.com/bar/baz/qux.html", True, True, True),
+            ("http://foo.com/", False, False, False),
+            ("http://www.foo.com/bar.baz", True, False, False),
+            ("www.foo.com", False, False, False),
+            ("randoscheme://foo.com", False, False, False),
+            ("foo", False, False, False),
+            ("https://www.foo.com/bar/baz", True, False, False),
+            ("", False, False, False),
+        ],
+    )
+    def test_it_returns_True_if_uri_matches_one_or_more_scopes(
+        self, scope_lists, uri, in_origin, in_path, in_other
+    ):
+        assert scope_util.uri_in_scope(uri, scope_lists["origin_only"]) == in_origin
+        assert scope_util.uri_in_scope(uri, scope_lists["with_path"]) == in_path
+        assert scope_util.uri_in_scope(uri, scope_lists["with_other"]) == in_other
+
+    @pytest.fixture
+    def match(self, patch):
+        return patch("h.util.group_scope.re.match")
+
+    @pytest.fixture
+    def scope_lists(self):
+        return {
+            "origin_only": ["http://www.foo.com", "https://www.foo.com"],
+            "with_path": [
+                "http://www.foo.com/bar/baz",
+                "http://www.foo.com/bar/qux",
+                "http://www.foo.com/bar/baz/qux",
+            ],
+            "with_other": [
+                "http://www.foo.com/bar/baz/qux.html",
+                "http://www.foo.com/bar/qux",
+            ],
+        }
+
+
 class TestScopeMatch(object):
     @pytest.mark.parametrize(
         "uri,expected",
@@ -87,3 +131,18 @@ class TestURIToScope(object):
     )
     def test_it_parses_origin_and_path_from_uri(self, uri, expected_scope):
         assert scope_util.uri_to_scope(uri) == expected_scope
+
+
+class TestParseOrigin(object):
+    @pytest.mark.parametrize(
+        "uri,expected_origin",
+        [
+            ("https://www.foo.com/foo/qux/bar.html", "https://www.foo.com"),
+            ("http://foo.com/baz", "http://foo.com"),
+            ("http://foo.com:3553/bar", "http://foo.com:3553"),
+            ("http://www.foo.bar.com", "http://www.foo.bar.com"),
+            ("foo.com", None),
+        ],
+    )
+    def test_it_parses_origin_from_uri(self, uri, expected_origin):
+        assert scope_util.parse_origin(uri) == expected_origin
