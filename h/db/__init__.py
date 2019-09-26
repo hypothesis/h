f93330a54a6680d5f125d907a1755771b6aed6a4
Sean Hammond <git@snhmnd.fastmail.com>
2019-09-26 18:31:42 +0100

    Don't warn "uncommitted changes" if no open connections
    Don't log a "closing a session with uncommitted changes" warning if
    there are no unclosed DB connections.
    
    It's normal for there to be changes that don't get committed (nor
    flushed or rolled back etc): this happens when pyramid_tm aborts the
    transaction and zope.sqlalchemy aborts the database transaction. When
    this happens there will be no open DB connection anymore
    (zope.sqlalchemy closes it) but there might be ORM changes that were
    never committed. This is deliberate and shouldn't generate a warning.
    
    If there _are_ still open DB connections and uncommitted changes that
    might indicate a problem, so do log that.

--- h/db/__init__.py
+++ h/db/__init__.py
@@ -107,24 +107,23 @@ def _session(request):
     # See: https://github.com/Pylons/pyramid_tm/issues/40
     @request.add_finished_callback
     def close_the_sqlalchemy_session(request):
-        changes = tracker.uncommitted_changes() if tracker else []
-        if changes:
-            msg = "closing a session with uncommitted changes %s"
-            log.warning(msg, changes, extra={"stack": True, "changes": changes})
-
-        elif len(session.transaction._connections) > 1:
-            # There are no uncommitted changes but there do appear to be
-            # unclosed database connections.
-            log.warning(
-                "closing an unclosed DB session (no uncommitted changes)",
-                extra={"stack": True},
-            )
-
+        if len(session.transaction._connections) > 1:
+            # There appear to still be open DB connections belonging to this
+            # request. This shouldn't happen.
+            changes = tracker.uncommitted_changes() if tracker else []
+            if changes:
+                msg = "closing a session with uncommitted changes %s"
+                log.warning(msg, changes, extra={"stack": True, "changes": changes})
+            else:
+                log.warning(
+                    "closing an unclosed DB session (no uncommitted changes)",
+                    extra={"stack": True},
+                )
         # Close any unclosed DB connections.
-        # This is done outside of either of the `if` statements above just in
-        # case: it's okay to call `session.close()` even if the session does
-        # not need to be closed, so just call it unconditionally so that
-        # there's no chance of leaking any unclosed DB connections.
+        # This is done outside of the `if` statement above just in case: it's
+        # okay to call `session.close()` even if the session does not need to
+        # be closed, so just call it unconditionally so that there's no chance
+        # of leaking any unclosed DB connections.
         session.close()
 
     return session
