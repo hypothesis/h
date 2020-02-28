36df4d962e53206179eb77a237c93d5d37d7677f
Jon Betts <jon.betts@hypothes.is>
2020-02-28 12:40:29 +0000

    Removing un-used script

--- scripts/elasticsearch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Install Elasticsearch in /tmp and run it.
-set -ev
-mkdir /tmp/elasticsearch
-wget -O - https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.4.tar.gz | tar xz --directory=/tmp/elasticsearch --strip-components=1
-/tmp/elasticsearch/bin/elasticsearch-plugin install analysis-icu
-/tmp/elasticsearch/bin/elasticsearch -E http.port=9200 -E path.data=/tmp/elasticsearch_data -d
