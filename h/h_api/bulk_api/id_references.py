67ee5e3c2df6edd637cf00bf28819654106bc23b
Jon Betts <jon.betts@hypothes.is>
2020-03-03 12:31:17 +0000

    Add id reference store
    This is an object for finding, storing and filling out id
    references.

---
+++ h/h_api/bulk_api/id_references.py
@@ -0,0 +1,81 @@
+"""Objects for referencing and de-referencing ids."""
+
+from collections import defaultdict
+
+from h.h_api.enums import DataType
+from h.h_api.exceptions import UnpopulatedReferenceError
+
+
+class IdReferences:
+    """
+    A store of id references which can discover and populate them.
+
+    If we create one object which relies on another (group membership on a
+    group) we will not know the id of the group until the group has been
+    updated.
+
+    To get around this you can add a temporary id reference to the group,
+    e.g. "my_ref" which is swapped for a concrete id when the group is
+    created. We can reference this in the group membership with
+    `{"$ref": "my_id"}`.
+
+    It's this classes' job to store and fill out these references.
+    """
+
+    REF_KEY = "$ref"
+
+    def __init__(self):
+        self.id_refs = defaultdict(dict)
+
+    def fill_out(self, body):
+        """
+        Find any references to ids in the data and fill them out.
+
+        We are looking for items like this:
+
+            {"type": <data_type>, "id": {"$ref": <id_ref>}}
+
+        If this matching data is found the object will have the `id` field
+        set to the concrete id.
+
+        :param body: Raw JSON API compatible data to modify
+        :raise UnpopulatedReferenceError: When no concrete reference can be found
+        """
+        for data_type, id_ref, data_key, data in self._find_references(body):
+            data[data_key] = self._get_concrete_id(data_type, id_ref)
+
+    def add_concrete_id(self, data_type, id_ref, concrete_id):
+        """
+        Add a concrete id for a reference.
+
+        :data_type: Data type of the object being referenced
+        :id_ref: The reference string
+        :concrete_id: The real id
+        """
+        data_type = DataType(data_type)
+
+        self.id_refs[data_type][id_ref] = concrete_id
+
+    def _get_concrete_id(self, data_type, id_ref):
+        data_type = DataType(data_type)
+        try:
+            return self.id_refs[data_type][id_ref]
+        except KeyError:
+            raise UnpopulatedReferenceError(data_type, id_ref)
+
+    @classmethod
+    def _find_references(cls, body):
+        """Search for references in the relationships of an object.
+
+        :param body: Raw JSON API compatible data to search
+        :return: A list of tuples with data type, reference, key and parent
+        """
+
+        for relationship in body["data"].get("relationships", {}).values():
+            relationship = relationship["data"]
+            if not isinstance(relationship["id"], dict):
+                continue
+
+            id_ref = relationship["id"].get(cls.REF_KEY)
+            if id_ref is not None:
+                yield (DataType(relationship["type"]), id_ref, "id", relationship)
---
+++ tests/h/h_api/bulk_api/id_references_test.py
@@ -0,0 +1,41 @@
+import pytest
+
+from h.h_api.bulk_api.id_references import IdReferences
+from h.h_api.bulk_api.model.data_body import CreateGroupMembership
+from h.h_api.enums import DataType
+from h.h_api.exceptions import UnpopulatedReferenceError
+
+
+class TestIdReferences:
+    def test_we_can_add_a_concrete_id(self):
+        id_refs = IdReferences()
+
+        id_refs.add_concrete_id(DataType.GROUP.value, "my_ref", "real_id")
+
+        assert id_refs.id_refs[DataType.GROUP]["my_ref"] == "real_id"
+
+    def test_we_can_fill_out_a_reference(self, group_membership_body):
+        id_refs = IdReferences()
+        id_refs.add_concrete_id(DataType.GROUP, "group_ref", "real_id")
+
+        id_refs.fill_out(group_membership_body)
+
+        group_id = group_membership_body["data"]["relationships"]["group"]["data"]["id"]
+
+        assert group_id == "real_id"
+
+    def test_with_missing_references_we_raise_UnpopulatedReferenceError(
+        self, group_membership_body
+    ):
+        id_refs = IdReferences()
+
+        with pytest.raises(UnpopulatedReferenceError):
+            id_refs.fill_out(group_membership_body)
+
+    @pytest.fixture
+    def group_membership_body(self):
+        group_membership = CreateGroupMembership.create(
+            "acct:user@example.com", "group_ref"
+        )
+
+        return group_membership.raw
