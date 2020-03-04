03fcb2be715f3e71a0eb19039894db46552a6d75
Jon Betts <jon.betts@hypothes.is>
2020-03-04 14:00:18 +0000

    Add a user activation date migration script

---
+++ h/migrations/versions/b9de5c897f73_add_user_activation_date.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Add user activation date"""
+import sqlalchemy as sa
+from alembic import op
+
+revision = "b9de5c897f73"
+down_revision = "8bd83598ad77"
+
+
+def upgrade():
+    op.add_column(
+        "user",
+        sa.Column("activation_date", sa.TIMESTAMP(timezone=False), nullable=True),
+    )
+
+
+def downgrade():
+    op.drop_column("user", "activation_date")
