e14f88c5a0ba0360e5511d66df483236ce0dc894
Jon Betts <jon.betts@hypothes.is>
2020-03-09 12:11:48 +0000

    Remove default user last login date and make it nullable

---
+++ h/migrations/versions/3359d7d8ec29_remove_user_last_login_date_default_values.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Remove user last login date default values"""
+import datetime
+
+import sqlalchemy as sa
+from alembic import op
+
+revision = "3359d7d8ec29"
+down_revision = "b9de5c897f73"
+
+
+def upgrade():
+    op.alter_column("user", "last_login_date", nullable=True, server_default=None)
+
+
+def downgrade():
+    op.alter_column(
+        "user", "last_login_date", server_default=sa.func.now(), nullable=False,
+    )
