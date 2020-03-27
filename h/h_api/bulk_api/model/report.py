721f6938dc73aef6d4db6b28c6a285dc0ad5e12d
Jon Betts <jon.betts@hypothes.is>
2020-03-27 17:01:47 +0000

    Extend reports to have a public and private id

--- h/h_api/bulk_api/model/report.py
+++ h/h_api/bulk_api/model/report.py
@@ -9,13 +9,18 @@ class Report:
     # Attach this here for the convenience of the consuming library
     CommandResult = CommandResult
 
-    def __init__(self, outcome, id_):
+    def __init__(self, outcome, id_, public_id=None):
         """
         :param outcome: An instance of CommandResult
-        :param id_: The id of the resource update
+        :param id_: The id of the updated resource
+        :param public_id: The user friendly id of the resource
         """
         if id_ is None:
             raise ValueError("id_ is required for successful outcomes")
 
         self.outcome = self.CommandResult(outcome)
         self.id = id_
+        self.public_id = id_ if public_id is None else public_id
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} {self.outcome.value}: '{self.id}' ({self.public_id})>"
