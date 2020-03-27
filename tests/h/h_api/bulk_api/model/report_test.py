07543e6eb90b20b97838e9c715e76d80e91c1ab6
Jon Betts <jon.betts@hypothes.is>
2020-03-27 17:01:47 +0000

    Extend reports to have a public and private id

--- tests/h/h_api/bulk_api/model/report_test.py
+++ tests/h/h_api/bulk_api/model/report_test.py
@@ -11,3 +11,10 @@ class TestReport:
 
         assert report.outcome == CommandResult.UPDATED
         assert report.id == "id"
+        assert report.public_id == "id"
+
+    def test_different_ids(self):
+        report = Report(CommandResult.UPSERTED, "private_id", "public_id")
+
+        assert report.id == "private_id"
+        assert report.public_id == "public_id"
