d46a24664ca067e456ff5a71899f8993c6baea52
Jon Betts <jon.betts@hypothes.is>
2020-04-14 16:37:27 +0100

    #2 Check the return value of the bulk API and use a real user in the func tests (#5993)
    * Check the return value of the bulk API and use a real user
    
    Previously our effective user was just a string. We now actually set one
    up in the DB so the request can work. This will be important as we
    start to look this value up.

--- tests/functional/api/test_bulk.py
+++ tests/functional/api/test_bulk.py
@@ -4,25 +4,19 @@ import pytest
 from h_matchers import Any
 
 from h.h_api.bulk_api import CommandBuilder
+from h.h_api.enums import ViewType
+from h.models import User
 
 
 class TestBulk:
+    AUTHORITY = "lms.hypothes.is"
+
     def test_it_requires_authentication(self, app, bad_header):
         response = app.post(
             "/api/bulk", b"dummy", headers=bad_header, expect_errors=True
         )
 
         assert response.status_int == 404
-
-    def test_it_accepts_a_valid_request(self, app, nd_json, lms_auth_header):
-        response = app.post("/api/bulk", nd_json, headers=lms_auth_header)
-
-        assert response.status_int == 200
-        assert response.content_type == "application/x-ndjson"
-        assert (
-            response.headers["Hypothesis-Media-Type"]
-            == "application/vnd.hypothesis.v1+x-ndjson"
-        )
 
     def test_it_raises_errors_for_invalid_request(self, app, lms_auth_header):
         response = app.post(
@@ -38,17 +32,50 @@ class TestBulk:
             )
         }
 
-    @pytest.fixture
-    def commands(self):
-        authority = "lms.hypothes.is"
+    def test_it_accepts_a_valid_request(
+        self, app, nd_json, lms_auth_header, db_session
+    ):
+        app.post("/api/bulk", nd_json, headers=lms_auth_header)
 
-        return [
-            CommandBuilder.configure(f"acct:user1@{authority}", total_instructions=4),
+        response = app.post("/api/bulk", nd_json, headers=lms_auth_header)
+
+        assert response.status_int == 200
+        assert response.content_type == "application/x-ndjson"
+        assert (
+            response.headers["Hypothesis-Media-Type"]
+            == "application/vnd.hypothesis.v1+x-ndjson"
+        )
+
+        self._assert_response_has_expected_values(response)
+
+    @staticmethod
+    def _assert_response_has_expected_values(response):
+        lines = response.body.decode("utf-8").split("\n")
+        lines = [json.loads(line) for line in lines if line]
+
+        assert lines == [
+            {"data": {"id": Any(), "type": "user"}},
+            {"data": {"id": Any(), "type": "group"}},
+            {"data": {"id": Any(), "type": "group"}},
+            {"data": {"id": Any(), "type": "group"}},
+            {"data": {"id": Any(), "type": "group_membership"}},
+            {"data": {"id": Any(), "type": "group_membership"}},
+            {"data": {"id": Any(), "type": "group_membership"}},
+        ]
+
+    @pytest.fixture
+    def commands(self, user):
+        group_count = 3
+
+        commands = [
+            CommandBuilder.configure(
+                user.userid, total_instructions=group_count * 2 + 2, view=ViewType.BASIC
+            ),
             CommandBuilder.user.upsert(
                 {
                     "username": "user2",
                     "display_name": "display_name",
-                    "authority": authority,
+                    "authority": self.AUTHORITY,
                     "identities": [
                         {
                             "provider": "provider",
@@ -58,16 +85,33 @@ class TestBulk:
                 },
                 "user_ref",
             ),
-            CommandBuilder.group.upsert(
-                {
-                    "authority": authority,
-                    "authority_provided_id": "name",
-                    "name": "name",
-                },
-                "group_ref",
-            ),
-            CommandBuilder.group_membership.create("user_ref", "group_ref"),
         ]
+
+        for i in range(group_count):
+            commands.append(
+                CommandBuilder.group.upsert(
+                    {
+                        "authority": self.AUTHORITY,
+                        "authority_provided_id": f"id_{i}",
+                        "name": f"name_{i}",
+                    },
+                    f"group_ref_{i}",
+                )
+            )
+
+        for i in range(group_count):
+            commands.append(
+                CommandBuilder.group_membership.create("user_ref", f"group_ref_{i}")
+            )
+
+        return commands
+
+    @pytest.fixture
+    def user(self, db_session):
+        user = User(authority=self.AUTHORITY, _username="username")
+        db_session.add(user)
+
+        return user
 
     @pytest.fixture
     def nd_json(self, commands):
