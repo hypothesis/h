e2467f5b5d4a5348b944f07a7f4587bdb1daf157
Jon Betts <jon.betts@hypothes.is>
2020-04-20 13:31:53 +0100

    Remove H-API code

--- h/h_api/bulk_api/__init__.py
+++
@@ -1,7 +0,0 @@
-from h.h_api.bulk_api.command_builder import CommandBuilder
-from h.h_api.bulk_api.entry_point import BulkAPI
-from h.h_api.bulk_api.executor import Executor
-from h.h_api.bulk_api.model.report import Report
-
-# Export the minimum items a user will need to work with us
-__all__ = ["CommandBuilder", "BulkAPI", "Report", "Executor"]
