a2562964ef1aa8e420956c4c475b783c3c51fa28
Jon Betts <jon.betts@hypothes.is>
2020-09-14 18:48:40 +0100

    Pre-compile as much as possible in the filter function
     * This should reduce the time required to apply the filter
     * This is mostly due to avoiding ifs and repeated computation
     * There has also been an optimisation which was previously wasting time
       for no benefit
     * For me this represents about a 3.4x speed up in time to calculate the filter

--- h/streamer/filter.py
+++ h/streamer/filter.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import unicodedata
+from operator import attrgetter
 
 from h.util.uri import normalize as normalize_uri
 
@@ -29,6 +30,12 @@ SCHEMA = {
 
 
 class FilterHandler:
+    FIELD_GETTERS = {
+        "/uri": attrgetter("target_uri"),
+        "/references": attrgetter("references"),
+        "/id": attrgetter("id"),
+    }
+
     def __init__(self, filter_json):
         self.filter = filter_json
 
@@ -65,56 +72,54 @@ class FilterHandler:
 
     @classmethod
     def _get_filter(cls, clause):
-        def clause_filter(annotation):
-            field_path = clause["field"]
+        """Get a filter function for a given clause.
 
-            # Extract the annotation property that corresponds to the "field"
-            # path in the filter. This only supports a small set of paths
-            # which are actually used by the Hypothesis client.
-            field_value = None
+        :param clause: The decoded JSON clause
+        :return: A function which accepts an annotation and returns a truth
+            value depending on whether the value applies
+        """
+        field_path = clause["field"]
+
+        try:
+            field_getter = cls.FIELD_GETTERS[field_path]
+        except KeyError:
+            return
+
+        def normalize(term, known_term=False):
+            if not known_term and isinstance(term, list):
+                return [normalize(sub_term, known_term=True) for sub_term in term]
+
+            # Notice closure around field_path here
             if field_path == "/uri":
-                field_value = annotation.target_uri
-            elif field_path == "/references":
-                field_value = annotation.references
-            elif field_path == "/id":
-                field_value = annotation.id
+                # Apply field-specific normalization.
+                return normalize_uri(term)
+
+            # Apply generic normalization.
+            return uni_fold(term)
+
+        filter_term = normalize(clause["value"])
+
+        def clause_filter(annotation):
+            # Extract the annotation property that corresponds to the "field"
+            # path in the filter.
+            field_value = field_getter(annotation)
 
             if field_value is None:
                 return False
 
-            filter_term = clause["value"]
-
-            def normalize(term):
-                # Apply generic normalization.
-                normalized = uni_fold(term)
-
-                # Apply field-specific normalization.
-                if field_path == "/uri":
-                    normalized = normalize_uri(term)
-
-                return normalized
-
-            if isinstance(filter_term, list):
-                filter_term = [normalize(t) for t in filter_term]
-            else:
-                filter_term = normalize(filter_term)
-
-            if isinstance(field_value, list):
-                field_value = [normalize(v) for v in field_value]
-            else:
-                field_value = normalize(field_value)
+            field_value = normalize(field_value)
 
             if clause["operator"] == "one_of":
-                # The `one_of` operator behaves differently depending on whether
-                # the annotation's field value is a list (eg. tags) or atom (eg. id).
-                #
-                # This is not ideal but the client currently relies on it.
+                # The `one_of` operator behaves differently depending on
+                # whether the annotation's field value is a list (eg. tags)
+                # or atom (eg. id). This is not ideal but the client currently
+                # relies on it.
                 if isinstance(field_value, list):
                     return filter_term in field_value
-                else:
-                    return field_value in filter_term
-            else:
-                return field_value == filter_term
+
+                return field_value in filter_term
+
+            return field_value == filter_term
 
         return clause_filter
 
