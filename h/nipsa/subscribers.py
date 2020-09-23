07fcfc89b0b9a94614524b59015a3e44b8b83d9f
Jon Betts <jon.betts@hypothes.is>
2020-09-23 19:31:22 +0100

    Simplify and namespace the Nipsa event subscriber and tests
    The tests had a whole section testing that a service we don't rely on
    doesn't effect our output if we change values from it...

--- h/nipsa/subscribers.py
+++ h/nipsa/subscribers.py
@@ -1,18 +1,12 @@
-def transform_annotation(event):
-    """Add a {"nipsa": True} field on moderated annotations or those whose users are flagged."""
-    payload = event.annotation_dict
+def nipsa_transform_annotation(event):
+    """Mark moderated or flagged annotations.
 
-    nipsa = _user_nipsa(event.request, payload)
+    Adds `{"nipsa": True}` to an annotation.
+    """
+    user = event.annotation_dict.get("user")
+    if user is None:
+        return
 
-    if nipsa:
-        payload["nipsa"] = True
-
-
-def _user_nipsa(request, payload):
-    nipsa_service = request.find_service(name="nipsa")
-    return "user" in payload and nipsa_service.is_flagged(payload["user"])
-
-
-def _annotation_moderated(request, annotation):
-    svc = request.find_service(name="annotation_moderation")
-    return svc.hidden(annotation)
+    nipsa_service = event.request.find_service(name="nipsa")
+    if nipsa_service.is_flagged(user):
+        event.annotation_dict["nipsa"] = True
