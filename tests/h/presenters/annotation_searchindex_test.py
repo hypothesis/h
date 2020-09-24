184130678dd17f112c11cc71fa33cfb37fffcd75
Jon Betts <jon.betts@hypothes.is>
2020-09-24 14:30:47 +0100

    Refactor AnnotationSearchIndexPresenter tests to remove duplication

--- tests/h/presenters/annotation_searchindex_test.py
+++ tests/h/presenters/annotation_searchindex_test.py
@@ -7,12 +7,9 @@ from h.presenters.annotation_searchindex import AnnotationSearchIndexPresenter
 from h.services.annotation_moderation import AnnotationModerationService
 
 
-@pytest.mark.usefixtures(
-    "DocumentSearchIndexPresenter", "moderation_service", "thread_ids", "nipsa_service"
-)
+@pytest.mark.usefixtures("nipsa_service")
 class TestAnnotationSearchIndexPresenter:
-    def test_asdict(self, DocumentSearchIndexPresenter, pyramid_request, thread_ids):
-
+    def test_asdict(self, DocumentSearchIndexPresenter, pyramid_request):
         annotation = mock.MagicMock(
             id="xyz123",
             created=datetime.datetime(2016, 2, 24, 18, 3, 25, 768),
@@ -26,7 +23,7 @@ class TestAnnotationSearchIndexPresenter:
             shared=True,
             target_selectors=[{"TestSelector": "foobar"}],
             references=["referenced-id-1", "referenced-id-2"],
-            thread_ids=thread_ids,
+            thread_ids=["thread-id-1", "thread-id-2"],
             extra={"extra-1": "foo", "extra-2": "bar"},
         )
         DocumentSearchIndexPresenter.return_value.asdict.return_value = {"foo": "bar"}
@@ -57,101 +54,37 @@ class TestAnnotationSearchIndexPresenter:
             ],
             "document": {"foo": "bar"},
             "references": ["referenced-id-1", "referenced-id-2"],
-            "thread_ids": thread_ids,
+            "thread_ids": ["thread-id-1", "thread-id-2"],
             "hidden": False,
         }
 
-    def test_it_copies_target_uri_normalized_to_target_scope(self, pyramid_request):
-        annotation = mock.MagicMock(
-            userid="acct:luke@hypothes.is",
-            target_uri_normalized="http://example.com/normalized",
-            extra={},
-        )
-
-        annotation_dict = AnnotationSearchIndexPresenter(
-            annotation, pyramid_request
-        ).asdict()
-
-        assert annotation_dict["target"][0]["scope"] == [
-            "http://example.com/normalized"
-        ]
-
-    def test_it_marks_annotation_hidden_when_it_and_all_children_are_moderated(
-        self, pyramid_request, moderation_service, thread_ids
+    @pytest.mark.parametrize("is_moderated", [True, False])
+    @pytest.mark.parametrize("replies_moderated", [True, False])
+    def test_it_marks_annotation_hidden_correctly(
+        self, pyramid_request, moderation_service, is_moderated, replies_moderated
     ):
+        # Annotation reply ids are referred to as thread_ids in our code base.
+        reply_ids = ["thread-id-1", "thread-id-2"]
         annotation = mock.MagicMock(
-            userid="acct:luke@hypothes.is", thread_ids=thread_ids
+            userid="acct:luke@hypothes.is", thread_ids=reply_ids
         )
 
-        moderation_service.all_hidden.return_value = [annotation.id] + thread_ids
+        # Configure moderation return value
+        moderated_ids = []
+        if is_moderated:
+            moderated_ids.append(annotation.id)
+        if replies_moderated:
+            moderated_ids.extend(reply_ids)
+        moderation_service.all_hidden.return_value = moderated_ids
 
         annotation_dict = AnnotationSearchIndexPresenter(
             annotation, pyramid_request
         ).asdict()
 
-        assert annotation_dict["hidden"] is True
+        # We are hidden if both we, and all of our replies are moderated
+        assert annotation_dict["hidden"] == bool(is_moderated and replies_moderated)
 
-    def test_it_does_not_mark_annotation_hidden_when_it_is_not_moderated(
-        self, pyramid_request, moderation_service, thread_ids
-    ):
-        annotation = mock.MagicMock(
-            userid="acct:luke@hypothes.is", thread_ids=thread_ids
-        )
-
-        moderation_service.all_hidden.return_value = thread_ids
-
-        annotation_dict = AnnotationSearchIndexPresenter(
-            annotation, pyramid_request
-        ).asdict()
-
-        assert annotation_dict["hidden"] is False
-
-    def test_it_does_not_mark_annotation_hidden_when_children_are_not_moderated(
-        self, pyramid_request, moderation_service, thread_ids
-    ):
-        annotation = mock.MagicMock(
-            userid="acct:luke@hypothes.is", thread_ids=thread_ids
-        )
-
-        moderation_service.all_hidden.return_value = [annotation.id] + thread_ids[1:]
-
-        annotation_dict = AnnotationSearchIndexPresenter(
-            annotation, pyramid_request
-        ).asdict()
-
-        assert annotation_dict["hidden"] is False
-
-    def test_it_does_not_mark_annotation_hidden_when_not_moderated_and_no_replies(
-        self, pyramid_request
-    ):
-        thread_ids = []
-        annotation = mock.MagicMock(
-            userid="acct:luke@hypothes.is", thread_ids=thread_ids
-        )
-
-        annotation_dict = AnnotationSearchIndexPresenter(
-            annotation, pyramid_request
-        ).asdict()
-
-        assert annotation_dict["hidden"] is False
-
-    def test_it_marks_annotation_hidden_when_moderated_and_no_replies(
-        self, pyramid_request, moderation_service
-    ):
-        thread_ids = []
-        annotation = mock.MagicMock(
-            userid="acct:luke@hypothes.is", thread_ids=thread_ids
-        )
-
-        moderation_service.all_hidden.return_value = [annotation.id] + thread_ids
-
-        annotation_dict = AnnotationSearchIndexPresenter(
-            annotation, pyramid_request
-        ).asdict()
-
-        assert annotation_dict["hidden"] is True
-
-    @pytest.fixture
+    @pytest.fixture(autouse=True)
     def DocumentSearchIndexPresenter(self, patch):
         class_ = patch(
             "h.presenters.annotation_searchindex.DocumentSearchIndexPresenter"
@@ -159,18 +92,11 @@ class TestAnnotationSearchIndexPresenter:
         class_.return_value.asdict.return_value = {}
         return class_
 
-
-@pytest.fixture
-def moderation_service(pyramid_config):
-    svc = mock.create_autospec(
-        AnnotationModerationService, spec_set=True, instance=True
-    )
-    svc.all_hidden.return_value = []
-    pyramid_config.register_service(svc, name="annotation_moderation")
-    return svc
-
-
-@pytest.fixture
-def thread_ids():
-    # Annotation reply ids are referred to as thread_ids in our code base.
-    return ["thread-id-1", "thread-id-2"]
+    @pytest.fixture(autouse=True)
+    def moderation_service(self, pyramid_config):
+        svc = mock.create_autospec(
+            AnnotationModerationService, spec_set=True, instance=True
+        )
+        svc.all_hidden.return_value = []
+        pyramid_config.register_service(svc, name="annotation_moderation")
+        return svc
