33e4e9a8ab555507e3dbf685a0ac4bb92247c882
Jon Betts <jon.betts@hypothes.is>
2020-09-29 18:41:30 +0100

    Implement some review suggestions and fix some rebasing mess

--- tests/common/fixtures/services.py
+++ tests/common/fixtures/services.py
@@ -5,25 +5,28 @@ import pytest
 from h.services.nipsa import NipsaService
 from h.services.search_index import SearchIndexService
 
-__all__ = ("service_mocker", "search_index")
+__all__ = ("mock_service", "search_index", "nipsa_service")
 
 
 @pytest.fixture
-def service_mocker(pyramid_config):
-    def service_mocker(class_, name):
-        service = create_autospec(class_, instance=True, spec_set=True)
+def mock_service(pyramid_config):
+    def mock_service(service_class, name):
+        service = create_autospec(service_class, instance=True, spec_set=True)
         pyramid_config.register_service(service, name=name)
 
         return service
 
-    return service_mocker
+    return mock_service
 
 
 @pytest.fixture
-def search_index(service_mocker):
-    return service_mocker(SearchIndexService, "search_index")
+def search_index(mock_service):
+    return mock_service(SearchIndexService, "search_index")
 
 
 @pytest.fixture
-def nipsa_service(pyramid_config):
-    return service_mocker(NipsaService, name="nipsa")
+def nipsa_service(mock_service):
+    nipsa_service = mock_service(NipsaService, name="nipsa")
+    nipsa_service.is_flagged.return_value = False
+
+    return nipsa_service
