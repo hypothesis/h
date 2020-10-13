7218f1bf9d5c82685ddc989ec07c4e868b5507d2
Jon Betts <jon.betts@hypothes.is>
2020-10-13 14:52:31 +0100

    Add reporting over the number of connections (in various statuses)

--- h/websocket.py
+++ h/websocket.py
@@ -37,6 +37,7 @@ license distributed with the ws4py project. Such code remains copyright (c)
 import logging
 import os
 
+import newrelic.agent
 import pyramid
 from gevent.pool import Pool
 from gunicorn.workers.ggevent import GeventPyWSGIWorker, PyWSGIHandler, PyWSGIServer
@@ -44,6 +45,7 @@ from ws4py import format_addresses
 
 from h.config import configure
 from h.sentry_filters import SENTRY_FILTERS
+from h.streamer.metrics import websocket_metrics
 
 log = logging.getLogger(__name__)
 
@@ -204,5 +206,8 @@ def create_app(_global_config, **settings):
     # Add support for logging exceptions whenever they arise
     config.include("pyramid_exclog")
     config.add_settings({"exclog.extra_info": True})
+
+    # Set up metrics collection
+    newrelic.agent.register_data_source(websocket_metrics)
 
     return config.make_wsgi_app()
