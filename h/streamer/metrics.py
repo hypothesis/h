d3199688eaa15e0580ea9b9e7d5139bd754e7b6a
Jon Betts <jon.betts@hypothes.is>
2020-10-14 17:13:16 +0100

    Use simpler version of data collector from newrelic (generator vs factory)

--- h/streamer/metrics.py
+++ h/streamer/metrics.py
@@ -1,30 +1,26 @@
-from newrelic.agent import data_source_factory
+from newrelic.agent import data_source_generator
 
 from h.streamer.streamer import WORK_QUEUE
 from h.streamer.websocket import WebSocket
 
+PREFIX = "Custom/WebSocket"
 
-@data_source_factory(name="WebSocket Metrics")
-def websocket_metrics(_settings, _environ):
+
+@data_source_generator(name="WebSocket Metrics")
+def websocket_metrics():
     """
-    A New Relic metric data source which provides metrics about WebSocket
-    connections.
+    Report metrics about the websocket service to New Relic.
 
     See https://docs.newrelic.com/docs/agents/python-agent/supported-features/python-custom-metrics.
     """
 
-    prefix = "Custom/WebSocket"
+    connections_active = len(WebSocket.instances)
+    connections_anonymous = sum(
+        1 for ws in WebSocket.instances if not ws.authenticated_userid
+    )
 
-    def generate_metrics():
-        connections_active = len(WebSocket.instances)
-        connections_anonymous = sum(
-            1 for ws in WebSocket.instances if not ws.authenticated_userid
-        )
+    yield f"{PREFIX}/Connections/Active", connections_active
+    yield f"{PREFIX}/Connections/Authenticated", connections_active - connections_anonymous
+    yield f"{PREFIX}/Connections/Anonymous", connections_anonymous
 
-        yield f"{prefix}/Connections/Active", connections_active
-        yield f"{prefix}/Connections/Authenticated", connections_active - connections_anonymous
-        yield f"{prefix}/Connections/Anonymous", connections_anonymous
-
-        yield f"{prefix}/WorkQueueSize", WORK_QUEUE.qsize()
-
-    return generate_metrics
+    yield f"{PREFIX}/WorkQueueSize", WORK_QUEUE.qsize()
