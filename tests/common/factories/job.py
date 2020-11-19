3f1f3851e183cdd58838615e1a59dc44d3f208ac
Sean Hammond <git@snhmnd.fastmail.com>
2020-11-19 10:20:58 +0000

    Simplify SyncAnnotationJobFactory
    I think the code is actually better if this factory is *less* clever and
    doesn't support index the job's annotation into Elasticsearch for you.
    
    Tests now have to do this:
    
        annotation = factories.Annotation()
        index(annotation)
        factories.SyncAnnotationJob(annotation=annotation)
    
    Whereas previously, at least if the test didn't need access to
    `annotation` it could be a one-liner:
    
        SyncAnnotationJobFactory(index=True)
    
    But I think the three-line version is arguably better because it makes
    it clearer in the test code what is happening: 1. An annotation is being
    created; 2. The annotation is being indexed; 3. A "sync_annotation" job
    for the annotation is being created. Rather than the reader having to
    know about magic that happens in SyncAnnotationJobFactory to create an
    annotation and index it.
    
    And the new factories.SyncAnnotationJob class is much simpler than
    SyncAnnotationJobFactory, with less use of factory_boy magic. It doesn't
    have to take a special `index` argument and override _create().
    
    Also, since the factory no longer depends on the `index()` fixture, it
    no longer needs to be defined as an inner-class inside a pytest fixture
    method. It can just be defined like a normal factory class.

--- tests/common/factories/job.py
+++ tests/common/factories/job.py
@@ -1,7 +1,11 @@
-from factory import Faker
+import datetime
+
+from factory import Faker, LazyAttribute, LazyFunction, SubFactory
 
 from h import models
+from h.db.types import URLSafeUUID
 
+from .annotation import Annotation
 from .base import ModelFactory
 
 
@@ -12,3 +16,35 @@ class Job(ModelFactory):
     name = "test_job"
     priority = Faker("random_element", elements=[1, 100, 1000, 10000])
     tag = "test_tag"
+
+
+class SyncAnnotationJob(Job):
+    """
+    A factory for creating "sync_annotation" jobs.
+
+    By default this creates jobs with job.name="sync_annotation", with a
+    scheduled_at time in the past, and with a job.kwargs that contains an
+    annotation_id and force=False.
+
+    By default a new annotation will be created for the job to use.
+    The annotation will exist in the DB but will *not* be in
+    Elasticsearch -- tests must index the annotation themselves if they want it
+    to be in Elasticsearch.
+    """
+
+    class Meta:
+        exclude = ("annotation", "force")
+
+    annotation = SubFactory(Annotation)
+    force = False
+
+    name = "sync_annotation"
+    scheduled_at = LazyFunction(
+        lambda: datetime.datetime.utcnow() - datetime.timedelta(hours=1)
+    )
+    kwargs = LazyAttribute(
+        lambda o: {
+            "annotation_id": URLSafeUUID.url_safe_to_hex(o.annotation.id),
+            "force": o.force,
+        }
+    )
