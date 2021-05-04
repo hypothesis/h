30e55e79bf6c0a3cd54e91a6fa53e3c4dc2733f4
Robert Knight <robertknight@gmail.com>
2021-05-04 10:30:26 +0100

    Remove polyfills not required by supported browsers
    Our current browser baseline for all apps is Safari 10.1, Edge 17,
    Chrome 57 and Firefox 53. See the `browserslist` key in the client's
    `package.json` file. None of the polyfills loaded by the h frontend are
    required in these browsers.
    
    We do still need some of these polyfills in PhantomJS (core-js,
    element-closest, whatwg-fetch, unorm) which runs the tests.
    
     - Remove polyfills not needed by production browsers
    
     - Update PhantomJS-only polyfills (core-js, element-closest,
       whatwg-fetch, unorm) to latest versions and load them from
       `bootstrap.js` which is only used in tests
    
     - When adding the above dependencies, npm updated `package-lock.json`
       to the V2 format used by npm v7+

--- h/static/scripts/polyfills.js
+++ h/static/scripts/polyfills.js
@@ -1,45 +1,5 @@
 'use strict';
 
-// ES2015 polyfills
-require('core-js/es6/promise');
-require('core-js/fn/array/find');
-require('core-js/fn/array/find-index');
-require('core-js/fn/array/from');
-require('core-js/fn/array/includes');
-require('core-js/fn/object/assign');
-require('core-js/fn/string/starts-with');
+// There are no polyfills needed at present.
+// This module is a place to add them if needed in future.
 
-// Sets Element.prototype.closest and Element.prototype.matches
-require('element-closest');
-
-// String.prototype.normalize()
-// FIXME: This is a large polyfill which should be only loaded when necessary
-require('unorm');
-
-// Element.prototype.dataset, required by IE 10
-require('element-dataset')();
-
-// Element.prototype.remove. Required by IE 10/11
-if (!Element.prototype.remove) {
-  Element.prototype.remove = function() {
-    if (this.parentNode) {
-      this.parentNode.removeChild(this);
-    }
-  };
-}
-
-// URL constructor, required by IE 10/11,
-// early versions of Microsoft Edge.
-try {
-  new window.URL('https://hypothes.is');
-} catch (err) {
-  require('js-polyfills/url');
-}
-
-// KeyboardEvent.prototype.key
-// (Native in Chrome >= 51, Firefox >= 23, IE >= 9)
-require('keyboardevent-key-polyfill').polyfill();
-
-// Fetch API
-// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
-require('whatwg-fetch');
--- h/static/scripts/tests/bootstrap.js
+++ h/static/scripts/tests/bootstrap.js
@@ -1,4 +1,21 @@
 'use strict';
 
+// Polyfills needed only for PhantomJS, not any of our supported minimum
+// browser versions.
+require('core-js/features/array/find');
+require('core-js/features/array/from');
+require('core-js/features/array/includes');
+require('core-js/features/object/assign');
+require('core-js/features/promise');
+
+// `Element.prototype.closest`
+require('element-closest')(window);
+
+// `String.prototype.normalize`
+require('unorm');
+
+// `fetch` and associated classes
+require('whatwg-fetch');
+
 // Expose the sinon assertions.
 sinon.assert.expose(assert, { prefix: null });
