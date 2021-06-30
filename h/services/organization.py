71248faf687e4ac3b5006667936894b441f2bbae
Jon Betts <jon.betts@hypothes.is>
2021-06-30 13:41:36 +0100

    Move the default methods to the organization service

--- h/services/organization.py
+++ h/services/organization.py
@@ -1,3 +1,5 @@
+import importlib_resources
+
 from h.models import Organization
 
 
@@ -28,6 +30,34 @@ class OrganizationService:
         organization = Organization(name=name, authority=authority, logo=logo)
         self.session.add(organization)
         return organization
+
+    def get_default(self, authority=None):
+        """Get the default org with an option to create it if missing.
+
+        :param authority: Create an org with this authority if none is found
+        :return: The public organization or None.
+        """
+
+        query_extra = {"authority": authority} if authority else {}
+
+        default_org = (
+            self.session.query(Organization)
+            .filter_by(pubid=Organization.DEFAULT_PUBID, **query_extra)
+            .one_or_none()
+        )
+
+        if not default_org and authority:
+            default_org = Organization(
+                name="Hypothesis", authority=authority, pubid=Organization.DEFAULT_PUBID
+            )
+
+            default_org.logo = (
+                importlib_resources.files("h") / "static/images/icons/logo.svg"
+            ).read_text()
+
+            self.session.add(default_org)
+
+        return default_org
 
 
 def organization_factory(context, request):
