61bf471c16ceb92cda14c4d1f5507f4b947d0d10
Jon Betts <jon.betts@hypothes.is>
2021-07-07 19:46:15 +0100

    Remove the context from the presenter entirely

--- h/presenters/organization_json.py
+++ h/presenters/organization_json.py
@@ -1,17 +1,20 @@
-from h.traversal import OrganizationContext
-
-
 class OrganizationJSONPresenter:
     """Present an organization in the JSON format returned by API requests."""
 
     def __init__(self, organization, request):
-        self.context = OrganizationContext(organization, request)
+        self.request = request
         self.organization = organization
 
     def asdict(self):
+        logo = (
+            self.request.route_url("organization_logo", pubid=self.organization.pubid)
+            if self.organization.logo
+            else None
+        )
+
         return {
             "id": self.organization.pubid,
             "default": self.organization.is_default,
-            "logo": self.context.logo_url,
+            "logo": logo,
             "name": self.organization.name,
         }
