a26a715f222e8be94caa082f7934a58fb19b74f6
Jon Betts <jon.betts@hypothes.is>
2021-07-13 18:36:24 +0100

    Simplify tests a bit

--- tests/h/traversal/user_test.py
+++ tests/h/traversal/user_test.py
@@ -1,6 +1,6 @@
-import pyramid.authorization
+from unittest.mock import sentinel
+
 import pytest
-from pyramid import security
 from pyramid.authorization import ACLAuthorizationPolicy
 from pyramid.httpexceptions import HTTPBadRequest
 
@@ -10,71 +10,55 @@ from h.traversal.user import UserContext, UserRoot, UserUserIDRoot
 
 
 class TestUserContext:
-    def test_acl_assigns_read_to_AuthClient_with_user_authority(self, factories):
-        user = factories.User(username="fiona", authority="myauthority.com")
-        res = UserContext(user)
-        actual = res.__acl__()
-        expect = [(security.Allow, "client_authority:myauthority.com", "read")]
-        assert actual == expect
-
     def test_acl_matching_authority_allows_read(self, factories):
+        user = factories.User()
+
+        context = UserContext(user)
+
         policy = ACLAuthorizationPolicy()
-
-        user = factories.User(username="fiona", authority="myauthority.com")
-        res = UserContext(user)
-
-        assert policy.permits(res, ["client_authority:myauthority.com"], "read")
-        assert not policy.permits(res, ["client_authority:example.com"], "read")
+        assert policy.permits(context, [f"client_authority:{user.authority}"], "read")
+        assert not policy.permits(
+            context, ["client_authority:DIFFERENT_AUTHORITY"], "read"
+        )
 
 
 @pytest.mark.usefixtures("user_service", "client_authority")
 class TestUserRoot:
+    @pytest.mark.parametrize(
+        "principals,has_create", (([], False), ([role.AuthClient], True))
+    )
     def test_it_does_not_assign_create_permission_without_auth_client_role(
-        self, pyramid_config, pyramid_request
+        self, pyramid_config, pyramid_request, set_permissions, principals, has_create
     ):
-        policy = pyramid.authorization.ACLAuthorizationPolicy()
-        pyramid_config.testing_securitypolicy("acct:adminuser@foo")
-        pyramid_config.set_authorization_policy(policy)
+        set_permissions(user_id="*any*", principals=principals)
 
         context = UserRoot(pyramid_request)
 
-        assert not pyramid_request.has_permission("create", context)
-
-    def test_it_assigns_create_permission_to_auth_client_role(
-        self, set_permissions, pyramid_request
-    ):
-        set_permissions("acct:adminuser@foo", principals=[role.AuthClient])
-
-        context = UserRoot(pyramid_request)
-
-        assert pyramid_request.has_permission("create", context)
+        assert bool(pyramid_request.has_permission("create", context)) == has_create
 
     def test_it_fetches_the_requested_user(
-        self, pyramid_request, user_factory, user_service
+        self, pyramid_request, user_factory, user_service, client_authority
     ):
-        user_factory["bob"]
+        client_authority.return_value = sentinel.client_authority
 
-        user_service.fetch.assert_called_once_with(
-            "bob", pyramid_request.default_authority
-        )
-
-    def test_it_proxies_to_client_authority(
-        self, pyramid_request, user_factory, client_authority, user_service
-    ):
-        user_factory["bob"]
+        user = user_factory[sentinel.username]
 
         client_authority.assert_called_once_with(pyramid_request)
         user_service.fetch.assert_called_once_with(
-            "bob", pyramid_request.default_authority
+            sentinel.username, sentinel.client_authority
         )
+        assert user == user_service.fetch.return_value
 
-    def test_it_fetches_with_client_authority_if_present(
-        self, pyramid_request, user_factory, client_authority, user_service
+    def test_it_uses_the_default_authority_when_theres_no_client_one(
+        self, user_factory, user_service, pyramid_request
     ):
-        client_authority.return_value = "something.com"
-        user_factory["bob"]
+        client_authority.return_value = None
 
-        user_service.fetch.assert_called_once_with("bob", client_authority.return_value)
+        user_factory[sentinel.username]
+
+        user_service.fetch.assert_called_once_with(
+            sentinel.username, pyramid_request.default_authority
+        )
 
     def test_it_raises_KeyError_if_the_user_does_not_exist(
         self, user_factory, user_service
@@ -84,11 +68,6 @@ class TestUserRoot:
         with pytest.raises(KeyError):
             user_factory["does_not_exist"]
 
-    def test_it_returns_users(self, factories, user_factory, user_service):
-        user_service.fetch.return_value = user = factories.User.build()
-
-        assert user_factory[user.username] == user
-
     @pytest.fixture
     def user_factory(self, pyramid_request):
         return UserRoot(pyramid_request)
@@ -97,14 +76,16 @@ class TestUserRoot:
 @pytest.mark.usefixtures("user_service")
 class TestUserUserIDRoot:
     def test_it_fetches_the_requested_user(
-        self, pyramid_request, user_userid_root, user_service
+        self, user_userid_root, user_service, UserContext
     ):
-        user_userid_root["acct:bob@example.com"]
+        context = user_userid_root[sentinel.userid]
 
-        user_service.fetch.assert_called_once_with("acct:bob@example.com")
+        user_service.fetch.assert_called_once_with(sentinel.userid)
+        UserContext.assert_called_with(user_service.fetch.return_value)
+        assert context == UserContext.return_value
 
     def test_it_fails_with_bad_request_if_the_userid_is_invalid(
-        self, pyramid_request, user_userid_root, user_service
+        self, user_userid_root, user_service
     ):
         user_service.fetch.side_effect = InvalidUserId("dummy id")
 
@@ -119,16 +100,13 @@ class TestUserUserIDRoot:
         with pytest.raises(KeyError):
             user_userid_root["does_not_exist"]
 
-    def test_it_returns_UserContexts(self, factories, user_userid_root, user_service):
-        user_service.fetch.return_value = user = factories.User.build()
-
-        resource = user_userid_root[user.username]
-
-        assert isinstance(resource, UserContext)
-
     @pytest.fixture
     def user_userid_root(self, pyramid_request):
         return UserUserIDRoot(pyramid_request)
+
+    @pytest.fixture(autouse=True)
+    def UserContext(self, patch):
+        return patch("h.traversal.user.UserContext")
 
 
 @pytest.fixture
