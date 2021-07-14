340b5c337eaea33abc816b212a0f98a2ae0aca50
Jon Betts <jon.betts@hypothes.is>
2021-07-14 14:27:35 +0100

    Use an Enum like class for profile permissions

--- h/traversal/profile.py
+++ h/traversal/profile.py
@@ -1,6 +1,7 @@
 from pyramid.security import Allow
 
 from h.auth import role
+from h.security.permissions import Permission
 from h.traversal.root import RootFactory
 
 
@@ -9,4 +10,4 @@ class ProfileRoot(RootFactory):
     Simple Root for API profile endpoints
     """
 
-    __acl__ = [(Allow, role.User, "update")]
+    __acl__ = [(Allow, role.User, Permission.Profile.UPDATE)]
