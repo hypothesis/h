120f9aadf47148eaa8f414e1c991e49b0852f773
Jon Betts <jon.betts@hypothes.is>
2021-07-26 17:51:04 +0100

    Move the tests to reflect the files they are testing

--- tests/functional/h/views/test_activity.py
+++ tests/functional/h/views/test_activity.py
@@ -2,6 +2,27 @@ import pytest
 
 
 class TestGroupSearchController:
+    @pytest.mark.usefixtures("with_logged_in_user")
+    @pytest.mark.xfail  # See https://github.com/hypothesis/product-backlog/issues/109
+    def test_group_page_includes_referrer_tag(self, app, user_owned_group):
+        """
+        The group read page should include a referrer tag.
+
+        When a logged-in user who is a member of the group visits the group's page,
+        the page should include a `<meta name="referrer" ...` tag that asks the
+        browser not to send the path part of the page's URL to third-party servers
+        in the Referer header when following links on the page.
+
+        This is because the group's URL is secret - if you have it you can join
+        the group.
+        """
+
+        response = app.get(f"/groups/{user_owned_group.pubid}/{user_owned_group.slug}")
+
+        assert response.html.head.find(
+            "meta", attrs={"name": "referrer"}, content="origin"
+        )
+
     @pytest.mark.parametrize("should_login", (True, False))
     def test_join_page_is_shown_instead_of_search_without_read_permissions(
         self, login_user, app, group, should_login
