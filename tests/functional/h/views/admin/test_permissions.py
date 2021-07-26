e93d7a7f37531aaee9498fc1aa96a2eb1c18942d
Jon Betts <jon.betts@hypothes.is>
2021-07-26 14:55:06 +0100

    Split off the fixtures to do with logging in into a separate file

--- tests/functional/h/views/admin/test_permissions.py
+++ tests/functional/h/views/admin/test_permissions.py
@@ -34,39 +34,3 @@ class TestAdminPermissions:
         res = app.get(url, expect_errors=not accessible)
 
         assert res.status_code == 200 if accessible else 404
-
-    @pytest.fixture
-    def user(self, factories):
-        return factories.User()
-
-    @pytest.fixture
-    def with_logged_in_user(self, login_user):
-        login_user()
-
-    @pytest.fixture
-    def with_logged_in_staff_member(self, login_user, user):
-        user.staff = True
-
-        login_user()
-
-    @pytest.fixture
-    def with_logged_in_admin(self, login_user, user):
-        user.admin = True
-
-        login_user()
-
-    @pytest.fixture
-    def login_user(self, db_session, app, user):
-        def login_user():
-            # This is the hash for `pass` used below
-            user.password = (
-                "$2b$12$21I1LjTlGJmLXzTDrQA8gusckjHEMepTmLY5WN3Kx8hSaqEEKj9V6"
-            )
-            db_session.commit()
-
-            login_page = app.get("/login")
-            login_page.form["username"] = user.username
-            login_page.form["password"] = "pass"
-            login_page.form.submit()
-
-        return login_user
