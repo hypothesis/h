e2788a6750862063d6dfceda27b1eeebd7a83dac
Jon Betts <jon.betts@hypothes.is>
2021-07-26 17:37:42 +0100

    Add more permission related tests for the activity pages

--- tests/functional/h/views/test_activity.py
+++ tests/functional/h/views/test_activity.py
@@ -1,18 +1,65 @@
-# Permission.Group.ADMIN
-# The `group_edit_url` should be visible for those with edit permissions
-# OAuth clients, staff and admins can edit a group
-# The creator of thr group can also edit the group
+import pytest
 
 
-# Permission.Group.JOIN / Permission.Group.READ
-# If the group is world readable, any logged in user can read
-# If the group is readable by members, any logged in user in the group can read
-# An OAuth client can read those in the same authority
+class TestGroupSearchController:
+    @pytest.mark.parametrize("should_login", (True, False))
+    def test_join_page_is_shown_instead_of_search_without_read_permissions(
+        self, login_user, app, group, should_login
+    ):
+        # If you don't have read permissions
+        # But you do have join permissions / or you aren't logged in
+        # Show the join page
+        if should_login:
+            login_user()
 
-# If you don't have permissions to read a group and you are either:
-#  * Not logged in
-#  * ... or are and have the JOIN permission
-# The  template rendered is changed to "h:templates/groups/join.html.jinja2"
+        response = app.get(f"/groups/{group.pubid}/{group.slug}")
 
-# A user has the JOIN permission if a group is marked as joinable by authority,
-# they are logged in user and in the right authority
+        assert "join-group-form" in str(response.html)
+
+    @pytest.mark.usefixtures("with_logged_in_user")
+    def test_404_is_raised_if_you_do_not_have_join_permission(
+        self, app, other_authority_group
+    ):
+        # A user has the JOIN permission if a group is marked as joinable by
+        # authority, they are logged in user and in the right authority
+
+        # We use a group in another authority as it means we don't have join
+        app.get(
+            f"/groups/{other_authority_group.pubid}/{other_authority_group.slug}",
+            status=404,
+        )
+
+    @pytest.mark.usefixtures("with_logged_in_admin")
+    def test_admin_permissions(self, app, open_group):
+        # For ease of testing, we're just going to test the admin route. We
+        # also make sure the group is open so the admin user can read it
+
+        response = app.get(f"/groups/{open_group.pubid}/{open_group.slug}")
+
+        # Permission.Group.ADMIN
+        # The `group_edit_url` should be visible for those with edit
+        # permissions
+        # OAuth clients, staff and admins can edit a group
+        # The creator of thr group can also edit the group
+        assert f"http://localhost/groups/{open_group.pubid}/edit" in str(response.html)
+
+    @pytest.fixture
+    def group(self, factories, db_session, user):
+        group = factories.Group()
+        db_session.commit()
+
+        return group
+
+    @pytest.fixture
+    def other_authority_group(self, factories, db_session, user):
+        group = factories.Group(authority="different_authority")
+        db_session.commit()
+
+        return group
+
+    @pytest.fixture
+    def open_group(self, factories, db_session, user):
+        group = factories.OpenGroup()
+        db_session.commit()
+
+        return group
