1a9e6de2f17d1d8f3ebf87eb20a392edba8c0140
Jon Betts <jon.betts@hypothes.is>
2021-07-26 15:32:35 +0100

    Add permissions testing for the GroupEditController

--- tests/functional/test_groups.py
+++ tests/functional/test_groups.py
@@ -1,14 +1,34 @@
 import pytest
-
-# Permission.Group.ADMIN
-# OAuth clients, staff and admins can edit a group
-# The creator of thr group can also edit the group
+from pytest import param
 
 
-@pytest.mark.usefixtures("with_logged_in_user")
 class TestGroupEditController:
+    # These permissions tests are a stand-in for testing all functionality of
+    # the group edit controller as they all need the same permission. We will
+    # just test GET as it's the simplest.
+    @pytest.mark.usefixtures("with_logged_in_user")
+    def test_a_logged_in_user_can_edit_their_own_groups(self, app, user_owned_group):
+        app.get(f"/groups/{user_owned_group.pubid}/edit")
+
+    @pytest.mark.parametrize(
+        "is_staff,is_admin,expected_status",
+        (
+            param(True, False, 200, id="staff"),
+            param(False, True, 200, id="admin"),
+            # Regular users can't edit other people's groups
+            param(False, False, 404, id="regular_user"),
+        ),
+    )
+    def test_editing_other_peoples_groups(
+        self, app, group, login_user, is_staff, is_admin, expected_status
+    ):
+        login_user(staff=is_staff, admin=is_admin)
+
+        app.get(f"/groups/{group.pubid}/edit", status=expected_status)
+
+    @pytest.mark.usefixtures("with_logged_in_user")
     @pytest.mark.xfail  # See https://github.com/hypothesis/product-backlog/issues/109
-    def test_group_page_includes_referrer_tag(self, app, db_session, factories, user):
+    def test_group_page_includes_referrer_tag(self, app, user_owned_group):
         """
         The group read page should include a referrer tag.
 
@@ -20,34 +40,48 @@ class TestGroupEditController:
         This is because the group's URL is secret - if you have it you can join
         the group.
         """
-        group = factories.Group(creator=user)
-        db_session.commit()
 
-        res = app.get(
-            "/groups/{pubid}/{slug}".format(pubid=group.pubid, slug=group.slug)
+        response = app.get(f"/groups/{user_owned_group.pubid}/{user_owned_group.slug}")
+
+        assert response.html.head.find(
+            "meta", attrs={"name": "referrer"}, content="origin"
         )
 
-        assert res.html.head.find("meta", attrs={"name": "referrer"}, content="origin")
-
+    @pytest.mark.usefixtures("with_logged_in_user")
     def test_submit_create_group_form_without_xhr_returns_full_html_page(
         self, filled_out_group_form
     ):
-        res = filled_out_group_form.submit().follow()
+        response = filled_out_group_form.submit().follow()
 
-        assert res.text.startswith("<!DOCTYPE html>")
+        assert response.text.startswith("<!DOCTYPE html>")
 
+    @pytest.mark.usefixtures("with_logged_in_user")
     def test_submit_create_group_form_with_xhr_returns_partial_html_snippet(
         self, filled_out_group_form
     ):
-        res = filled_out_group_form.submit(xhr=True)
+        response = filled_out_group_form.submit(xhr=True)
 
-        assert res.body.strip(b"\n").startswith(b"<form")
-        assert res.content_type == "text/plain"
+        assert response.body.strip(b"\n").startswith(b"<form")
+        assert response.content_type == "text/plain"
+
+    @pytest.fixture
+    def group(self, factories, db_session):
+        group = factories.Group()
+        db_session.commit()
+
+        return group
+
+    @pytest.fixture
+    def user_owned_group(self, factories, db_session, user):
+        group = factories.Group(creator=user)
+        db_session.commit()
+
+        return group
 
     @pytest.fixture
     def filled_out_group_form(self, app):
-        res = app.get("/groups/new")
-        group_form = res.forms["deform"]
+        response = app.get("/groups/new")
+        group_form = response.forms["deform"]
         group_form["name"] = "My New Group"
 
         return group_form
