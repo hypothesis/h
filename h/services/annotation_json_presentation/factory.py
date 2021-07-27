54d99d55c80607e74242fd7cfce4e7924ad3565a
Jon Betts <jon.betts@hypothes.is>
2021-07-27 17:43:19 +0100

    Use common service fixtures and a single test for the factory

--- h/services/annotation_json_presentation/factory.py
+++ h/services/annotation_json_presentation/factory.py
@@ -5,20 +5,15 @@ from h.services.annotation_json_presentation.service import (
 
 
 def annotation_json_presentation_service_factory(_context, request):
-    group_svc = request.find_service(IGroupService)
-    links_svc = request.find_service(name="links")
-    flag_svc = request.find_service(name="flag")
-    flag_count_svc = request.find_service(name="flag_count")
-    moderation_svc = request.find_service(name="annotation_moderation")
-    user_svc = request.find_service(name="user")
     return AnnotationJSONPresentationService(
         session=request.db,
         user=request.user,
-        group_svc=group_svc,
-        links_svc=links_svc,
-        flag_svc=flag_svc,
-        flag_count_svc=flag_count_svc,
-        moderation_svc=moderation_svc,
-        user_svc=user_svc,
         has_permission=request.has_permission,
+        # Services
+        group_svc=request.find_service(IGroupService),
+        links_svc=request.find_service(name="links"),
+        flag_svc=request.find_service(name="flag"),
+        flag_count_svc=request.find_service(name="flag_count"),
+        moderation_svc=request.find_service(name="annotation_moderation"),
+        user_svc=request.find_service(name="user"),
     )
