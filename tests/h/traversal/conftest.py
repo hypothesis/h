0ec954564852c6208d539a41ffcdd20192ac9835
Jon Betts <jon.betts@hypothes.is>
2021-07-28 20:09:01 +0100

    Remove un-used code path in some shared fixtures
    The last cases of calling this without values have been removed

--- tests/h/traversal/conftest.py
+++ tests/h/traversal/conftest.py
@@ -1,18 +1,11 @@
-import pyramid.authorization
-import pyramid.security
 import pytest
+from pyramid.authorization import ACLAuthorizationPolicy
 
 
 @pytest.fixture
 def set_permissions(pyramid_config):
-    default = object()
-
-    def request_with_permissions(user_id=None, principals=default):
-        if principals is default:
-            principals = [pyramid.security.Everyone]
-
-        policy = pyramid.authorization.ACLAuthorizationPolicy()
+    def request_with_permissions(user_id, principals):
         pyramid_config.testing_securitypolicy(user_id, groupids=principals)
-        pyramid_config.set_authorization_policy(policy)
+        pyramid_config.set_authorization_policy(ACLAuthorizationPolicy())
 
     return request_with_permissions
