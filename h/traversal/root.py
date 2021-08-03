450c7b9369fd96b65a708db983a578c05b4a6243
Jon Betts <jon.betts@hypothes.is>
2021-08-03 15:37:37 +0100

    Move admin ACLs to the security module

--- h/traversal/root.py
+++ h/traversal/root.py
@@ -1,7 +1,4 @@
-from pyramid.security import ALL_PERMISSIONS, DENY_ALL, Allow
-
-from h.auth import role
-from h.security.permissions import Permission
+from h.security.acl import ACL
 
 
 class RootFactory:
@@ -14,12 +11,6 @@ class RootFactory:
 class Root(RootFactory):
     """This app's default root factory."""
 
-    __acl__ = [
-        (Allow, role.Staff, Permission.AdminPage.INDEX),
-        (Allow, role.Staff, Permission.AdminPage.GROUPS),
-        (Allow, role.Staff, Permission.AdminPage.MAILER),
-        (Allow, role.Staff, Permission.AdminPage.ORGANIZATIONS),
-        (Allow, role.Staff, Permission.AdminPage.USERS),
-        (Allow, role.Admin, ALL_PERMISSIONS),
-        DENY_ALL,
-    ]
+    @classmethod
+    def __acl__(cls):
+        return ACL.for_admin_pages()
