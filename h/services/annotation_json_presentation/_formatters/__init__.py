65900342c5fc4e6d1022e7fdd18f0edaf4279b3a
Jon Betts <jon.betts@hypothes.is>
2021-08-09 15:11:52 +0100

    Inline the moderation stats into the presentation service

--- h/services/annotation_json_presentation/_formatters/__init__.py
+++ h/services/annotation_json_presentation/_formatters/__init__.py
@@ -1,6 +1,3 @@
 from h.services.annotation_json_presentation._formatters.hidden import HiddenFormatter
-from h.services.annotation_json_presentation._formatters.moderation import (
-    ModerationFormatter,
-)
 
-__all__ = ("HiddenFormatter", "ModerationFormatter")
+__all__ = ("HiddenFormatter",)
