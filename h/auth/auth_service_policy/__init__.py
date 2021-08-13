10111376f89d974a7e30382873f8ffb8f70a16c3
Jon Betts <jon.betts@hypothes.is>
2021-08-13 17:52:12 +0100

    Simplify the cookie source with a request parameter

--- h/auth/auth_service_policy/__init__.py
+++ h/auth/auth_service_policy/__init__.py
@@ -1,2 +1,2 @@
-from h.auth.auth_service_policy.cookie_source import factory_factory
+from h.auth.auth_service_policy.auth_cookie import AuthCookie
 from h.auth.auth_service_policy.policy import AuthServicePolicy
--- h/auth/auth_service_policy/auth_cookie.py
+++ h/auth/auth_service_policy/auth_cookie.py
@@ -1,24 +1,22 @@
 from webob.cookies import SignedCookieProfile
 
 
-class CookieAuthService:
+class AuthCookie:
     """An authentication source that uses a unique cookie."""
 
     vary = ["Cookie"]
 
-    def __init__(self, request, secret):
+    def __init__(self, request):
         cookie = SignedCookieProfile(
-            secret=secret,
+            secret=request.registry.settings["h_auth_cookie_secret"],
             salt="authsanity",
             cookie_name="auth",
             secure=False,
             max_age=2592000,
             httponly=True,
-            path="/",
-            domains=[],
-            hashalg="sha512",
         )
 
+        # Bind the cookie to the current request
         self.cookie = cookie.bind(request)
 
     def get_value(self):
@@ -30,15 +28,7 @@ class CookieAuthService:
         return val
 
     def headers_remember(self, value):
-        return self.cookie.get_headers(value, domains=self.cookie.domains)
+        return self.cookie.get_headers(value)
 
     def headers_forget(self):
         return self.cookie.get_headers(None, max_age=0)
-
-
-def factory_factory(secret):
-    def factory(context, request):
-        # Bind the cookie to the current request
-        return CookieAuthService(request, secret)
-
-    return factory
