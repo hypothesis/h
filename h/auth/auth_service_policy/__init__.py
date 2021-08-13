f81708c1f9f8c59d5ba18af5f845c929c032cf7e
Jon Betts <jon.betts@hypothes.is>
2021-08-13 17:21:38 +0100

    Write this in a more service style

--- h/auth/auth_service_policy/__init__.py
+++ h/auth/auth_service_policy/__init__.py
@@ -1,6 +1,4 @@
-from pyramid_authsanity.interfaces import IAuthSourceService
-
-from h.auth.auth_service_policy.cookie_source import CookieAuthSourceInitializer
+from h.auth.auth_service_policy.cookie_source import factory_factory
 from h.auth.auth_service_policy.policy import AuthServicePolicy
 from h.security import derive_key
 
@@ -9,10 +7,10 @@ def includeme(config):  # pragma: no cover
     settings = config.registry.settings
 
     config.register_service_factory(
-        CookieAuthSourceInitializer(
+        factory_factory(
             secret=derive_key(
                 settings["secret_key"], settings["secret_salt"], b"h.auth.cookie_secret"
             ),
         ),
-        iface=IAuthSourceService,
+        name="cookie",
     )
--- h/auth/auth_service_policy/cookie_source.py
+++ h/auth/auth_service_policy/cookie_source.py
@@ -1,50 +1,44 @@
-from pyramid_authsanity import IAuthSourceService
 from webob.cookies import SignedCookieProfile
-from zope.interface import implementer
 
 
-def CookieAuthSourceInitializer(secret):
+class CookieAuthService:
     """An authentication source that uses a unique cookie."""
 
-    domains = []
+    vary = ["Cookie"]
 
-    @implementer(IAuthSourceService)
-    class CookieAuthSource(object):
-        vary = ["Cookie"]
+    def __init__(self, request, secret):
+        cookie = SignedCookieProfile(
+            secret=secret,
+            salt="authsanity",
+            cookie_name="auth",
+            secure=False,
+            max_age=2592000,
+            httponly=True,
+            path="/",
+            domains=[],
+            hashalg="sha512",
+        )
 
-        def __init__(self, context, request):
-            self.domains = domains
+        self.cookie = cookie.bind(request)
 
-            if self.domains is None:
-                self.domains = []
-                self.domains.append(request.domain)
+    def get_value(self):
+        val = self.cookie.get_value()
 
-            self.cookie = SignedCookieProfile(
-                secret=secret,
-                salt="authsanity",
-                cookie_name="auth",
-                secure=False,
-                max_age=2592000,
-                httponly=True,
-                path="/",
-                domains=domains,
-                hashalg="sha512",
-            )
-            # Bind the cookie to the current request
-            self.cookie = self.cookie.bind(request)
+        if val is None:
+            return [None, None]
 
-        def get_value(self):
-            val = self.cookie.get_value()
+        return val
 
-            if val is None:
-                return [None, None]
+    def headers_remember(self, value):
+        return self.cookie.get_headers(value, domains=self.cookie.domains)
 
-            return val
+    def headers_forget(self):
+        return self.cookie.get_headers(None, max_age=0)
 
-        def headers_remember(self, value):
-            return self.cookie.get_headers(value, domains=self.domains)
 
-        def headers_forget(self):
-            return self.cookie.get_headers(None, max_age=0)
+def factory_factory(secret):
+    def factory(context, request):
+        # Bind the cookie to the current request
+        return CookieAuthService(request, secret)
 
-    return CookieAuthSource
+    return factory
