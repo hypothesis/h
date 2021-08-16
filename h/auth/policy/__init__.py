f87b5fc5f985683be2a0f66636f4fed4bc99c67c
Jon Betts <jon.betts@hypothes.is>
2021-08-16 18:10:32 +0100

    Move all sub-policy creation inside the combining policies

--- h/auth/policy/__init__.py
+++ h/auth/policy/__init__.py
@@ -1,6 +1,2 @@
-# These will become private soon
-from h.auth.policy._basic_http_auth import AuthClientPolicy
-from h.auth.policy._cookie import CookieAuthenticationPolicy
-from h.auth.policy._remote_user import RemoteUserAuthenticationPolicy
 from h.auth.policy.bearer_token import TokenAuthenticationPolicy
-from h.auth.policy.combined import APIAuthenticationPolicy, AuthenticationPolicy
+from h.auth.policy.combined import AuthenticationPolicy
