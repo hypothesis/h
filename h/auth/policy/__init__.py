436b17dff76e908e8febc5c2fb1812539712e603
Jon Betts <jon.betts@hypothes.is>
2021-08-16 17:55:02 +0100

    Move the cookie policy + tests into the policy directory

--- h/auth/policy/__init__.py
+++ h/auth/policy/__init__.py
@@ -1,6 +1,6 @@
-# These should all become private soon
+# These will become private soon
 from h.auth.policy._basic_http_auth import AuthClientPolicy
+from h.auth.policy._cookie import CookieAuthenticationPolicy
 from h.auth.policy._remote_user import RemoteUserAuthenticationPolicy
-# These will be public for now
 from h.auth.policy.bearer_token import TokenAuthenticationPolicy
 from h.auth.policy.combined import APIAuthenticationPolicy, AuthenticationPolicy
