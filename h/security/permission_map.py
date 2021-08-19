4ad07c47cf5de027d4aafdd7f438beca9e0bf2c5
Jon Betts <jon.betts@hypothes.is>
2021-08-19 18:15:49 +0100

    A tidied up version

--- h/security/permission_map.py
+++ h/security/permission_map.py
@@ -1,5 +1,20 @@
-from h.security import Permission
-from h.security.permits.predicates import *
+"""
+Map of `Permission`s to "predicates" and a method for testing permissions.
+
+Predicates are simple functions which accept identity and context objects and
+return a truthy value.
+
+In order for a permission to be granted at least one clause (a row of
+predicates) must all evaluate to True. A clause is True if every predicate in
+it evaluates to True. If any predicate in a clause is False the whole clause is
+False.
+"""
+
+from h.security import Identity, Permission
+from h.security.predicates import *
+
+__all__ = ("permits",)
+
 
 PERMISSION_MAP = {
     # Admin pages
@@ -68,6 +83,10 @@ PERMISSION_MAP = {
     # You can be notified about an annotation even if it's been deleted
     Permission.Annotation.READ_REALTIME_UPDATES: [
         [annotation_not_shared, annotation_created_by_user],
+        # For shared annotations the permissions are copied from the group
+        # of the annotation. We bend the predicate system here a little and
+        # put a raw permission in which will cause us to look up that
+        # permission.
         [annotation_shared, Permission.Group.READ],
     ],
     Permission.Annotation.READ: [
@@ -85,3 +104,52 @@ PERMISSION_MAP = {
     Permission.Annotation.UPDATE: [[annotation_live, annotation_created_by_user]],
     Permission.Annotation.DELETE: [[annotation_live, annotation_created_by_user]],
 }
+
+# Predicates can define parents which they need to be true before they are
+# true. We don't put all the parents here because it would be fiddly and make
+# it harder to read.
+
+# This turns the abstract predicates above into lists which include all of
+# their parents in the correct order to evaluate them.
+PERMISSION_MAP = resolve_predicates(PERMISSION_MAP)
+
+
+def permits(identity: Identity, context, permission) -> bool:
+    """
+    Check whether a given identity has permission to operate on a context.
+
+    For example the identity might include a user, and the context a group
+    and the permission might ask whether the user can edit that group.
+
+    :param identity: Identity object of the user
+    :param context: Context object representing the objects acted upon
+    :param permission: Permission requested
+    """
+    if clauses := PERMISSION_MAP.get(permission):
+        for clause in clauses:
+            if _clause_true(clause, identity, context):
+                return True
+
+        return False
+
+    return False
+
+
+def _clause_true(clause, identity, context):
+    """Check whether every item in a clause is true."""
+
+    # There are lots of optimisations you could do with this (like caching
+    # predicate results), but at the time of writing our predicates are so fast
+    # to evaluate it generally makes it slower to try.
+    for predicate in clause:
+        try:
+            if not predicate(identity, context):
+                return False
+
+        # If the "predicate" isn't callable, we'll assume it's a permission
+        # and work out if we have that permission
+        except TypeError:
+            if not permits(identity, context, predicate):
+                return False
+
+    return True
---
+++ h/security/predicates.py
@@ -0,0 +1,199 @@
+"""Define authorization predicates.
+
+These are functions which accept an `Identity` object and a context object and
+return a truthy value. These represent building blocks of our permission map
+which define when people do, or don't have permissions.
+
+For example a predicate might define "group_created_by_user" which is only
+true when a user is present, a group is present and the user created that
+group.
+"""
+from itertools import chain
+
+from h.models.group import JoinableBy, ReadableBy, WriteableBy
+
+
+def requires(*parent_predicates):
+    """
+    Decorate a predicate to say it requires other predicates to be True first.
+
+    :param parent_predicates: A list of predicates that have to be true for
+    this predicate to be true as well.
+    """
+
+    def decorator(function):
+        function.requires = parent_predicates
+        return function
+
+    return decorator
+
+
+# Identity things
+
+
+def authenticated(identity, context):
+    return identity
+
+
+# The `requires` here means that this requires needs `authenticate` to be True
+# before it's True. It also avoids attribute errors if identity is None
+@requires(authenticated)
+def authenticated_user(identity, context):
+    return identity.user
+
+
+@requires(authenticated_user)
+def user_is_staff(identity, context):
+    return identity.user.staff
+
+
+@requires(authenticated_user)
+def user_is_admin(identity, context):
+    return identity.user.admin
+
+
+@requires(authenticated)
+def authenticated_client(identity, context):
+    return bool(identity.auth_client)
+
+
+@requires(authenticated_client)
+def authenticated_client_is_lms(identity, context):
+    authority = identity.auth_client.authority
+
+    return authority.startswith("lms.") and authority.endswith(".hypothes.is")
+
+
+# Users
+
+
+def user_found(identity, context):
+    return hasattr(context, "user") and context.user
+
+
+@requires(authenticated_client, user_found)
+def user_authority_matches_authenticated_client(identity, context):
+    return context.user.authority == identity.auth_client.authority
+
+
+# Annotations
+
+
+def annotation_found(identity, context):
+    return hasattr(context, "annotation") and context.annotation
+
+
+@requires(annotation_found)
+def annotation_shared(identity, context):
+    return context.annotation.shared
+
+
+@requires(annotation_found)
+def annotation_not_shared(identity, context):
+    return not context.annotation.shared
+
+
+@requires(annotation_found)
+def annotation_live(identity, context):
+    return not context.annotation.deleted
+
+
+@requires(authenticated_user, annotation_found)
+def annotation_created_by_user(identity, context):
+    return identity.user.userid == context.annotation.userid
+
+
+# Groups
+
+
+def group_found(identity, context):
+    return hasattr(context, "group") and context.group
+
+
+def group_not_found(identity, context):
+    return not hasattr(context, "group") or not context.group
+
+
+@requires(group_found)
+def group_writable_by_members(identity, context):
+    return context.group.writeable_by == WriteableBy.members
+
+
+@requires(group_found)
+def group_writable_by_world(identity, context):
+    return context.group.writeable_by == WriteableBy.authority
+
+
+@requires(group_found)
+def group_writable_by_authority(identity, context):
+    return context.group.writeable_by == WriteableBy.authority
+
+
+@requires(group_found)
+def group_readable_by_world(identity, context):
+    return context.group.readable_by == ReadableBy.world
+
+
+@requires(group_found)
+def group_readable_by_members(identity, context):
+    return context.group.readable_by == ReadableBy.members
+
+
+@requires(group_found)
+def group_joinable_by_authority(identity, context):
+    return context.group.joinable_by == JoinableBy.authority
+
+
+@requires(authenticated_user, group_found)
+def group_matches_user_authority(identity, context):
+    return context.group.authority == identity.user.authority
+
+
+@requires(authenticated_user, group_found)
+def group_created_by_user(identity, context):
+    return context.group.creator and context.group.creator == identity.user
+
+
+@requires(authenticated_user, group_found)
+def group_has_user_as_member(identity, context):
+    return context.group in identity.user.groups
+
+
+@requires(authenticated_client, group_found)
+def group_authority_matches_authenticated_client(identity, context):
+    return context.user.authority == identity.group.authority
+
+
+def resolve_predicates(mapping):
+    """
+    Expand predicates with requirements into concrete lists of predicates.
+
+    This takes a permission map which contains predicates which reference
+    other ones (using `requires()`), and converts each clause to include the
+    parents in parent first order. This means any parent which is referred to
+    by a predicate is executed before it, and no predicate appears more than once.
+    """
+
+    return {
+        key: [
+            # The chain.from_iterable here flattens nested iterables
+            list(chain.from_iterable(_expand(predicate) for predicate in clause))
+            for clause in clauses
+        ]
+        for key, clauses in mapping.items()
+    }
+
+
+def _expand(predicate, seen_before=None):
+    """Generate all of the parents and the predicate in parents first order."""
+
+    if seen_before is None:
+        seen_before = set()
+
+    if hasattr(predicate, "requires"):
+        for parent in predicate.requires:
+            yield from _expand(parent, seen_before)
+
+    if predicate not in seen_before:
+        seen_before.add(predicate)
+        yield predicate
