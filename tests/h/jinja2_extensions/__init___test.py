74476326f5907e12c306c01e7cb89ecfdd4b32b1
Jon Betts <jon.betts@hypothes.is>
2021-08-23 19:26:00 +0100

    Use a more testable style for the initialisation

---
+++ tests/h/jinja2_extensions/__init___test.py
@@ -0,0 +1,24 @@
+from urllib.parse import unquote
+
+from h_matchers import Any
+from jinja2 import Environment
+
+from h.jinja_extensions import filters, setup_jinja2_env, svg_icon
+
+
+class TestSetupJinja2Env:
+    def test_it(self):
+        environment = Environment()
+
+        setup_jinja2_env(environment)
+
+        assert environment.filters == Any.dict.containing(
+            {
+                "to_json": filters.to_json,
+                "human_timestamp": filters.human_timestamp,
+                "format_number": filters.format_number,
+                "url_unquote": unquote,
+            }
+        )
+
+        assert environment.globals == Any.dict.containing({"svg_icon": svg_icon})
