53bb278b7a608bb2cf018e9434b071fed206a0c7
Jon Betts <jon.betts@hypothes.is>
2021-09-15 16:55:36 +0100

    Add coverage for permissions checking for the group end-points
    We're looking to change permissions around these, and they don't follow
    the same permissions structure as the `/admin/groups`. This should high
    light any mistakes with luck.

--- tests/functional/h/views/admin/test_permissions.py
+++ tests/functional/h/views/admin/test_permissions.py
@@ -34,3 +34,43 @@ class TestAdminPermissions:
         res = app.get(url, expect_errors=not accessible)
 
         assert res.status_code == 200 if accessible else 404
+
+    GROUP_PAGES = (
+        ("POST", "/admin/groups/delete/{pubid}", 302),
+        ("GET", "/admin/groups/{pubid}", 200),
+    )
+
+    @pytest.mark.usefixtures("with_logged_in_admin")
+    @pytest.mark.parametrize("method,url_template,success_code", GROUP_PAGES)
+    def test_group_end_points_accessible_by_admin(
+        self, app, group, method, url_template, success_code
+    ):
+        url = url_template.format(pubid=group.pubid)
+
+        app.request(url, method=method, status=success_code)
+
+    @pytest.mark.usefixtures("with_logged_in_staff_member")
+    @pytest.mark.parametrize("method,url_template,success_code", GROUP_PAGES)
+    def test_group_end_points_accessible_by_staff(
+        self, app, group, method, url_template, success_code
+    ):
+        url = url_template.format(pubid=group.pubid)
+
+        app.request(url, method=method, status=success_code)
+
+    @pytest.mark.usefixtures("with_logged_in_user")
+    @pytest.mark.parametrize("method,url_template,_", GROUP_PAGES)
+    def test_group_end_points_not_accessible_by_regular_user(
+        self, app, group, method, url_template, _
+    ):
+        url = url_template.format(pubid=group.pubid)
+
+        app.request(url, method=method, status=404)
+
+    @pytest.fixture
+    def group(self, factories, db_session):
+        # Without an org `views.admin.groups:GroupEditViews._update_appstruct`
+        # fails
+        group = factories.Group(organization=factories.Organization())
+        db_session.commit()
+        return group
