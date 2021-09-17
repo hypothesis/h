d38d2769cb1a83bd81fbb57154f3f3b07ec621d5
Jon Betts <jon.betts@hypothes.is>
2021-09-17 12:45:32 +0100

    Revert "Revert "Add a permits method to calculate permissions without (external) principals""
    This reverts commit 9409b028acdf1205de58fd893d209d7120e69d97.

--- h/security/__init__.py
+++ h/security/__init__.py
@@ -6,6 +6,7 @@ from h.security.encryption import (  # noqa:F401
 )
 from h.security.identity import Identity, get_identity  # noqa:F401
 from h.security.permissions import Permission  # noqa:F401
+from h.security.permits import identity_permits
 from h.security.principals import principals_for_identity  # noqa:F401
 
 
