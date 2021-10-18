351d25a309524bab7919f1d9313d09b84b906c6f
Robert Knight <robertknight@gmail.com>
2021-10-18 22:37:18 +0100

    Modernize JS and CSS bundling
    Convert JS and CSS bundling in this project to follow the conventions of
    other projects, using Rollup for JS bundling and helpers from
    @hypothesis/frontend-build to generate the bundles.
    
     - Replace Browserify configuration, dependencies and bundling scripts
       with Rollup and @hypothesis/frontend-build
    
     - Replace asset manifest generation with `generateManifest` function
       from @hypothesis/frontend-build
    
     - Replace CSS bundling script with `buildCSS` from
       @hypothesis/frontend-build
    
     - Remove the separate Raven and Bootstrap bundles. This code is now
       bundled directly into the "site" and "admin-site" bundles. This
       simplifies the build configuration a little.
    
     - Adjust a `url(...)` expression in icomoon.css to avoid the need for
       custom processing of this file.

---
+++ rollup.config.mjs
@@ -0,0 +1,44 @@
+import { babel } from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+const isProd = process.env.NODE_ENV === 'production';
+const prodPlugins = [];
+if (isProd) {
+  prodPlugins.push(terser());
+}
+
+function bundleConfig(name, entryFile) {
+  return {
+    input: {
+      [name]: entryFile,
+    },
+    output: {
+      dir: 'build/scripts/',
+      format: 'iife',
+      chunkFileNames: '[name].bundle.js',
+      entryFileNames: '[name].bundle.js',
+    },
+    plugins: [
+      babel({
+        babelHelpers: 'bundled',
+        exclude: 'node_modules/**',
+      }),
+      nodeResolve(),
+      commonjs(),
+      ...prodPlugins,
+    ],
+  };
+}
+
+export default [
+  // Public-facing website
+  bundleConfig('site', 'h/static/scripts/site.js'),
+  // Admin areas of the site
+  bundleConfig('admin-site', 'h/static/scripts/admin-site.js'),
+  // Header script inserted inline at the top of the page
+  bundleConfig('header', 'h/static/scripts/header.js'),
+  // Helper script for the OAuth post-authorization page.
+  bundleConfig('post-auth', 'h/static/scripts/post-auth.js'),
+];
