e9af29c187bf2e7957e6d984a5b5e119531fd2b9
Robert Knight <robertknight@gmail.com>
2021-10-25 08:28:41 +0100

    Use named exports in controllers/index.js

--- h/static/scripts/admin-site.js
+++ h/static/scripts/admin-site.js
@@ -3,7 +3,7 @@ import 'bootstrap';
 import { init as initRaven } from './base/raven';
 import { settings } from './base/settings';
 import { upgradeElements } from './base/upgrade-elements';
-import * as sharedControllers from './controllers';
+import { sharedControllers } from './controllers';
 import { AdminUsersController } from './controllers/admin-users-controller';
 
 const appSettings = settings(document);
