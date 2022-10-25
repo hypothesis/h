ad2413c66d03be59ec3d52dfdb15b0910e37dc8b
Jon Betts <jon.betts@hypothes.is>
2022-10-25 20:15:14 +0100

    Add the new reporting SQL tasks to the functional test

--- tests/functional/bin/run_sql_task_test.py
+++ tests/functional/bin/run_sql_task_test.py
@@ -13,7 +13,16 @@ class TestRunSQLTask:
     # We use "clean DB" here to ensure the schema is created
     @pytest.mark.usefixtures("with_clean_db")
     def test_reporting_tasks(self, environ):
-        for task_name in ("report/create_from_scratch",):
+        for task_name in (
+            # Run all the jobs in one line in the order they are expected to
+            # be able to run. We need "create from scratch" to have been done
+            # to tests the others, but doing it over and over is slow
+            "report/create_from_scratch",
+            "report/fast_recreate",
+            "report/refresh",
+            # Ensure we can run the "create from scratch" after everything
+            "report/create_from_scratch",
+        ):
             result = check_output(
                 [
                     sys.executable,
