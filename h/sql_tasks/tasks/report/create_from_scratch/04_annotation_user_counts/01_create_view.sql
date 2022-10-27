689ce4ec3ab3b64d8794fa9d2c5b02cdead542f3
Jon Betts <jon.betts@hypothes.is>
2022-10-27 19:22:03 +0100

    Use simpler queries for our count roll ups
    We are going to need these dates sliced and diced in so many ways, the
    tables can't / shouldn't really store them all.

--- h/sql_tasks/tasks/report/create_from_scratch/04_annotation_user_counts/01_create_view.sql
+++ h/sql_tasks/tasks/report/create_from_scratch/04_annotation_user_counts/01_create_view.sql
@@ -2,22 +2,12 @@ DROP MATERIALIZED VIEW IF EXISTS report.annotation_user_counts CASCADE;
 
 CREATE MATERIALIZED VIEW report.annotation_user_counts AS (
     SELECT
-        data.*,
-        EXTRACT('year' FROM created_month - INTERVAL '6 month')::smallint AS academic_year,
-        CONCAT(
-            EXTRACT('year' FROM created_month - INTERVAL '6 month'),
-            '-',
-            CASE WHEN EXTRACT('quarter' FROM created_month - INTERVAL '6 month') < 3 THEN 1 ELSE 2 END
-        ) AS academic_half_year,
+        -- Cast to a date as it's 4 bytes instead of 8
+        DATE_TRUNC('month', created)::date AS created_month,
+        authority_id,
+        user_id,
         COUNT(1)::integer AS count
-    FROM (
-        SELECT
-            -- Cast to a date as it's 4 bytes instead of 8
-            DATE_TRUNC('month', created)::date AS created_month,
-            authority_id,
-            user_id
-        FROM report.annotations
-    ) as data
+    FROM report.annotations
     GROUP BY created_month, authority_id, user_id
     ORDER BY created_month, authority_id, user_id
 ) WITH NO DATA;
