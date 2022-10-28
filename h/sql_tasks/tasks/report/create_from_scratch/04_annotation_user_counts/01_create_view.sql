d6032d096474c000d9671c99c8f37c453eb3e7be
Jon Betts <jon.betts@hypothes.is>
2022-10-28 13:30:40 +0100

    Switch to weekly counts for users, as this is the minimum required resolution
    This results in a lot of rows 3+ million, so we will need to aggregate
    this table again to get other values in the timeframe we need.

--- h/sql_tasks/tasks/report/create_from_scratch/04_annotation_user_counts/01_create_view.sql
+++ h/sql_tasks/tasks/report/create_from_scratch/04_annotation_user_counts/01_create_view.sql
@@ -3,11 +3,11 @@ DROP MATERIALIZED VIEW IF EXISTS report.annotation_user_counts CASCADE;
 CREATE MATERIALIZED VIEW report.annotation_user_counts AS (
     SELECT
         -- Cast to a date as it's 4 bytes instead of 8
-        DATE_TRUNC('month', created)::date AS created_month,
+        DATE_TRUNC('week', created)::date AS created_week,
         authority_id,
         user_id,
         COUNT(1)::integer AS count
     FROM report.annotations
-    GROUP BY created_month, authority_id, user_id
-    ORDER BY created_month, authority_id, user_id
+    GROUP BY created_week, authority_id, user_id
+    ORDER BY created_week, authority_id, user_id
 ) WITH NO DATA;
