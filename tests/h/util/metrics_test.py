b971fd09968c43006aba367800f4e7184f1686da
Marcos Prieto <marcospri@gmail.com>
2022-11-09 09:45:31 +0100

    Use the renamed add_custom_attribute new relic method
    From https://docs.newrelic.com/docs/release-notes/agent-release-notes/python-release-notes/python-agent-80300/
    
    add_custom_parameter(s) API is now deprecated.
    
    The add_custom_parameter(s) API has been renamed to add_custom_attribute(s).
    A deprecation warning will be emitted when calling add_custom_parameter() from this agent version and higher.

--- tests/h/util/metrics_test.py
+++ tests/h/util/metrics_test.py
@@ -10,7 +10,7 @@ class TestRecordSearchQueryParams:
     def test_it_passes_parameters_to_newrelic(self, newrelic_agent):
         params = MultiDict(tag="tagsvalue", _separate_replies=True, url="urlvalue")
         metrics.record_search_query_params(params, True)
-        newrelic_agent.add_custom_parameters.assert_called_once_with(
+        newrelic_agent.add_custom_attributes.assert_called_once_with(
             [
                 ("es_url", "urlvalue"),
                 ("es_tag", "tagsvalue"),
@@ -21,18 +21,18 @@ class TestRecordSearchQueryParams:
     def test_it_does_not_pass_unrecognized_parameters_to_newrelic(self, newrelic_agent):
         params = MultiDict(bad="unwanted")
         metrics.record_search_query_params(params, True)
-        newrelic_agent.add_custom_parameters.assert_called_once_with(
+        newrelic_agent.add_custom_attributes.assert_called_once_with(
             [("es__separate_replies", True)]
         )
 
     def test_it_does_not_record_separate_replies_if_False(self, newrelic_agent):
         params = MultiDict({})
         metrics.record_search_query_params(params, False)
-        newrelic_agent.add_custom_parameters.assert_called_once_with([])
+        newrelic_agent.add_custom_attributes.assert_called_once_with([])
 
     @pytest.fixture
     def newrelic_agent(self, newrelic_agent):
-        newrelic_agent.add_custom_parameters = mock.Mock()
+        newrelic_agent.add_custom_attributes = mock.Mock()
         return newrelic_agent
 
 
