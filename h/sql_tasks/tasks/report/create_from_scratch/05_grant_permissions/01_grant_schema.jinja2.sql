b8a8d9633dc74380f596258f71c32bb3ca4e272c
Jon Betts <jon.betts@hypothes.is>
2022-11-22 14:03:06 +0000

    Limit permissions to those required and ensure old permissions are removed

--- h/sql_tasks/tasks/report/create_from_scratch/05_grant_permissions/01_grant_schema.jinja2.sql
+++ h/sql_tasks/tasks/report/create_from_scratch/05_grant_permissions/01_grant_schema.jinja2.sql
@@ -1,16 +1,26 @@
 -- The public schema already exists but we need to grant usage to the user we
 -- will map via FDW.
+
 {% for fdw_user in fdw_users %}
+    -- Permissions exist independently of the schema, so dropping the schema
+    -- does not revoke permissions. We need to remove the existing permissions
+    -- otherwise removing something the list below does not remove the
+    -- permission.
+    REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM "{{fdw_user}}";
+
     GRANT USAGE ON SCHEMA public TO "{{fdw_user}}";
+
     GRANT SELECT ON public.user_group TO "{{fdw_user}}";
     GRANT SELECT ON public."user" TO "{{fdw_user}}";
     GRANT SELECT ON public."group" TO "{{fdw_user}}";
 {% endfor %}
 
 {% for fdw_user in fdw_users %}
-    GRANT USAGE ON SCHEMA report TO "{{fdw_user}}";
-    GRANT SELECT ON ALL TABLES IN SCHEMA report TO "{{fdw_user}}";
-{% endfor %}
+    REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA report FROM "{{fdw_user}}";
 
--- Empty query in case `fdw_users` is empty.
-SELECT NULL;
+    GRANT USAGE ON SCHEMA report TO "{{fdw_user}}";
+
+    GRANT SELECT ON report.authorities TO "{{fdw_user}}";
+    GRANT SELECT ON report.annotation_group_counts TO "{{fdw_user}}";
+    GRANT SELECT ON report.annotation_user_counts TO "{{fdw_user}}";
+{% endfor %}
