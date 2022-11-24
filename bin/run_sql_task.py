3c2979d1a513142a23b01a609d50eb4e8bba61b0
Marcos Prieto <marcospri@gmail.com>
2022-11-24 11:33:58 +0100

    Use data-task library

--- bin/run_sql_task.py
+++ bin/run_sql_task.py
@@ -7,11 +7,11 @@ reporting.
 """
 from argparse import ArgumentParser
 
+import data_tasks
 import importlib_resources
+from data_tasks.python_script import PythonScript
 from psycopg2.extensions import parse_dsn
 from pyramid.paster import bootstrap
-
-from h.sql_tasks.sql_script import SQLScript
 
 TASK_ROOT = importlib_resources.files("h.sql_tasks") / "tasks"
 
@@ -27,6 +27,21 @@ parser.add_argument(
 
 parser.add_argument("-t", "--task", required=True, help="The SQL task name to run")
 
+parser.add_argument(
+    "--no-python",
+    action="store_const",
+    default=False,
+    const=True,
+    help="Skip Python executables",
+)
+parser.add_argument(
+    "--dry-run",
+    action="store_const",
+    default=False,
+    const=True,
+    help="Run through the task without executing anything for real",
+)
+
 
 def main():
     args = parser.parse_args()
@@ -35,7 +50,7 @@ def main():
         request = env["request"]
         dsn = env["registry"].settings["sqlalchemy.url"].strip()
 
-        scripts = SQLScript.from_dir(
+        scripts = data_tasks.from_dir(
             task_dir=TASK_ROOT / args.task,
             template_vars={
                 "db_user": parse_dsn(dsn)["user"],
@@ -47,11 +62,15 @@ def main():
         with request.db.bind.connect() as connection:
             with connection.begin():
                 for script in scripts:
-                    print(f"Executing: {script.path}")
+                    if args.no_python and isinstance(script, PythonScript):
+                        print(f"Skipping: {script}")
+                        continue
 
-                    for query in script.queries:
-                        query.execute(connection)
-                        print(query.dump(indent="    ") + "\n")
+                    for step in script.execute(connection, dry_run=args.dry_run):
+                        if args.dry_run:
+                            print("Dry run!")
+
+                        print(step.dump(indent="    ") + "\n")
 
 
 if __name__ == "__main__":
--- tests/h/sql_tasks/script_fixture/01_file.sql
+++
@@ -1,4 +0,0 @@
--- Comment 1
-SELECT 1;
--- Comment 2
-SELECT 2;
