eb754a3805dc6528a2a8c772d45c871289f93dc1
Jon Betts <jon.betts@hypothes.is>
2022-12-05 17:54:06 +0000

    Move subscription token generation to the subscription service

--- h/notification/__init__.py
+++ h/notification/__init__.py
@@ -1,13 +1,2 @@
-from webob.cookies import SignedSerializer
-
-from ..security import derive_key
-
-
 def includeme(config):
     config.include(".reply")
-
-    secret = config.registry.settings["secret_key"]
-    salt = config.registry.settings["secret_salt"]
-    derived = derive_key(secret, salt, b"h.notification")
-
-    config.registry.notification_serializer = SignedSerializer(derived, None)
--- h/services/subscription.py
+++ h/services/subscription.py
@@ -2,8 +2,14 @@ from typing import List
 
 from sqlalchemy import func
 from sqlalchemy.orm import Session
+from webob.cookies import SignedSerializer
 
 from h.models import Subscriptions
+from h.security import derive_key
+
+
+class InvalidUnsubscribeToken(Exception):
+    """When an unsubscribe token has an invalid format."""
 
 
 class SubscriptionService:
@@ -12,8 +18,12 @@ class SubscriptionService:
     MISSING_SUBSCRIPTION_ACTIVE = True
     """Default behavior when we find a missing subscription."""
 
-    def __init__(self, db_session: Session):
+    def __init__(self, db_session: Session, secret: bytes, salt: bytes):
         self._db_session = db_session
+        self._token_serializer = SignedSerializer(
+            secret=derive_key(key_material=secret, salt=salt, info=b"h.notification"),
+            salt=None,
+        )
 
     def get_subscription(
         self, user_id: str, type_: Subscriptions.Type
@@ -47,8 +57,40 @@ class SubscriptionService:
         """
         return [self.get_subscription(user_id, type_) for type_ in Subscriptions.Type]
 
+    def get_unsubscribe_token(self, user_id: str, type_: Subscriptions.Type) -> str:
+        """
+        Create a signed token encoding a user and subscription type.
+
+        This is intended to be placed in an email, so we can verify that a user
+        wants to unsubscribe from a particular subscription.
+
+        :param user_id: User id to unsubscribe
+        :param type_: The subscription type to unsubscribe from
+        """
+        return self._token_serializer.dumps({"type": type_.value, "uri": user_id})
+
+    def unsubscribe_using_token(self, token: str):
+        """
+        Unsubscribes a user based on details encoded in an unsubscribe token.
+
+        :param token: A token provided by `get_unsubscribe_token()`
+        :raises InvalidUnsubscribeToken: When the token format is invalid
+        """
+        try:
+            payload = self._token_serializer.loads(token)
+        except ValueError as err:
+            raise InvalidUnsubscribeToken() from err
+
+        self.get_subscription(
+            user_id=payload["uri"], type_=Subscriptions.Type(payload["type"])
+        ).active = False
+
 
 def service_factory(_context, request) -> SubscriptionService:
     """Generate a subscription service object."""
 
-    return SubscriptionService(db_session=request.db)
+    return SubscriptionService(
+        db_session=request.db,
+        secret=request.registry.settings["secret_key"],
+        salt=request.registry.settings["secret_salt"],
+    )
