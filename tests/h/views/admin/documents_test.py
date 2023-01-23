8a2de2eb5df49c5bb0c3b7ea5c3636dd6942ffdc
Robert Knight <robertknight@gmail.com>
2023-01-23 12:45:56 +0000

    Refine tests for URL migration tools
    Refine tests in response to PR feedback
    
     - Assert various calls are made only once
     - Autouse several fixtures to ensure real function is not used
       accidentally in any tests
     - s/raises/errors to match current behavior of code
     - Factor out URLServiceMigration service constructor into a fixture

--- tests/h/views/admin/documents_test.py
+++ tests/h/views/admin/documents_test.py
@@ -17,11 +17,11 @@ class TestDocumentsAdminViews:
 
         views.update_annotation_urls()
 
-        move_annotations_by_url.chunks.assert_called_with(mappings.items(), 10)
+        move_annotations_by_url.chunks.assert_called_once_with(mappings.items(), 10)
         move_annotations_by_url.chunks.return_value.apply_async.assert_called_once()
-        flash.assert_called_with("URL migration started for 1 URL(s)", "success")
+        flash.assert_called_once_with("URL migration started for 1 URL(s)", "success")
 
-    def test_it_raises_if_json_does_not_parse(
+    def test_it_errors_if_json_does_not_parse(
         self, pyramid_request, move_annotations_by_url, flash
     ):
         pyramid_request.POST["url_mappings"] = "not-json"
@@ -29,13 +29,13 @@ class TestDocumentsAdminViews:
 
         views.update_annotation_urls()
 
-        flash.assert_called_with(
+        flash.assert_called_once_with(
             Any.string.matching("Failed to parse URL mappings:.*"), "error"
         )
         assert pyramid_request.response.status_code == 400
         move_annotations_by_url.chunks.assert_not_called()
 
-    def test_it_raises_if_validation_fails(
+    def test_it_errors_if_validation_fails(
         self, pyramid_request, move_annotations_by_url, flash
     ):
         pyramid_request.POST["url_mappings"] = json.dumps({"not-a-url": "foo"})
@@ -43,13 +43,13 @@ class TestDocumentsAdminViews:
 
         views.update_annotation_urls()
 
-        flash.assert_called_with(
+        flash.assert_called_once_with(
             Any.string.matching("Failed to validate URL mappings:.*"), "error"
         )
         assert pyramid_request.response.status_code == 400
         move_annotations_by_url.chunks.assert_not_called()
 
-    @pytest.fixture
+    @pytest.fixture(autouse=True)
     def move_annotations_by_url(self, patch):
         return patch("h.views.admin.documents.move_annotations_by_url")
 
