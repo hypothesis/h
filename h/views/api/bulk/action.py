5e5aa4ffddca7a0e7165a29d2dd322dda3ec7bc8
Jon Betts <jon.betts@hypothes.is>
2023-02-22 17:24:16 +0000

    Move the NDJSON functionality into a separate method.

--- h/views/api/bulk/action.py
+++ h/views/api/bulk/action.py
@@ -1,11 +1,8 @@
-import json
-from itertools import chain
-
 from h_api.bulk_api import BulkAPI
-from pyramid.response import Response
 
 from h.security import Permission
 from h.services.bulk_executor import BulkExecutor
+from h.views.api.bulk._ndjson import get_ndjson_response
 from h.views.api.config import api_config
 
 
@@ -32,30 +29,11 @@ def bulk_action(request):
     `h_api.bulk_api`.
     """
 
-    results = BulkAPI.from_byte_stream(
-        request.body_file,
-        executor=BulkExecutor(
-            db=request.db, authority=request.identity.auth_client.authority
-        ),
-    )
-
-    if results is None:
-        return Response(status=204)
-
-    # When we get an iterator we must force the first return value to be
-    # created to be sure input validation has occurred. Otherwise we might
-    # raise errors outside of the view when called
-
-    try:
-        results = chain(  # pylint: disable=redefined-variable-type
-            [next(results)], results
+    return get_ndjson_response(
+        BulkAPI.from_byte_stream(
+            request.body_file,
+            executor=BulkExecutor(
+                db=request.db, authority=request.identity.auth_client.authority
+            ),
         )
-    except StopIteration:
-        results = []
-
-    # An NDJSON response is required
-    return Response(
-        app_iter=((json.dumps(result) + "\n").encode("utf-8") for result in results),
-        status=200,
-        content_type="application/x-ndjson",
     )
--- tests/h/views/api/bulk/action_test.py
+++ tests/h/views/api/bulk/action_test.py
@@ -1,18 +1,17 @@
-import json
 from io import BytesIO
 from unittest.mock import create_autospec
 
 import pytest
-from h_api.exceptions import SchemaValidationError
-from webob import Response
 
 from h.security import Identity
 from h.views.api.bulk.action import bulk_action
 
 
 class TestBulkAction:
-    def test_it_calls_bulk_api_correctly(self, pyramid_request, BulkAPI, bulk_executor):
-        bulk_action(pyramid_request)
+    def test_it_calls_bulk_api_correctly(
+        self, pyramid_request, BulkAPI, bulk_executor, get_ndjson_response
+    ):
+        response = bulk_action(pyramid_request)
 
         BulkAPI.from_byte_stream.assert_called_once_with(
             pyramid_request.body_file, executor=bulk_executor.return_value
@@ -21,50 +20,17 @@ class TestBulkAction:
         bulk_executor.assert_called_once_with(
             pyramid_request.db, authority=pyramid_request.identity.auth_client.authority
         )
+        get_ndjson_response.assert_called_once_with(
+            BulkAPI.from_byte_stream.return_value
+        )
 
-    def test_it_formats_responses_correctly(self, pyramid_request, return_values):
-        result = bulk_action(pyramid_request)
-
-        assert isinstance(result, Response)
-        assert result.status == "200 OK"
-        assert result.content_type == "application/x-ndjson"
-
-        lines = result.body.decode("utf-8").split("\n")
-        lines = [json.loads(line) for line in lines if line]
-        assert lines == return_values
-
-    @pytest.mark.usefixtures("no_return_content")
-    def test_it_returns_204_if_no_content_is_to_be_returned(self, pyramid_request):
-        result = bulk_action(pyramid_request)
-
-        assert result.status == "204 No Content"
-
-    def test_it_raises_with_output_and_invalid_input(self, BulkAPI, pyramid_request):
-        def bad_generator(*_, **__):
-            raise SchemaValidationError([], "Bad!")
-            # Looks like this doesn't do anything, but it turns this into a
-            # generator. Unless the first item is retrieved the above is not
-            # executed.
-            yield {}  # pragma: no cover pylint:disable=unreachable
-
-        BulkAPI.from_byte_stream.side_effect = bad_generator
-
-        with pytest.raises(SchemaValidationError):
-            bulk_action(pyramid_request)
+        assert response == get_ndjson_response.return_value
 
     @pytest.fixture
     def pyramid_request(self, pyramid_request):
         pyramid_request.body_file = create_autospec(BytesIO)
 
         return pyramid_request
-
-    @pytest.fixture
-    def return_values(self):
-        return [{f"row_{i}": "value"} for i in range(3)]
-
-    @pytest.fixture
-    def no_return_content(self, BulkAPI):
-        BulkAPI.from_byte_stream.return_value = None
 
     @pytest.fixture(autouse=True)
     def with_auth_client(self, factories, pyramid_config):
@@ -73,11 +39,12 @@ class TestBulkAction:
         )
 
     @pytest.fixture(autouse=True)
-    def BulkAPI(self, patch, return_values):
-        BulkAPI = patch("h.views.api.bulk.action.BulkAPI")
-        BulkAPI.from_byte_stream.return_value = (value for value in return_values)
+    def BulkAPI(self, patch):
+        return patch("h.views.api.bulk.action.BulkAPI")
 
-        return BulkAPI
+    @pytest.fixture(autouse=True)
+    def get_ndjson_response(self, patch):
+        return patch("h.views.api.bulk.action.get_ndjson_response")
 
     @pytest.fixture(autouse=True)
     def bulk_executor(self, patch):
