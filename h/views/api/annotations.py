61effd63f7b2523644bfa58f13bad293cd4cfd5e
Jon Betts <jon.betts@hypothes.is>
2023-04-06 17:40:14 +0100

    Consumer fixup

--- h/views/api/annotations.py
+++ h/views/api/annotations.py
@@ -17,7 +17,6 @@ objects and Pyramid ACLs in :mod:`h.traversal`.
 from pyramid import i18n
 
 from h import search as search_lib
-from h import storage
 from h.events import AnnotationEvent
 from h.presenters import AnnotationJSONLDPresenter
 from h.schemas.annotation import (
