6bf1f746c7b6856ab5e7a9fb9def012a9ee6c402
Jon Betts <jon.betts@hypothes.is>
2023-04-06 15:24:07 +0100

    Add `create_annotation` to the annotation service

--- tests/h/services/annotation_test.py
+++ tests/h/services/annotation_test.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from unittest.mock import patch, sentinel
+from unittest.mock import Mock, patch, sentinel
 
 import pytest
 from h_matchers import Any
@@ -7,8 +7,10 @@ from sqlalchemy import event
 
 from h.models import Annotation
 from h.schemas import ValidationError
+from h.security import Permission
 from h.services import AnnotationService
 from h.services.annotation import service_factory
+from h.traversal.group import GroupContext
 
 
 class TestAnnotationService:
@@ -71,6 +73,56 @@ class TestAnnotationService:
         )
 
         assert results == [annotation_1]
+
+    def test_create_annotation(
+        self,
+        svc,
+        create_data,
+        factories,
+        update_document_metadata,
+        search_index,
+        _validate_group,
+    ):
+        root_annotation = factories.Annotation()
+        reply_annotation = factories.Annotation()
+
+        create_data["references"] = [root_annotation.id, reply_annotation.id]
+        create_data["groupid"] = "IGNORED"
+
+        update_document_metadata.return_value = factories.Document()
+
+        result = svc.create_annotation(create_data)
+
+        _validate_group.assert_called_once_with(result)
+        # pylint: disable=protected-access
+        search_index._queue.add_by_id.assert_called_once_with(
+            result.id, tag="storage.create_annotation", schedule_in=60
+        )
+
+        assert result == Any.instance_of(Annotation).with_attrs(
+            {
+                "userid": create_data["userid"],
+                "groupid": root_annotation.groupid,
+                "target_uri": create_data["target_uri"],
+                "references": [root_annotation.id, reply_annotation.id],
+                "document": update_document_metadata.return_value,
+            }
+        )
+
+    def test_create_annotation_as_root(self, svc, create_data, factories):
+        group = factories.Group()
+        create_data["references"] = None
+        create_data["groupid"] = group.pubid
+
+        result = svc.create_annotation(create_data)
+
+        assert result.groupid == group.pubid
+
+    def test_create_annotation_with_invalid_parent(self, svc, create_data):
+        create_data["references"] = ["NOPE!"]
+
+        with pytest.raises(ValidationError):
+            svc.create_annotation(create_data)
 
     def test_update_annotation(
         self,
@@ -138,6 +190,16 @@ class TestAnnotationService:
         with pytest.raises(ValidationError):
             svc._validate_group(annotation)  # pylint: disable=protected-access
 
+    def test__validate_group_with_no_permission(self, svc, annotation, has_permission):
+        has_permission.return_value = False
+
+        with pytest.raises(ValidationError):
+            svc._validate_group(annotation)  # pylint: disable=protected-access
+
+        has_permission.assert_called_once_with(
+            Permission.Group.WRITE, context=GroupContext(annotation.group)
+        )
+
     @pytest.mark.parametrize("enforce_scope", (True, False))
     @pytest.mark.parametrize("matching_scope", (True, False))
     @pytest.mark.parametrize("has_scopes", (True, False))
@@ -154,6 +216,19 @@ class TestAnnotationService:
                 svc._validate_group(annotation)  # pylint: disable=protected-access
         else:
             svc._validate_group(annotation)  # pylint: disable=protected-access
+
+    @pytest.fixture
+    def create_data(self, factories):
+        user = factories.User()
+
+        return {
+            "userid": user.userid,
+            "target_uri": "http://example.com/target",
+            "document": {
+                "document_uri_dicts": sentinel.uri_dicts,
+                "document_meta_dicts": sentinel.document_dicts,
+            },
+        }
 
     @pytest.fixture
     def _validate_group(self, svc):
@@ -180,9 +255,15 @@ class TestAnnotationService:
         return factories.Annotation()
 
     @pytest.fixture
-    def svc(self, db_session, search_index):
+    def has_permission(self):
+        return Mock(return_value=True)
+
+    @pytest.fixture
+    def svc(self, db_session, has_permission, search_index):
         return AnnotationService(
-            db_session=db_session, search_index_service=search_index
+            db_session=db_session,
+            has_permission=has_permission,
+            search_index_service=search_index,
         )
 
     @pytest.fixture(autouse=True)
@@ -199,7 +280,9 @@ class TestServiceFactory:
         svc = service_factory(sentinel.context, pyramid_request)
 
         AnnotationService.assert_called_once_with(
-            db_session=pyramid_request.db, search_index_service=search_index
+            db_session=pyramid_request.db,
+            has_permission=pyramid_request.has_permission,
+            search_index_service=search_index,
         )
         assert svc == AnnotationService.return_value
 
