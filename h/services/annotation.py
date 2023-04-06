c98979300c1fc619b41ccffdb367d7cfadbde584
Jon Betts <jon.betts@hypothes.is>
2023-04-06 14:38:57 +0100

    Add a `update_annotation` method to the annotation service

--- h/services/annotation.py
+++ h/services/annotation.py
@@ -1,17 +1,26 @@
+from datetime import datetime
 from typing import Iterable, List, Optional
 
 from sqlalchemy import select
 from sqlalchemy.orm import Query, Session, subqueryload
 
+from h import i18n
 from h.models import Annotation, DocumentURI
+from h.models.document import update_document_metadata
+from h.schemas import ValidationError
+from h.services.search_index import SearchIndexService
+from h.util.group_scope import url_in_scope
 from h.util.uri import normalize
+
+_ = i18n.TranslationStringFactory(__package__)
 
 
 class AnnotationService:
     """A service for storing and retrieving annotations."""
 
-    def __init__(self, db_session: Session):
+    def __init__(self, db_session: Session, search_index_service: SearchIndexService):
         self._db = db_session
+        self._search_index_service = search_index_service
 
     def get_annotations_by_id(
         self, ids: List[str], eager_load: Optional[List] = None
@@ -52,6 +61,90 @@ class AnnotationService:
 
         return self._db.execute(query).scalars().all()
 
+    def update_annotation(
+        self,
+        annotation: Annotation,
+        data: dict,
+        update_timestamp: bool = True,
+        reindex_tag: str = "storage.update_annotation",
+    ) -> Annotation:
+        """
+        Update an annotation and its associated document metadata.
+
+        :param annotation: Annotation to be updated
+        :param data: Validated data with which to update the annotation
+        :param update_timestamp: Whether to update the last-edited timestamp of
+            the annotation.
+        :param reindex_tag: Tag used by the reindexing job to identify the
+            source of the reindexing request.
+        """
+        initial_target_uri = annotation.target_uri
+
+        self._update_annotation_values(annotation, data)
+        if update_timestamp:
+            annotation.updated = datetime.utcnow()
+
+        # Expire the group relationship, so we get the most up-to-date value
+        # instead of the one which was present when we loaded the model
+        # https://docs.sqlalchemy.org/en/13/faq/sessions.html#i-set-the-foo-id-attribute-on-my-instance-to-7-but-the-foo-attribute-is-still-none-shouldn-t-it-have-loaded-foo-with-id-7
+        self._db.expire(annotation, ["group"])
+        self._validate_group(annotation)
+
+        if (
+            document := data.get("document", {})
+        ) or annotation.target_uri != initial_target_uri:
+            annotation.document = update_document_metadata(
+                self._db,
+                annotation.target_uri,
+                document.get("document_meta_dicts", {}),
+                document.get("document_uri_dicts", {}),
+                updated=annotation.updated,
+            )
+
+        # The search index service by default does not reindex if the existing ES
+        # entry's timestamp matches the DB timestamp. If we're not changing this
+        # timestamp, we need to force reindexing.
+        self._search_index_service._queue.add_by_id(  # pylint: disable=protected-access
+            annotation.id, tag=reindex_tag, schedule_in=60, force=not update_timestamp
+        )
+
+        return annotation
+
+    @staticmethod
+    def _update_annotation_values(annotation: Annotation, data: dict):
+        for key, value in data.items():
+            # Don't set complex things
+            if key in ("document", "extra"):
+                continue
+
+            setattr(annotation, key, value)
+
+        extra = data.get("extra", {})
+        annotation.extra.update(extra)
+
+    @staticmethod
+    def _validate_group(annotation: Annotation):
+        group = annotation.group
+        if not group:
+            raise ValidationError(
+                "group: " + _(f"Invalid group id {annotation.groupid}")
+            )
+
+        # If no scopes are present, or if the group is configured to allow
+        # annotations outside its scope, there's nothing to do here
+        if not group.scopes or not group.enforce_scope:
+            return
+
+        # The target URI must match at least one
+        # of a group's defined scopes, if the group has any
+        if not url_in_scope(
+            annotation.target_uri, [scope.scope for scope in group.scopes]
+        ):
+            raise ValidationError(
+                "group scope: "
+                + _("Annotations for this target URI are not allowed in this group")
+            )
+
     @staticmethod
     def _annotation_search_query(
         ids: Optional[List[str]] = None,
@@ -86,4 +179,9 @@ class AnnotationService:
 def service_factory(_context, request) -> AnnotationService:
     """Get an annotation service instance."""
 
-    return AnnotationService(db_session=request.db)
+    return AnnotationService(
+        db_session=request.db,
+        search_index_service=request.find_service(  # pylint: disable=protected-access
+            name="search_index"
+        ),
+    )
--- tests/h/services/annotation_test.py
+++ tests/h/services/annotation_test.py
@@ -1,9 +1,12 @@
-from unittest.mock import sentinel
+from datetime import datetime, timedelta
+from unittest.mock import patch, sentinel
 
 import pytest
+from h_matchers import Any
 from sqlalchemy import event
 
 from h.models import Annotation
+from h.schemas import ValidationError
 from h.services import AnnotationService
 from h.services.annotation import service_factory
 
@@ -63,6 +66,94 @@ class TestAnnotationService:
 
         assert results == [annotation_1]
 
+    def test_update_annotation(
+        self,
+        svc,
+        db_session,
+        annotation,
+        update_document_metadata,
+        search_index,
+        _validate_group,
+    ):
+        then = datetime.now() - timedelta(days=1)
+        annotation.extra = {"key": "value"}
+        annotation.updated = then
+
+        result = svc.update_annotation(
+            annotation,
+            {
+                "target_uri": "new_target_uri",
+                "text": "new_text",
+                "extra": {"extra_key": "extra_value"},
+                "document": {
+                    "document_meta_dicts": {"meta": 1},
+                    "document_uri_dicts": {"uri": 1},
+                },
+            },
+            update_timestamp=True,
+        )
+
+        _validate_group.assert_called_once_with(annotation)
+        update_document_metadata.assert_called_once_with(
+            db_session,
+            result.target_uri,
+            {"meta": 1},
+            {"uri": 1},
+            updated=result.updated,
+        )
+
+        # pylint: disable=protected-access
+        search_index._queue.add_by_id.assert_called_once_with(
+            annotation.id, tag="storage.update_annotation", schedule_in=60, force=False
+        )
+        assert result.document == update_document_metadata.return_value
+        assert result.target_uri == "new_target_uri"
+        assert result.text == "new_text"
+        assert result.updated > then
+        assert result.extra == {"key": "value", "extra_key": "extra_value"}
+
+    def test_update_annotation_with_non_defaults(self, svc, annotation, search_index):
+        then = datetime.now() - timedelta(days=1)
+        annotation.updated = then
+
+        result = svc.update_annotation(
+            annotation, {}, update_timestamp=False, reindex_tag="custom_tag"
+        )
+
+        # pylint: disable=protected-access
+        search_index._queue.add_by_id.assert_called_once_with(
+            Any(), tag="custom_tag", schedule_in=Any(), force=True
+        )
+        assert result.updated == then
+
+    def test__validate_group_with_no_group(self, svc, annotation):
+        annotation.group = None
+
+        with pytest.raises(ValidationError):
+            svc._validate_group(annotation)  # pylint: disable=protected-access
+
+    @pytest.mark.parametrize("enforce_scope", (True, False))
+    @pytest.mark.parametrize("matching_scope", (True, False))
+    @pytest.mark.parametrize("has_scopes", (True, False))
+    def test__validate_group_with_url_not_in_scopes(
+        self, svc, annotation, factories, enforce_scope, matching_scope, has_scopes
+    ):
+        annotation.group.enforce_scope = enforce_scope
+        annotation.target_uri = "http://scope" if matching_scope else "http://MISMATCH"
+        if has_scopes:
+            annotation.group.scopes = [factories.GroupScope(scope="http://scope")]
+
+        if enforce_scope and has_scopes and not matching_scope:
+            with pytest.raises(ValidationError):
+                svc._validate_group(annotation)  # pylint: disable=protected-access
+        else:
+            svc._validate_group(annotation)  # pylint: disable=protected-access
+
+    @pytest.fixture
+    def _validate_group(self, svc):
+        with patch.object(svc, "_validate_group") as _validate_group:
+            yield _validate_group
+
     @pytest.fixture
     def query_counter(self, db_engine):
         class QueryCounter:
@@ -79,15 +170,31 @@ class TestAnnotationService:
         return query_counter
 
     @pytest.fixture
-    def svc(self, db_session):
-        return AnnotationService(db_session)
+    def annotation(self, factories):
+        return factories.Annotation()
+
+    @pytest.fixture
+    def svc(self, db_session, search_index):
+        return AnnotationService(
+            db_session=db_session, search_index_service=search_index
+        )
+
+    @pytest.fixture(autouse=True)
+    def update_document_metadata(self, patch, factories):
+        update_document_metadata = patch(
+            "h.services.annotation.update_document_metadata"
+        )
+        update_document_metadata.return_value = factories.Document()
+        return update_document_metadata
 
 
 class TestServiceFactory:
-    def test_it(self, pyramid_request, AnnotationService):
+    def test_it(self, pyramid_request, AnnotationService, search_index):
         svc = service_factory(sentinel.context, pyramid_request)
 
-        AnnotationService.assert_called_once_with(db_session=pyramid_request.db)
+        AnnotationService.assert_called_once_with(
+            db_session=pyramid_request.db, search_index_service=search_index
+        )
         assert svc == AnnotationService.return_value
 
     @pytest.fixture
