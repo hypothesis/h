713701ed1427761d1927bd7a5280025fc1b0aeec
Jon Betts <jon.betts@hypothes.is>
2023-04-20 20:17:30 +0100

    Add `get_annotations_by_id` to the `AnnotationReadService`

--- h/services/annotation_read.py
+++ h/services/annotation_read.py
@@ -1,6 +1,7 @@
-from typing import Optional
+from typing import Iterable, List, Optional
 
-from sqlalchemy.orm import Session
+from sqlalchemy import select
+from sqlalchemy.orm import Query, Session, subqueryload
 
 from h.db.types import InvalidUUID
 from h.models import Annotation
@@ -22,6 +23,41 @@ class AnnotationReadService:
             return self._db.query(Annotation).get(id_)
         except InvalidUUID:
             return None
+
+    def get_annotations_by_id(
+        self, ids: List[str], eager_load: Optional[List] = None
+    ) -> Iterable[Annotation]:
+        """
+        Get annotations in the same order as the provided ids.
+        :param ids: the list of annotation ids
+        :param eager_load: A list of annotation relationships to eager load
+            like `Annotation.document`
+        """
+
+        if not ids:
+            return []
+
+        annotations = self._db.execute(
+            self._annotation_search_query(ids=ids, eager_load=eager_load)
+        ).scalars()
+
+        return sorted(annotations, key=lambda annotation: ids.index(annotation.id))
+
+    @staticmethod
+    def _annotation_search_query(
+        ids: Optional[List[str]] = None, eager_load: Optional[List] = None
+    ) -> Query:
+        """Create a query for searching for annotations."""
+
+        query = select(Annotation)
+
+        if ids:
+            query = query.where(Annotation.id.in_(ids))
+
+        if eager_load:
+            query = query.options(*(subqueryload(prop) for prop in eager_load))
+
+        return query
 
 
 def service_factory(_context, request) -> AnnotationReadService:
