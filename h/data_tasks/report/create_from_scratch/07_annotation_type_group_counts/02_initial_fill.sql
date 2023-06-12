7d4030aa47cc0aa9436dc31cdd2c164098c4775c
Jon Betts <jon.betts@hypothes.is>
2023-06-12 15:47:13 +0100

    Add a view to count different sub-types of annotation for a group

---
+++ h/data_tasks/report/create_from_scratch/07_annotation_type_group_counts/02_initial_fill.sql
@@ -0,0 +1,9 @@
+DROP INDEX IF EXISTS report.annotation_type_group_counts_created_day_authority_id_idx;
+
+REFRESH MATERIALIZED VIEW report.annotation_type_group_counts;
+
+ANALYSE report.annotation_type_group_counts;
+
+-- A unique index is mandatory for concurrent updates used in the refresh
+CREATE UNIQUE INDEX annotation_type_group_counts_created_day_authority_id_idx
+    ON report.annotation_type_group_counts (created_day, authority_id, group_id, sub_type, shared);
