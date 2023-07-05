90ced940ef40a8fbe888d2dad7d4f5d9feebfbeb
Jon Betts <jon.betts@hypothes.is>
2023-07-05 18:32:19 +0100

    Once permissions have been removed once, we are done

--- h/data_tasks/report/create_from_scratch/99_grant_permissions/01_grant_schema.jinja2.sql
+++ h/data_tasks/report/create_from_scratch/99_grant_permissions/01_grant_schema.jinja2.sql
@@ -2,11 +2,6 @@
 -- will map via FDW.
 
 {% for fdw_user in fdw_users %}
-    -- Once this has been run once, it can be removed
-    REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM "{{fdw_user}}";
-{% endfor %}
-
-{% for fdw_user in fdw_users %}
     -- Permissions exist independently of the schema, so dropping the schema
     -- does not revoke permissions. We need to remove the existing permissions
     -- otherwise removing something the list below does not remove the
