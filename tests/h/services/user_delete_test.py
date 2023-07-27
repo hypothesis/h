5adbe481dbe89008cfcdbe5901383748f5effa61
Jon Betts <jon.betts@hypothes.is>
2023-07-27 18:28:48 +0100

    Write the tests to be more self contained

--- tests/h/services/user_delete_test.py
+++ tests/h/services/user_delete_test.py
@@ -1,111 +1,91 @@
-from unittest import mock
 from unittest.mock import sentinel
 
 import pytest
 import sqlalchemy
 from h_matchers import Any
 
-from h.models import Annotation, Document
-from h.services.annotation_delete import AnnotationDeleteService
+from h.models import GroupMembership
 from h.services.user_delete import UserDeleteService, service_factory
 
 
 class TestDeleteUserService:
-    def test_delete_disassociate_group_memberships(self, svc, factories):
-        user = factories.User()
-
-        svc.delete_user(user)
-
-        assert user.groups == []
-
-    def test_delete_deletes_annotations(
-        self, svc, factories, annotation_delete_service
+    def test_it(
+        self,
+        svc,
+        db_session,
+        annotation_delete_service,
+        user,
+        created_group,
+        joined_group,
+        user_annotations,
     ):
-        user = factories.User(username="bob")
-        annotations = factories.Annotation.create_batch(2, userid=user.userid)
-
         svc.delete_user(user)
 
+        # Check the user was deleted
+        assert user in db_session.deleted
+        # Check we delete this user's annotations
         annotation_delete_service.delete_annotations.assert_called_once_with(
-            Any.iterable.containing(annotations).only()
+            Any.iterable.containing(user_annotations).only()
+        )
+        # Check we delete groups we created, but not ones we joined
+        assert sqlalchemy.inspect(created_group).was_deleted
+        assert not sqlalchemy.inspect(joined_group).was_deleted
+        # Check we remove their group memberships
+        assert (
+            not db_session.query(GroupMembership)
+            .where(GroupMembership.user_id == user.id)
+            .all()
+        )
+        assert (
+            not db_session.query(GroupMembership)
+            .where(GroupMembership.group_id == created_group.id)
+            .all()
         )
 
-    def test_delete_deletes_user(self, svc, db_session, factories):
-        user = factories.User()
+    def test_it_doesnt_delete_groups_others_have_annotated_in(
+        self, svc, factories, user, member, created_group
+    ):
+        factories.Annotation(userid=member.userid, groupid=created_group.pubid)
 
         svc.delete_user(user)
 
-        assert user in db_session.deleted
+        # Check we don't delete groups which other people have annotated in
+        assert not sqlalchemy.inspect(created_group).was_deleted
+        # But that we are removed as the creator
+        assert not created_group.creator
 
-    def test_delete_user_removes_groups_if_no_collaborators(
-        self, svc, db_session, group_with_two_users
-    ):
-        (group, creator, _, _, member_ann) = group_with_two_users
-        db_session.delete(member_ann)
+    @pytest.fixture
+    def user(self, factories):
+        return factories.User()
 
-        svc.delete_user(creator)
+    @pytest.fixture
+    def member(self, factories):
+        return factories.User()
 
-        assert sqlalchemy.inspect(group).was_deleted
+    @pytest.fixture
+    def created_group(self, factories, user, member):
+        return factories.Group(
+            authority=user.authority, creator=user, members=[user, member]
+        )
 
-    def test_creator_is_none_if_groups_have_collaborators(
-        self, svc, group_with_two_users
-    ):
-        (group, creator, _, _, _) = group_with_two_users
+    @pytest.fixture
+    def joined_group(self, factories, user, member):
+        return factories.Group(
+            authority=user.authority, creator=member, members=[user, member]
+        )
 
-        svc.delete_user(creator)
-
-        assert group.creator is None
-
-    def test_delete_user_removes_only_groups_created_by_user(
-        self, svc, db_session, group_with_two_users
-    ):
-        (group, _, member, _, _) = group_with_two_users
-
-        svc.delete_user(member)
-
-        assert group not in db_session.deleted
+    @pytest.fixture
+    def user_annotations(self, factories, user, created_group, joined_group):
+        return [
+            factories.Annotation(userid=user.userid, groupid=group.pubid)
+            for group in (created_group, joined_group)
+        ]
 
     @pytest.fixture
     def svc(self, db_session, annotation_delete_service):
         return UserDeleteService(
             db_session=db_session, annotation_delete_service=annotation_delete_service
         )
-
-
-@pytest.fixture
-def pyramid_request(pyramid_request):
-    pyramid_request.notify_after_commit = mock.Mock()
-    return pyramid_request
-
-
-@pytest.fixture
-def group_with_two_users(db_session, factories):
-    """Create a group with two members and an annotation created by each."""
-    creator = factories.User()
-    member = factories.User()
-
-    group = factories.Group(
-        authority=creator.authority, creator=creator, members=[creator, member]
-    )
-
-    doc = Document(web_uri="https://example.org")
-    creator_ann = Annotation(userid=creator.userid, groupid=group.pubid, document=doc)
-    member_ann = Annotation(userid=member.userid, groupid=group.pubid, document=doc)
-
-    db_session.add(creator_ann)
-    db_session.add(member_ann)
-    db_session.flush()
-
-    return (group, creator, member, creator_ann, member_ann)
-
-
-@pytest.fixture
-def annotation_delete_service(pyramid_config):
-    service = mock.create_autospec(
-        AnnotationDeleteService, spec_set=True, instance=True
-    )
-    pyramid_config.register_service(service, name="annotation_delete")
-    return service
 
 
 class TestServiceFactory:
