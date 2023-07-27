6c1975f134c8316f7d6c989fb5282009812e8040
Jon Betts <jon.betts@hypothes.is>
2023-07-27 16:25:01 +0100

    Consistently use absolute paths to services
    This makes it so you can grep for "h.service.blah" and actually find things,
    which is helpful during refactoring, and probably a good idea in general.

--- h/services/__init__.py
+++ h/services/__init__.py
@@ -7,7 +7,9 @@ from h.services.subscription import SubscriptionService
 
 
 def includeme(config):  # pragma: no cover
-    config.register_service_factory(".annotation_json.factory", name="annotation_json")
+    config.register_service_factory(
+        "h.services.annotation_json.factory", name="annotation_json"
+    )
     config.register_service_factory(
         "h.services.annotation_read.service_factory", iface=AnnotationReadService
     )
@@ -15,92 +17,109 @@ def includeme(config):  # pragma: no cover
         "h.services.annotation_write.service_factory", iface=AnnotationWriteService
     )
     config.register_service_factory(
-        ".annotation_moderation.annotation_moderation_service_factory",
+        "h.services.annotation_moderation.annotation_moderation_service_factory",
         name="annotation_moderation",
     )
     config.register_service_factory(
-        ".annotation_stats.annotation_stats_factory", name="annotation_stats"
-    )
-    config.register_service_factory(".auth_cookie.factory", iface=AuthCookieService)
-    config.register_service_factory(
-        ".auth_token.auth_token_service_factory", name="auth_token"
+        "h.services.annotation_stats.annotation_stats_factory", name="annotation_stats"
     )
     config.register_service_factory(
-        ".annotation_delete.annotation_delete_service_factory", name="annotation_delete"
+        "h.services.auth_cookie.factory", iface=AuthCookieService
     )
     config.register_service_factory(
-        ".delete_group.delete_group_service_factory", name="delete_group"
+        "h.services.auth_token.auth_token_service_factory", name="auth_token"
     )
     config.register_service_factory(
-        ".delete_user.delete_user_service_factory", name="delete_user"
+        "h.services.annotation_delete.annotation_delete_service_factory",
+        name="annotation_delete",
     )
     config.register_service_factory(
-        ".developer_token.developer_token_service_factory", name="developer_token"
+        "h.services.delete_group.delete_group_service_factory", name="delete_group"
     )
     config.register_service_factory(
-        ".document.document_service_factory", name="document"
-    )
-    config.register_service_factory(".feature.feature_service_factory", name="feature")
-    config.register_service_factory(".flag.flag_service_factory", name="flag")
-    config.register_service_factory(".group.groups_factory", name="group")
-    config.register_service_factory(
-        ".group_create.group_create_factory", name="group_create"
+        "h.services.delete_user.delete_user_service_factory", name="delete_user"
     )
     config.register_service_factory(
-        ".group_update.group_update_factory", name="group_update"
+        "h.services.developer_token.developer_token_service_factory",
+        name="developer_token",
     )
     config.register_service_factory(
-        ".group_links.group_links_factory", name="group_links"
+        "h.services.document.document_service_factory", name="document"
     )
     config.register_service_factory(
-        ".group_members.group_members_factory", name="group_members"
+        "h.services.feature.feature_service_factory", name="feature"
     )
-    config.register_service_factory(".links.links_factory", name="links")
-    config.register_service_factory(".group_list.group_list_factory", name="group_list")
+    config.register_service_factory("h.services.flag.flag_service_factory", name="flag")
+
+    config.register_service_factory("h.services.group.groups_factory", name="group")
     config.register_service_factory(
-        ".group_scope.group_scope_factory", name="group_scope"
-    )
-    config.register_service_factory(
-        ".job_queue.metrics.factory", name="job_queue_metrics"
+        "h.services.group_create.group_create_factory", name="group_create"
     )
     config.register_service_factory(
-        ".list_organizations.list_organizations_factory", name="list_organizations"
-    )
-    config.register_service_factory(".nipsa.nipsa_factory", name="nipsa")
-    config.register_service_factory(".oauth.service.factory", name="oauth_provider")
-    config.register_service_factory(
-        ".organization.organization_factory", name="organization"
+        "h.services.group_update.group_update_factory", name="group_update"
     )
     config.register_service_factory(
-        ".rename_user.rename_user_factory", name="rename_user"
+        "h.services.group_links.group_links_factory", name="group_links"
     )
-    config.register_service_factory(".search_index.factory", name="search_index")
-    config.register_service_factory(".settings.settings_factory", name="settings")
     config.register_service_factory(
-        ".url_migration.service_factory", name="url_migration"
+        "h.services.group_members.group_members_factory", name="group_members"
+    )
+    config.register_service_factory("h.services.links.links_factory", name="links")
+    config.register_service_factory(
+        "h.services.group_list.group_list_factory", name="group_list"
+    )
+    config.register_service_factory(
+        "h.services.group_scope.group_scope_factory", name="group_scope"
+    )
+    config.register_service_factory(
+        "h.services.job_queue.metrics.factory", name="job_queue_metrics"
+    )
+    config.register_service_factory(
+        "h.services.list_organizations.list_organizations_factory",
+        name="list_organizations",
+    )
+    config.register_service_factory("h.services.nipsa.nipsa_factory", name="nipsa")
+    config.register_service_factory(
+        "h.services.oauth.service.factory", name="oauth_provider"
+    )
+    config.register_service_factory(
+        "h.services.organization.organization_factory", name="organization"
+    )
+    config.register_service_factory(
+        "h.services.rename_user.rename_user_factory", name="rename_user"
+    )
+    config.register_service_factory(
+        "h.services.search_index.factory", name="search_index"
+    )
+    config.register_service_factory(
+        "h.services.settings.settings_factory", name="settings"
+    )
+    config.register_service_factory(
+        "h.services.url_migration.service_factory", name="url_migration"
     )
     config.register_service_factory(".user.user_service_factory", name="user")
     config.register_service_factory(
-        ".user_unique.user_unique_factory", name="user_unique"
+        "h.services.user_unique.user_unique_factory", name="user_unique"
     )
     config.register_service_factory(
-        ".user_password.user_password_service_factory", name="user_password"
+        "h.services.user_password.user_password_service_factory", name="user_password"
     )
     config.register_service_factory(
-        ".user_signup.user_signup_service_factory", name="user_signup"
+        "h.services.user_signup.user_signup_service_factory", name="user_signup"
     )
     config.register_service_factory(
-        ".user_update.user_update_factory", name="user_update"
+        "h.services.user_update.user_update_factory", name="user_update"
     )
     config.register_service_factory(
         "h.services.subscription.service_factory", iface=SubscriptionService
     )
 
     config.add_directive(
-        "add_annotation_link_generator", ".links.add_annotation_link_generator"
+        "add_annotation_link_generator",
+        "h.services.links.add_annotation_link_generator",
     )
     config.add_request_method(
-        ".feature.FeatureRequestProperty", name="feature", reify=True
+        "h.services.feature.FeatureRequestProperty", name="feature", reify=True
     )
 
     config.register_service_factory(
