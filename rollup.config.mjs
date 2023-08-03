2770974cb6a223735f6f58ecb950a8821e6806f3
Robert Knight <robertknight@gmail.com>
2023-08-03 11:01:41 +0100

    Use official Rollup plugin for Terser
    Replace the third-party minification plugin with the one maintained by the
    Rollup developers.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 const isProd = process.env.NODE_ENV === 'production';
 const prodPlugins = [];
