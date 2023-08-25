5bd5eb415e48d04ca6977e04adaafa18f10a0e97
Marcos Prieto <marcospri@gmail.com>
2023-08-25 13:45:45 +0200

    Migration for AnnotationMetadata

---
+++ h/migrations/versions/076ac6c7a8e0_annotation_metadata.py
@@ -0,0 +1,35 @@
+"""annotation metadata"""
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+revision = "076ac6c7a8e0"
+down_revision = "f064c2b2e04a"
+
+
+def upgrade():
+    op.create_table(
+        "annotation_metadata",
+        sa.Column("id", sa.Integer(), autoincrement=True, nullable=False),
+        sa.Column("annotation_pk", sa.Integer(), nullable=False),
+        sa.Column(
+            "lms",
+            postgresql.JSONB(astext_type=sa.Text()),
+            server_default=sa.text("jsonb('{}')"),
+            nullable=True,
+        ),
+        sa.ForeignKeyConstraint(
+            ["annotation_pk"],
+            ["annotation.pk"],
+            name=op.f("fk__annotation_metadata__annotation_pk__annotation"),
+            ondelete="cascade",
+        ),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk__annotation_metadata")),
+        sa.UniqueConstraint(
+            "annotation_pk", name=op.f("uq__annotation_metadata__annotation_pk")
+        ),
+    )
+
+
+def downgrade():
+    op.drop_table("annotation_metadata")
