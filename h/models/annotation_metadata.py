5adaea4700559b12a34eb680d96c9ae806ad465e
Marcos Prieto <marcospri@gmail.com>
2023-09-05 11:25:51 +0200

    Model for the annotation_metadata table

---
+++ h/models/annotation_metadata.py
@@ -0,0 +1,25 @@
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql as pg
+from sqlalchemy.ext.mutable import MutableDict
+
+from h.db import Base
+
+
+class AnnotationMetadata(Base):
+    __tablename__ = "annotation_metadata"
+
+    annotation_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey("annotation_slim.id", ondelete="cascade"),
+        nullable=False,
+        unique=True,
+        primary_key=True,
+    )
+    """FK to annotation_slim.id"""
+
+    data = sa.Column(
+        MutableDict.as_mutable(pg.JSONB),
+        default=dict,
+        server_default=sa.func.jsonb("{}"),
+        nullable=True,
+    )
