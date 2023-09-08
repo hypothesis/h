59dbfd367593cb0e2bde405f473a99856b555a77
Marcos Prieto <marcospri@gmail.com>
2023-09-08 11:03:50 +0200

    Use both `FOR UPDATE` and `SKIP LOCKED` in filling task

--- h/tasks/annotations.py
+++ h/tasks/annotations.py
@@ -26,6 +26,7 @@ def fill_pk_and_user_id(batch_size=1000):
         )
         .where(Annotation.pk.is_(None))
         .order_by(Annotation.updated.asc())
+        .with_for_update(skip_locked=True)
         .limit(batch_size)
     ).cte("annotations")
 
