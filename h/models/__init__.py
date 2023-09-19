f9960ae3ca3c2f411c8359fa53002cf318734368
Marcos Prieto <marcospri@gmail.com>
2023-09-19 14:44:45 +0200

    Revert "Model for annotation metadata"
    This reverts commit b067cfe6beb54e0dcedc5920e871222f1cba4cad.

--- h/models/__init__.py
+++ h/models/__init__.py
@@ -18,7 +18,6 @@ key to. So for convenience the test module can instead just do
 """
 from h.models.activation import Activation
 from h.models.annotation import Annotation
-from h.models.annotation_metadata import AnnotationMetadata
 from h.models.annotation_moderation import AnnotationModeration
 from h.models.auth_client import AuthClient
 from h.models.auth_ticket import AuthTicket
