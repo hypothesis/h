3d7f7efe2fd98c2adf593f43e359150a38b835bb
Marcos Prieto <marcospri@gmail.com>
2023-09-19 15:05:23 +0200

    Revert the creation of the annotation_metadata table

---
+++ h/migrations/versions/5d1abac3c1a1_revert_annotation_metadata.py
@@ -0,0 +1,15 @@
+"""Revert annotation_metadatai."""
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+revision = "5d1abac3c1a1"
+down_revision = "6df1c8c3e423"
+
+
+def upgrade():
+    op.drop_table("annotation_metadata")
+
+
+def downgrade():
+    """No downgrade, see orignal migration 5d1abac3c1a1."""
+    pass
