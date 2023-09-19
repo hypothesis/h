c1d25c73f05ee04e99dfed4ce38a0cfccb3a3b2d
Marcos Prieto <marcospri@gmail.com>
2023-09-19 15:22:11 +0200

    Migration to revert the creation of annotation.pk

---
+++ h/migrations/versions/77bc5b4f2205_revert_annotation_pk.py
@@ -0,0 +1,17 @@
+"""Revert creation of annotation.pk."""
+from alembic import op
+from sqlalchemy.schema import CreateSequence, DropSequence, Sequence
+
+revision = "77bc5b4f2205"
+down_revision = "5d1abac3c1a1"
+
+
+def upgrade():
+    op.execute(DropSequence(Sequence("annotation_id_seq")))
+    op.drop_constraint(op.f("uq__annotation__pk"), "annotation", type_="unique")
+    op.drop_column("annotation", "pk")
+
+
+def downgrade():
+    """No downgrade, check version f064c2b2e04a."""
+    pass
