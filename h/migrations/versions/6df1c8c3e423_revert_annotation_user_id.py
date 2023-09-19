3014c75563dd58d7e7370fdb24d3cddef15ff08a
Marcos Prieto <marcospri@gmail.com>
2023-09-19 14:24:47 +0200

    Revert migration adding annotation.user_id
    Investigating performance problems in H's DB. Trying to rule this table
    changes as the direct cause.

---
+++ h/migrations/versions/6df1c8c3e423_revert_annotation_user_id.py
@@ -0,0 +1,18 @@
+"""Revert annotation.user_id."""
+from alembic import op
+
+revision = "6df1c8c3e423"
+down_revision = "076ac6c7a8e0"
+
+
+def upgrade():
+    op.drop_index("ix__annotation_user_id", table_name="annotation")
+    op.drop_constraint(
+        "fk__annotation__user_id__user", "annotation", type_="foreignkey"
+    )
+    op.drop_column("annotation", "user_id")
+
+
+def downgrade():
+    """No downgrade, see the upgrade for migration 8250dce465f2."""
+    pass
