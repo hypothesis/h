63b1cab7e1938755c0f523cd3e81c34da62c130e
Marcos Prieto <marcospri@gmail.com>
2023-09-22 15:34:07 +0200

    Add new model for annotation_slim

--- h/models/__init__.py
+++ h/models/__init__.py
@@ -19,6 +19,7 @@ key to. So for convenience the test module can instead just do
 from h.models.activation import Activation
 from h.models.annotation import Annotation
 from h.models.annotation_moderation import AnnotationModeration
+from h.models.annotation_slim import AnnotationSlim
 from h.models.auth_client import AuthClient
 from h.models.auth_ticket import AuthTicket
 from h.models.authz_code import AuthzCode
---
+++ h/models/annotation_slim.py
@@ -0,0 +1,72 @@
+import datetime
+
+import sqlalchemy as sa
+
+from h.db import Base, types
+
+
+class AnnotationSlim(Base):
+    """
+    AnnotationSlim represents fundamental properties of annotations.
+
+    It doesn't include the annotation text, selector and extra fields.
+    It does however have FK to "User", "Group" that the main table doesn't have.
+
+    Until this table is completely fill up with a row for each row in Annotation
+    we'll have duplicated information and most of the code base will read from
+    "Annotation" but write to both "Annotation" and "AnnotationSlim".
+    """
+
+    __tablename__ = "annotation_slim"
+
+    id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+    """PK on this table"""
+
+    pubid = sa.Column(
+        types.URLSafeUUID,
+        sa.ForeignKey("annotation.id", ondelete="CASCADE"),
+        nullable=False,
+        unique=True,
+    )
+    """The value of annotation.id, named here pubid following the convention of group.pubid"""
+
+    created = sa.Column(
+        sa.DateTime,
+        default=datetime.datetime.utcnow,
+        server_default=sa.func.now(),
+        nullable=False,
+        index=True,
+    )
+
+    updated = sa.Column(
+        sa.DateTime,
+        server_default=sa.func.now(),
+        default=datetime.datetime.utcnow,
+        nullable=False,
+        index=True,
+    )
+
+    deleted = sa.Column(
+        sa.Boolean,
+        nullable=False,
+        default=False,
+        server_default=sa.sql.expression.false(),
+    )
+
+    moderated = sa.Column(
+        sa.Boolean,
+        nullable=False,
+        default=False,
+        server_default=sa.sql.expression.false(),
+    )
+
+    shared = sa.Column(
+        sa.Boolean,
+        nullable=False,
+        default=False,
+        server_default=sa.sql.expression.false(),
+    )
+
+    document_id = sa.Column(sa.Integer, sa.ForeignKey("document.id"), nullable=False)
+    user_id = sa.Column(sa.Integer, sa.ForeignKey("user.id"), nullable=False)
+    group_id = sa.Column(sa.Integer, sa.ForeignKey("group.id"), nullable=False)
