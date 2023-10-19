2a38fa0a45a25197fe8fcd80ffe7b6f2a4d5a7f1
Marcos Prieto <marcospri@gmail.com>
2023-10-19 15:46:38 +0200

    Change the approach of the slim filling task
    Query the candidate annotations and use the existing service method to
    create the new rows.

--- tests/h/tasks/annotations_test.py
+++ tests/h/tasks/annotations_test.py
@@ -1,6 +1,7 @@
+from unittest.mock import call
+
 import pytest
 
-from h.models import Annotation, AnnotationSlim
 from h.tasks.annotations import fill_annotation_slim
 
 
@@ -11,41 +12,17 @@ class TestFillPKAndUserId:
     USERNAME_1 = "USERNAME_1"
     USERNAME_2 = "USERNAME_2"
 
-    def test_it(self, factories, db_session):
-        author = factories.User(authority=self.AUTHORITY_1, username=self.USERNAME_1)
-
-        annos = factories.Annotation.create_batch(
-            10,
-            userid=author.userid,
-        )
-        factories.Annotation.create_batch(5)
+    def test_it(self, factories, annotation_write_service):
+        annos = factories.Annotation.create_batch(10)
 
         fill_annotation_slim(batch_size=10)
 
-        assert db_session.query(AnnotationSlim).count() == 10
-        assert (
-            db_session.query(Annotation)
-            .outerjoin(AnnotationSlim)
-            .filter(AnnotationSlim.id.is_(None))
-            .count()
-            == 5
+        annotation_write_service.upsert_annotation_slim.assert_has_calls(
+            [call(anno) for anno in reversed(annos)]
         )
 
-        # Refresh data for the annotations
-        _ = [db_session.refresh(anno) for anno in annos]
-
-        for anno in annos:
-            assert anno.slim.pubid == anno.id
-            assert anno.slim.created == anno.created
-            assert anno.slim.updated == anno.updated
-            assert anno.slim.deleted == anno.deleted
-            assert anno.slim.shared == anno.shared
-            assert anno.slim.document_id == anno.document_id
-            assert anno.slim.group_id == anno.group.id
-            assert anno.slim.user_id == author.id
-
     @pytest.fixture(autouse=True)
-    def celery(self, patch, db_session):
+    def celery(self, patch, pyramid_request):
         cel = patch("h.tasks.annotations.celery", autospec=False)
-        cel.request.db = db_session
+        cel.request = pyramid_request
         return cel
