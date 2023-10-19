9399c7f0682ed17dda18601df58c24f8e477094a
Marcos Prieto <marcospri@gmail.com>
2023-10-19 15:46:38 +0200

    Change the approach of the slim filling task
    Query the candidate annotations and use the existing service method to
    create the new rows.

--- h/tasks/annotations.py
+++ h/tasks/annotations.py
@@ -1,7 +1,6 @@
-from sqlalchemy import func, insert
-
 from h.celery import celery, get_task_logger
-from h.models import Annotation, AnnotationModeration, AnnotationSlim, Group, User
+from h.models import Annotation, AnnotationSlim
+from h.services.annotation_write import AnnotationWriteService
 
 log = get_task_logger(__name__)
 
@@ -9,47 +8,18 @@ log = get_task_logger(__name__)
 @celery.task
 def fill_annotation_slim(batch_size=1000):
     """Task to fill the new AnnotationSlim table in batches."""
-    # pylint: disable=no-member
-    db = celery.request.db
+    # pylint:disable=no-member
+
+    anno_write_svc = celery.request.find_service(AnnotationWriteService)
 
     annotations = (
-        db.query(
-            Annotation.id.label("pubid"),
-            Annotation.created,
-            Annotation.updated,
-            Annotation.deleted,
-            Annotation.shared,
-            Annotation.document_id,
-            Group.id.label("group_id"),
-            User.id.label("user_id"),
-            AnnotationModeration.id.is_not(None).label("moderated"),
-        )
-        .join(Group, Group.pubid == Annotation.groupid)
-        .join(
-            User,
-            User.username
-            == func.split_part(func.split_part(Annotation.userid, "@", 1), ":", 2),
-        )
+        celery.request.db.query(Annotation)
         .outerjoin(AnnotationSlim)
-        .outerjoin(AnnotationModeration)
         .where(AnnotationSlim.id.is_(None))
         .order_by(Annotation.updated.desc())
         .limit(batch_size)
-    ).cte("annotations")
-
-    db.execute(
-        insert(AnnotationSlim).from_select(
-            [
-                annotations.c.pubid,
-                annotations.c.created,
-                annotations.c.updated,
-                annotations.c.deleted,
-                annotations.c.shared,
-                annotations.c.document_id,
-                annotations.c.group_id,
-                annotations.c.user_id,
-                annotations.c.moderated,
-            ],
-            annotations,
-        )
     )
+
+    for annotation in annotations:
+        log.info("Created AnnotationSlim record for: %s", annotation.id)
+        anno_write_svc.upsert_annotation_slim(annotation)
