cb7da7ba81073d037dfd017c48bad137156237ea
Robert Knight <robertknight@gmail.com>
2023-10-25 15:43:27 +0100

    Increase minimum password length to 8 characters
    This affects newly created passwords, for new or existing accounts, but not
    existing logins.
    
    Fixes https://github.com/hypothesis/product-backlog/issues/1523

--- tests/h/schemas/forms/accounts/reset_password_test.py
+++ tests/h/schemas/forms/accounts/reset_password_test.py
@@ -12,9 +12,9 @@ from h.schemas.forms.accounts import ResetPasswordSchema
 @pytest.mark.usefixtures("pyramid_config")
 class TestResetPasswordSchemaDeserialize:
     def test_it_is_valid_with_a_long_password(self, schema):
-        # Yeah... our minimum password length is 2 chars. See
+        # Yeah... our minimum password length is 8 chars. See
         # `h.schema.forms.accounts.util`
-        schema.deserialize({"user": "*any*", "password": "aa"})
+        schema.deserialize({"user": "*any*", "password": "new-password"})
 
     @pytest.mark.parametrize("password", ("", "a"))
     def test_it_is_invalid_with_password_too_short(self, schema, password):
@@ -25,7 +25,7 @@ class TestResetPasswordSchemaDeserialize:
 
     def test_it_is_invalid_with_a_null_user(self, schema):
         with pytest.raises(colander.Invalid) as exc:
-            schema.deserialize({"user": colander.null, "password": "*any*"})
+            schema.deserialize({"user": colander.null, "password": "new-password"})
 
         assert "user" in exc.value.asdict()
 
@@ -33,7 +33,7 @@ class TestResetPasswordSchemaDeserialize:
         models.User.get_by_username.return_value = None
 
         with pytest.raises(colander.Invalid) as exc:
-            schema.deserialize({"user": "encoded_token", "password": "*any*"})
+            schema.deserialize({"user": "encoded_token", "password": "new-password"})
 
         assert "user" in exc.value.asdict()
 
@@ -41,7 +41,7 @@ class TestResetPasswordSchemaDeserialize:
         serializer.loads.side_effect = BadData("Invalid token")
 
         with pytest.raises(colander.Invalid) as exc:
-            schema.deserialize({"user": "INVALID_TOKEN", "password": "*any*"})
+            schema.deserialize({"user": "INVALID_TOKEN", "password": "new-password"})
 
         assert "user" in exc.value.asdict()
         assert "Wrong reset code." in exc.value.asdict()["user"]
@@ -50,7 +50,7 @@ class TestResetPasswordSchemaDeserialize:
         serializer.loads.side_effect = SignatureExpired("Token has expired")
 
         with pytest.raises(colander.Invalid) as exc:
-            schema.deserialize({"user": "encoded_token", "password": "*any*"})
+            schema.deserialize({"user": "encoded_token", "password": "new-password"})
 
         serializer.loads.assert_called_once_with(
             "encoded_token", max_age=72 * 3600, return_timestamp=True
@@ -74,7 +74,9 @@ class TestResetPasswordSchemaDeserialize:
     ):
         user.password_updated = password_updated
 
-        appstruct = schema.deserialize({"user": "encoded_token", "password": "secret"})
+        appstruct = schema.deserialize(
+            {"user": "encoded_token", "password": "new-password"}
+        )
 
         models.User.get_by_username.assert_called_once_with(
             pyramid_csrf_request.db,
@@ -89,7 +91,7 @@ class TestResetPasswordSchemaDeserialize:
         user.password_updated = datetime.now() + timedelta(days=1)
 
         with pytest.raises(colander.Invalid) as exc:
-            schema.deserialize({"user": "EXPIRED_TOKEN", "password": "*any*"})
+            schema.deserialize({"user": "EXPIRED_TOKEN", "password": "new-password"})
 
         assert "user" in exc.value.asdict()
         assert "This reset code has already been used." in exc.value.asdict()["user"]
