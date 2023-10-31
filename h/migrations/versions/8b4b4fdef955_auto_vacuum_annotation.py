711f3d75441a3c73d2ad1b911d79fd7ab1133b40
Marcos Prieto <marcospri@gmail.com>
2023-10-31 07:45:33 +0100

    Change auto vacuum settings on big tables
    This should allow autovacuum to kick in for big tables where it has
    never run.

---
+++ h/migrations/versions/8b4b4fdef955_auto_vacuum_annotation.py
@@ -0,0 +1,30 @@
+"""Change auto-vacuum settings on big tables."""
+from alembic import op
+
+revision = "8b4b4fdef955"
+down_revision = "0de98307b3c0"
+
+BIG_TABLES = [
+    "annotation",
+    "annotation_slim",
+    "annotation_metadata",
+    "document",
+    "user",
+    "group",
+]
+
+
+def upgrade():
+    for table in BIG_TABLES:
+        # Set auto vacuum to kick in after 5% of dead tuples (vs total in the table) are detected
+        op.execute(
+            f'ALTER TABLE "{table}" SET (autovacuum_vacuum_scale_factor = 0.05);'
+        )
+
+
+def downgrade():
+    # Set it back to the default value
+    for table in BIG_TABLES:
+        op.execute(
+            f'ALTER TABLE "{table}" SET (autovacuum_vacuum_scale_factor = 0.20);'
+        )
