adc05beeedb4bc756d947c4181dc34e664c6c9aa
Sean Hammond <seanh@users.noreply.github.com>
2024-01-02 18:05:52 +0000

    Re-add missing Gunicorn config
    A previous commit adding `--config conf/gunicorn*.conf.py` arguments to
    the Gunicorn commands in the `supervisord[-dev].conf` files, meaning
    that the root `gunicorn.conf.py` file (Gunicorn's default config file
    location) was no longer used and the settings in that root
    `gunicorn.conf.py` file were no longer being applied.
    
    This commit moves the settings from the root `gunicorn.conf.py` file
    into the appropriate `conf/gunicorn*.conf.py` files.
    
    The `certfile` and `keyfile` settings are only needed in development
    (they're used by the `make devssl` command) so those are only needed in
    `gunicorn-dev.conf.py` and `gunicorn-websocket-dev.conf.py`. (The
    duplication is unfortunate but necessary now that we have separate
    Gunicorn config files.)
    
    The `WEB_NUM_WORKERS` and `WEBSOCKET_NUM_WORKERS` envvars are only
    needed in production. The app will now crash if either of these envvars
    is missing from a production environment, rather than silently
    continuing with Gunicorn's default number of workers (`1`) which can be
    difficult to debug.

--- conf/gunicorn-websocket-dev.conf.py
+++ conf/gunicorn-websocket-dev.conf.py
@@ -1,5 +1,15 @@
+from os import environ
+
+
 bind = "localhost:5001"
 worker_class = "h.streamer.Worker"
 graceful_timeout = 0
 workers = 2
 worker_connections = 8
+
+
+if 'H_GUNICORN_CERTFILE' in environ:
+    certfile = environ['H_GUNICORN_CERTFILE']
+
+if 'H_GUNICORN_KEYFILE' in environ:
+    keyfile = environ['H_GUNICORN_KEYFILE']
--- conf/gunicorn.conf.py
+++ conf/gunicorn.conf.py
@@ -1,1 +1,5 @@
+from os import environ
+
+
 bind = "unix:/tmp/gunicorn-web.sock"
+workers = environ["WEB_NUM_WORKERS"]
