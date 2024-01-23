534cca89b94c1b82037ce1c17a995ca5f3271a4a
Sean Hammond <seanh@users.noreply.github.com>
2024-01-23 12:39:48 +0000

    Use h-testkit
    Instead of duplicating test fixtures.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,5 @@
-from os import environ
-
 import pytest
-from sqlalchemy import create_engine, event
-from sqlalchemy.orm import sessionmaker
-
-
-@pytest.fixture(scope="session")
-def db_engine():
-    return create_engine(environ["DATABASE_URL"])
-
-
-@pytest.fixture(scope="session")
-def db_sessionfactory():
-    return sessionmaker()
+from sqlalchemy import event
 
 
 @pytest.fixture
@@ -20,20 +7,14 @@ def db_session(db_engine, db_sessionfactory):
     """
     Return the SQLAlchemy database session.
 
-    This returns a session that is wrapped in an external transaction that is
-    rolled back after each test, so tests can't make database changes that
-    affect later tests.  Even if the test (or the code under test) calls
-    session.commit() this won't touch the external transaction.
-
-    This is the same technique as used in SQLAlchemy's own CI.
-    Since h still uses SQLAlchemy 1.4 it uses the older, 1.4 version of the
-    technique:
+    h overrides the db_session fixture from h-testkit because h still uses
+    SQLAlchemy 1.4 so it has to use the older, 1.4 version of the SQLAlchemy
+    test suite technique:
 
     https://docs.sqlalchemy.org/en/14/orm/session_transaction.html#joining-a-session-into-an-external-transaction-such-as-for-test-suites
 
-    When h is upgraded to SQLAlchemy 2 we can upgrade this fixture to use the
-    newer, SQLAlchemy 2 version of the technique:
-    https://docs.sqlalchemy.org/en/20/orm/session_transaction.html#joining-a-session-into-an-external-transaction-such-as-for-test-suites
+    When h is upgraded to SQLAlchemy 2 this fixture can be removed and it can
+    just use the one from h-testkit.
     """
     connection = db_engine.connect()
     transaction = connection.begin()
