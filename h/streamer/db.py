c6197ca3e7e78b7a4952b7dbb4340fa654f2c2ac
Marcos Prieto <marcospri@gmail.com>
2024-01-23 15:29:32 +0100

    Use text for raw SQL queries for compatibility with SQLA 2

--- h/streamer/db.py
+++ h/streamer/db.py
@@ -1,6 +1,8 @@
 import logging
 from contextlib import contextmanager
 
+from sqlalchemy import text
+
 from h import db
 
 LOG = logging.getLogger(__name__)
@@ -16,7 +18,7 @@ def read_only_transaction(session):
     """Wrap a call in a read only transaction context manager."""
     try:
         session.execute(
-            "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE READ ONLY DEFERRABLE"
+            text("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE READ ONLY DEFERRABLE")
         )
 
         yield
