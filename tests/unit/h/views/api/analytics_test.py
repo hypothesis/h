a9c10c1eca431af7b292911f368517cd51abc1bc
Alejandro Celaya <alejandrocelaya@gmail.com>
2024-04-03 11:32:11 +0200

    Switch to {app}.{area}.{event} format for analytics events (#8643)

--- tests/unit/h/views/api/analytics_test.py
+++ tests/unit/h/views/api/analytics_test.py
@@ -7,7 +7,7 @@ from h.views.api.analytics import create_event
 @pytest.mark.usefixtures("analytics_service")
 class TestCreateEvent:
     def test_analytics_service_is_invoked(self, pyramid_request, analytics_service):
-        pyramid_request.json_body = {"event": "APPLY_PENDING_UPDATES"}
+        pyramid_request.json_body = {"event": "client.realtime.apply_updates"}
         res = create_event(pyramid_request)
 
         assert analytics_service.create.called
