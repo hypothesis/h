35beffd63fc5747f9a103a9e1d49522494318edb
Sean Hammond <seanh@users.noreply.github.com>
2024-07-04 11:17:08 +0100

    Remove animations settings from Tailwind config
    These are now in our component library so not needed here.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,6 @@ export default {
   ],
   theme: {
     extend: {
-      animation: {
-        'fade-in': 'fade-in 0.3s forwards',
-        'fade-out': 'fade-out 0.3s forwards',
-        'slide-in-from-right': 'slide-in-from-right 0.3s forwards ease-in-out',
-      },
       fontFamily: {
         sans: [
           '"Helvetica Neue"',
@@ -26,37 +21,6 @@ export default {
         // Similar to tailwind's default `shadow-inner` but coming from the
         // right edge instead of the left
         'r-inner': 'inset -2px 0 4px 0 rgb(0,0,0,.05)',
-      },
-      keyframes: {
-        'fade-in': {
-          '0%': {
-            opacity: '0',
-          },
-          '100%': {
-            opacity: '1',
-          },
-        },
-        'fade-out': {
-          '0%': {
-            opacity: '1',
-          },
-          '100%': {
-            opacity: '0',
-          },
-        },
-        'slide-in-from-right': {
-          '0%': {
-            opacity: '0',
-            left: '100%',
-          },
-          '80%': {
-            left: '-10px',
-          },
-          '100%': {
-            left: '0',
-            opacity: '1',
-          },
-        },
       },
     },
   },
