4d84bdb07b1010013d71f3eaf6b9604c621691c8
Sean Hammond <seanh@users.noreply.github.com>
2024-07-04 11:14:10 +0100

    Add TypeScript type-checking to `make sure` and CI

--- h/static/scripts/group-forms/index.tsx
+++ h/static/scripts/group-forms/index.tsx
@@ -3,7 +3,7 @@ import { render } from 'preact';
 import CreateGroupForm from './components/CreateGroupForm';
 
 function init() {
-  render(<CreateGroupForm />, document.querySelector('#create-group-form'));
+  render(<CreateGroupForm />, document.querySelector('#create-group-form')!);
 }
 
 init();
