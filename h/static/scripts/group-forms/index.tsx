4803c83f03bc21f3cd3bdd2d4930d16af0323f9b
Sean Hammond <seanh@users.noreply.github.com>
2024-07-04 15:31:58 +0100

    Use Shadow DOM for the `CreateGroupForm` app
    So that the surrounding page's JavaScript and CSS can't affect the
    Preact app, and vice-versa.

--- h/static/scripts/group-forms/index.tsx
+++ h/static/scripts/group-forms/index.tsx
@@ -3,7 +3,9 @@ import { render } from 'preact';
 import CreateGroupForm from './components/CreateGroupForm';
 
 function init() {
-  render(<CreateGroupForm />, document.querySelector('#create-group-form')!);
+  const shadowHost = document.querySelector('#create-group-form')!;
+  const shadowRoot = shadowHost.attachShadow({ mode: 'open' });
+  render(<CreateGroupForm />, shadowRoot);
 }
 
 init();
