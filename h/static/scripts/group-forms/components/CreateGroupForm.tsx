84933b551abc88ac4fcfdf4819137789aadfc611
Sean Hammond <seanh@users.noreply.github.com>
2024-07-09 16:44:01 +0100

    Change `class` to `className` in JSX
    `class` works just fine but `className` is conventional.

--- h/static/scripts/group-forms/components/CreateGroupForm.tsx
+++ h/static/scripts/group-forms/components/CreateGroupForm.tsx
@@ -3,49 +3,49 @@ import { Button, Input, Textarea } from '@hypothesis/frontend-shared';
 export default function CreateGroupForm() {
   return (
     <>
-      <h1 class="mt-[55px] mb-[30px] text-[#3f3f3f] text-[19px] leading-[15px]">
+      <h1 className="mt-[55px] mb-[30px] text-[#3f3f3f] text-[19px] leading-[15px]">
         Create a new private group
       </h1>
 
       <form>
-        <div class="mb-[15px]">
-          <label for="name" class="text-[#7a7a7a] text-[13px] leading-[15px]">
-            Name<span class="text-[#d00032]">*</span>
+        <div className="mb-[15px]">
+          <label for="name" className="text-[#7a7a7a] text-[13px] leading-[15px]">
+            Name<span className="text-[#d00032]">*</span>
           </label>
           <Input id="name" autofocus autocomplete="off" required />
-          <div class="flex">
-            <div class="grow"></div>
-            <span class="text-[#7a7a7a] text-[13px] leading-[15px]">0/25</span>
+          <div className="flex">
+            <div className="grow"></div>
+            <span className="text-[#7a7a7a] text-[13px] leading-[15px]">0/25</span>
           </div>
         </div>
 
-        <div class="mb-[15px]">
+        <div className="mb-[15px]">
           <label
             for="description"
-            class="text-[#7a7a7a] text-[13px] leading-[15px]"
+            className="text-[#7a7a7a] text-[13px] leading-[15px]"
           >
             Description
           </label>
           <Textarea id="description" />
-          <div class="flex">
-            <div class="grow"></div>
-            <span class="text-[#7a7a7a] text-[13px] leading-[15px]">0/250</span>
+          <div className="flex">
+            <div className="grow"></div>
+            <span className="text-[#7a7a7a] text-[13px] leading-[15px]">0/250</span>
           </div>
         </div>
 
-        <div class="flex">
-          <div class="grow"></div>
+        <div className="flex">
+          <div className="grow"></div>
           <div>
             <Button variant="primary">Create group</Button>
           </div>
         </div>
       </form>
 
-      <footer class="text-[#7a7a7a] text-[13px] leading-[15px] mt-[50px] pt-[15px] border-t border-t-[#dbdbdb]">
-        <div class="flex">
-          <div class="grow"></div>
+      <footer className="text-[#7a7a7a] text-[13px] leading-[15px] mt-[50px] pt-[15px] border-t border-t-[#dbdbdb]">
+        <div className="flex">
+          <div className="grow"></div>
           <div>
-            <span class="text-[#d00032]">*</span> Required
+            <span className="text-[#d00032]">*</span> Required
           </div>
         </div>
       </footer>
