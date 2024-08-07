9ae05d9874b4b93ca18fac6eb0ed88bcbc719ea3
Sean Hammond <seanh@users.noreply.github.com>
2024-08-07 15:00:31 +0100

    Use a separate auth cookie for API requests
    Use two separate auth cookies to authenticate requests for HTML pages
    (these use the pre-existing auth cookie) and requests from h's own
    frontend code to h's API endpoints (these use a new, separate API auth
    cookie that's introduced by this commit).
    
    This enables the API auth cookie to be more secure. In particular, the
    API auth cookie can use SameSite=Strict rather than Lax, which gives
    better protection against CSRF attacks but would not be desirable for
    the HTML auth cookie (because whenever a user followed a link from
    another site to Hypothesis they would find themselves not authenticated).
    
    How this works: `CookiePolicy` (which is used for HTML page requests)
    sets and deletes both the HTML auth cookie and the API auth cookie on
    log in and log out, but reads only the HTML auth cookie to authenticate
    HTML page requests. `APICookiePolicy` reads only the API auth cookie to
    authenticate API requests, and never sets or deletes either cookie.
    
    In detail:
    
    1. Recall that the top-level security policy (`TopLevelPolicy`)
       delegates all method calls to either `CookiePolicy` (for HTML
       endpoints) or `APIPolicy` (for API endpoints).
    
    2. When a user logs in the view calls `pyramid.security.remember()`
       which calls `TopLevelPolicy.remember()`. The login form submission
       is an HTML endpoint so `TopLevelPolicy.remember()` delegates to
       `CookiePolicy.remember()` which sets both the HTML and API auth
       cookies.
    
    3. Similarly when the user logs out `CookiePolicy.forget()` deletes both
       the HTML and API auth cookies.
    
    4. To authenticate HTML page requests `CookiePolicy.identity()` reads
       only the HTML auth cookie.
    
    5. To authenticate API requests `TopLevelPolicy` delegates to
       `APIPolicy` which delegates to a list of API subpolicies
       (`BearerTokenPolicy`, `AuthClientPolicy` and `APICookiePolicy`).
       If there's no bearer token or auth client authentication in the
       request then `APICookiePolicy` reads only the API auth cookie
       to authenticate API requests.

--- tests/unit/h/security/policy/top_level_test.py
+++ tests/unit/h/security/policy/top_level_test.py
@@ -1,6 +1,7 @@
-from unittest.mock import sentinel
+from unittest.mock import call, create_autospec, sentinel
 
 import pytest
+from webob.cookies import SignedCookieProfile
 
 from h.security.policy.top_level import TopLevelPolicy, get_subpolicy
 
@@ -69,25 +70,28 @@ class TestGetSubpolicy:
         AuthTicketCookieHelper,
     ):
         is_api_request.return_value = True
+        api_authcookie = create_autospec(
+            SignedCookieProfile, instance=True, spec_set=True
+        )
+        webob.cookies.SignedCookieProfile.return_value = api_authcookie
 
         policy = get_subpolicy(pyramid_request)
 
         BearerTokenPolicy.assert_called_once_with()
         AuthClientPolicy.assert_called_once_with()
         webob.cookies.SignedCookieProfile.assert_called_once_with(
-            secret="test_h_auth_cookie_secret",
-            salt="authsanity",
-            cookie_name="auth",
+            secret="test_h_api_auth_cookie_secret",
+            salt="test_h_api_auth_cookie_salt",
+            cookie_name="h_api_authcookie",
             max_age=2592000,
             httponly=True,
             secure=True,
+            samesite="strict",
         )
-        webob.cookies.SignedCookieProfile.return_value.bind.assert_called_once_with(
-            pyramid_request
-        )
+        api_authcookie.bind.assert_called_once_with(pyramid_request)
         AuthTicketCookieHelper.assert_called_once_with()
         APICookiePolicy.assert_called_once_with(
-            webob.cookies.SignedCookieProfile.return_value.bind.return_value,
+            api_authcookie.bind.return_value,
             AuthTicketCookieHelper.return_value,
         )
         APIPolicy.assert_called_once_with(
@@ -108,23 +112,44 @@ class TestGetSubpolicy:
         AuthTicketCookieHelper,
     ):
         is_api_request.return_value = False
+        html_authcookie = create_autospec(
+            SignedCookieProfile, instance=True, spec_set=True
+        )
+        api_authcookie = create_autospec(
+            SignedCookieProfile, instance=True, spec_set=True
+        )
+        webob.cookies.SignedCookieProfile.side_effect = [
+            api_authcookie,
+            html_authcookie,
+        ]
 
         policy = get_subpolicy(pyramid_request)
 
-        webob.cookies.SignedCookieProfile.assert_called_once_with(
-            secret="test_h_auth_cookie_secret",
-            salt="authsanity",
-            cookie_name="auth",
-            max_age=2592000,
-            httponly=True,
-            secure=True,
-        )
-        webob.cookies.SignedCookieProfile.return_value.bind.assert_called_once_with(
-            pyramid_request
-        )
+        assert webob.cookies.SignedCookieProfile.call_args_list == [
+            call(
+                secret="test_h_api_auth_cookie_secret",
+                salt="test_h_api_auth_cookie_salt",
+                cookie_name="h_api_authcookie",
+                max_age=2592000,
+                httponly=True,
+                secure=True,
+                samesite="strict",
+            ),
+            call(
+                secret="test_h_auth_cookie_secret",
+                salt="authsanity",
+                cookie_name="auth",
+                max_age=2592000,
+                httponly=True,
+                secure=True,
+            ),
+        ]
+        api_authcookie.bind.assert_called_once_with(pyramid_request)
+        html_authcookie.bind.assert_called_once_with(pyramid_request)
         AuthTicketCookieHelper.assert_called_once_with()
         CookiePolicy.assert_called_once_with(
-            webob.cookies.SignedCookieProfile.return_value.bind.return_value,
+            html_authcookie.bind.return_value,
+            api_authcookie.bind.return_value,
             AuthTicketCookieHelper.return_value,
         )
         assert policy == CookiePolicy.return_value
@@ -170,6 +195,8 @@ def CookiePolicy(mocker):
 @pytest.fixture
 def pyramid_settings(pyramid_settings):
     pyramid_settings["h_auth_cookie_secret"] = "test_h_auth_cookie_secret"
+    pyramid_settings["h_api_auth_cookie_secret"] = "test_h_api_auth_cookie_secret"
+    pyramid_settings["h_api_auth_cookie_salt"] = "test_h_api_auth_cookie_salt"
     return pyramid_settings
 
 
