82acc0103d8d1dfded0491c4df4cbc855dfcf8b6
Sean Hammond <seanh@users.noreply.github.com>
2024-08-08 15:31:37 +0100

    Remove no-longer-needed transitional cookie code

--- tests/unit/h/security/policy/_cookie_test.py
+++ tests/unit/h/security/policy/_cookie_test.py
@@ -3,7 +3,6 @@ from unittest.mock import call, create_autospec, sentinel
 import pytest
 from webob.cookies import SignedCookieProfile
 
-from h.security.identity import Identity as Identity_
 from h.security.policy._cookie import CookiePolicy
 from h.security.policy.helpers import AuthTicketCookieHelper
 
@@ -15,76 +14,6 @@ class TestCookiePolicy:
         helper.add_vary_by_cookie.assert_called_once_with(pyramid_request)
         helper.identity.assert_called_once_with(html_authcookie, pyramid_request)
         assert identity == helper.identity.return_value
-
-    def test_identity_issues_api_authcookie(
-        self, cookie_policy, helper, pyramid_request, api_authcookie
-    ):
-        helper.remember.return_value = [
-            ("header_name_1", "header_value_1"),
-            ("header_name_2", "header_value_2"),
-        ]
-        del pyramid_request.cookies[api_authcookie.cookie_name]
-
-        cookie_policy.identity(pyramid_request)
-
-        pyramid_request._process_response_callbacks(  # pylint:disable=protected-access
-            pyramid_request.response
-        )
-        helper.remember.assert_called_once_with(
-            api_authcookie, pyramid_request, helper.identity.return_value.user.userid
-        )
-        for header in helper.remember.return_value:
-            assert header in pyramid_request.response.headerlist
-
-    @pytest.mark.parametrize(
-        "identity",
-        [
-            None,
-            Identity_(user=None, auth_client=None),
-        ],
-    )
-    def test_identity_doesnt_issue_api_authcookie_if_user_not_authenticated(
-        self,
-        cookie_policy,
-        helper,
-        pyramid_request,
-        api_authcookie,
-        identity,
-        assert_api_authcookie_not_issued,
-    ):
-        helper.identity.return_value = identity
-        del pyramid_request.cookies[api_authcookie.cookie_name]
-
-        cookie_policy.identity(pyramid_request)
-
-        assert_api_authcookie_not_issued()
-
-    def test_identity_doesnt_issue_api_authcookie_if_already_issued(
-        self, cookie_policy, pyramid_request, assert_api_authcookie_not_issued
-    ):
-        cookie_policy.identity(pyramid_request)
-
-        assert_api_authcookie_not_issued()
-
-    def test_identity_issues_api_authcookie_only_once(
-        self,
-        cookie_policy,
-        helper,
-        pyramid_request,
-        api_authcookie,
-        assert_api_authcookie_not_issued,
-    ):
-        helper.remember.return_value = [("header_name_1", "header_value_1")]
-        del pyramid_request.cookies[api_authcookie.cookie_name]
-        cookie_policy.identity(pyramid_request)
-        pyramid_request._process_response_callbacks(  # pylint:disable=protected-access
-            pyramid_request.response
-        )
-
-        cookie_policy.identity(pyramid_request)
-
-        helper.reset_mock()
-        assert_api_authcookie_not_issued()
 
     def test_authenticated_userid(
         self, cookie_policy, helper, pyramid_request, Identity, html_authcookie
@@ -206,12 +135,6 @@ class TestCookiePolicy:
             "secret", "salt", cookie_name="h_api_authcookie"
         )
 
-        # Add the API auth cookie to the request.
-        # In future, if we set the API auth cookie's `path` attribute to
-        # "/api/" so that the API auth cookie is no longer included in HTML
-        # requests, we should remove this line from the unit tests as well.
-        pyramid_request.cookies[api_authcookie.cookie_name] = api_authcookie
-
         return api_authcookie.bind(pyramid_request)
 
     @pytest.fixture
@@ -221,18 +144,6 @@ class TestCookiePolicy:
     @pytest.fixture
     def cookie_policy(self, html_authcookie, api_authcookie, helper):
         return CookiePolicy(html_authcookie, api_authcookie, helper)
-
-    @pytest.fixture
-    def assert_api_authcookie_not_issued(self, helper, pyramid_request):
-        def assert_api_authcookie_not_issued():
-            headerlist_before = list(pyramid_request.response.headerlist)
-            pyramid_request._process_response_callbacks(  # pylint:disable=protected-access
-                pyramid_request.response
-            )
-            helper.remember.assert_not_called()
-            assert pyramid_request.response.headerlist == headerlist_before
-
-        return assert_api_authcookie_not_issued
 
 
 @pytest.fixture(autouse=True)
