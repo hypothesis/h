a31d36dbb15c8e0a05c2aa29c45b215c3e7c2dc6
Marcos Prieto <marcospri@gmail.com>
2024-08-27 09:57:55 +0200

    Read max_requets gunicorn settings from environment variables
    Allows to have different value in different environments like staging,
    prod-us and prod ca.

--- conf/gunicorn.conf.py
+++ conf/gunicorn.conf.py
@@ -5,5 +5,5 @@ bind = "unix:/tmp/gunicorn-web.sock"
 worker_tmp_dir = "/dev/shm"
 workers = environ["WEB_NUM_WORKERS"]
 
-max_requests = 500_000
-max_requests_jitter = 100_000
+max_requests = environ.get("GUNICORN_MAX_REQUESTS", 500_000)
+max_requests_jitter = environ.get("GUNICORN_MAX_REQUESTS_JITTER", 100_000)
