ba28b61d4ac83c716ef76af8ba2c54f2eb6265cf
Robert Knight <robertknight@gmail.com>
2024-11-14 13:51:31 +0000

    Ignore `setLocation` helper in code coverage

--- h/static/scripts/group-forms/utils/set-location.ts
+++ h/static/scripts/group-forms/utils/set-location.ts
@@ -1,3 +1,7 @@
+/**
+ * Test seam for updating {@link location.href}.
+ */
+// istanbul ignore next
 export function setLocation(newLocation: string): void {
   window.location.href = newLocation;
 }
