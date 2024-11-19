0cd5b18c4b34ec43cd62621638c2534f462cc16a
Robert Knight <robertknight@gmail.com>
2024-11-19 09:33:45 +0000

    Add a custom router that can warn about unsaved changes on navigation
    Add a `useUnsavedChanges` hook which allows components to declare they have
    unsaved changes. Such changes will then cause a confirmation dialog to be shown
    on a server-side navigation (via `beforeunload`) or a client side navigation in
    a Preact app using the `Router` component

---
+++ h/static/scripts/group-forms/components/test/Router-test.js
@@ -0,0 +1,82 @@
+import { Link } from 'wouter-preact';
+import { mount } from 'enzyme';
+import { act } from 'preact/test-utils';
+
+import { $imports, default as Router } from '../Router';
+
+function App() {
+  return (
+    <Router>
+      <Link data-testid="link" href="/test/path">
+        Test link
+      </Link>
+    </Router>
+  );
+}
+
+describe('Router', () => {
+  let fakeHasUnsavedChanges;
+  let initialHistoryLen;
+
+  beforeEach(() => {
+    fakeHasUnsavedChanges = sinon.stub().returns(false);
+    initialHistoryLen = history.length;
+
+    $imports.$mock({
+      '../utils/unsaved-changes': {
+        hasUnsavedChanges: fakeHasUnsavedChanges,
+      },
+    });
+  });
+
+  afterEach(() => {
+    $imports.$restore();
+
+    const backSteps = history.length - initialHistoryLen;
+    if (backSteps > 0) {
+      history.go(-backSteps);
+    }
+  });
+
+  function clickLink(wrapper) {
+    act(() => {
+      wrapper.find('a').getDOMNode().click();
+    });
+    wrapper.update();
+  }
+
+  it('navigates without warning if there are no unsaved changes', () => {
+    const wrapper = mount(<App />);
+    clickLink(wrapper);
+    assert.isFalse(wrapper.exists('WarningDialog'));
+    assert.equal(location.pathname, '/test/path');
+  });
+
+  it('shows a warning if there are unsaved changes', () => {
+    // Click link when there are unsaved changes.
+    history.pushState({}, '', '/original/url');
+    fakeHasUnsavedChanges.returns(true);
+    const wrapper = mount(<App />);
+    clickLink(wrapper);
+
+    // The confirmation dialog should be shown.
+    const warning = wrapper.find('WarningDialog');
+    assert.isTrue(warning.exists());
+    assert.notEqual(location.pathname, '/test/path');
+
+    // Canceling the dialog should leave the location unchanged
+    warning.prop('onCancel')();
+    wrapper.update();
+    assert.isFalse(wrapper.exists('WarningDialog'));
+    assert.notEqual(location.pathname, '/test/path');
+
+    // Click the link again, but this time confirm the navigation.
+    clickLink(wrapper);
+    const warning2 = wrapper.find('WarningDialog');
+    warning2.prop('onConfirm')();
+    wrapper.update();
+
+    assert.isFalse(wrapper.exists('WarningDialog'));
+    assert.equal(location.pathname, '/test/path');
+  });
+});
---
+++ h/static/scripts/group-forms/utils/test/unsaved-changes-test.js
@@ -0,0 +1,60 @@
+import { mount } from 'enzyme';
+import { useUnsavedChanges, hasUnsavedChanges } from '../unsaved-changes';
+
+function TestUseUnsavedChanges({ unsaved, fakeWindow }) {
+  useUnsavedChanges(unsaved, fakeWindow);
+  return <div />;
+}
+
+describe('useUnsavedChanges', () => {
+  let fakeWindow;
+
+  function dispatchBeforeUnload() {
+    const event = new Event('beforeunload', { cancelable: true });
+    fakeWindow.dispatchEvent(event);
+    return event;
+  }
+
+  function createWidget(unsaved) {
+    return mount(
+      <TestUseUnsavedChanges fakeWindow={fakeWindow} unsaved={unsaved} />,
+    );
+  }
+
+  beforeEach(() => {
+    // Use a dummy window to avoid triggering any handlers that respond to
+    // "beforeunload" on the real window.
+    fakeWindow = new EventTarget();
+  });
+
+  it('does not increment unsaved-changes count if argument is false', () => {
+    const wrapper = createWidget(false);
+    assert.isFalse(hasUnsavedChanges());
+    wrapper.unmount();
+  });
+
+  it('does not register "beforeunload" handler if argument is false', () => {
+    const wrapper = createWidget(false);
+    const event = dispatchBeforeUnload();
+    assert.isFalse(event.defaultPrevented);
+    wrapper.unmount();
+  });
+
+  it('increments unsaved-changes count if argument is true', () => {
+    const wrapper = createWidget(true);
+    assert.isTrue(hasUnsavedChanges());
+    wrapper.unmount();
+    assert.isFalse(hasUnsavedChanges());
+  });
+
+  it('registers "beforeunload" handler if argument is true', () => {
+    const wrapper = createWidget(true);
+    const event = dispatchBeforeUnload();
+    assert.isTrue(event.defaultPrevented);
+
+    // Unmount the widget, this should remove the handler.
+    wrapper.unmount();
+    const event2 = dispatchBeforeUnload();
+    assert.isFalse(event2.defaultPrevented);
+  });
+});
