7a43ca4bae3c5a7b761c236bd19a3765574c8a10
Robert Knight <robertknight@gmail.com>
2024-12-09 09:38:18 +0000

    Make group members table wider
    Make this table wider so it can accomodate additional columns. The maximum width
    comes from the `form-container` class which was used for both group join and
    edit pages. Move this container into the join/edit-specific templates so we can
    customize it for each use case.

--- h/static/scripts/group-forms/components/forms/FormContainer.tsx
+++ h/static/scripts/group-forms/components/forms/FormContainer.tsx
@@ -1,14 +1,20 @@
+import classnames from 'classnames';
 import type { ComponentChildren } from 'preact';
 
 export type FormContainerProps = {
   title: string;
   children: ComponentChildren;
+  classes?: string;
 };
 
 /** A container for a form with a title. */
-export default function FormContainer({ children, title }: FormContainerProps) {
+export default function FormContainer({
+  children,
+  classes,
+  title,
+}: FormContainerProps) {
   return (
-    <div className="text-grey-6 text-sm/relaxed">
+    <div className={classnames('text-grey-6 text-sm/relaxed', classes)}>
       <h1 className="mt-14 mb-8 text-grey-7 text-xl/none" data-testid="header">
         {title}
       </h1>
