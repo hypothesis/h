f3ffd0af60ae5a3a4abd399b020a40a9391361c7
Sean Hammond <seanh@users.noreply.github.com>
2024-12-11 13:33:25 +0000

    Pass the right contexts to add_member() and edit_member()
    Problem
    -------
    
    `group_membership_api_factory()` is the context factory for all views
    that use the `"api.group_member"` route:
    
    * `remove_member()` (`request_method="DELETE"`)
    * `add_member()` (`request_method="POST"`)
    * `edit_member()` (`request_method="PATCH"`)
    * Add in https://github.com/hypothesis/h/pull/9197 also `get_member()`
      (`request_method="GET"`)
    
    `group_membership_api_factory()` returns a `GroupMembershipContext`
    object but:
    
    * `GroupMembershipContext` isn't the right context for `edit_member()`:
      it lacks the `context.new_roles` attribute for the new roles that the
      request wants to change `context.membership.roles` to.
    
    * `GroupMembershipContext` isn't the right context for `add_member()`:
      it has an inappropriate `context.membership` attribute (when adding a
      new membership there shouldn't be an existing membership in the
      context) and it lacks the `context.new_roles` attribute for the roles
      that the request wants to create a membership with.
    
    The context for the `edit_member()` view should be an
    `EditGroupMembershipContext` object, and for `add_member()` it should be
    an `AddGroupMembershipContext`.
    
    As a result the `edit_member()` view has to create its own context
    object to pass to `request.has_permission()`:
    
        def edit_member(context: GroupMembershipContext, request):
            appstruct = EditGroupMembershipAPISchema().validate(json_payload(request))
            new_roles = appstruct["roles"]
    
            if not request.has_permission(
                Permission.Group.MEMBER_EDIT,
                EditGroupMembershipContext(
                    context.group, context.user, context.membership, new_roles
                ),
            ):
                raise HTTPNotFound()
    
    When a future PR enables users (not just auth clients) to call the
    add-membership API the `add_member()` view will have to do something
    similar: constructing its own `AddGroupMembershipContext` object and
    passing it to `request.has_permission()`.
    
    This means there are two different context objects in play for the
    `edit_member()` and `add_member()` views: the `context` that is passed
    to the view is a `GroupMembershipContext`, but the `context` that is
    passed to `request.has_permission()` is an `EditGroupMembershipContext`
    or `AddGroupMembershipContext` constructed by the view itself.
    
    Solution
    -------
    
    This commit changes `group_membership_api_factory()` to return a
    `GroupMembershipContext` for `GET` and `DELETE` requests but return an
    `EditGroupMembershipContext` for `PATCH` requests and an
    `AddGroupMembershipContext` for `POST`s.
    
    It's not possible for `group_membership_api_factory()` to set the
    context's `new_roles` attribute: the value for `new_roles` isn't
    available until later in the request processing cycle after the view has
    parsed and validated the request's JSON body. So the factory returns
    `context` objects with `context.new_roles=None` and the `edit_member()`
    view has to set `new_roles` before calling `has_permission()`:
    
        appstruct = EditGroupMembershipAPISchema().validate(json_payload(request))
        context.new_roles = appstruct["roles"]
    
        if not request.has_permission(Permission.Group.MEMBER_EDIT, context):
            raise HTTPNotFound()
    
    In future the `add_member()` view will have to do the same. So this is
    still a little weird, but I think it's better than having two different
    context objects for a single request.

--- tests/unit/h/traversal/group_membership_test.py
+++ tests/unit/h/traversal/group_membership_test.py
@@ -6,6 +6,8 @@ from pyramid.httpexceptions import HTTPNotFound
 
 from h.exceptions import InvalidUserId
 from h.traversal.group_membership import (
+    AddGroupMembershipContext,
+    EditGroupMembershipContext,
     GroupMembershipContext,
     group_membership_api_factory,
 )
@@ -13,9 +15,17 @@ from h.traversal.group_membership import (
 
 @pytest.mark.usefixtures("group_service", "user_service", "group_members_service")
 class TestGroupMembershipAPIFactory:
-    def test_it(
-        self, group_service, user_service, group_members_service, pyramid_request
+    @pytest.mark.parametrize("request_method", ["GET", "DELETE"])
+    def test_get_delete(
+        self,
+        group_service,
+        user_service,
+        group_members_service,
+        pyramid_request,
+        request_method,
     ):
+        pyramid_request.method = request_method
+
         context = group_membership_api_factory(pyramid_request)
 
         group_service.fetch.assert_called_once_with(sentinel.pubid)
@@ -28,25 +38,70 @@ class TestGroupMembershipAPIFactory:
         assert context.user == user_service.fetch.return_value
         assert context.membership == group_members_service.get_membership.return_value
 
-    def test_when_no_matching_group(self, group_service, pyramid_request):
+    def test_post(
+        self, group_service, user_service, group_members_service, pyramid_request
+    ):
+        pyramid_request.method = "POST"
+
+        context = group_membership_api_factory(pyramid_request)
+
+        group_service.fetch.assert_called_once_with(sentinel.pubid)
+        user_service.fetch.assert_called_once_with(sentinel.userid)
+        group_members_service.get_membership.assert_not_called()
+        assert isinstance(context, AddGroupMembershipContext)
+        assert context.group == group_service.fetch.return_value
+        assert context.user == user_service.fetch.return_value
+        assert context.new_roles is None
+
+    def test_patch(
+        self, group_service, user_service, group_members_service, pyramid_request
+    ):
+        pyramid_request.method = "PATCH"
+
+        context = group_membership_api_factory(pyramid_request)
+
+        group_service.fetch.assert_called_once_with(sentinel.pubid)
+        user_service.fetch.assert_called_once_with(sentinel.userid)
+        group_members_service.get_membership.assert_called_once_with(
+            group_service.fetch.return_value, user_service.fetch.return_value
+        )
+        assert isinstance(context, EditGroupMembershipContext)
+        assert context.group == group_service.fetch.return_value
+        assert context.user == user_service.fetch.return_value
+        assert context.membership == group_members_service.get_membership.return_value
+        assert context.new_roles is None
+
+    @pytest.mark.parametrize("request_method", ["GET", "POST", "PATCH", "DELETE"])
+    def test_when_no_matching_group(
+        self, group_service, pyramid_request, request_method
+    ):
+        pyramid_request.method = request_method
         group_service.fetch.return_value = None
 
         with pytest.raises(HTTPNotFound, match="Group not found: sentinel.pubid"):
             group_membership_api_factory(pyramid_request)
 
-    def test_when_no_matching_user(self, user_service, pyramid_request):
+    @pytest.mark.parametrize("request_method", ["GET", "POST", "PATCH", "DELETE"])
+    def test_when_no_matching_user(self, user_service, pyramid_request, request_method):
+        pyramid_request.method = request_method
         user_service.fetch.return_value = None
 
         with pytest.raises(HTTPNotFound, match="User not found: sentinel.userid"):
             group_membership_api_factory(pyramid_request)
 
-    def test_when_invalid_userid(self, user_service, pyramid_request):
+    @pytest.mark.parametrize("request_method", ["GET", "POST", "PATCH", "DELETE"])
+    def test_when_invalid_userid(self, user_service, pyramid_request, request_method):
+        pyramid_request.method = request_method
         user_service.fetch.side_effect = InvalidUserId(sentinel.userid)
 
         with pytest.raises(HTTPNotFound, match="User not found: sentinel.userid"):
             group_membership_api_factory(pyramid_request)
 
-    def test_when_no_matching_membership(self, group_members_service, pyramid_request):
+    @pytest.mark.parametrize("request_method", ["GET", "PATCH", "DELETE"])
+    def test_when_no_matching_membership(
+        self, group_members_service, pyramid_request, request_method
+    ):
+        pyramid_request.method = request_method
         group_members_service.get_membership.return_value = None
 
         with pytest.raises(
@@ -55,7 +110,11 @@ class TestGroupMembershipAPIFactory:
         ):
             group_membership_api_factory(pyramid_request)
 
-    def test_me_alias(self, pyramid_config, pyramid_request, user_service):
+    @pytest.mark.parametrize("request_method", ["GET", "POST", "PATCH", "DELETE"])
+    def test_me_alias(
+        self, pyramid_config, pyramid_request, user_service, request_method
+    ):
+        pyramid_request.method = request_method
         pyramid_config.testing_securitypolicy(userid=sentinel.userid)
         pyramid_request.matchdict["userid"] = "me"
 
@@ -63,7 +122,9 @@ class TestGroupMembershipAPIFactory:
 
         user_service.fetch.assert_called_once_with(sentinel.userid)
 
-    def test_me_alias_when_not_authenticated(self, pyramid_request):
+    @pytest.mark.parametrize("request_method", ["GET", "POST", "PATCH", "DELETE"])
+    def test_me_alias_when_not_authenticated(self, pyramid_request, request_method):
+        pyramid_request.method = request_method
         pyramid_request.matchdict["userid"] = "me"
 
         with pytest.raises(HTTPNotFound, match="User not found: me"):
