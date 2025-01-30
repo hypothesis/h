2547a26b4d3e40f36064a677b148ccffa277c3a9
Misha Tomilov <mtomilov@gmail.com>
2025-01-30 18:39:35 +0100

    Update mention to reference annotation

--- h/models/mention.py
+++ h/models/mention.py
@@ -1,7 +1,7 @@
 import sqlalchemy as sa
 from sqlalchemy.orm import Mapped, mapped_column
 
-from h.db import Base
+from h.db import Base, types
 from h.db.mixins import Timestamps
 from h.models import helpers
 
@@ -11,13 +11,13 @@ class Mention(Base, Timestamps):  # pragma: nocover
 
     id: Mapped[int] = mapped_column(sa.Integer, autoincrement=True, primary_key=True)
 
-    annotation_id: Mapped[int] = mapped_column(
-        sa.Integer,
-        sa.ForeignKey("annotation_slim.id", ondelete="CASCADE"),
+    annotation_id: Mapped[types.URLSafeUUID] = mapped_column(
+        types.URLSafeUUID,
+        sa.ForeignKey("annotation.id", ondelete="CASCADE"),
         nullable=False,
     )
-    """FK to annotation_slim.id"""
-    annotation = sa.orm.relationship("AnnotationSlim")
+    """FK to annotation.id"""
+    annotation = sa.orm.relationship("Annotation", back_populates="mentions")
 
     user_id: Mapped[int] = mapped_column(
         sa.Integer,
