1c776ed09cb6057360fe8f32900b76ffcc7f13e2
Misha Tomilov <mtomilov@gmail.com>
2025-02-03 16:43:11 +0100

    Fix MentionService

--- h/presenters/mention_json.py
+++ h/presenters/mention_json.py
@@ -11,7 +11,7 @@ class MentionJSONPresenter:
 
     def asdict(self) -> dict[str, Any]:
         return {
-            "userid": self._mention.annotation.userid,
+            "userid": self._mention.user.userid,
             "username": self._mention.user.username,
             "display_name": self._mention.user.display_name,
             "link": self._mention.user.uri,
