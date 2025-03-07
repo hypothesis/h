15567a35795e91fd708947a33f9c79cdf1bfb66e
Misha Tomilov <mtomilov@gmail.com>
2025-03-07 16:49:45 +0100

    Remove mailer send args support

--- h/tasks/mailer.py
+++ h/tasks/mailer.py
@@ -4,7 +4,7 @@ A module for sending email.
 This module defines a Celery task for sending emails in a worker process.
 """
 
-from h.services.email import EmailData, EmailService, EmailTag
+from h.services.email import EmailData, EmailService
 from h.tasks.celery import celery, get_task_logger
 
 __all__ = ("send",)
@@ -19,32 +19,10 @@ logger = get_task_logger(__name__)
     max_retries=3,
     retry_jitter=False,
 )
-def send(  # noqa: PLR0913
-    self,  # noqa: ARG001
-    recipients: list[str] | EmailData,
-    subject: str | None = None,
-    body: str | None = None,
-    tag: EmailTag | None = None,
-    html: str | None = None,
-) -> None:
+def send(self, email: EmailData) -> None:  # noqa: ARG001
     """Send an email.
 
-    :param recipients: the list of email addresses to send to or an EmailData object
-    :param subject: the email subject
-    :param body: the email body
-    :param tag: the email tag
-    :param html: HTML version of the email
+    :param email: The email data to send.
     """
     service = celery.request.find_service(EmailService)
-
-    if isinstance(recipients, EmailData):
-        email = recipients
-    else:
-        email = EmailData(
-            recipients=recipients,
-            subject=subject,
-            body=body,
-            tag=tag,
-            html=html,
-        )
     service.send(email)
