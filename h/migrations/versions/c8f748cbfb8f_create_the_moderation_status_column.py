3978d9edea4fcc398a503c3a5f2fa2dc007a757f
Marcos Prieto <marcospri@gmail.com>
2025-04-04 14:16:39 +0200

    Create migration for new moderation column

---
+++ h/migrations/versions/c8f748cbfb8f_create_the_moderation_status_column.py
@@ -0,0 +1,44 @@
+"""Create the moderation_status column
+
+Revision ID: c8f748cbfb8f
+Revises: 372308320143
+"""
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+revision = "c8f748cbfb8f"
+down_revision = "372308320143"
+
+
+def upgrade() -> None:
+    moderation_status_type = postgresql.ENUM(
+        "APPROVED",
+        "DENIED",
+        "SPAM",
+        "PRIVATE",
+        name="moderationstatus",
+    )
+    moderation_status_type.create(op.get_bind(), checkfirst=True)
+    op.add_column(
+        "annotation",
+        sa.Column(
+            "moderation_status",
+            moderation_status_type,
+            nullable=True,
+        ),
+    )
+    op.add_column(
+        "annotation_slim",
+        sa.Column(
+            "moderation_status",
+            moderation_status_type,
+            nullable=True,
+        ),
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("annotation_slim", "moderation_status")
+    op.drop_column("annotation", "moderation_status")
