fc4f741f0d859f41c0b804135bc6e41d6f5b8c80
Marcos Prieto <marcospri@gmail.com>
2025-04-08 16:02:13 +0200

    Migration for moderation log

---
+++ h/migrations/versions/bd226cc1c359_create_moderation_log_table.py
@@ -0,0 +1,51 @@
+"""Create moderation log table
+
+Revision ID: bd226cc1c359
+Revises: 96cde96b2fd7
+"""
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+from h.db import types
+
+revision = "bd226cc1c359"
+down_revision = "96cde96b2fd7"
+
+
+def upgrade() -> None:
+    op.create_table(
+        "moderation_log",
+        sa.Column("id", sa.Integer(), autoincrement=True, nullable=False),
+        sa.Column("user_id", sa.Integer(), nullable=False),
+        sa.Column("annotation_id", types.URLSafeUUID(), nullable=False),
+        sa.Column("old_moderation_status", sa.String(), nullable=False),
+        sa.Column("new_moderation_status", sa.String(), nullable=False),
+        sa.Column(
+            "created", sa.DateTime(), server_default=sa.text("now()"), nullable=False
+        ),
+        sa.ForeignKeyConstraint(
+            ["annotation_id"],
+            ["annotation.id"],
+            name=op.f("fk__moderation_log__annotation_id__annotation"),
+            ondelete="CASCADE",
+        ),
+        sa.ForeignKeyConstraint(
+            ["user_id"],
+            ["user.id"],
+            name=op.f("fk__moderation_log__user_id__user"),
+            ondelete="CASCADE",
+        ),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk__moderation_log")),
+    )
+    op.create_index(
+        op.f("ix__moderation_log_annotation_id"),
+        "moderation_log",
+        ["annotation_id"],
+        unique=False,
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("moderation_log")
+    # ### end Alembic commands ###
