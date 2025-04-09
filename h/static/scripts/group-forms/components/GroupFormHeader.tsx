2eb14c5e6b1e740f1bae2dcbffc83b8e08e5062a
Marcos Prieto <marcospri@gmail.com>
2025-04-09 16:29:59 +0200

    Add moderation link on header

--- h/static/scripts/group-forms/components/GroupFormHeader.tsx
+++ h/static/scripts/group-forms/components/GroupFormHeader.tsx
@@ -49,6 +49,10 @@ export default function GroupFormHeader({
     ? routes.groups.editMembers.replace(':pubid', group.pubid)
     : null;
 
+  const moderateLink = group
+    ? routes.groups.moderate.replace(':pubid', group.pubid)
+    : null;
+
   return (
     <div className="mb-4 pb-1 border-b border-b-text-grey-6">
       {group && (
@@ -76,6 +80,11 @@ export default function GroupFormHeader({
         {enableMembers && editMembersLinks && (
           <TabLink testId="members-link" href={editMembersLinks}>
             Members
+          </TabLink>
+        )}
+        {moderateLink && (
+          <TabLink testId="moderate-link" href={moderateLink}>
+            Moderate
           </TabLink>
         )}
       </div>
