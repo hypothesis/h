67f5d0ab08a0f8f6ea8a4627bd42503cede3b39d
Marcos Prieto <marcospri@gmail.com>
2025-04-14 09:03:44 +0200

    Migration for annotation.moderation_status

---
+++ h/migrations/versions/bd0cc0e6ed54_create_the_moderation_status_column.py
@@ -0,0 +1,28 @@
+"""Create the moderation_status column."""
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+revision = "bd0cc0e6ed54"
+down_revision = "9d97a3e4921e"
+
+
+def upgrade() -> None:
+    moderation_status_type = postgresql.ENUM(
+        "APPROVED",
+        "DENIED",
+        "SPAM",
+        "PENDING",
+        name="moderationstatus",
+    )
+    moderation_status_type.create(op.get_bind(), checkfirst=True)
+
+    op.add_column(
+        "annotation",
+        sa.Column("moderation_status", moderation_status_type, nullable=True),
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("annotation", "moderation_status")
