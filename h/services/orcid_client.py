6957d51818577c6e32c791eb173a4af321accf4f
Misha Tomilov <mtomilov@gmail.com>
2025-04-21 16:43:56 +0200

    Add noqa comment to suppress linting warning in ORCIDClientService constructor

--- h/services/orcid_client.py
+++ h/services/orcid_client.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 class ORCIDClientService:
-    def __init__(
+    def __init__(  # noqa: PLR0913
         self,
         session: Session,
         host: str,
