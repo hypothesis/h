09cbc61481af8091ff08386c6067753d28e3e507
Marcos Prieto <marcospri@gmail.com>
2025-04-29 11:27:06 +0200

    Add endpoint to set the moderation of an annotation
    The endpoint will set the moderation status unconditionally.
    
    This is required by the new moderation queue functionality.

--- h/views/api/moderation.py
+++ h/views/api/moderation.py
@@ -1,6 +1,8 @@
 from pyramid.httpexceptions import HTTPNoContent
 
 from h import events
+from h.schemas.api.moderation import ChangeAnnotationModerationStatusSchema
+from h.schemas.util import validate_query_params
 from h.security import Permission
 from h.services import AnnotationWriteService
 from h.views.api.config import api_config
@@ -40,3 +42,26 @@ def unhide(context, request):
     request.notify_after_commit(event)
 
     return HTTPNoContent()
+
+
+@api_config(
+    versions=["v1", "v2"],
+    route_name="api.annotation_moderation",
+    request_method="PATCH",
+    link_name="annotation_moderation",
+    permission=Permission.Annotation.MODERATE,
+)
+def change_annotation_moderation_status(context, request):
+    params = validate_query_params(
+        ChangeAnnotationModerationStatusSchema(), request.params
+    )
+    status = params["moderation_status"]
+    request.find_service(name="annotation_moderation").set_status(
+        context.annotation, request.user, status
+    )
+    event = events.AnnotationEvent(request, context.annotation.id, "update")
+    request.notify_after_commit(event)
+
+    return request.find_service(name="annotation_json").present_for_user(
+        annotation=context.annotation, user=request.user
+    )
