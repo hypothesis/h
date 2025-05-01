fb152a50b5d6a9f029e438e033a70fa94b6b9424
Sean Hammond <seanh@users.noreply.github.com>
2025-05-01 17:22:57 +0100

    Add comment explaining Sentry's releases feature

--- h/app.py
+++ h/app.py
@@ -117,6 +117,15 @@ def _configure_sentry(config):
             "h_pyramid_sentry.retry_support": True,
             "h_pyramid_sentry.celery_support": True,
             "h_pyramid_sentry.sqlalchemy_support": True,
+            # Enable Sentry's "Releases" feature, see:
+            # https://docs.sentry.io/platforms/python/configuration/options/#release
+            #
+            # h_pyramid_sentry passes any h_pyramid_sentry.init.* Pyramid settings
+            # through to sentry_sdk.init(), see:
+            # https://github.com/hypothesis/h-pyramid-sentry?tab=readme-ov-file#settings
+            #
+            # For the full list of options that sentry_sdk.init() supports see:
+            # https://docs.sentry.io/platforms/python/configuration/options/
             "h_pyramid_sentry.init.release": get_version(),
         }
     )
