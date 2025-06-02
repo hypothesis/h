02bb2b50fbac4e4e08af49d32433ba9a3ff91bd0
Robert Knight <robertknight@gmail.com>
2025-06-02 16:19:22 +0100

    Avoid using Shadow DOM for groups forms
    Shadow DOM was used to isolate the Preact/Tailwind-based group forms
    from the legacy site CSS. However this also results in us needing to
    deal with Shadow DOM quirks like loading styles being more fiddly and
    autofocus not working in Safari/Firefox [^1]. The group forms app works fine
    without the style isolation, so just remove it for now.
    
    [^1]: https://github.com/hypothesis/h/pull/9596#issuecomment-2922175483

--- h/static/scripts/group-forms/components/AppRoot.tsx
+++ h/static/scripts/group-forms/components/AppRoot.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'preact/hooks';
+import { useState } from 'preact/hooks';
 import { Route, Switch } from 'wouter-preact';
 
 import Router from '../../forms-common/components/Router';
@@ -14,46 +14,31 @@ export type AppRootProps = {
 };
 
 export default function AppRoot({ config }: AppRootProps) {
-  const stylesheetLinks = useMemo(
-    () =>
-      config.styles.map((stylesheetURL, index) => (
-        <link
-          key={`${stylesheetURL}${index}`}
-          rel="stylesheet"
-          href={stylesheetURL}
-        />
-      )),
-    [config],
-  );
-
   // Saved group details. Extracted out of `config` so we can update them after
   // saving changes to the backend.
   const [group, setGroup] = useState(config.context.group);
 
   return (
-    <>
-      {stylesheetLinks}
-      <Config.Provider value={config}>
-        <Router>
-          <Switch>
-            <Route path={routes.groups.new}>
-              <CreateEditGroupForm group={group} onUpdateGroup={setGroup} />
-            </Route>
-            <Route path={routes.groups.edit}>
-              <CreateEditGroupForm group={group} onUpdateGroup={setGroup} />
-            </Route>
-            <Route path={routes.groups.editMembers}>
-              <EditGroupMembersForm group={group!} />
-            </Route>
-            <Route path={routes.groups.moderation}>
-              <GroupModeration group={group!} />
-            </Route>
-            <Route>
-              <h1 data-testid="unknown-route">Page not found</h1>
-            </Route>
-          </Switch>
-        </Router>
-      </Config.Provider>
-    </>
+    <Config.Provider value={config}>
+      <Router>
+        <Switch>
+          <Route path={routes.groups.new}>
+            <CreateEditGroupForm group={group} onUpdateGroup={setGroup} />
+          </Route>
+          <Route path={routes.groups.edit}>
+            <CreateEditGroupForm group={group} onUpdateGroup={setGroup} />
+          </Route>
+          <Route path={routes.groups.editMembers}>
+            <EditGroupMembersForm group={group!} />
+          </Route>
+          <Route path={routes.groups.moderation}>
+            <GroupModeration group={group!} />
+          </Route>
+          <Route>
+            <h1 data-testid="unknown-route">Page not found</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </Config.Provider>
   );
 }
