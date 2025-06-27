5168122cebfd5b56d799bc8d07203148f2512150
Sean Hammond <seanh@users.noreply.github.com>
2025-06-27 16:51:30 +0100

    Improve naming of `RetrieveOpenIDTokenSchema` and friends
    A few problems with the name `RetrieveOpenIDTokenSchema`:
    
    * It's located in the wrong file: `h/schemas/oauth.py` but this
      is an OpenID Connect schema not an OAuth schema
    * I'm not sure what "retrieve" means in this name
    * The protocol is called OpenID Connect not just OpenID
    * The schema is for validating token responses not just tokens,
      which are only part of the response
    
    Move the schema to `h/schemas/oidc.py` and rename it to
    `OIDCTokenResponseSchema`.
    
    Also, the `OpenIDTokenSchema` class is a private helper for
    `OIDCTokenResponseSchema.validate()` and not part of the module's public
    interface. Inline it into the method that uses it.
    
    Also, `OpenIDTokenData` is a type definition for
    `OIDCTokenResponseSchema.validate()`'s return type.  Rename it to
    `OIDCTokenResponseData` for greater clarity and consistency with the
    other new class name in this commit, and inline it into the class that
    it belongs to.
    
    Also add docstrings.
    
    This results in a nicer overall organisation:
    
        # h/schemas/oidc.py
        """Validation schemas used in our implementation of OpenID Connect (OIDC)."""
    
        class OIDCTokenResponseSchema:
            """Schema for validating OIDC token responses."""
    
            class OIDCTokenResponseData(TypedDict):
                """Return type for OIDCTokenResponseSchema.validate()."""
    
            def validate(self, data: dict[str, Any]) -> OIDCTokenResponseData:
                """Validate `data` and return the validated copy."""

--- h/schemas/oauth.py
+++ h/schemas/oauth.py
@@ -70,31 +70,3 @@ class OAuth2RedirectSchema:
         state = secrets.token_hex()
         self._request.session[self.SESSION_KEY] = state
         return state
-
-
-class OpenIDTokenSchema(JSONSchema):
-    schema: ClassVar = {  # type: ignore[misc]
-        "type": "object",
-        "required": ["id_token"],
-        "properties": {
-            "access_token": {"type": "string"},
-            "refresh_token": {"type": "string"},
-            "expires_in": {"type": "integer", "minimum": 1},
-            "id_token": {"type": "string"},
-        },
-    }
-
-
-class OpenIDTokenData(TypedDict):
-    access_token: str | None
-    refresh_token: str | None
-    expires_in: int | None
-    id_token: str
-
-
-class RetrieveOpenIDTokenSchema:
-    def __init__(self) -> None:
-        self._schema = OpenIDTokenSchema()
-
-    def validate(self, data: dict[str, Any]) -> OpenIDTokenData:
-        return self._schema.validate(data)
