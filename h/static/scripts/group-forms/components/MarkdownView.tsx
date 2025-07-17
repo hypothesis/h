9e724ccf175509b02e1418dc0754ae2ff189314f
Robert Knight <robertknight@gmail.com>
2025-07-17 10:57:59 +0100

    Revert "Revert "Lazily load MarkdownView from @hypothesis/annotation-ui""
    This reverts commit dec1a0e7b79f477e8bb6fabad3082154dde3cd95.

---
+++ h/static/scripts/group-forms/components/MarkdownView.tsx
@@ -0,0 +1,5 @@
+// Entry point for MarkdownView component bundle.
+//
+// The main purpose of this wrapper is to cause Rollup to generate a bundle
+// with a meaningful name, as it uses the file name of the entry point.
+export { MarkdownView as default } from '@hypothesis/annotation-ui';
