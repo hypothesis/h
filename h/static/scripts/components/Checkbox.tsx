hypothesis/h

commit 87998e982ebb384fd9b719e6165d6b707c081176

Move style container element to root and adjust text color
Rename the `FormContainer` component which sets default styles for a
Preact-rendered section of a page to `AppContainer` and move it to the root of
each frontend app so it doesn't have to be added in every individual view.

Also change the default text color from `text-grey-6` to `text-grey-7`. Some
views were using the former, others the latter. The darker option is easier
to read, so use it everywhere.

h/static/scripts/components/Checkbox.tsx
@@ -32,7 +32,7 @@ export default function Checkbox({
         {...checkboxProps}
         aria-describedby={description ? descriptionId : undefined}
       >
-        <span className="text-grey-7">{children}</span>
+        {children}
       </BaseCheckbox>
       {description && (
         <div

h/static/scripts/components/AccountSettingsForms.tsx
@@ -8,7 +8,6 @@ import { useFormValue } from '../util/form-value';
 import type { FormValue } from '../util/form-value';
 import FacebookIcon from './FacebookIcon';
 import Form from './Form';
-import FormContainer from './FormContainer';
 import FormFooter from './FormFooter';
 import GoogleIcon from './GoogleIcon';
 import ORCIDIcon from './ORCIDIcon';
@@ -33,7 +32,7 @@ function ChangeEmailForm({ config }: { config: AccountSettingsConfigObject }) {
     : 'Add an email address';
 
   return (
-    <FormContainer>
+    <>
       <Heading text={heading} />
 
       <Form csrfToken={config.csrfToken} data-testid="email-form">
@@ -65,7 +64,7 @@ function ChangeEmailForm({ config }: { config: AccountSettingsConfigObject }) {
         )}
         <FormFooter />
       </Form>
-    </FormContainer>
+    </>
   );
 }
 
@@ -83,7 +82,7 @@ function ChangePasswordForm({
   );
 
   return (
-    <FormContainer>
+    <>
       {config.context.user.has_password ? (
         <Heading text="Change your password" />
       ) : (
@@ -118,7 +117,7 @@ function ChangePasswordForm({
         />
         <FormFooter />
       </Form>
-    </FormContainer>
+    </>
   );
 }
 
@@ -225,7 +224,7 @@ function ConnectAccountButtons({
   }
 
   return (
-    <FormContainer>
+    <>
       <Heading text="Connect your account" />
 
       <p className="mb-9">
@@ -244,7 +243,7 @@ function ConnectAccountButtons({
           <ConnectAccountButton config={config} provider="orcid" />
         )}
       </div>
-    </FormContainer>
+    </>
   );
 }
 

h/static/scripts/components/AppContainer.tsx
@@ -1,0 +1,18 @@
+import classnames from 'classnames';
+import type { ComponentChildren } from 'preact';
+
+export type AppContainerProps = {
+  children: ComponentChildren;
+  classes?: string;
+};
+
+/**
+ * Container for frontend applications that sets default styles.
+ */
+export default function AppContainer({ children, classes }: AppContainerProps) {
+  return (
+    <div className={classnames('text-grey-7 text-sm/relaxed', classes)}>
+      {children}
+    </div>
+  );
+}

h/static/scripts/components/CreateEditGroupForm.tsx
@@ -21,7 +21,6 @@ import { useUnsavedChanges } from '../util/unsaved-changes';
 import Checkbox from './Checkbox';
 import ErrorNotice from './ErrorNotice';
 import Form from './Form';
-import FormContainer from './FormContainer';
 import GroupFormHeader from './GroupFormHeader';
 import Label from './Label';
 import SaveStateIcon from './SaveStateIcon';
@@ -248,7 +247,7 @@ export default function CreateEditGroupForm({
   };
 
   return (
-    <FormContainer>
+    <>
       <GroupFormHeader group={group} title={heading} />
       <Form onSubmit={onSubmit} csrfToken={null}>
         <TextField
@@ -354,6 +353,6 @@ export default function CreateEditGroupForm({
           }}
         />
       )}
-    </FormContainer>
+    </>
   );
 }

h/static/scripts/components/EditGroupMembersForm.tsx
@@ -19,7 +19,6 @@ import type {
   Role,
 } from '../util/api';
 import ErrorNotice from './ErrorNotice';
-import FormContainer from './FormContainer';
 import GroupFormHeader from './GroupFormHeader';
 import WarningDialog from './WarningDialog';
 
@@ -386,32 +385,30 @@ export default function EditGroupMembersForm({
 
   return (
     <>
-      <FormContainer>
-        <GroupFormHeader title="Edit group members" group={group} />
-        <ErrorNotice message={errorMessage} />
-        <div className="w-full">
-          <Scroll>
-            <DataTable
-              grid
-              striped={false}
-              title="Group members"
-              rows={members ?? []}
-              columns={columns}
-              renderItem={renderRow}
-              loading={!members}
-            />
-          </Scroll>
+      <GroupFormHeader title="Edit group members" group={group} />
+      <ErrorNotice message={errorMessage} />
+      <div className="w-full">
+        <Scroll>
+          <DataTable
+            grid
+            striped={false}
+            title="Group members"
+            rows={members ?? []}
+            columns={columns}
+            renderItem={renderRow}
+            loading={!members}
+          />
+        </Scroll>
+      </div>
+      {typeof totalPages === 'number' && totalPages > 1 && (
+        <div className="mt-4 flex justify-center">
+          <Pagination
+            currentPage={pageNumber}
+            onChangePage={setPageNumber}
+            totalPages={totalPages}
+          />
         </div>
-        {typeof totalPages === 'number' && totalPages > 1 && (
-          <div className="mt-4 flex justify-center">
-            <Pagination
-              currentPage={pageNumber}
-              onChangePage={setPageNumber}
-              totalPages={totalPages}
-            />
-          </div>
-        )}
-      </FormContainer>
+      )}
       {pendingRemoval && (
         <WarningDialog
           title="Remove member?"

h/static/scripts/components/FormContainer.tsx
@@ -1,21 +1,0 @@
-import classnames from 'classnames';
-import type { ComponentChildren } from 'preact';
-
-export type FormContainerProps = {
-  children: ComponentChildren;
-  classes?: string;
-};
-
-/**
- * Container that sets default styles for a form.
- */
-export default function FormContainer({
-  children,
-  classes,
-}: FormContainerProps) {
-  return (
-    <div className={classnames('text-grey-6 text-sm/relaxed', classes)}>
-      {children}
-    </div>
-  );
-}

h/static/scripts/components/FormHeader.tsx
@@ -18,7 +18,7 @@ export default function FormHeader({
   return (
     <h1
       className={classnames(
-        'text-grey-7 text-xl/none',
+        'text-xl/none',
         center && 'text-center',
         variant === 'default' && 'my-8',
         classes,

h/static/scripts/components/GroupFormHeader.tsx
@@ -39,10 +39,10 @@ export default function GroupFormHeader({
       {group && (
         <div className="mb-4">
           <Link
-            classes="text-grey-7"
             href={group.link}
             underline="always"
             data-testid="activity-link"
+            variant="text"
           >
             View group activity
           </Link>

commit 24b115a3a6907bc15e24e79df559d3efa76b78a4

Fix layout of checkbox children
Fix a regression from 87998e982ebb384fd9b719e6165d6b707c081176. The children of
`Checkbox` get placed in a flex layout. We need the `<span>` even if unstyled to
place all children in one flex item.

h/static/scripts/components/Checkbox.tsx
@@ -32,7 +32,8 @@ export default function Checkbox({
         {...checkboxProps}
         aria-describedby={description ? descriptionId : undefined}
       >
-        {children}
+        {/* Unstyled span needed to put children in a single flex item. */}
+        <span>{children}</span>
       </BaseCheckbox>
       {description && (
         <div