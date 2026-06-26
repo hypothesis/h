86be302e6295e58f48821f6c516747e12b77bb36
Karen Rasmussen <rasmussenkaren@outlook.com>
2026-06-26 18:19:05 -0300

    Add checkpoint reveal endpoint and generalize user role setting

--- h/routes.py
+++ h/routes.py
@@ -162,6 +162,11 @@ def includeme(config):  # noqa: PLR0915
     config.add_route(
         "api.bulk.checkpoint", "/api/bulk/checkpoint", request_method="POST"
     )
+    config.add_route(
+        "api.bulk.checkpoint.reveal",
+        "/api/bulk/checkpoint/reveal",
+        request_method="POST",
+    )
 
     config.add_route("api.groups", "/api/groups", factory="h.traversal.GroupRoot")
     config.add_route(
